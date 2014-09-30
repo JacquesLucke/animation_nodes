c3269d58e5d16b87d533cb8fd3f12f786959b24b
Jacques Lucke <mail@jlucke.com>
2014-09-30 17:50:44 +0200

    file for math and expression nodes

---
+++ nodes/math/mn_float_math.py
@@ -0,0 +1,50 @@
+import bpy
+from bpy.types import Node
+from mn_node_helper import AnimationNode
+from mn_execution import nodePropertyChanged
+
+class FloatMathNode(Node, AnimationNode):
+	bl_idname = "FloatMathNode"
+	bl_label = "Math (Float)"
+	
+	mathTypes = [
+		("ADD", "Add", ""),
+		("SUBTRACT", "Subtract", ""),
+		("MULITPLY", "Multiply", ""),
+		("DIVIDE", "Divide", ""),
+		("MODULO", "Modulo", "")]
+	mathTypesProperty = bpy.props.EnumProperty(name = "Type", items = mathTypes, default = "ADD", update = nodePropertyChanged)
+	
+	def init(self, context):
+		self.inputs.new("FloatSocket", "A")
+		self.inputs.new("FloatSocket", "B")
+		self.outputs.new("FloatSocket", "Result")
+		
+	def draw_buttons(self, context, layout):
+		layout.prop(self, "mathTypesProperty")
+		
+	def execute(self, input):
+		a = input["A"]
+		b = input["B"]
+		result = 0
+		type = self.mathTypesProperty
+		try:
+			if type == "ADD": result = a + b
+			elif type == "SUBTRACT": result = a - b
+			elif type == "MULITPLY": result = a * b
+			elif type == "DIVIDE": result = a / b
+			elif type == "MODULO": result = a % b
+		except (ZeroDivisionError):
+			print("ZeroDivisionError - " + self.name)
+		output = {}
+		output["Result"] = result
+		return output
+		
+# register
+################################
+		
+def register():
+	bpy.utils.register_module(__name__)
+
+def unregister():
+	bpy.utils.unregister_module(__name__)
---
+++ nodes/script/mn_expression.py
@@ -0,0 +1,36 @@
+import bpy
+from bpy.types import Node
+from mn_node_helper import AnimationNode
+from mn_execution import nodePropertyChanged
+
+class ExpressionNode(Node, AnimationNode):
+	bl_idname = "ExpressionNode"
+	bl_label = "Expression"
+	
+	def init(self, context):
+		self.inputs.new("StringSocket", "Expression").string = "a+b"
+		self.inputs.new("GenericSocket", "a")
+		self.inputs.new("GenericSocket", "b")
+		self.outputs.new("GenericSocket", "Result")
+		
+	def execute(self, input):
+		a = input["a"]
+		b = input["b"]
+		expression = input["Expression"]
+		result = 0
+		try:
+			result = eval(expression)
+		except (ZeroDivisionError):
+			print("expression error - " + self.name)
+		output = {}
+		output["Result"] = result
+		return output
+		
+# register
+################################
+		
+def register():
+	bpy.utils.register_module(__name__)
+
+def unregister():
+	bpy.utils.unregister_module(__name__)
