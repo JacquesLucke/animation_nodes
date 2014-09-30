513491cc408e5614fb4a3b613a5e6ff882702539
Jacques Lucke <mail@jlucke.com>
2014-09-30 17:21:15 +0200

    file for random string node

---
+++ nodes/Input/mn_random_string.py
@@ -0,0 +1,32 @@
+import bpy, random
+from bpy.types import Node
+from mn_node_helper import AnimationNode
+from mn_execution import nodePropertyChanged
+
+class RandomStringNode(Node, AnimationNode):
+	bl_idname = "RandomStringNode"
+	bl_label = "Random Text"
+	
+	def init(self, context):
+		self.inputs.new("IntegerSocket", "Seed")
+		self.inputs.new("IntegerSocket", "Length").number = 5
+		self.inputs.new("StringSocket", "Characters").string = "abcdefghijklmnopqrstuvwxyz"
+		self.outputs.new("StringSocket", "Text")
+		
+	def execute(self, input):
+		output = {}
+		seed = input["Seed"]
+		length = input["Length"]
+		characters = input["Characters"]
+		random.seed(seed)
+		output["Text"] = ''.join(random.choice(characters) for _ in range(length))
+		return output
+		
+# register
+################################
+		
+def register():
+	bpy.utils.register_module(__name__)
+
+def unregister():
+	bpy.utils.unregister_module(__name__)
--- nodes/mn_input_nodes.py
+++ nodes/mn_input_nodes.py
@@ -26,25 +26,6 @@ from mn_utils import *
 from mn_execution import nodePropertyChanged
 
 		
-class RandomStringNode(Node, AnimationNode):
-	bl_idname = "RandomStringNode"
-	bl_label = "Random Text"
-	
-	def init(self, context):
-		self.inputs.new("IntegerSocket", "Seed")
-		self.inputs.new("IntegerSocket", "Length").number = 5
-		self.inputs.new("StringSocket", "Characters").string = "abcdefghijklmnopqrstuvwxyz"
-		self.outputs.new("StringSocket", "Text")
-		
-	def execute(self, input):
-		output = {}
-		seed = input["Seed"]
-		length = input["Length"]
-		characters = input["Characters"]
-		random.seed(seed)
-		output["Text"] = ''.join(random.choice(characters) for _ in range(length))
-		return output
-		
 class CharactersNode(Node, AnimationNode):
 	bl_idname = "CharactersNode"
 	bl_label = "Characters"
