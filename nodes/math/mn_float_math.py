81325d71eb706c0b6895ffa5351eaad4ce81abf8
jasperge <jaspervannieuwenhuizen@gmail.com>
2014-10-04 00:50:30 +0200

    Expanded the math node (made is consistent with the Cycles math node)

--- nodes/math/mn_float_math.py
+++ nodes/math/mn_float_math.py
@@ -2,6 +2,22 @@ import bpy
 from bpy.types import Node
 from mn_node_base import AnimationNode
 from mn_execution import nodePropertyChanged
+import math
+
+def update_node(node, context):
+	nodePropertyChanged(node, context)
+	single_value_list = ["SINE",
+						 "COSINE",
+						 "TANGENT",
+						 "ARCSINE",
+						 "ARCCOSINE",
+						 "ARCTANGENT",
+						 "ABSOLUTE"]
+	if node.mathTypesProperty in single_value_list and not node.inputs[-1].hide:
+		node.inputs[-1].hide = True
+	elif node.mathTypesProperty not in single_value_list and node.inputs[-1].hide:
+		node.inputs[-1].hide = False
+
 
 class FloatMathNode(Node, AnimationNode):
 	bl_idname = "FloatMathNode"
@@ -12,8 +28,22 @@ class FloatMathNode(Node, AnimationNode):
 		("SUBTRACT", "Subtract", ""),
 		("MULITPLY", "Multiply", ""),
 		("DIVIDE", "Divide", ""),
-		("MODULO", "Modulo", "")]
-	mathTypesProperty = bpy.props.EnumProperty(name = "Type", items = mathTypes, default = "ADD", update = nodePropertyChanged)
+		("SINE", "Sine", ""),
+		("COSINE", "Cosine", ""),
+		("TANGENT", "Tangent", ""),
+		("ARCSINE", "Arcsine", ""),
+		("ARCCOSINE", "Arccosine", ""),
+		("ARCTANGENT", "Arctangent", ""),
+		("POWER", "Power", ""),
+		("LOGARITHM", "Logarithm", ""),
+		("MINIMUM", "Minimum", ""),
+		("MAXIMUM", "Maximum", ""),
+		("ROUND", "Round", ""),
+		("LESSTHAN", "Less Than", ""),
+		("GREATHERTHAN", "Greather Than", ""),
+		("MODULO", "Modulo", ""),
+		("ABSOLUTE", "Absolute", "")]
+	mathTypesProperty = bpy.props.EnumProperty(name="Operation", items=mathTypes, default="ADD", update=update_node)
 	
 	def init(self, context):
 		self.inputs.new("FloatSocket", "A")
@@ -27,15 +57,57 @@ class FloatMathNode(Node, AnimationNode):
 		a = input["A"]
 		b = input["B"]
 		result = 0
-		type = self.mathTypesProperty
+		operation = self.mathTypesProperty
 		try:
-			if type == "ADD": result = a + b
-			elif type == "SUBTRACT": result = a - b
-			elif type == "MULITPLY": result = a * b
-			elif type == "DIVIDE": result = a / b
-			elif type == "MODULO": result = a % b
-		except (ZeroDivisionError):
-			print("ZeroDivisionError - " + self.name)
+			if operation == "ADD": result = a + b
+			elif operation == "SUBTRACT": result = a - b
+			elif operation == "MULITPLY": result = a * b
+			elif operation == "DIVIDE": result = a / b
+			elif operation == "SINE": result = math.sin(a)
+			elif operation == "COSINE": result = math.cos(a)
+			elif operation == "TANGENT": result = math.tan(a)
+			elif operation == "ARCSINE":
+				if a > 1:
+					a = 1
+					print("Clamping A to 1")
+				elif a < -1:
+					a = -1
+					print("Clamping A to -1")
+				result = math.asin(a)
+			elif operation == "ARCCOSINE":
+				if a > 1:
+					a = 1
+					print("Clamping A to 1")
+				elif a < -1:
+					a = -1
+					print("Clamping A to -1")
+				result = math.acos(a)
+			elif operation == "ARCTANGENT": result = math.atan(a)
+			elif operation == "POWER": result = math.pow(a, b)
+			elif operation == "LOGARITHM":
+				if b == 0:
+					result = math.log(a)
+				else:
+					result = math.log(a, b)
+			elif operation == "MINIMUM": result = min(a, b)
+			elif operation == "MAXIMUM": result = max(a, b)
+			elif operation == "ROUND": result = round(a, int(b))
+			elif operation == "LESSTHAN":
+				if b < a:
+					result = True
+				else:
+					result = False
+			elif operation == "GREATHERTHAN":
+				if b > a:
+					result = True
+				else:
+					result = False
+			elif operation == "MODULO": result = a % b
+			elif operation == "ABSOLUTE": result = abs(a)
+		except ZeroDivisionError as e:
+			print("ZeroDivisionError: {error} - {name}".format(error=e, name=self.name))
+		except ValueError as e:
+			print("ValueError: {error} - {name}".format(error=e, name=self.name))
 		output = {}
 		output["Result"] = result
 		return output
