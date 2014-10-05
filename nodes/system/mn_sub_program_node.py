16cd2cc578955b27b14434801593f3296eebd6fc
Jacques Lucke <mail@jlucke.com>
2014-10-05 17:00:51 +0200

    add sockets to loop

--- nodes/system/mn_sub_program_node.py
+++ nodes/system/mn_sub_program_node.py
@@ -2,6 +2,7 @@ import bpy
 from bpy.types import Node
 from mn_node_base import AnimationNode
 from mn_execution import nodePropertyChanged
+from mn_utils import *
 
 class SubProgramNode(Node, AnimationNode):
 	bl_idname = "SubProgramNode"
@@ -10,8 +11,22 @@ class SubProgramNode(Node, AnimationNode):
 	def init(self, context):
 		self.inputs.new("SubProgramSocket", "Sub-Program")
 		self.inputs.new("IntegerSocket", "Amount")
-		self.inputs.new("ObjectListSocket", "Objects")
-		self.outputs.new("ObjectListSocket", "Objects")
+		
+	def update(self):
+		if hasLinks(self.inputs["Sub-Program"]):
+			startNode = self.inputs["Sub-Program"].links[0].from_node
+			for i, output in enumerate(startNode.outputs):
+				if i >= 2:
+					if len(self.inputs) > i:
+						if self.inputs[i].name != output.name:
+							self.inputs.remove(self.inputs[i])
+							self.inputs.new(output.bl_idname, output.name)
+							self.outputs.remove(self.inputs[i])
+							self.outputs.new(output.bl_idname, output.name)
+					else:
+						self.inputs.new(output.bl_idname, output.name)
+						self.outputs.new(output.bl_idname, output.name)
+					
 
 		
 # register
--- nodes/system/mn_sub_program_start_node.py
+++ nodes/system/mn_sub_program_start_node.py
@@ -2,6 +2,7 @@ import bpy
 from bpy.types import Node
 from mn_node_base import AnimationNode
 from mn_execution import nodePropertyChanged
+from mn_utils import *
 
 class SubProgramStartNode(Node, AnimationNode):
 	bl_idname = "SubProgramStartNode"
@@ -9,11 +10,49 @@ class SubProgramStartNode(Node, AnimationNode):
 	
 	def init(self, context):
 		self.outputs.new("SubProgramSocket", "Sub-Program")
-		self.outputs.new("ObjectListSocket", "Objects")
 		self.outputs.new("IntegerSocket", "Index")
+		
+	def draw_buttons(self, context, layout):
+		layout.label("Add New Socket")
+		
+		objectList = layout.operator("mn.new_sub_program_socket", text = "Object List")
+		objectList.nodeTreeName = self.id_data.name
+		objectList.nodeName = self.name
+		objectList.socketType = "ObjectListSocket"
+		
+		objectList = layout.operator("mn.new_sub_program_socket", text = "Float")
+		objectList.nodeTreeName = self.id_data.name
+		objectList.nodeName = self.name
+		objectList.socketType = "FloatSocket"
 		
 	def execute(self, input):
 		return input
+		
+	def newSocket(self, socketType):
+		self.outputs.new(socketType, self.getPossibleName("Socket"))
+	def getPossibleName(self, name):
+		counter = 1
+		while self.outputs.get(name + " " + str(counter)) is not None:
+			counter += 1
+		
+		return name + " " + str(counter)
+		
+class NewSubProgramSocketNode(bpy.types.Operator):
+	bl_idname = "mn.new_sub_program_socket"
+	bl_label = "New Sub-Program Socket"
+	
+	nodeTreeName = bpy.props.StringProperty()
+	nodeName = bpy.props.StringProperty()
+	socketType = bpy.props.StringProperty()
+	
+	@classmethod
+	def poll(cls, context):
+		return getActive() is not None
+		
+	def execute(self, context):
+		node = getNode(self.nodeTreeName, self.nodeName)
+		node.newSocket(self.socketType)
+		return {'FINISHED'}	
 		
 # register
 ################################
