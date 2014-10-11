5396aa401158ced12af5e03b903ec4cb08ca293c
Jacques Lucke <mail@jlucke.com>
2014-10-11 19:20:12 +0200

    print debug node to console

--- nodes/output/mn_debug_output.py
+++ nodes/output/mn_debug_output.py
@@ -7,16 +7,19 @@ class DebugOutputNode(Node, AnimationNode):
 	bl_idname = "DebugOutputNode"
 	bl_label = "Debug"
 	
+	printDebugString = bpy.props.BoolProperty(default = False)
 	debugOutputString = bpy.props.StringProperty(default = "")
 	
 	def init(self, context):
 		self.inputs.new("GenericSocket", "Data")
 		
 	def draw_buttons(self, context, layout):
+		layout.prop(self, "printDebugString", text = "Print")
 		layout.label(self.debugOutputString)
 		
 	def execute(self, input):
 		self.debugOutputString = str(input["Data"])
+		if self.printDebugString: print(self.debugOutputString)
 		return {}
 		
 # register
