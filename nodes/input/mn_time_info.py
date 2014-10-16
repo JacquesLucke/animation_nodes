97475c8f70e8e491e60ca8a95aeaa8d715de9468
Jacques Lucke <mail@jlucke.com>
2014-10-16 16:09:42 +0200

    more time info outputs

--- nodes/input/mn_time_info.py
+++ nodes/input/mn_time_info.py
@@ -11,12 +11,19 @@ class mn_TimeInfoNode(Node, AnimationNode):
 	def init(self, context):
 		forbidCompiling()
 		self.outputs.new("FloatSocket", "Frame")
+		self.outputs.new("FloatSocket", "Start Frame")
+		self.outputs.new("FloatSocket", "End Frame")
+		self.outputs.new("FloatSocket", "Frame Rate")
 		allowCompiling()
 		
 	def getInputSocketNames(self):
 		return {}
 	def getOutputSocketNames(self):
-		return {"Frame" : "frame"}
+		return {"Frame" : "frame",
+				"Start Frame" : "start_frame",
+				"End Frame" : "end_frame",
+				"Frame Rate" : "frame_rate"}
 		
 	def execute(self):
-		return getCurrentFrame()
+		scene = bpy.context.scene
+		return getCurrentFrame(), scene.frame_start, scene.frame_end, scene.render.fps
