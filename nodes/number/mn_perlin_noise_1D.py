70a08914d6e573ac66007f07b9f0f015fe3c85f6
Jacques Lucke <mail@jlucke.com>
2014-10-18 12:35:11 +0200

    perlin noise in mn_math_utils

--- nodes/number/mn_perlin_noise_1D.py
+++ nodes/number/mn_perlin_noise_1D.py
@@ -1,5 +1,5 @@
 import bpy, random
-from mn_cache import getRandomNoise
+from mn_math_utils import perlinNoise
 from bpy.types import Node
 from mn_node_base import AnimationNode
 from mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
@@ -7,7 +7,7 @@ from mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 class mn_PerlinNoise1D(Node, AnimationNode):
 	bl_idname = "mn_PerlinNoise1D"
-	bl_label = "Noise 1D"
+	bl_label = "Number Wiggle"
 	
 	additionalSeed = bpy.props.IntProperty(update = nodePropertyChanged)
 	
@@ -34,34 +34,7 @@ class mn_PerlinNoise1D(Node, AnimationNode):
 		return {"Noise" : "noise"}
 		
 	def execute(self, x, slowness, amplitude, persistance, octaves):
-		total = 0
-		x = x / slowness
+		x = x / slowness + 823 * self.additionalSeed
+		total = perlinNoise(x, persistance, octaves)
+		return total * amplitude
 		
-		for i in range(octaves):
-			frequency = 2**i
-			localAmplitude = persistance**i * amplitude
-			total += interpolatedNoise(x * frequency + 823 * self.additionalSeed) * localAmplitude
-			
-		return total
-		
-def interpolatedNoise(x):
-	intX = int(x)
-	v1 = smoothedNoise(intX)
-	v2 = smoothedNoise(intX + 1)
-	v3 = smoothedNoise(intX + 2)
-	v4 = smoothedNoise(intX + 3)
-	return cubicInterpolation(v1, v2, v3, v4, x - intX)
-	
-def smoothedNoise(x):
-	return getRandomNoise(x)/2.0 + getRandomNoise(x-1)/4.0 + getRandomNoise(x+1)/4.0
-	
-def cubicInterpolation(v0, v1, v2, v3, x):
-	p = v3 - v2 - v0 + v1
-	return p * x**3 + ((v0 - v1) - p) * x**2 + (v2 - v0) * x + v1
-	
-	# P = (v3 - v2) - (v0 - v1)
-	# Q = (v0 - v1) - P
-	# R = v2 - v0
-	# S = v1
-
-	# return P*x**3 + Q*x**2 + R*x + S
---
+++ utils/mn_math_utils.py
@@ -0,0 +1,35 @@
+from mn_utils import *
+from mn_cache import *
+
+# perlin noise
+################################
+
+def perlinNoise(x, persistance, octaves):
+	total = 0
+	for i in range(octaves):
+		frequency = 2**i
+		localAmplitude = persistance**i
+		total += interpolatedNoise(x * frequency) * localAmplitude
+	return total
+
+def interpolatedNoise(x):
+	intX = int(x)
+	v1 = smoothedNoise(intX)
+	v2 = smoothedNoise(intX + 1)
+	v3 = smoothedNoise(intX + 2)
+	v4 = smoothedNoise(intX + 3)
+	return cubicInterpolation(v1, v2, v3, v4, x - intX)
+	
+def smoothedNoise(x):
+	return getRandomNoise(x)/2.0 + getRandomNoise(x-1)/4.0 + getRandomNoise(x+1)/4.0
+	
+def cubicInterpolation(v0, v1, v2, v3, x):
+	p = v3 - v2 - v0 + v1
+	return p * x**3 + ((v0 - v1) - p) * x**2 + (v2 - v0) * x + v1
+	
+	# P = (v3 - v2) - (v0 - v1)
+	# Q = (v0 - v1) - P
+	# R = v2 - v0
+	# S = v1
+
+	# return P*x**3 + Q*x**2 + R*x + S
