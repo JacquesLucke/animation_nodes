28f75057778d680c7dbc7af1f623ffbfaaa478f2
Jacques Lucke <mail@jlucke.com>
2014-10-19 04:04:37 +0200

    use modifier output with dataPath again: close #11

--- nodes/object/mn_modifier_output.py
+++ nodes/object/mn_modifier_output.py
@@ -1,7 +1,7 @@
 import bpy
 from bpy.types import Node
 from mn_node_base import AnimationNode
-from mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
 class mn_ModifierOutputNode(Node, AnimationNode):
 	bl_idname = "mn_ModifierOutputNode"
@@ -9,18 +9,20 @@ class mn_ModifierOutputNode(Node, AnimationNode):
 	
 	def init(self, context):
 		forbidCompiling()
-		self.inputs.new("mn_ObjectSocket", "Object")
-		self.inputs.new("mn_StringSocket", "Name").string = ""
-		self.inputs.new("mn_StringSocket", "Attribute").string = ""
-		self.inputs.new("mn_GenericSocket", "Value")
+		objectSocket = self.inputs.new("mn_ObjectSocket", "Object")
+		objectSocket.showName = True
+		self.inputs.new("mn_StringSocket", "Path")
+		self.inputs.new("mn_GenericSocket", "Data")
 		allowCompiling()
 		
-	def execute(self, input):
-		object = input["Object"]
-		dataPath = 'modifiers["' + input["Name"] + '"].' + input["Attribute"]
-		value = input["Value"]
-		try:
-			exec("object." + dataPath + " = value")
-		except:
-			pass
+	def getInputSocketNames(self):
+		return {"Object" : "object",
+				"Path" : "dataPath",
+				"Data" : "data"}
+	def getOutputSocketNames(self):
 		return {}
+		
+	def execute(self, object, dataPath, data):
+		try: exec("object." + dataPath + " = data")
+		except:	pass
+		return None
