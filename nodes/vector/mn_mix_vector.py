1a7bbc3c5de6c3931a1d877897f4b090d40204ac
Jacques Lucke <mail@jlucke.com>
2014-10-22 02:18:14 +0200

    subtract value in mix vector node

--- nodes/vector/mn_mix_vector.py
+++ nodes/vector/mn_mix_vector.py
@@ -14,20 +14,22 @@ class mn_MixVectorNode(Node, AnimationNode):
 		self.inputs.new("mn_FloatSocket", "Value")
 		self.inputs.new("mn_VectorSocket", "A")
 		self.inputs.new("mn_VectorSocket", "B")
+		self.inputs.new("mn_FloatSocket", "Subtract").number = 1.0
 		self.outputs.new("mn_VectorSocket", "Vector")
+		self.outputs.new("mn_FloatSocket", "Subtracted Value")
 		allowCompiling()
 		
 	def draw_buttons(self, context, layout):
 		pass
 		
 	def getInputSocketNames(self):
-		return {"Value" : "value", "A" : "a", "B" : "b"}
+		return {"Value" : "value", "A" : "a", "B" : "b", "Subtract" : "subtract"}
 	def getOutputSocketNames(self):
-		return {"Vector" : "vector"}
+		return {"Vector" : "vector", "Subtracted Value" : "newValue"}
 		
-	def execute(self, value, a, b):
+	def execute(self, value, a, b, subtract):
 		influence = getBackOutNormalInfluence(max(min(value, 1.0), 0.0))
 		return [a[0] * (1 - influence) + b[0] * influence,
 				a[1] * (1 - influence) + b[1] * influence,
-				a[2] * (1 - influence) + b[2] * influence]
+				a[2] * (1 - influence) + b[2] * influence], value - subtract
 		
