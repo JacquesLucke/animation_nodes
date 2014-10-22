1a47b05b860a7affbab87642c49c6f57117cc004
Jacques Lucke <mail@jlucke.com>
2014-10-22 11:35:39 +0200

    movement + stay time

--- nodes/vector/mn_mix_vector.py
+++ nodes/vector/mn_mix_vector.py
@@ -14,22 +14,25 @@ class mn_MixVectorNode(Node, AnimationNode):
 		self.inputs.new("mn_FloatSocket", "Value")
 		self.inputs.new("mn_VectorSocket", "A")
 		self.inputs.new("mn_VectorSocket", "B")
-		self.inputs.new("mn_FloatSocket", "Subtract").number = 1.0
+		self.inputs.new("mn_FloatSocket", "Movement Time").number = 1.0
+		self.inputs.new("mn_FloatSocket", "Stay Time").number = 0.0
+		self.outputs.new("mn_FloatSocket", "New Value")
 		self.outputs.new("mn_VectorSocket", "Vector")
-		self.outputs.new("mn_FloatSocket", "Subtracted Value")
 		allowCompiling()
 		
 	def draw_buttons(self, context, layout):
 		pass
 		
 	def getInputSocketNames(self):
-		return {"Value" : "value", "A" : "a", "B" : "b", "Subtract" : "subtract"}
+		return {"Value" : "value", "A" : "a", "B" : "b", "Movement Time" : "moveTime", "Stay Time" : "stayTime"}
 	def getOutputSocketNames(self):
-		return {"Vector" : "vector", "Subtracted Value" : "newValue"}
+		return {"Vector" : "vector", "New Value" : "newValue"}
 		
-	def execute(self, value, a, b, subtract):
+	def execute(self, value, a, b, moveTime, stayTime):
+		value /= moveTime
 		influence = getBackOutNormalInfluence(max(min(value, 1.0), 0.0))
-		return [a[0] * (1 - influence) + b[0] * influence,
-				a[1] * (1 - influence) + b[1] * influence,
-				a[2] * (1 - influence) + b[2] * influence], value - subtract
+		newVector = [a[0] * (1 - influence) + b[0] * influence,
+					a[1] * (1 - influence) + b[1] * influence,
+					a[2] * (1 - influence) + b[2] * influence]
+		return value * moveTime - moveTime - stayTime, newVector
 		
