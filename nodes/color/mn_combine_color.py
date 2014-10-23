84f5648248b6f531f1beebcfcb4774f4462a6e31
Jacques Lucke <mail@jlucke.com>
2014-10-23 22:18:34 +0200

    combine color node

---
+++ nodes/color/mn_combine_color.py
@@ -0,0 +1,31 @@
+import bpy, random
+from mn_math_utils import perlinNoise
+from bpy.types import Node
+from mn_node_base import AnimationNode
+from mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+
+
+class mn_CombineColor(Node, AnimationNode):
+	bl_idname = "mn_CombineColor"
+	bl_label = "Combine Color"
+	
+	def init(self, context):
+		forbidCompiling()
+		self.inputs.new("mn_FloatSocket", "Red")
+		self.inputs.new("mn_FloatSocket", "Green")
+		self.inputs.new("mn_FloatSocket", "Blue")
+		self.inputs.new("mn_FloatSocket", "Alpha").number = 1
+		self.outputs.new("mn_ColorSocket", "Color")
+		allowCompiling()
+
+	def getInputSocketNames(self):
+		return {"Red" : "red",
+				"Green" : "green",
+				"Blue" : "blue",
+				"Alpha" : "alpha"}
+	def getOutputSocketNames(self):
+		return {"Color" : "color"}
+
+	def execute(self, red, green, blue, alpha):
+		return [red, green, blue, alpha] 
+		
