3744b56c95affd2cade7efc4cfc87741995297b5
Jacques Lucke <mail@jlucke.com>
2014-10-24 11:47:23 +0200

    api test

--- mn_node_base.py
+++ mn_node_base.py
@@ -1,5 +1,7 @@
 import bpy
 from mn_execution import nodeTreeChanged
+from bpy.types import NodeTree, Node, NodeSocket
+from mn_utils import *
 
 class AnimationNodeTree(bpy.types.NodeTree):
 	bl_idname = "AnimationNodeTreeType"
@@ -11,17 +13,22 @@ class AnimationNodeTree(bpy.types.NodeTree):
 	def update(self):
 		nodeTreeChanged()
 		
+		
 class AnimationNode:
 	@classmethod
 	def poll(cls, nodeTree):
 		return nodeTree == "AnimationNodeTreeType"
+		
+		
+class mn_BaseSocket(NodeSocket):
+	bl_idname = "mn_BaseSocket"
+	bl_label = "Base Socket"
 	
-	
-# register
-################################
-	
-def register():
-	bpy.utils.register_module(__name__)
-
-def unregister():
-	bpy.utils.unregister_module(__name__)
+	def draw(self, context, layout, node, text):
+		if not self.is_output and not isSocketLinked(self):
+			self.drawInput(layout, node, text)
+		else:
+			layout.label(text)
+			
+	def draw_color(self, context, node):
+		return self.drawColor
--- sockets/mn_float_socket.py
+++ sockets/mn_float_socket.py
@@ -1,25 +1,19 @@
 import bpy
-from bpy.types import NodeTree, Node, NodeSocket
-from mn_utils import *
 from mn_execution import nodePropertyChanged
+from mn_node_base import * 
 
-class mn_FloatSocket(NodeSocket):
+class mn_FloatSocket(mn_BaseSocket):
 	bl_idname = "mn_FloatSocket"
 	bl_label = "Float Socket"
 	dataType = "Float"
 	allowedInputTypes = ["Float", "Integer"]
+	drawColor = (0.4, 0.4, 0.7, 1)
 	
 	number = bpy.props.FloatProperty(default = 0.0, update = nodePropertyChanged)
 	
-	def draw(self, context, layout, node, text):
-		if not self.is_output and not isSocketLinked(self):
-			layout.prop(self, "number", text = text)
-		else:
-			layout.label(text)
-			
-	def draw_color(self, context, node):
-		return (0.4, 0.4, 0.7, 1)
-		
+	def drawInput(self, layout, node, text):
+		layout.prop(self, "number", text = text)
+	
 	def getValue(self):
 		return self.number
 		
@@ -27,12 +21,3 @@ class mn_FloatSocket(NodeSocket):
 		self.number = data
 	def getStoreableValue(self):
 		return self.number
-		
-# register
-################################
-	
-def register():
-	bpy.utils.register_module(__name__)
-
-def unregister():
-	bpy.utils.unregister_module(__name__)
