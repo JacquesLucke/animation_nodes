f1a79c3f503a7bdce8b91a4b16a3abb3583d22c7
Jacques Lucke <mail@jlucke.com>
2014-10-24 13:14:22 +0200

    cleanup animate vector

--- nodes/vector/mn_animate_vector.py
+++ nodes/vector/mn_animate_vector.py
@@ -16,9 +16,9 @@ class mn_AnimateVectorNode(Node, AnimationNode):
 		self.inputs.new("mn_VectorSocket", "End")
 		self.inputs.new("mn_FloatSocket", "Time")
 		self.inputs.new("mn_InterpolationSocket", "Interpolation")
-		self.inputs.new("mn_FloatSocket", "Movement Time").number = 20.0
-		self.inputs.new("mn_FloatSocket", "Stay Time").number = 0.0
-		self.outputs.new("mn_VectorSocket", "Vector")
+		self.inputs.new("mn_FloatSocket", "Duration").number = 20.0
+		self.inputs.new("mn_FloatSocket", "Delay").number = 0.0
+		self.outputs.new("mn_VectorSocket", "Current")
 		self.outputs.new("mn_FloatSocket", "New Time")
 		self.outputs.new("mn_VectorSocket", "Difference")
 		allowCompiling()
@@ -27,21 +27,21 @@ class mn_AnimateVectorNode(Node, AnimationNode):
 		pass
 		
 	def getInputSocketNames(self):
-		return {"Start" : "start", "End" : "end", "Time" : "time", "Interpolation" : "interpolation", "Movement Time" : "moveTime", "Stay Time" : "stayTime"}
+		return {"Start" : "start", "End" : "end", "Time" : "time", "Interpolation" : "interpolation", "Duration" : "duration", "Delay" : "delay"}
 	def getOutputSocketNames(self):
-		return {"Vector" : "vector", "New Time" : "newTime", "Difference" : "difference"}
+		return {"Current" : "current", "New Time" : "newTime", "Difference" : "difference"}
 		
-	def execute(self, useOutput, start, end, time, interpolation, moveTime, stayTime):
-		influence = interpolation[0](max(min(time / moveTime, 1.0), 0.0), interpolation[1])
-		newVector = [start[0] * (1 - influence) + end[0] * influence,
+	def execute(self, useOutput, start, end, time, interpolation, duration, delay):
+		influence = interpolation[0](max(min(time / duration, 1.0), 0.0), interpolation[1])
+		current = [start[0] * (1 - influence) + end[0] * influence,
 					start[1] * (1 - influence) + end[1] * influence,
 					start[2] * (1 - influence) + end[2] * influence]
 		difference = [0, 0, 0]
 		if useOutput["Difference"]:
-			influence = interpolation[0](max(min((time - 1) / moveTime, 1.0), 0.0), interpolation[1])
+			influence = interpolation[0](max(min((time - 1) / duration, 1.0), 0.0), interpolation[1])
 			oldVector = [start[0] * (1 - influence) + end[0] * influence,
 						start[1] * (1 - influence) + end[1] * influence,
 						start[2] * (1 - influence) + end[2] * influence]
-			difference = [newVector[0] - oldVector[0], newVector[1] - oldVector[1], newVector[2] - oldVector[2]]
-		return newVector, time - moveTime - stayTime, difference
+			difference = [current[0] - oldVector[0], current[1] - oldVector[1], current[2] - oldVector[2]]
+		return current, time - duration - delay, difference
 		
