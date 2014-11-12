5e6a6213803bb2e0476ed992cdd1d735661e0f1b
Jacques Lucke <mail@jlucke.com>
2014-11-12 18:04:50 +0100

    min/max property for float socket

--- sockets/mn_float_socket.py
+++ sockets/mn_float_socket.py
@@ -2,6 +2,11 @@ import bpy
 from mn_execution import nodePropertyChanged
 from mn_node_base import * 
 
+def getValue(self):
+	return min(max(self.min, self.get("number", 0)), self.max)
+def setValue(self, value):
+	self["number"] = min(max(self.min, value), self.max)
+
 class mn_FloatSocket(mn_BaseSocket, mn_SocketProperties):
 	bl_idname = "mn_FloatSocket"
 	bl_label = "Float Socket"
@@ -9,8 +14,11 @@ class mn_FloatSocket(mn_BaseSocket, mn_SocketProperties):
 	allowedInputTypes = ["Float", "Integer"]
 	drawColor = (0.4, 0.4, 0.7, 1)
 	
-	number = bpy.props.FloatProperty(default = 0.0, update = nodePropertyChanged)
+	number = bpy.props.FloatProperty(default = 0.0, update = nodePropertyChanged, set = setValue, get = getValue)
 	showName = bpy.props.BoolProperty(default = True)
+	
+	min = bpy.props.FloatProperty(default = -10000000)
+	max = bpy.props.FloatProperty(default = 10000000)
 	
 	def drawInput(self, layout, node, text):
 		if not self.showName: text = ""
@@ -23,3 +31,4 @@ class mn_FloatSocket(mn_BaseSocket, mn_SocketProperties):
 		self.number = data
 	def getStoreableValue(self):
 		return self.number
+		
