a062639a1e627fa674ab337dbc8ece0911919dc3
Jacques Lucke <mail@jlucke.com>
2014-11-14 15:15:58 +0100

    vertex list socket

---
+++ sockets/mn_vertex_list_socket.py
@@ -0,0 +1,21 @@
+import bpy
+from mn_execution import nodePropertyChanged
+from mn_node_base import * 
+
+class mn_VertexListSocket(mn_BaseSocket, mn_SocketProperties):
+	bl_idname = "mn_VertexListSocket"
+	bl_label = "Vertex List Socket"
+	dataType = "Vertex List"
+	allowedInputTypes = ["Vertex List"]
+	drawColor = (0.3, 1.0, 0.4, 1.0)
+	
+	def drawInput(self, layout, node, text):
+		layout.label(text)
+		
+	def getValue(self):
+		return []
+		
+	def setStoreableValue(self, data):
+		pass
+	def getStoreableValue(self):
+		return []
