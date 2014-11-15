9d51e3346ed9b30fc761a5a9bda7aeda464fa31b
Jacques Lucke <mail@jlucke.com>
2014-11-15 12:18:22 +0100

    polygon (list) socket

---
+++ sockets/mn_polygon_list_socket.py
@@ -0,0 +1,21 @@
+import bpy
+from mn_execution import nodePropertyChanged
+from mn_node_base import * 
+
+class mn_PolygonListSocket(mn_BaseSocket, mn_SocketProperties):
+	bl_idname = "mn_PolygonListSocket"
+	bl_label = "Polygon List Socket"
+	dataType = "Polygon List"
+	allowedInputTypes = ["Polygon List"]
+	drawColor = (0.25, 0.55, 0.23, 1)
+	
+	def drawInput(self, layout, node, text):
+		layout.label(text)
+		
+	def getValue(self):
+		return []
+		
+	def setStoreableValue(self, data):
+		pass
+	def getStoreableValue(self):
+		return []
