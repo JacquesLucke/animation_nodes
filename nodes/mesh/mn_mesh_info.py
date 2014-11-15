2facc7e9392ebf2c4b350e8c36e5f3e8730a1232
Jacques Lucke <mail@jlucke.com>
2014-11-15 12:18:47 +0100

    polygons output for mesh info

--- nodes/mesh/mn_mesh_info.py
+++ nodes/mesh/mn_mesh_info.py
@@ -11,21 +11,26 @@ class mn_MeshInfo(Node, AnimationNode):
 		forbidCompiling()
 		self.inputs.new("mn_MeshSocket", "Mesh").showName = False
 		self.outputs.new("mn_VertexListSocket", "Vertices")
+		self.outputs.new("mn_PolygonListSocket", "Polygons")
 		allowCompiling()
 		
 	def getInputSocketNames(self):
 		return {"Mesh" : "mesh"}
 	def getOutputSocketNames(self):
-		return {"Vertices" : "vertices"}
+		return {"Vertices" : "vertices",
+				"Polygons" : "polygons"}
 		
 	def useInLineExecution(self):
 		return True
 	def getInLineExecutionString(self, outputUse):
 		codeLines = []
 		codeLines.append("$vertices$ = []")
+		codeLines.append("$polygons$ = []")
 		codeLines.append("try:")
 		codeLines.append("    for vertex in %mesh%.vertices:")
 		codeLines.append("        $vertices$.append([vertex.co, vertex.normal])")
+		codeLines.append("    for polygon in %mesh%.polygons:")
+		codeLines.append("        $polygons$.append([polygon.center, polygon.normal, polygon.area, polygon.material_index])")
 		codeLines.append("except: pass")
 		return "\n".join(codeLines)
 		
