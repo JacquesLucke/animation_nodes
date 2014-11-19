e2f51c35d089968dd57420680d68fc95b12fb7ad
Jacques Lucke <mail@jlucke.com>
2014-11-19 14:47:39 +0100

    group input node beginning

---
+++ nodes/system/mn_group_input.py
@@ -0,0 +1,66 @@
+import bpy
+from bpy.types import Node
+from mn_node_base import AnimationNode
+from mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
+from mn_utils import *
+from mn_node_utils import *
+from mn_socket_info import *
+
+newInputSocketName = "New Input"
+
+class mn_GroupInput(Node, AnimationNode):
+	bl_idname = "mn_GroupInput"
+	bl_label = "Group Input"
+	
+	def init(self, context):
+		forbidCompiling()
+		self.outputs.new("mn_EmptySocket", newInputSocketName)
+		allowCompiling()
+		
+	def draw_buttons(self, context, layout):
+		row = layout.row(align = True)
+		
+	def execute(self, input):
+		return input
+		
+	def update(self):
+		forbidCompiling()
+		socket = self.outputs.get(newInputSocketName)
+		targetSocket = self.getValidTargetSocket(socket)
+		if targetSocket is not None:
+			socketType = self.getTargetSocketType(targetSocket)
+			newSocket = self.newOutputSocket(socketType, namePrefix = "socket")
+			self.id_data.links.new(targetSocket, newSocket)
+			newIndex = self.outputs.find(socket.name)
+			self.outputs.move(len(self.outputs) - 1, newIndex)
+		else:
+			removeLinksFromSocket(socket)
+		allowCompiling()
+	
+	def getValidTargetSocket(self, socket):
+		if socket is None:
+			return None
+		links = socket.links
+		if len(links) == 0:
+			return None
+		toSocket = links[0].to_socket
+		if toSocket.node.type == "REROUTE":
+			return None
+		return toSocket
+		
+	def getTargetSocketType(self, targetSocket):
+		idName = targetSocket.bl_idname
+		if idName == "mn_EmptySocket":
+			if targetSocket.passiveSocketType != "":
+				idName = targetSocket.passiveSocketType
+		return idName
+		
+	def newOutputSocket(self, idName, namePrefix):		
+		socket = self.outputs.new(idName, getNotUsedSocketName(self, prefix = "socket"))
+		socket.customName = getNotUsedCustomSocketName(self, prefix = namePrefix)
+		targetSocket = None
+		socket.editableCustomName = True
+		socket.callNodeWhenCustomNameChanged = True
+		socket.removeable = True
+		socket.callNodeToRemove = True
+		return socket
