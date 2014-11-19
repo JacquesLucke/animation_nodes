9db3f73f91f36304a2247bdd7a0bf7c08aa69484
Jacques Lucke <mail@jlucke.com>
2014-11-19 15:06:41 +0100

    fix

--- nodes/system/mn_group_input.py
+++ nodes/system/mn_group_input.py
@@ -1,10 +1,10 @@
 import bpy
 from bpy.types import Node
-from mn_node_base import AnimationNode
-from mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
-from mn_utils import *
-from mn_node_utils import *
-from mn_socket_info import *
+from animation_nodes.mn_node_base import AnimationNode
+from animation_nodes.mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
+from animation_nodes.mn_utils import *
+from animation_nodes.utils.mn_node_utils import *
+from animation_nodes.sockets.mn_socket_info import *
 
 newInputSocketName = "New Input"
 
@@ -64,3 +64,6 @@ class mn_GroupInput(Node, AnimationNode):
 		socket.removeable = True
 		socket.callNodeToRemove = True
 		return socket
+		
+	def removeSocket(self, socket):
+		self.outputs.remove(socket)
