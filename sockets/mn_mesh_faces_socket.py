173221e03319abee7221041a64c0cb132fc745fa
Jacques Lucke <mail@jlucke.com>
2014-12-26 18:35:22 +0100

    mesh utils and socket for faces

---
+++ sockets/mn_mesh_faces_socket.py
@@ -0,0 +1,24 @@
+import bpy
+from animation_nodes.mn_execution import nodePropertyChanged
+from animation_nodes.mn_node_base import *
+from animation_nodes.utils.mn_mesh_utils import Face
+from mathutils import Matrix
+
+class mn_MeshFaces(mn_BaseSocket, mn_SocketProperties):
+	bl_idname = "mn_MeshFaces"
+	bl_label = "Mesh Faces Socket"
+	dataType = "Mesh Faces"
+	allowedInputTypes = ["Mesh Faces"]
+	drawColor = (0.25, 0.55, 0.23, 1)
+	
+	def drawInput(self, layout, node, text):
+		layout.label(text)
+		
+	def getValue(self):
+		return []
+		
+	def setStoreableValue(self, data):
+		pass
+	def getStoreableValue(self):
+		pass
+
---
+++ utils/mn_mesh_utils.py
@@ -0,0 +1,40 @@
+import bpy
+from mathutils import Vector
+
+class Face:
+    def __init__(self, 
+				vertex_positions = [], 
+				area = 0, 
+				center = mathutils.Vector((0, 0, 0)), 
+				normal = mathutils.Vector((0, 0, 0)),
+				material_index = 0):
+        self.vertices = vertex_positions
+        self.area = area
+        self.center = center
+        self.normal = normal
+        self.material_index = material_index
+		
+def get_faces_from_mesh(mesh):
+	faces = []
+	for polygon in mesh.polygons:
+		vertices = []
+		for vertex_index in polygon.vertices:
+			vertices.append(mesh.vertices[vertex_index].co)
+		face = Face(vertices, polygon.area, polygon.center, polygon.normal, polygon.material_index)
+		faces.append(face)
+	return faces
+	
+def new_mesh_from_faces(faces, name = "mesh"):
+	vertex_data = []
+	face_data = []
+	
+	index = 0
+	for face in faces:
+		vertex_data.extend(face.vertices)
+		vertex_amount = len(face.vertices)
+		face_data.append(range(index, index + vertex_amount))
+		index += vertex_amount
+		
+	mesh = bpy.data.meshes.new(name = name)
+	mesh.from_pydata(vertex_data, [], face_data)
+	return mesh
