96a3292e26651ee3a0e5546995ffe7b5a125ecb2
Jacques Lucke <mail@jlucke.com>
2014-12-27 15:20:38 +0100

    transform polygon node

---
+++ nodes/mesh/mn_transform_polygon.py
@@ -0,0 +1,35 @@
+import bpy
+from bpy.types import Node
+from animation_nodes.mn_node_base import AnimationNode
+from animation_nodes.mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from animation_nodes.utils.mn_mesh_utils import *
+from mathutils import Matrix
+
+class mn_TransformPolygon(Node, AnimationNode):
+	bl_idname = "mn_TransformPolygon"
+	bl_label = "Transform Polygon"
+	
+	def init(self, context):
+		forbidCompiling()
+		self.inputs.new("mn_PolygonSocket", "Polygon")
+		self.inputs.new("mn_MatrixSocket", "Matrix")
+		self.outputs.new("mn_PolygonSocket", "Polygon")
+		allowCompiling()
+		
+	def getInputSocketNames(self):
+		return {"Polygon" : "polygon",
+				"Matrix" : "matrix"}
+	def getOutputSocketNames(self):
+		return {"Polygon" : "polygon"}
+		
+	def execute(self, polygon, matrix):
+		offsetMatrix = Matrix.Translation(polygon.center)
+		invertedOffsetMatrix = offsetMatrix.inverted()
+		vertices = polygon.vertices
+		for i in range(len(vertices)):
+			vertex = vertices[i]
+			polygon.vertices[i] = offsetMatrix * matrix *  invertedOffsetMatrix * vertex
+		return polygon
+		
+		
+
