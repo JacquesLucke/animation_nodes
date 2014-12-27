56fe9b3191025cc65c0f098e86b13f4fafa77543
Jacques Lucke <mail@jlucke.com>
2014-12-27 22:05:17 +0100

    transform vertex node

---
+++ nodes/mesh/mn_transform_vertex.py
@@ -0,0 +1,30 @@
+import bpy
+from bpy.types import Node
+from animation_nodes.mn_node_base import AnimationNode
+from animation_nodes.mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from animation_nodes.utils.mn_mesh_utils import *
+from mathutils import Matrix
+
+class mn_TransformVertex(Node, AnimationNode):
+	bl_idname = "mn_TransformVertex"
+	bl_label = "Transform Vertex"
+	
+	def init(self, context):
+		forbidCompiling()
+		self.inputs.new("mn_VertexSocket", "Vertex")
+		self.inputs.new("mn_MatrixSocket", "Matrix")
+		self.outputs.new("mn_VertexSocket", "Vertex")
+		allowCompiling()
+		
+	def getInputSocketNames(self):
+		return {"Vertex" : "vertex",
+				"Matrix" : "matrix"}
+	def getOutputSocketNames(self):
+		return {"Vertex" : "vertex"}
+		
+	def execute(self, vertex, matrix):
+		vertex.location = matrix * vertex.location
+		return vertex
+		
+		
+
