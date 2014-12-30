5c6b34604048dff1aab5ab7ac8d566cb63d2d83d
Jacques Lucke <mail@jlucke.com>
2014-12-30 01:02:43 +0100

    separate mesh data node

---
+++ nodes/mesh/mn_separate_mesh_data.py
@@ -0,0 +1,31 @@
+import bpy, time
+from bpy.types import Node
+from animation_nodes.mn_node_base import AnimationNode
+from animation_nodes.mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from animation_nodes.utils.mn_mesh_utils import *
+from animation_nodes.mn_cache import *
+
+class mn_SeparateMeshData(Node, AnimationNode):
+	bl_idname = "mn_SeparateMeshData"
+	bl_label = "Separate Mesh Data"
+	
+	def init(self, context):
+		forbidCompiling()
+		self.inputs.new("mn_MeshDataSocket", "Mesh Data").showName = False
+		self.outputs.new("mn_VertexListSocket", "Vertex Locations")
+		self.outputs.new("mn_IntegerList2DSocket", "Edges Indices")
+		self.outputs.new("mn_IntegerList2DSocket", "Polygons Indices")
+		allowCompiling()
+		
+	def draw_buttons(self, context, layout):
+		pass
+		
+	def getInputSocketNames(self):
+		return {"Mesh Data" : "meshData"}
+	def getOutputSocketNames(self):
+		return {"Vertex Locations" : "vertexLocations",
+				"Edges Indices" : "edgesIndices",
+				"Polygons Indices" : "polygonsIndices"}
+		
+	def execute(self, meshData):	
+		return meshData.vertices, meshData.edges, meshData.polygons
