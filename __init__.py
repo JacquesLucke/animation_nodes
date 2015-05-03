43761d54ee2c680f5eb1feb62faab4dc3aa6e5a6
Jacques Lucke <mail@jlucke.com>
2015-05-03 12:04:52 +0200

    increase version number to 0.0.2

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ bl_info = {
 	"name":        "Animation Nodes",
 	"description": "Node system for more flexible animations.",
 	"author":      "Jacques Lucke",
-	"version":     (0, 0, 1),
+	"version":     (0, 0, 2),
 	"blender":     (2, 7, 2),
 	"location":    "Node Editor",
 	"category":    "Node",
