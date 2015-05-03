d1b5657122f494eb3ea92f286a6e7d1cfcf84819
Jacques Lucke <mail@jlucke.com>
2015-05-03 19:51:11 +0200

    ability to invert the calculated normals: close # 122

--- nodes/mesh/mn_mesh_recalculate_face_normals.py
+++ nodes/mesh/mn_mesh_recalculate_face_normals.py
@@ -7,11 +7,16 @@ class mn_MeshRecalculateFaceNormals(Node, AnimationNode):
     bl_idname = "mn_MeshRecalculateFaceNormals"
     bl_label = "Recalculate Normals"
     
+    invert = bpy.props.BoolProperty(name = "Invert Normals", update = nodePropertyChanged)
+    
     def init(self, context):
         forbidCompiling()
         self.inputs.new("mn_MeshSocket", "Mesh")
         self.outputs.new("mn_MeshSocket", "Mesh")
         allowCompiling()
+        
+    def draw_buttons(self, context, layout):
+        layout.prop(self, "invert")
         
     def getInputSocketNames(self):
         return {"Mesh" : "bm"}
@@ -19,5 +24,10 @@ class mn_MeshRecalculateFaceNormals(Node, AnimationNode):
         return {"Mesh" : "mesh"}
         
     def execute(self, bm):
-        bmesh.ops.recalc_face_normals(bm, faces = bm.faces)
+        self.calculate_face_normals(bm)
+        if self.invert:
+            self.calculate_face_normals(bm)
         return bm
+        
+    def calculate_face_normals(self, bm):
+        bmesh.ops.recalc_face_normals(bm, faces = bm.faces)
