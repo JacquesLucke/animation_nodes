11f79e7d1395a3d41e37e6b5609bdb5d2d246c34
Jacques Lucke <mail@jlucke.com>
2015-05-15 00:34:23 +0200

    add is_exist, is_visible and age as particle infos

--- nodes/particles/mn_particle_info.py
+++ nodes/particles/mn_particle_info.py
@@ -1,5 +1,6 @@
 import bpy, random, mathutils
 from bpy.types import Node
+from bpy.props import *
 from mathutils import Vector
 from ... mn_node_base import AnimationNode
 from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
@@ -18,9 +19,12 @@ class mn_ParticleInfo(Node, AnimationNode):
         self.outputs.new("mn_VectorSocket", "Angular Velocity")
         self.outputs.new("mn_FloatSocket", "Size")
         self.outputs.new("mn_StringSocket", "Alive State")
+        self.outputs.new("mn_BooleanSocket", "Is Exist")
+        self.outputs.new("mn_BooleanSocket", "Is Visible")
         self.outputs.new("mn_FloatSocket", "Lifetime")
         self.outputs.new("mn_FloatSocket", "Birth Time")
         self.outputs.new("mn_FloatSocket", "Die Time")
+        self.outputs.new("mn_FloatSocket", "Age")
         allowCompiling()
         
     def getInputSocketNames(self):
@@ -32,38 +36,50 @@ class mn_ParticleInfo(Node, AnimationNode):
                 "Angular Velocity" : "angularVelocity",
                 "Size" : "size",
                 "Alive State" : "aliveState",
+                "Is Exist" : "isExist",
+                "Is Visible" : "isVisible",
                 "Lifetime" : "lifetime",
                 "Birth Time" : "birthTime",
-                "Die Time" : "dieTime"}
-
-    def execute(self, particle):
-        if not particle: return Vector((0, 0, 0)), Vector((0, 0, 0)), Vector((0, 0, 0)), Vector((0, 0, 0)), 1, "DEAD", 0, 0, 0
-        location = particle.location
-        rotation = Vector(particle.rotation.to_euler())
-        velocity = particle.velocity
-        angularVelocity = particle.angular_velocity
-        size = particle.size
-        aliveState = particle.alive_state
-        lifetime = particle.lifetime
-        birthTime = particle.birth_time
-        dieTime = particle.die_time
-        return location, rotation, velocity, angularVelocity, size, aliveState, lifetime, birthTime, dieTime
+                "Die Time" : "dieTime",
+                "Age" : "age"}
         
     def useInLineExecution(self):
         return True
+        
     def getInLineExecutionString(self, outputUse):
         codeLines = []
-        codeLines.append("if %particle% is not None:")
+        
+        codeLines.append("if %particle%:")
         if outputUse["Location"]: codeLines.append("    $location$ = %particle%.location")
         if outputUse["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector(%particle%.rotation.to_euler())")
         if outputUse["Velocity"]: codeLines.append("    $velocity$ = %particle%.velocity")
         if outputUse["Angular Velocity"]: codeLines.append("    $angularVelocity$ = %particle%.angular_velocity")
         if outputUse["Size"]: codeLines.append("    $size$ = %particle%.size")
         if outputUse["Alive State"]: codeLines.append("    $aliveState$ = %particle%.alive_state")
+        if outputUse["Is Exist"]: codeLines.append("    $isExist$ = %particle%.is_exist")
+        if outputUse["Is Visible"]: codeLines.append("    $isVisible$ = %particle%.is_visible")
         if outputUse["Lifetime"]: codeLines.append("    $lifetime$ = %particle%.lifetime")
         if outputUse["Birth Time"]: codeLines.append("    $birthTime$ = %particle%.birth_time")
         if outputUse["Die Time"]: codeLines.append("    $dieTime$ = %particle%.die_time")
+        if outputUse["Age"]: codeLines.append("    $age$ = max(0, scene.frame_current - %particle%.birth_time)")
+        codeLines.append("    pass")
+        
+        codeLines.append("else:")
+        if outputUse["Location"]: codeLines.append("    $location$ = mathutils.Vector((0, 0, 0))")
+        if outputUse["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector((0, 0, 0))")
+        if outputUse["Velocity"]: codeLines.append("    $velocity$ = mathutils.Vector((0, 0, 0))")
+        if outputUse["Angular Velocity"]: codeLines.append("    $angularVelocity$ = mathutils.Vector((0, 0, 0))")
+        if outputUse["Size"]: codeLines.append("    $size$ = 1")
+        if outputUse["Alive State"]: codeLines.append("    $aliveState$ = 'DEAD'")
+        if outputUse["Is Exist"]: codeLines.append("    $isExist$ = False")
+        if outputUse["Is Visible"]: codeLines.append("    $isVisible$ = False")
+        if outputUse["Lifetime"]: codeLines.append("    $lifetime$ = 0")
+        if outputUse["Birth Time"]: codeLines.append("    $birthTime$ = 0")
+        if outputUse["Die Time"]: codeLines.append("    $dieTime$ = 0")
+        if outputUse["Age"]: codeLines.append("    $age$ = 0")
         codeLines.append("    pass")        
+        
         return "\n".join(codeLines)
+        
     def getModuleList(self):
         return ["mathutils"]
