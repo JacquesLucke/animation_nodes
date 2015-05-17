543d9f2f40b12c1b74a758e9f9a24fd1a99d6c46
Jacques Lucke <mail@jlucke.com>
2015-05-17 22:39:19 +0200

    make 'create list' node sockets moveable

--- nodes/list/mn_create_list.py
+++ nodes/list/mn_create_list.py
@@ -4,7 +4,7 @@ from bpy.props import *
 from ... mn_node_base import AnimationNode
 from ... utils.mn_node_utils import *
 from ... sockets.mn_socket_info import *
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
 def getListTypeItems(self, context):
     listTypeItems = []
@@ -19,8 +19,14 @@ class mn_CreateList(Node, AnimationNode):
     bl_idname = "mn_CreateList"
     bl_label = "Create List"
     
+    def settingChanged(self, context):
+        for socket in self.inputs:
+            socket.moveable = self.manageSockets
+            socket.removeable = self.manageSockets
+    
     selectedListType = EnumProperty(name = "List Type", items = getListTypeItems)
     listType = StringProperty(default = "mn_FloatListSocket")
+    manageSockets = BoolProperty(name = "Manage Sockets", default = False, description = "Allows to (re)move the input sockets", update = settingChanged)
     
     def init(self, context):
         forbidCompiling()
@@ -31,6 +37,7 @@ class mn_CreateList(Node, AnimationNode):
         props = layout.operator("mn.append_socket_to_list_create_node", text = "New Input", icon = "PLUS")
         props.nodeTreeName = self.id_data.name
         props.nodeName = self.name
+        layout.prop(self, "manageSockets")
         
     def draw_buttons_ext(self, context, layout):
         col = layout.column(align = True)
@@ -50,14 +57,15 @@ class mn_CreateList(Node, AnimationNode):
         self.recreateSockets()
         
     def recreateSockets(self):
+        forbidCompiling()
         self.inputs.clear()
         self.outputs.clear()
         
         self.newInputSocket()
         self.newInputSocket()
-        self.newInputSocket()
-        self.newInputSocket()
         self.outputs.new(self.listType, "List")
+        allowCompiling()
+        nodeTreeChanged()
         
     def newInputSocket(self):
         baseIdName = getBaseSocketType(self.listType)
@@ -65,7 +73,8 @@ class mn_CreateList(Node, AnimationNode):
         socket.displayCustomName = True
         socket.uniqueCustomName = False
         socket.customName = "Element"
-        socket.removeable = True
+        socket.removeable = self.manageSockets
+        socket.moveable = self.manageSockets
         if hasattr(socket, "showName"):
             socket.showName = False
         
