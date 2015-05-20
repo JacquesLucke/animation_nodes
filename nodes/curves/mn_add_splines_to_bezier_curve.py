0bef6e050918ba914b98b74f685e061e4bc22fea
Jacques Lucke <mail@jlucke.com>
2015-05-20 12:53:53 +0200

    add splines to bezier curve

---
+++ nodes/curves/mn_add_splines_to_bezier_curve.py
@@ -0,0 +1,29 @@
+import bpy, bmesh
+from bpy.types import Node
+from ... mn_node_base import AnimationNode
+from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from ... data_structures.mesh import *
+
+class mn_AddSplinesToBezierCurve(Node, AnimationNode):
+    bl_idname = "mn_AddSplinesToBezierCurve"
+    bl_label = "Add Splines to Bezier Curve"
+    
+    def init(self, context):
+        forbidCompiling()
+        self.inputs.new("mn_BezierCurveSocket", "Curve").showName = False
+        self.inputs.new("mn_BezierSplineListSocket", "Splines").showName = False
+        self.outputs.new("mn_BezierCurveSocket", "Curve")
+        allowCompiling()
+        
+    def draw_buttons(self, context, layout):
+        pass
+        
+    def getInputSocketNames(self):
+        return {"Curve" : "curve",
+                "Splines" : "splines"}
+    def getOutputSocketNames(self):
+        return {"Curve" : "curve"}
+        
+    def execute(self, curve, splines):
+        curve.splines.extend(splines)
+        return curve
