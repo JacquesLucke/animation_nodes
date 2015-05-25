a7197313c78e3c9fe0a06509e194470156d95505
Jacques Lucke <mail@jlucke.com>
2015-05-25 21:34:53 +0200

    default values + enum for fill mode in curve properties node

--- nodes/spline/mn_curve_properties.py
+++ nodes/spline/mn_curve_properties.py
@@ -7,16 +7,16 @@ from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompilin
 from ... sockets.mn_socket_info import getIdNameFromDataType
 
 possibleProperties = {
-    "bevel_depth" : ("Bevel Depth", "Float"),
-    "bevel_resolution" : ("Bevel Resolution", "Integer"),
-    "offset" : ("Offset", "Float"),
-    "extrude" : ("Extrude", "Float"),
-    "taper_object" : ("Taper Object", "Object"),
-    "bevel_object" : ("Bevel Object", "Object"),
-    "resolution_u" : ("Preview Resolution", "Integer"),
-    "bevel_factor_start" : ("Bevel Start", "Float"),
-    "bevel_factor_end" : ("Bevel End", "Float"),
-    "fill_mode" : ("Fill Mode", "String")}
+    "bevel_depth" : ("Bevel Depth", "Float", 0),
+    "bevel_resolution" : ("Bevel Resolution", "Integer", 0),
+    "offset" : ("Offset", "Float", 0),
+    "extrude" : ("Extrude", "Float", 0),
+    "taper_object" : ("Taper Object", "Object", ""),
+    "bevel_object" : ("Bevel Object", "Object", ""),
+    "resolution_u" : ("Preview Resolution", "Integer", 12),
+    "bevel_factor_start" : ("Bevel Start", "Float", 0),
+    "bevel_factor_end" : ("Bevel End", "Float", 1),
+    "fill_mode" : ("Fill Mode", "String", "FULL")}
 
 class mn_CurveProperties(Node, AnimationNode):
     bl_idname = "mn_CurveProperties"
@@ -24,36 +24,36 @@ class mn_CurveProperties(Node, AnimationNode):
     
     def getPossiblePropertyItems(self, context):
         items = []
-        for path, (name, dataType) in possibleProperties.items():
+        for path, (name, dataType, default) in possibleProperties.items():
             if not path in self.inputs:
                 items.append((path, name, ""))
         if len(items) == 0: items.append(("NONE", "NONE", ""))
         items = sorted(items, key = lambda x: x[1])
         return items
         
-    def settingChanged(self, context):
+    def settingChanged(self, context = None):
         for socket in self.inputs[1:]:
             socket.removeable = self.manageSockets
             socket.moveable = self.manageSockets
             
     selectedPath = EnumProperty(name = "Type", items = getPossiblePropertyItems)
-    manageSockets = BoolProperty(name = "Manage Sockets", default = True, update = settingChanged, description = "Allows to add, move or remove sockets")
+    manageSockets = BoolProperty(name = "Manage Sockets", default = False, update = settingChanged, description = "Allows to move or remove sockets")
     
     def init(self, context):
         forbidCompiling()
-        self.inputs.new("mn_ObjectSocket", "Object").showName = False        
+        self.inputs.new("mn_ObjectSocket", "Object").showName = False   
         self.outputs.new("mn_ObjectSocket", "Object")
         self.width += 20
         allowCompiling()
         
     def draw_buttons(self, context, layout):
+        row = layout.row(align = True)
+        row.prop(self, "selectedPath", text = "")
+        props = row.operator("mn.append_curve_property_socket", text = "", icon = "PLUS")
+        props.nodeTreeName = self.id_data.name
+        props.nodeName = self.name
+        
         layout.prop(self, "manageSockets")
-        if self.manageSockets:
-            row = layout.row(align = True)
-            row.prop(self, "selectedPath", text = "")
-            props = row.operator("mn.append_curve_property_socket", text = "", icon = "PLUS")
-            props.nodeTreeName = self.id_data.name
-            props.nodeName = self.name
         
     def execute(self, inputs):
         object = inputs["Object"]
@@ -69,12 +69,17 @@ class mn_CurveProperties(Node, AnimationNode):
             self.newSocket(path)
         
     def newSocket(self, path):
-        name, dataType = possibleProperties[path]
+        name, dataType, default = possibleProperties[path]
         socket = self.inputs.new(getIdNameFromDataType(dataType), path)
+        socket.setStoreableValue(default)
         socket.customName = name
         socket.displayCustomName = True
-        socket.removeable = True
-        socket.moveable = True
+        self.settingChanged()
+        
+        # extra case to create an enum property
+        if path == "fill_mode":
+            socket.useEnum = True
+            socket.setEnumItems([("FULL",), ("BACK",), ("FRONT",), ("HALF",)])
         
 class AppendSocket(bpy.types.Operator):
     bl_idname = "mn.append_curve_property_socket"
