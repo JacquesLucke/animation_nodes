68b89816f4e120e6a2cb5cd94b2a688bb12368ee
Jacques Lucke <mail@jlucke.com>
2015-06-11 15:35:37 +0200

    connect splines

---
+++ nodes/spline/mn_connect_splines.py
@@ -0,0 +1,24 @@
+import bpy
+from bpy.types import Node
+from ... mn_node_base import AnimationNode
+from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from ... data_structures.splines.connect import connectSplines
+
+class mn_ConnectSplines(Node, AnimationNode):
+    bl_idname = "mn_ConnectSplines"
+    bl_label = "Connect Splines"
+    
+    def init(self, context):
+        forbidCompiling()
+        self.inputs.new("mn_SplineListSocket", "Splines").showName = False
+        self.outputs.new("mn_SplineSocket", "Spline")
+        allowCompiling()
+        
+    def getInputSocketNames(self):
+        return {"Splines" : "splines"}
+
+    def getOutputSocketNames(self):
+        return {"Spline" : "spline"}
+
+    def execute(self, splines):
+        return connectSplines(splines)
