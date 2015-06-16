09e18e25a22b2499a2694600bd785923a1a7e5c2
og76 <ovidiu.grigorescu@gmail.com>
2015-06-17 01:49:48 +0300

    Update mn_combine_lists.py
    heavy copy from create list, to make it work with 
    any list type
    n inputs
    sock management

--- nodes/list/mn_combine_lists.py
+++ nodes/list/mn_combine_lists.py
@@ -1,53 +1,95 @@
 import bpy
 from bpy.types import Node
+from bpy.props import *
 from ... mn_node_base import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... mn_utils import *
-
+from ... utils.mn_node_utils import *
+from ... utils.selection import getSortedSelectedObjectNames
+from ... sockets.mn_socket_info import *
+from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
+    #heavy copy from create list, comments in comparison to that, except execute..
+def getListTypeItems(self, context):
+    listTypeItems = []
+    for idName in getListSocketIdNames():
+        baseIdName = getListBaseSocketIdName(idName)
+        cls = getSocketClassFromIdName(baseIdName)
+        item = (idName, cls.dataType, "")
+        listTypeItems.append(item)
+    return listTypeItems
+    
 class mn_CombineListsNode(Node, AnimationNode):
     bl_idname = "mn_CombineListsNode"
     bl_label = "Combine Lists"
     
-    def setSocketTypes(self, context):
-        self.setSocketType(self.listTypesProperty)
-        nodePropertyChanged(self, context)
+    def settingChanged(self, context):
+        for socket in self.inputs:
+            socket.moveable = self.manageSockets
+            socket.removeable = self.manageSockets
+            socket.hide = self.hideInputs
     
-    listTypes = [
-        ("FLOAT", "Float", ""),
-        ("STRING", "String", ""),
-        ("OBJECT", "Object", "") ]
-    listTypesProperty = bpy.props.EnumProperty(name = "Type", items = listTypes, default = "OBJECT", update = setSocketTypes)
+    selectedListType = EnumProperty(name = "List Type", items = getListTypeItems)
+    listType = StringProperty(default = "mn_ObjectListSocket")
+    manageSockets = BoolProperty(name = "Manage Sockets", default = False, description = "Allows to (re)move the input sockets", update = settingChanged)
+    
+    hideInputs = BoolProperty(name = "Hide Inputs", default = False, update = settingChanged)
     
     def init(self, context):
         forbidCompiling()
-        self.setSocketType(self.listTypesProperty)
+        self.recreateSockets()
         allowCompiling()
         
     def draw_buttons(self, context, layout):
-        layout.prop(self, "listTypesProperty")
+        self.callFunctionFromUI(layout, "newInputSocket",
+            text = "New Input",
+            description = "Create a new input socket",
+            icon = "PLUS")
+        layout.prop(self, "manageSockets")
+    
+    def draw_buttons_ext(self, context, layout):
+        col = layout.column(align = True)
+        col.prop(self, "selectedListType", text = "")
+        self.callFunctionFromUI(col, "assignSelectedListType", 
+            text = "Assign", 
+            description = "Remove all sockets and set the selected socket type")
+            
+        layout.prop(self, "hideInputs")
+        #no drawTypeSpecificButtonsExt needed for list inputs
         
-    def execute(self, input):
-        output = {}
-        combination = input["List 1"][:]
-        combination.extend(input["List 2"])
-        output["Both Lists"] = combination
-        return output
+    def execute(self, inputs):  #should I try inline? hmm
+        inputLists = []
+        for socket in self.inputs:
+            inputLists.append(inputs[socket.identifier])
+        list = []
+        for el in inputLists:
+            list.extend(el)
+
+        return {"List" : list}
         
-    def setSocketType(self, type):
+    def assignSelectedListType(self):
+        self.assignListType(self.selectedListType)
+        
+    def assignListType(self, idName, inputAmount = 2):
+        self.listType = idName
+        self.recreateSockets(inputAmount)
+        
+    def recreateSockets(self, inputAmount = 2):
         forbidCompiling()
         self.inputs.clear()
         self.outputs.clear()
-        if type == "FLOAT":
-            self.inputs.new("mn_FloatListSocket", "List 1")
-            self.inputs.new("mn_FloatListSocket", "List 2")
-            self.outputs.new("mn_FloatListSocket", "Both Lists")
-        elif type == "STRING":
-            self.inputs.new("mn_StringListSocket", "List 1")
-            self.inputs.new("mn_StringListSocket", "List 2")
-            self.outputs.new("mn_StringListSocket", "Both Lists")
-        elif type == "OBJECT":
-            self.inputs.new("mn_ObjectListSocket", "List 1")
-            self.inputs.new("mn_ObjectListSocket", "List 2")
-            self.outputs.new("mn_ObjectListSocket", "Both Lists")
+        
+        for i in range(inputAmount):
+            self.newInputSocket()
+        self.outputs.new(self.listType, "List")
         allowCompiling()
-
+        nodeTreeChanged()
+        
+    def newInputSocket(self):
+        socket = self.inputs.new(self.listType, getNotUsedSocketName(self, "List"))#suffices for list type soc
+        socket.displayCustomName = True
+        socket.uniqueCustomName = False
+        socket.customName = "List"
+        socket.removeable = self.manageSockets
+        socket.moveable = self.manageSockets
+        # showName not the case on list sockets
+        return socket
+        
+        #no drawTypeSpecificButtonsExt needed for list inputs
