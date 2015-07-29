e6af7a634032d58f9e0bfaeab16e90d02d6faf1b
Jacques Lucke <mail@jlucke.com>
2015-07-29 23:56:58 +0200

    cleanup invert boolean nod

--- nodes/boolean/mn_invert_node.py
+++ nodes/boolean/mn_invert_node.py
@@ -1,20 +1,17 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
-
-class mn_InvertNode(bpy.types.Node, AnimationNode):
+class InvertNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_InvertNode"
     bl_label = "Invert Boolean"
     
-    def init(self, context):
-        forbidCompiling()
+    def create(self):
         self.inputs.new("mn_BooleanSocket", "Input")
         self.outputs.new("mn_BooleanSocket", "Output")
-        allowCompiling()
         
     def getInputSocketNames(self):
         return {"Input" : "input"}
+        
     def getOutputSocketNames(self):
         return {"Output" : "output"}
         
