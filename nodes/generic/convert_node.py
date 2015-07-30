d6ea97d692014cab08a97356d5a9ca2f4d1238a0
Jacques Lucke <mail@jlucke.com>
2015-07-30 15:48:02 +0200

    cleanup convert nod

---
+++ nodes/generic/convert_node.py
@@ -0,0 +1,66 @@
+import bpy
+from ... base_types.node import AnimationNode
+from ... mn_utils import getConnectionDictionaries, tryToSetConnectionDictionaries
+from ... sockets.mn_socket_info import *
+
+class ConvertNode(bpy.types.Node, AnimationNode):
+    bl_idname = "mn_ConvertNode"
+    bl_label = "Convert"
+    isDetermined = True
+    
+    inputNames = { "Old" : "old" }
+    outputNames = { "New" : "new"}
+    
+    convertType = bpy.props.StringProperty(default = "Integer")
+    
+    def create(self):
+        self.inputs.new("mn_GenericSocket", "Old")
+        self.buildOutputSocket()
+        
+    def editorChanged(self):
+        link = self.getFirstOutputLink()
+        if link is not None:
+            fromSocket = link.from_socket
+            toSocket = link.to_socket
+            if toSocket.node.type != "REROUTE":
+                if fromSocket.dataType != toSocket.dataType:
+                    self.convertType = toSocket.dataType
+                    self.buildOutputSocket()
+        
+    def getFirstOutputLink(self):
+        links = self.getLinksFromOutputSocket()
+        if len(links) == 1: return links[0]
+        return None
+        
+    def getLinksFromOutputSocket(self):
+        socket = self.outputs.get("New")
+        if socket is not None:
+            return socket.links
+        return []
+        
+    def buildOutputSocket(self):
+        connections = getConnectionDictionaries(self)
+        self.outputs.clear()
+        self.outputs.new(getIdNameFromDataType(self.convertType), "New")
+        tryToSetConnectionDictionaries(self, connections)
+        
+    def getInputSocketNames(self):
+        return {"Old" : "old"}
+    def getOutputSocketNames(self):
+        return {"New" : "new"}
+        
+    def getExecutionCode(self):
+        t = self.convertType
+        if t == "Float": return ("try: $new$ = float(%old%) \n"
+                                 "except: $new$ = 0")
+        elif t == "Integer": return ("try: $new$ = int(%old%) \n"
+                                     "except: $new$ = 0")
+        elif t == "String": return ("try: $new$ = str(%old%) \n"
+                                    "except: $new$ = ''")
+        else:
+            return "$new$ = %old%"
+            
+    def getModuleList(self):
+        t = self.convertType
+        if t == "Vector": return ["mathutils"]
+        return []
