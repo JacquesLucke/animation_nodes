1f515b9c72f37b5e3950826d412c0b0d156c54e9
Jacques Lucke <mail@jlucke.com>
2015-07-30 14:32:07 +0200

    cleanup bool invert node

--- nodes/boolean/invert_node.py
+++ nodes/boolean/invert_node.py
@@ -5,17 +5,12 @@ class InvertNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_InvertNode"
     bl_label = "Invert Boolean"
     
+    inputNames = { "Input" : "input" }
+    outputNames = { "Output" : "output" }
+    
     def create(self):
         self.inputs.new("mn_BooleanSocket", "Input")
         self.outputs.new("mn_BooleanSocket", "Output")
         
-    def getInputSocketNames(self):
-        return {"Input" : "input"}
-        
-    def getOutputSocketNames(self):
-        return {"Output" : "output"}
-        
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$output$ = not %input%"
