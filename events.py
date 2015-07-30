5f2140d35296fab7c04a33a94ba43c53e86a7e46
Jacques Lucke <mail@jlucke.com>
2015-07-30 12:42:19 +0200

    event abstraction

---
+++ events.py
@@ -0,0 +1,7 @@
+from . mn_execution import nodePropertyChanged, nodeTreeChanged
+
+def propertyChanged(self = None, context = None):
+    nodePropertyChanged()
+    
+def treeChanged(self = None, context = None):
+    nodeTreeChanged()
