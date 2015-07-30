ba1da34cab061936869e385f2d7969c743dd954e
Jacques Lucke <mail@jlucke.com>
2015-07-30 14:40:10 +0200

    cleanup mix color node

--- nodes/color/mix_color.py
+++ nodes/color/mix_color.py
@@ -1,27 +1,23 @@
 import bpy, random
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 
-class mn_ColorMix(bpy.types.Node, AnimationNode):
+class ColorMix(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ColorMix"
     bl_label = "Color Mix"
     isDetermined = True
     
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Factor" : "factor",
+                   "Color 1" : "a",
+                   "Color 2" : "b" }
+                   
+    outputNames = { "Color" : "color" }                  
+    
+    def create(self):
         self.inputs.new("mn_FloatSocket", "Factor")
         self.inputs.new("mn_ColorSocket", "Color 1")
         self.inputs.new("mn_ColorSocket", "Color 2")
         self.outputs.new("mn_ColorSocket", "Color")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Factor" : "factor",
-                "Color 1" : "a",
-                "Color 2" : "b"}
-    def getOutputSocketNames(self):
-        return {"Color" : "color"}
 
     def execute(self, factor, a, b):
         newColor = [0, 0, 0, 0]
