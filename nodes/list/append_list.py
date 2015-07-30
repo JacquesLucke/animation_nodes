d79c91c5281431b2d317c0ad836812531f7fd168
Jacques Lucke <mail@jlucke.com>
2015-07-30 15:59:10 +0200

    cleanup append list node

--- nodes/list/append_list.py
+++ nodes/list/append_list.py
@@ -1,39 +1,30 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... utils.mn_node_utils import *
-from ... sockets.mn_socket_info import *
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from ... utils.mn_node_utils import NodeTreeInfo
+from ... sockets.mn_socket_info import getListBaseSocketIdName, getListSocketIdName
 
-class mn_AppendListNode(bpy.types.Node, AnimationNode):
+class AppendListNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_AppendListNode"
     bl_label = "Append to List"
     
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "List" : "list",
+                   "Element" : "element" }
+    outputNames = { "List" : "list" }
+    
+    def create(self):
         self.generateSockets()
-        allowCompiling()
         
-    def getInputSocketNames(self):
-        return {"List" : "list",
-                "Element" : "element"}
-    def getOutputSocketNames(self):
-        return {"List" : "list"}
-        
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$list$ = %list%\n" + \
                 "$list$.append(%element%)"
                 
-    def update(self):
+    def editorChanged(self):
         nodeTree = self.id_data
         treeInfo = NodeTreeInfo(nodeTree)
         
         listInput = treeInfo.getDataOriginSocket(self.inputs.get("List"))
         elementInput = treeInfo.getDataOriginSocket(self.inputs.get("Element"))
         listOutputs = treeInfo.getDataTargetSockets(self.outputs.get("List"))
-        
-        forbidCompiling()
         
         if listInput is not None and elementInput is None and len(listOutputs) == 0:
             self.generateSockets(listInput.bl_idname)
@@ -46,8 +37,6 @@ class mn_AppendListNode(bpy.types.Node, AnimationNode):
         if listInput is None and elementInput is None and len(listOutputs) == 1:
             self.generateSockets(listOutputs[0].bl_idname)
             nodeTree.links.new(listOutputs[0], self.outputs.get("List"))
-            
-        allowCompiling()
         
     def generateSockets(self, listIdName = "mn_ObjectListSocket"):
         if listIdName is None: return
@@ -55,10 +44,10 @@ class mn_AppendListNode(bpy.types.Node, AnimationNode):
         if baseIdName is None: return
         if listIdName == getattr(self.inputs.get("List"), "bl_idname", None): return
         
-        forbidCompiling()
+        self.id_data.startEdit()
         self.inputs.clear()
         self.outputs.clear()
         self.inputs.new(listIdName, "List")
         self.inputs.new(baseIdName, "Element")
         self.outputs.new(listIdName, "List")
-        allowCompiling()
+        self.id_data.stopEdit()
