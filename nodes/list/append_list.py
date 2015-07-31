4c9772423a74d6c58396e948c55b2c7e074f6b88
Jacques Lucke <mail@jlucke.com>
2015-07-31 15:37:54 +0200

    fix create and append list nodes

--- nodes/list/append_list.py
+++ nodes/list/append_list.py
@@ -6,48 +6,46 @@ from ... sockets.mn_socket_info import getListBaseSocketIdName, getListSocketIdN
 class AppendListNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_AppendListNode"
     bl_label = "Append to List"
-    
+
     inputNames = { "List" : "list",
                    "Element" : "element" }
     outputNames = { "List" : "list" }
-    
+
     def create(self):
         self.generateSockets()
-        
+
     def getExecutionCode(self):
         return "$list$ = %list%\n" + \
                 "$list$.append(%element%)"
-                
+
     def edit(self):
         nodeTree = self.id_data
         treeInfo = NodeTreeInfo(nodeTree)
-        
+
         listInput = treeInfo.getDataOriginSocket(self.inputs.get("List"))
         elementInput = treeInfo.getDataOriginSocket(self.inputs.get("Element"))
         listOutputs = treeInfo.getDataTargetSockets(self.outputs.get("List"))
-        
+
         if listInput is not None and elementInput is None and len(listOutputs) == 0:
             self.generateSockets(listInput.bl_idname)
             nodeTree.links.new(self.inputs.get("List"), listInput)
-            
+
         if listInput is None and elementInput is not None and len(listOutputs) == 0:
             self.generateSockets(getListSocketIdName(elementInput.bl_idname))
             nodeTree.links.new(self.inputs.get("Element"), elementInput)
-            
+
         if listInput is None and elementInput is None and len(listOutputs) == 1:
             self.generateSockets(listOutputs[0].bl_idname)
             nodeTree.links.new(listOutputs[0], self.outputs.get("List"))
-        
+
     def generateSockets(self, listIdName = "mn_ObjectListSocket"):
         if listIdName is None: return
         baseIdName = getListBaseSocketIdName(listIdName)
         if baseIdName is None: return
         if listIdName == getattr(self.inputs.get("List"), "bl_idname", None): return
-        
-        self.id_data.startEdit()
+
         self.inputs.clear()
         self.outputs.clear()
         self.inputs.new(listIdName, "List")
         self.inputs.new(baseIdName, "Element")
         self.outputs.new(listIdName, "List")
-        self.id_data.stopEdit()
--- nodes/list/create_list.py
+++ nodes/list/create_list.py
@@ -18,65 +18,63 @@ def getListTypeItems(self, context):
 class CreateList(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CreateList"
     bl_label = "Create List"
-    
+
     def settingChanged(self, context):
         for socket in self.inputs:
             socket.moveable = self.manageSockets
             socket.removeable = self.manageSockets
             socket.hide = self.hideInputs
-    
+
     selectedListType = EnumProperty(name = "List Type", items = getListTypeItems)
-    listType = StringProperty(default = "mn_FloatListSocket")
+    assignedListType = StringProperty(default = "mn_FloatListSocket")
     manageSockets = BoolProperty(name = "Manage Sockets", default = False, description = "Allows to (re)move the input sockets", update = settingChanged)
-    
+
     hideInputs = BoolProperty(name = "Hide Inputs", default = False, update = settingChanged)
-    
+
     def create(self):
         self.recreateSockets()
-        
+
     def draw_buttons(self, context, layout):
         self.callFunctionFromUI(layout, "newInputSocket",
             text = "New Input",
             description = "Create a new input socket",
             icon = "PLUS")
         layout.prop(self, "manageSockets")
-        
+
     def draw_buttons_ext(self, context, layout):
         col = layout.column(align = True)
         col.prop(self, "selectedListType", text = "")
-        self.callFunctionFromUI(col, "assignSelectedListType", 
-            text = "Assign", 
+        self.callFunctionFromUI(col, "assignSelectedListType",
+            text = "Assign",
             description = "Remove all sockets and set the selected socket type")
-            
+
         layout.prop(self, "hideInputs")
-            
+
         self.drawTypeSpecificButtonsExt(layout)
-        
+
     def execute(self, inputs):
         elements = []
         for socket in self.inputs:
             elements.append(inputs[socket.identifier])
         return {"List" : elements}
-        
+
     def assignSelectedListType(self):
         self.assignListType(self.selectedListType)
-        
+
     def assignListType(self, idName, inputAmount = 2):
-        self.listType = idName
+        self.assignedListType = idName
         self.recreateSockets(inputAmount)
-        
+
     def recreateSockets(self, inputAmount = 2):
-        self.id_data.startEdit()
         self.inputs.clear()
         self.outputs.clear()
-        
+
         for i in range(inputAmount):
             self.newInputSocket()
-        self.outputs.new(self.listType, "List")
-        self.id_data.stopEdit()
-        
+        self.outputs.new(self.assignedListType, "List")
+
     def newInputSocket(self):
-        baseIdName = getListBaseSocketIdName(self.listType)
+        baseIdName = getListBaseSocketIdName(self.assignedListType)
         socket = self.inputs.new(baseIdName, getNotUsedSocketName(self, "Element"))
         socket.displayCustomName = True
         socket.uniqueCustomName = False
@@ -84,17 +82,17 @@ class CreateList(bpy.types.Node, AnimationNode):
         socket.removeable = self.manageSockets
         socket.moveable = self.manageSockets
         if hasattr(socket, "showName"):
-            socket.showName = False  
+            socket.showName = False
         return socket
 
 
     # type specific stuff
     #############################
-    
+
     def drawTypeSpecificButtonsExt(self, layout):
-        if self.listType == "mn_ObjectListSocket":
+        if self.assignedListType == "mn_ObjectListSocket":
             self.callFunctionFromUI(layout, "createInputsFromSelection", text = "From Selection", icon = "PLUS")
-            
+
     def createInputsFromSelection(self):
         names = getSortedSelectedObjectNames()
         for name in names:
