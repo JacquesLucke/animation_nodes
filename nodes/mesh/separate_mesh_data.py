fec70316350006108db06c8384ec2f5b725bbea2
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:47:16 +0200

    cleanup separate mesh data node

--- nodes/mesh/separate_mesh_data.py
+++ nodes/mesh/separate_mesh_data.py
@@ -1,30 +1,21 @@
-import bpy, time
+import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
-from ... mn_cache import *
 
-class mn_SeparateMeshData(bpy.types.Node, AnimationNode):
+class SeparateMeshData(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SeparateMeshData"
     bl_label = "Separate Mesh Data"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Mesh Data" : "meshData" }
+
+    outputNames = { "Vertex Locations" : "vertexLocations",
+                    "Edges Indices" : "edgesIndices",
+                    "Polygons Indices" : "polygonsIndices" }
+
+    def create(self):
         self.inputs.new("mn_MeshDataSocket", "Mesh Data")
         self.outputs.new("mn_VectorListSocket", "Vertex Locations")
         self.outputs.new("mn_EdgeIndicesListSocket", "Edges Indices")
         self.outputs.new("mn_PolygonIndicesListSocket", "Polygons Indices")
-        allowCompiling()
-
-    def draw_buttons(self, context, layout):
-        pass
-
-    def getInputSocketNames(self):
-        return {"Mesh Data" : "meshData"}
-    def getOutputSocketNames(self):
-        return {"Vertex Locations" : "vertexLocations",
-                "Edges Indices" : "edgesIndices",
-                "Polygons Indices" : "polygonsIndices"}
 
     def execute(self, meshData):
         return meshData.vertices, meshData.edges, meshData.polygons
