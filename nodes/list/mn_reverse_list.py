00337d092a4f3659cfa735eaff32fefe4217da74
Jacques Lucke <mail@jlucke.com>
2015-08-02 20:01:45 +0200

    cleanup reversse list node

--- nodes/list/mn_reverse_list.py
+++ nodes/list/mn_reverse_list.py
@@ -1,49 +1,39 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... utils.mn_node_utils import *
-from ... sockets.mn_socket_info import *
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
+from ... utils.mn_node_utils import NodeTreeInfo
 
-class mn_ReverseListNode(bpy.types.Node, AnimationNode):
+class ReverseListNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ReverseListNode"
     bl_label = "Reverse List"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "List" : "list" }
+    outputNames = { "Reversed List" : "list" }
+
+    def create(self):
         self.generateSockets()
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"List" : "list"}
-    def getOutputSocketNames(self):
-        return {"Reversed List" : "list"}
-        
+
     def execute(self, list):
         list.reverse()
         return list
-        
-    def update(self):
+
+    def edit(self):
         nodeTree = self.id_data
         treeInfo = NodeTreeInfo(nodeTree)
         originSocket = treeInfo.getDataOriginSocket(self.inputs.get("List"))
         targetSockets = treeInfo.getDataTargetSockets(self.outputs.get("Reversed List"))
-        
-        forbidCompiling()
+
         if originSocket is not None and len(targetSockets) == 0:
             self.generateSockets(originSocket.bl_idname)
             nodeTree.links.new(self.inputs.get("List"), originSocket)
         if originSocket is None and len(targetSockets) == 1:
             self.generateSockets(targetSockets[0].bl_idname)
             nodeTree.links.new(targetSockets[0], self.outputs.get("Reversed List"))
-        allowCompiling()
-        
+
     def generateSockets(self, listIdName = "mn_ObjectListSocket"):
         if listIdName is None: return
         if listIdName == getattr(self.inputs.get("List"), "bl_idname", None): return
-        
-        forbidCompiling()
+
         self.inputs.clear()
         self.outputs.clear()
         self.inputs.new(listIdName, "List")
         self.outputs.new(listIdName, "Reversed List")
-        allowCompiling()
