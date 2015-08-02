fa69f97e2e93ae6f952935778412bc0946fb4266
Jacques Lucke <mail@jlucke.com>
2015-08-02 22:47:09 +0200

    cleanup translation matrix node

--- nodes/matrix/translation_matrix.py
+++ nodes/matrix/translation_matrix.py
@@ -1,32 +1,24 @@
 import bpy
-from mathutils import *
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 
-class mn_TranslationMatrix(bpy.types.Node, AnimationNode):
+class TranslationMatrix(bpy.types.Node, AnimationNode):
     bl_idname = "mn_TranslationMatrix"
     bl_label = "Translation Matrix"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Translation" : "translation" }
+    outputNames = { "Matrix" : "matrix" }
+
+    def create(self):
         self.inputs.new("mn_VectorSocket", "Translation")
         self.outputs.new("mn_MatrixSocket", "Matrix")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Translation" : "translation"}
-    def getOutputSocketNames(self):
-        return {"Matrix" : "matrix"}
 
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$matrix$ = mathutils.Matrix.Translation(%translation%)"
-        
+
     def getModuleList(self):
         return ["mathutils"]
-        
-    def copy(self, node):
-        self.inputs[0].vector = [0, 0, 0]
+
+    def duplicate(self, sourceNode):
+        self.inputs[0].value = [0, 0, 0]
