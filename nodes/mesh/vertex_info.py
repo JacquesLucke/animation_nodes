8a73daa719896fea15002427bf99ffe6cc51132c
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:59:09 +0200

    cleanup vertex info node

--- nodes/mesh/vertex_info.py
+++ nodes/mesh/vertex_info.py
@@ -1,26 +1,21 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
 
-class mn_VertexInfo(bpy.types.Node, AnimationNode):
+class VertexInfo(bpy.types.Node, AnimationNode):
     bl_idname = "mn_VertexInfo"
     bl_label = "Vertex Info"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Vertex" : "vertex" }
+
+    outputNames = { "Location" : "location",
+                    "Normal" : "normal",
+                    "Group Weights" : "groupWeights" }
+
+    def create(self):
         self.inputs.new("mn_VertexSocket", "Vertex")
         self.outputs.new("mn_VectorSocket", "Location")
         self.outputs.new("mn_VectorSocket", "Normal")
         self.outputs.new("mn_FloatListSocket", "Group Weights")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Vertex" : "vertex"}
-    def getOutputSocketNames(self):
-        return {"Location" : "location",
-                "Normal" : "normal",
-                "Group Weights" : "groupWeights"}
 
     def execute(self, vertex):
         return vertex.location, vertex.normal, vertex.groupWeights
