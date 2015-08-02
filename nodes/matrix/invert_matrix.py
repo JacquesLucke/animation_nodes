b0c621c70af5f1fc33e6fada26fb4836bea418a4
Jacques Lucke <mail@jlucke.com>
2015-08-02 22:17:40 +0200

    cleanup invert matrix node

--- nodes/matrix/invert_matrix.py
+++ nodes/matrix/invert_matrix.py
@@ -1,32 +1,23 @@
 import bpy
-from mathutils import *
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-
-class mn_InvertMatrix(bpy.types.Node, AnimationNode):
+class InvertMatrix(bpy.types.Node, AnimationNode):
     bl_idname = "mn_InvertMatrix"
     bl_label = "Invert Matrix"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Matrix" : "matrix" }
+    outputNames = { "Inverted Matrix" : "matrix" }
+
+    def create(self):
         self.inputs.new("mn_MatrixSocket", "Matrix")
         self.outputs.new("mn_MatrixSocket", "Inverted Matrix")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.separator()
-        
-    def getInputSocketNames(self):
-        return {"Matrix" : "matrix"}
-    def getOutputSocketNames(self):
-        return {"Inverted Matrix" : "matrix"}
 
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self, outputUse):
         return "$matrix$ = %matrix%.inverted(mathutils.Matrix.Identity(4))"
-        
+
     def getModuleList(self):
         return ["mathutils"]
