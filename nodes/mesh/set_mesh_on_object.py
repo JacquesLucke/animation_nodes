062ed37640de4a1829084454cbce9d00797ad451
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:50:26 +0200

    cleanup set mesh on object node

--- nodes/mesh/set_mesh_on_object.py
+++ nodes/mesh/set_mesh_on_object.py
@@ -1,29 +1,21 @@
 import bpy, bmesh
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
 
-class mn_SetMeshOnObject(bpy.types.Node, AnimationNode):
+class SetMeshOnObject(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SetMeshOnObject"
     bl_label = "Set Mesh on Object"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Object" : "object",
+                   "Mesh" : "bm" }
+
+    outputNames = { "Object" : "object" }
+
+    def create(self):
         socket = self.inputs.new("mn_ObjectSocket", "Object")
         socket.showName = False
         socket.objectCreationType = "MESH"
         self.inputs.new("mn_MeshSocket", "Mesh")
         self.outputs.new("mn_ObjectSocket", "Object")
-        allowCompiling()
-
-    def draw_buttons(self, context, layout):
-        pass
-
-    def getInputSocketNames(self):
-        return {"Object" : "object",
-                "Mesh" : "bm"}
-    def getOutputSocketNames(self):
-        return {"Object" : "object"}
 
     def execute(self, object, bm):
         if object is None: return object
