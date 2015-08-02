8630330b2399f81d71925ce22d71c10d9b9742e9
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:00:24 +0200

    cleanup combine mesh data node

--- nodes/mesh/combine_mesh_data.py
+++ nodes/mesh/combine_mesh_data.py
@@ -1,45 +1,39 @@
-import bpy, time
+import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
-from ... mn_cache import *
+from ... data_structures.mesh import MeshData
+from ... events import propertyChanged
 
 sourceTypeItems = [
     ("MESH_DATA", "Vertices and Indices", ""),
     ("POLYGONS", "Polygons", "") ]
 
-class mn_CombineMeshData(bpy.types.Node, AnimationNode):
+class CombineMeshData(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CombineMeshData"
     bl_label = "Combine Mesh Data"
 
+    inputNames = { "Vertex Locations" : "vertexLocations",
+                   "Edges Indices" : "edgesIndices",
+                   "Polygons Indices" : "polygonsIndices",
+                   "Polygons" : "polygons" }
+
+    outputNames = { "Mesh Data" : "meshData" }
+
     def sourceTypeChanged(self, context):
         self.updateHideStatus()
-        nodePropertyChanged(self, context)
+        propertyChanged(self, context)
 
     sourceType = bpy.props.EnumProperty(items = sourceTypeItems, default = "MESH_DATA", name = "Source Type", update = sourceTypeChanged)
 
-    def init(self, context):
-        forbidCompiling()
+    def create(self):
         self.inputs.new("mn_VectorListSocket", "Vertex Locations")
         self.inputs.new("mn_EdgeIndicesListSocket", "Edges Indices")
         self.inputs.new("mn_PolygonIndicesListSocket", "Polygons Indices")
         self.inputs.new("mn_PolygonListSocket", "Polygons")
         self.updateHideStatus()
         self.outputs.new("mn_MeshDataSocket", "Mesh Data")
-        allowCompiling()
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "sourceType")
-
-    def getInputSocketNames(self):
-        return {"Vertex Locations" : "vertexLocations",
-                "Edges Indices" : "edgesIndices",
-                "Polygons Indices" : "polygonsIndices",
-                "Polygons" : "polygons"}
-
-    def getOutputSocketNames(self):
-        return {"Mesh Data" : "meshData"}
-
 
     def execute(self, vertexLocations, edgesIndices, polygonsIndices, polygons):
         if self.sourceType == "MESH_DATA":
