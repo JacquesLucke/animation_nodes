62fb0082c23cc57fdafaad34d6ceea83e9c2c8cd
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:45:21 +0200

    cleanup polygon info node

--- nodes/mesh/polygon_info.py
+++ nodes/mesh/polygon_info.py
@@ -1,30 +1,25 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
 
-class mn_PolygonInfo(bpy.types.Node, AnimationNode):
+class PolygonInfo(bpy.types.Node, AnimationNode):
     bl_idname = "mn_PolygonInfo"
     bl_label = "Polygon Info"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Polygon" : "polygon" }
+
+    outputNames = { "Center" : "center",
+                    "Normal" : "normal",
+                    "Material Index" : "materialIndex",
+                    "Area" : "area",
+                    "Vertices" : "vertices" }
+
+    def create(self):
         self.inputs.new("mn_PolygonSocket", "Polygon")
         self.outputs.new("mn_VectorSocket", "Center")
         self.outputs.new("mn_VectorSocket", "Normal")
         self.outputs.new("mn_VectorSocket", "Material Index")
         self.outputs.new("mn_FloatSocket", "Area")
         self.outputs.new("mn_VertexListSocket", "Vertices")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Polygon" : "polygon"}
-    def getOutputSocketNames(self):
-        return {"Center" : "center",
-                "Normal" : "normal",
-                "Material Index" : "materialIndex",
-                "Area" : "area",
-                "Vertices" : "vertices"}
 
     def execute(self, polygon):
         return polygon.center, polygon.normal, polygon.materialIndex, polygon.area, polygon.vertices
