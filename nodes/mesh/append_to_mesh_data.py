aa71ac5a9e1c69cbd1481a6ee7e80257a218c1cc
Jacques Lucke <mail@jlucke.com>
2015-08-02 22:56:17 +0200

    cleanup append to mesh data node

--- nodes/mesh/append_to_mesh_data.py
+++ nodes/mesh/append_to_mesh_data.py
@@ -1,25 +1,19 @@
-import bpy, bmesh
+import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
-import bmesh
 
-class mn_AppendToMeshData(bpy.types.Node, AnimationNode):
+class AppendToMeshData(bpy.types.Node, AnimationNode):
     bl_idname = "mn_AppendToMeshData"
     bl_label = "Append Mesh Data"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Mesh Data" : "meshDataA",
+                   "Other" : "meshDataB" }
+
+    outputNames = { "Joined Mesh Data" : "joinedMeshData" }            
+
+    def create(self):
         self.inputs.new("mn_MeshDataSocket", "Mesh Data")
         self.inputs.new("mn_MeshDataSocket", "Other")
         self.outputs.new("mn_MeshDataSocket", "Joined Mesh Data")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Mesh Data" : "meshDataA",
-                "Other" : "meshDataB"}
-    def getOutputSocketNames(self):
-        return {"Joined Mesh Data" : "meshData"}
 
     def execute(self, meshDataA, meshDataB):
         offset = len(meshDataA.vertices)
