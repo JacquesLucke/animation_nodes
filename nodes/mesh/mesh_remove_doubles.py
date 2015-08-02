f4dc182bba6ea698fc988e7bcb3318397837de24
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:35:02 +0200

    cleanup mesh remove doubles

--- nodes/mesh/mesh_remove_doubles.py
+++ nodes/mesh/mesh_remove_doubles.py
@@ -1,25 +1,21 @@
 import bpy, bmesh
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_MeshRemoveDoubles(bpy.types.Node, AnimationNode):
+class MeshRemoveDoubles(bpy.types.Node, AnimationNode):
     bl_idname = "mn_MeshRemoveDoubles"
     bl_label = "Mesh Remove Doubles"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Mesh" : "bm",
+                   "Distance" : "distance" }
+
+    outputNames = { "Mesh" : "mesh" }
+
+    def create(self):
         self.inputs.new("mn_MeshSocket", "Mesh")
         socket = self.inputs.new("mn_FloatSocket", "Distance")
         socket.value = 0.0001
         socket.setMinMax(0.0, 10000.0)
         self.outputs.new("mn_MeshSocket", "Mesh")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Mesh" : "bm",
-                "Distance" : "distance"}
-    def getOutputSocketNames(self):
-        return {"Mesh" : "mesh"}
 
     def execute(self, bm, distance):
         bmesh.ops.remove_doubles(bm, verts = bm.verts, dist = distance)
