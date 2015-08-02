d9713f334f405b40e484ae6079578d8597e3cd04
Jacques Lucke <mail@jlucke.com>
2015-08-02 19:51:37 +0200

    cleanup set keyframes node

--- nodes/animation/set_keyframes.py
+++ nodes/animation/set_keyframes.py
@@ -13,32 +13,30 @@ class mn_KeyframePath(bpy.types.PropertyGroup):
 class SetKeyframesNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SetKeyframesNode"
     bl_label = "Set Keyframes"
-    
+
     inputNames = { "Enable" : "enable",
                    "Set Keyframe" : "setKeyframe",
                    "Remove Unwanted" : "removeUnwanted",
                    "Object" : "object" }
-    outputNames = {}  
-        
+    outputNames = {}
+
     paths = bpy.props.CollectionProperty(type = mn_KeyframePath)
-    
+
     selectedPathType = bpy.props.EnumProperty(default = "Location", items = pathTypeItems, name = "Path Type")
     attributePath = bpy.props.StringProperty(default = "", name = "Attribute Path")
-    
+
     def create(self):
         self.width = 200
         self.inputs.new("mn_BooleanSocket", "Enable").value = False
         self.inputs.new("mn_BooleanSocket", "Set Keyframe")
         self.inputs.new("mn_BooleanSocket", "Remove Unwanted")
         self.inputs.new("mn_ObjectSocket", "Object")
-        
+
     def draw_buttons(self, context, layout):
         row = layout.row(align = True)
         row.prop(self, "selectedPathType", text = "")
-        new = row.operator("mn.new_property_to_list_node", text = "", icon = "PLUS")
-        new.nodeTreeName = self.id_data.name
-        new.nodeName = self.name
-        
+        self.callFunctionFromUI(row, "addKeyframePath", text = "", icon = "PLUS")
+
         col = layout.column(align = True)
         for i, item in enumerate(self.paths):
             row = col.row(align = True)
@@ -49,7 +47,7 @@ class SetKeyframesNode(bpy.types.Node, AnimationNode):
             remove.nodeTreeName = self.id_data.name
             remove.nodeName = self.name
             remove.index = i
-        
+
     def execute(self, enable, setKeyframe, removeUnwanted, object):
         if not enable: return
         frame = getCurrentFrame()
@@ -65,20 +63,20 @@ class SetKeyframesNode(bpy.types.Node, AnimationNode):
                     obj, path = self.getResolvedNestedPath(object, item.path)
                     obj.keyframe_delete(data_path = path, frame = frame, index = item.index)
                 except: pass
-                
+
     def getResolvedNestedPath(self, object, path):
         index = path.find(".")
         if index == -1: return object, path
         else:
             data = eval("object." + path[:index])
             return data, path[index+1:]
-            
+
     def newPath(self, path, index = -1):
         item = self.paths.add()
         item.path = path
         item.index = index
-        
-    def addItemToList(self):
+
+    def addKeyframePath(self):
         type = self.selectedPathType
         if type == "Custom": self.newPath("")
         elif type == "Location": self.newPath("location")
@@ -88,8 +86,6 @@ class SetKeyframesNode(bpy.types.Node, AnimationNode):
             self.newPath("location")
             self.newPath("rotation_euler")
             self.newPath("scale")
-    
+
     def removeItemFromList(self, index):
         self.paths.remove(index)
-        
-        
