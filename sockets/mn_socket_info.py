75380e83b787c7002e55f601cb52b3e091849613
Jacques Lucke <mail@jlucke.com>
2015-08-02 20:47:18 +0200

    remove whitespaces

--- sockets/mn_socket_info.py
+++ sockets/mn_socket_info.py
@@ -13,14 +13,14 @@ listChains = [
     ["mn_ParticleSocket", "mn_ParticleListSocket"],
     ["mn_ParticleSystemSocket", "mn_ParticleSystemListSocket"],
     ["mn_SplineSocket", "mn_SplineListSocket"] ]
-    
+
 def getListBaseSocketIdName(idName):
     for listChain in listChains:
         if idName in listChain:
             index = listChain.index(idName)
             if index == 0: return None
             else: return listChain[index - 1]
-    
+
 def getListSocketIdName(idName):
     for listChain in listChains:
         if idName in listChain:
@@ -28,33 +28,33 @@ def getListSocketIdName(idName):
             if index == len(listChain) - 1: return None
             return listChain[index + 1]
     return None
-    
+
 def isListSocketIdName(socketType):
     return not getListBaseSocketIdName(socketType) == None
-    
+
 def hasListSocket(socketType):
     return not getListSocketIdName(socketType) == None
-    
+
 def getIdNameFromDataType(dataType):
     for subClass in getSocketClasses():
         if getattr(subClass, "dataType") == dataType: return subClass.bl_idname
     return None
-    
+
 def getDataTypeFromIdName(idName):
     cls = getattr(bpy.types, idName)
     return cls.dataType
-    
+
 def getSocketClassFromIdName(idName):
     for cls in getSocketClasses():
         if cls.bl_idname == idName: return cls
     return None
-    
+
 def getListBaseSocketIdNames():
     idNames = []
     for listChain in listChains:
         idNames.extend(listChain[:-1])
     return idNames
-    
+
 def getListSocketIdNames():
     types = []
     for listChain in listChains:
@@ -67,10 +67,10 @@ def getSocketDataTypeItems(self, context):
     socketNames = getSocketDataTypes()
     socketNames.sort()
     return [(name, name, "") for name in socketNames]
-        
+
 def getSocketDataTypes():
     return [socketClass.dataType for socketClass in getSocketClasses()]
-    
+
 def getSocketClasses():
     from .. import mn_node_base
     return mn_node_base.mn_BaseSocket.__subclasses__()
