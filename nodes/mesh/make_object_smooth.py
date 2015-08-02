422b1e62cfbeeee91ac7589ac111723f1b8e64eb
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:31:42 +0200

    cleanup make object smooth node

--- nodes/mesh/make_object_smooth.py
+++ nodes/mesh/make_object_smooth.py
@@ -1,23 +1,19 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import allowCompiling, forbidCompiling
 
-class mn_MakeObjectSmooth(bpy.types.Node, AnimationNode):
+class MakeObjectSmooth(bpy.types.Node, AnimationNode):
     bl_idname = "mn_MakeObjectSmooth"
     bl_label = "Make Object Smooth"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Object" : "object",
+                   "Smooth" : "smooth" }
+
+    outputNames = { "Object" : "object" }
+
+    def create(self):
         self.inputs.new("mn_ObjectSocket", "Object").showName = False
         self.inputs.new("mn_BooleanSocket", "Smooth")
         self.outputs.new("mn_ObjectSocket", "Object")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Object" : "object",
-                "Smooth" : "smooth"}
-    def getOutputSocketNames(self):
-        return {"Object" : "object"}
 
     def execute(self, object, smooth):
         if getattr(object, "type", "") == "MESH":
