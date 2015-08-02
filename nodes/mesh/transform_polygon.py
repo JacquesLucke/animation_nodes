08196c2b7562646c6300cdbb7090014faaf16a41
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:52:36 +0200

    cleanup transform polygon node

--- nodes/mesh/transform_polygon.py
+++ nodes/mesh/transform_polygon.py
@@ -1,25 +1,21 @@
 import bpy
-from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.mesh import *
 from mathutils import Matrix
+from ... base_types.node import AnimationNode
+from ... data_structures.mesh import *
 
-class mn_TransformPolygon(bpy.types.Node, AnimationNode):
+class TransformPolygon(bpy.types.Node, AnimationNode):
     bl_idname = "mn_TransformPolygon"
     bl_label = "Transform Polygon"
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Polygon" : "polygon",
+                   "Matrix" : "matrix" }
+
+    outputNames = { "Polygon" : "polygon" }
+
+    def create(self):
         self.inputs.new("mn_PolygonSocket", "Polygon")
         self.inputs.new("mn_MatrixSocket", "Matrix")
         self.outputs.new("mn_PolygonSocket", "Polygon")
-        allowCompiling()
-
-    def getInputSocketNames(self):
-        return {"Polygon" : "polygon",
-                "Matrix" : "matrix"}
-    def getOutputSocketNames(self):
-        return {"Polygon" : "polygon"}
 
     def execute(self, polygon, matrix):
         offsetMatrix = Matrix.Translation(polygon.center)
