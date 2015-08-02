f9bd4c2d07f7bb6a757e843090ec17f8b880ca8b
Jacques Lucke <mail@jlucke.com>
2015-08-02 23:02:25 +0200

    cleanup create edge indices node

--- nodes/mesh/create_edge_indices.py
+++ nodes/mesh/create_edge_indices.py
@@ -1,31 +1,20 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodeTreeChanged, allowCompiling, forbidCompiling
-import math
 
-class mn_CreateEdgeIndices(bpy.types.Node, AnimationNode):
+class CreateEdgeIndices(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CreateEdgeIndices"
     bl_label = "Create Edge Indices"
-    node_category = "Mesh"
     isDetermined = True
 
-    def init(self, context):
-        forbidCompiling()
+    inputNames = { "Index 1" : "index1",
+                   "Index 2" : "index2" }
+
+    outputNames = { "Edge Indices" : "edgeIndices" }
+
+    def create(self):
         self.inputs.new("mn_IntegerSocket", "Index 1").value = 0
         self.inputs.new("mn_IntegerSocket", "Index 2").value = 1
         self.outputs.new("mn_EdgeIndicesSocket", "Edge Indices")
-        allowCompiling()
 
-    def draw_buttons(self, context, layout):
-        pass
-
-    def getInputSocketNames(self):
-        return {"Index 1" : "index1",
-                "Index 2" : "index2"}
-    def getOutputSocketNames(self):
-        return {"Edge Indices" : "edgeIndices"}
-
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self, outputUse):
         return "$edgeIndices$ = (%index1%, %index2%)"
