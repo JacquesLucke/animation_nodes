11c5766d1aa771702e6442efd65fc795e6be2e30
Jacques Lucke <mail@jlucke.com>
2015-08-03 21:53:15 +0200

    cleanup color socket

--- sockets/color.py
+++ sockets/color.py
@@ -1,27 +1,30 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
+from .. events import propertyChanged
 from .. base_types.socket import AnimationNodeSocket
 
-class mn_ColorSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class ColorSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_ColorSocket"
     bl_label = "Color Socket"
     dataType = "Color"
     allowedInputTypes = ["Color"]
     drawColor = (0.8, 0.8, 0.2, 1)
-    
-    value = bpy.props.FloatVectorProperty(default = [0.5, 0.5, 0.5], subtype = "COLOR", soft_min = 0.0, soft_max = 1.0, update = nodePropertyChanged)
-    
+
+    value = bpy.props.FloatVectorProperty(
+        default = [0.5, 0.5, 0.5], subtype = "COLOR",
+        soft_min = 0.0, soft_max = 1.0,
+        update = propertyChanged)
+
     def drawInput(self, layout, node, text):
         layout.prop(self, "value", text = text)
-        
+
     def getValue(self):
-        value = self.value
-        return [value[0], value[1], value[2], 1.0]
-        
+        return list(self.value) + [1.0]
+
     def setStoreableValue(self, data):
         self.value = data[:3]
+
     def getStoreableValue(self):
         return self.value
-        
+
     def getCopyValueFunctionString(self):
         return "return value[:]"
