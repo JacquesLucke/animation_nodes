7a82d28472589e35e75e71bf90e747576bd83a61
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:40:18 +0200

    cleanup direction to rotation node

--- nodes/vector/direction_to_rotation.py
+++ nodes/vector/direction_to_rotation.py
@@ -1,39 +1,33 @@
 import bpy
 from mathutils import Vector
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
+from ... events import propertyChanged
 
 trackAxisItems = [(axis, axis, "") for axis in ("X", "Y", "Z", "-X", "-Y", "-Z")]
 upAxisItems = [(axis, axis, "") for axis in ("X", "Y", "Z")]
 
-class mn_DirectionToRotation(bpy.types.Node, AnimationNode):
+class DirectionToRotation(bpy.types.Node, AnimationNode):
     bl_idname = "mn_DirectionToRotation"
     bl_label = "Direction to Rotation"
-    
-    trackAxis = bpy.props.EnumProperty(items = trackAxisItems, update = nodeTreeChanged, default = "Z")
-    upAxis = bpy.props.EnumProperty(items = upAxisItems, update = nodeTreeChanged, default = "X")
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Direction" : "direction" }
+    outputNames = { "Rotation" : "rotation" }
+
+    trackAxis = bpy.props.EnumProperty(items = trackAxisItems, update = propertyChanged, default = "Z")
+    upAxis = bpy.props.EnumProperty(items = upAxisItems, update = propertyChanged, default = "X")
+
+    def create(self):
         self.inputs.new("mn_VectorSocket", "Direction")
         self.outputs.new("mn_VectorSocket", "Rotation")
         self.width += 20
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "trackAxis", expand = True)
         layout.prop(self, "upAxis", expand = True)
-        
+
         if self.trackAxis == self.upAxis:
             layout.label("Must be different", icon = "ERROR")
-        
-    def getInputSocketNames(self):
-        return {"Direction" : "direction"}
-    def getOutputSocketNames(self):
-        return {"Rotation" : "rotation"}
 
     def execute(self, direction):
         if self.trackAxis == self.upAxis: return Vector((0, 0, 0))
         return Vector((direction.to_track_quat(self.trackAxis, self.upAxis).to_euler()))
-        
-
