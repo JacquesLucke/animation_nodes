8a7ae4d69f31e2c14580d6ff728227f9a56f9ee5
Jacques Lucke <mail@jlucke.com>
2015-08-03 10:14:21 +0200

    cleanup float range list node

--- nodes/number/float_range_list.py
+++ nodes/number/float_range_list.py
@@ -1,28 +1,22 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_FloatRangeListNode(bpy.types.Node, AnimationNode):
+class FloatRangeListNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_FloatRangeListNode"
     bl_label = "Number Range"
-    node_category = "Math"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Amount" : "amount",
+                   "Start" : "start",
+                   "Step" : "step" }
+
+    outputNames = { "List" : "list" }
+
+    def create(self):
         self.inputs.new("mn_IntegerSocket", "Amount").value = 5
         self.inputs.new("mn_FloatSocket", "Start")
         self.inputs.new("mn_FloatSocket", "Step").value = 1
         self.outputs.new("mn_FloatListSocket", "List")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Amount" : "amount", "Start" : "start", "Step" : "step"}
-    def getOutputSocketNames(self):
-        return {"List" : "list"}
-        
+
     def execute(self, amount, start, step):
-        list = []
-        for i in range(amount):
-            list.append(start + i * step)
-        return list
+        return [start + i * step for i in range(amount)]
