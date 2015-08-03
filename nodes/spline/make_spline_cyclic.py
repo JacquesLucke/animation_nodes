77135fb3ae5562171bae56dd160db1fc5b172001
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:20:34 +0200

    cleanup make spline cyclic node

--- nodes/spline/make_spline_cyclic.py
+++ nodes/spline/make_spline_cyclic.py
@@ -1,24 +1,19 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_MakeSplineCyclic(bpy.types.Node, AnimationNode):
+class MakeSplineCyclic(bpy.types.Node, AnimationNode):
     bl_idname = "mn_MakeSplineCyclic"
     bl_label = "Make Spline Cyclic"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Spline" : "spline",
+                   "Cyclic" : "cyclic" }
+
+    outputNames = { "Spline" : "spline" }
+
+    def create(self):
         self.inputs.new("mn_SplineSocket", "Spline").showName = False
         self.inputs.new("mn_BooleanSocket", "Cyclic").value = True
         self.outputs.new("mn_SplineSocket", "Spline")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Spline" : "spline",
-                "Cyclic" : "cyclic"}
-
-    def getOutputSocketNames(self):
-        return {"Spline" : "spline"}
 
     def execute(self, spline, cyclic):
         spline.isCyclic = cyclic
