d96b81e8587695de9c1548a2ef4940e4e006e7ae
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:51:46 +0200

    cleanup vector from value nod

--- nodes/vector/vector_from_value.py
+++ nodes/vector/vector_from_value.py
@@ -1,26 +1,19 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
-class mn_VectorFromValue(bpy.types.Node, AnimationNode):
+class VectorFromValue(bpy.types.Node, AnimationNode):
     bl_idname = "mn_VectorFromValue"
     bl_label = "Vector from Value"
-    node_category = "Math"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Value" : "value" }
+    outputNames = { "Vector" : "vector" }
+
+    def create(self):
         self.inputs.new("mn_FloatSocket", "Value")
         self.outputs.new("mn_VectorSocket", "Vector")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Value" : "value"}
-    def getOutputSocketNames(self):
-        return {"Vector" : "vector"}
 
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$vector$ = mathutils.Vector((%value%, %value%, %value%))"
+
     def getModuleList(self):
         return ["mathutils"]
