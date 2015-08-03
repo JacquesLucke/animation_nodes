a7eebb3b700b6f8d246cc3e884ebbb765337597f
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:11:57 +0200

    cleanup get spline samples node

--- nodes/spline/get_spline_samples.py
+++ nodes/spline/get_spline_samples.py
@@ -1,18 +1,21 @@
 import bpy
-from bpy.props import *
-from . spline_evaluation_base import SplineEvaluationBase
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.splines.bezier_spline import BezierSpline
-from ... data_structures.splines.poly_spline import PolySpline
+from . spline_evaluation_base import SplineEvaluationBase
 
-class mn_GetSplineSamples(bpy.types.Node, AnimationNode, SplineEvaluationBase):
+class GetSplineSamples(bpy.types.Node, AnimationNode, SplineEvaluationBase):
     bl_idname = "mn_GetSplineSamples"
     bl_label = "Get Spline Samples"
-    outputUseParameterName = "useOutput"
-    
-    def init(self, context):
-        forbidCompiling()
+    outputUseParameterName = "usedOutputs"
+
+    inputNames = { "Spline" : "spline",
+                   "Amount" : "amount",
+                   "Start" : "start",
+                   "End" : "end" }
+
+    outputNames = { "Positions" : "positions",
+                    "Tangents" : "tangents" }
+
+    def create(self):
         self.inputs.new("mn_SplineSocket", "Spline")
         self.inputs.new("mn_IntegerSocket", "Amount").value = 50
         socket = self.inputs.new("mn_FloatSocket", "Start")
@@ -23,35 +26,24 @@ class mn_GetSplineSamples(bpy.types.Node, AnimationNode, SplineEvaluationBase):
         socket.setMinMax(0.0, 1.0)
         self.outputs.new("mn_VectorListSocket", "Positions")
         self.outputs.new("mn_VectorListSocket", "Tangents")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "parameterType", text = "")
-        
+
     def draw_buttons_ext(self, context, layout):
         col = layout.column()
         col.active = self.parameterType == "UNIFORM"
         col.prop(self, "resolution")
-        
-    def getInputSocketNames(self):
-        return {"Spline" : "spline",
-                "Amount" : "amount",
-                "Start" : "start",
-                "End" : "end"}
 
-    def getOutputSocketNames(self):
-        return {"Positions" : "positions",
-                "Tangents" : "tangents"}
-
-    def execute(self, useOutput, spline, amount, start, end):
+    def execute(self, usedOutputs, spline, amount, start, end):
         spline.update()
         positions = []
         tangents = []
         if spline.isEvaluable:
-            if useOutput["Positions"]:
+            if usedOutputs["Positions"]:
                 if self.parameterType == "UNIFORM": positions = spline.getUniformSamples(amount, start = start, end = end, resolution = self.resolution)
                 else: positions = spline.getSamples(amount, start = start, end = end)
-            if useOutput["Tangents"]:
+            if usedOutputs["Tangents"]:
                 if self.parameterType == "UNIFORM": tangents = spline.getUniformTangentSamples(amount, start = start, end = end, resolution = self.resolution)
                 else: tangents = spline.getTangentSamples(amount, start = start, end = end)
         return positions, tangents
