0cd803a2aaea10f91f45659aefd655d4bd7da9c4
Jacques Lucke <mail@jlucke.com>
2015-08-03 11:02:45 +0200

    cleanup copy object data node

--- nodes/object/copy_object_data.py
+++ nodes/object/copy_object_data.py
@@ -1,28 +1,23 @@
-import bpy, random
-from ... mn_cache import getUniformRandom
+import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_CopyObjectData(bpy.types.Node, AnimationNode):
+class CopyObjectData(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CopyObjectData"
     bl_label = "Copy Object Data"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "From" : "fromObject",
+                   "To" : "toObject" }
+
+    outputNames = { "To" : "toObject" }
+
+    def create(self):
         self.inputs.new("mn_ObjectSocket", "From")
         self.inputs.new("mn_ObjectSocket", "To")
         self.outputs.new("mn_ObjectSocket", "To")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"From" : "fromObject", "To" : "toObject"}
-    def getOutputSocketNames(self):
-        return {"To" : "toObject"}
-        
+
     def execute(self, fromObject, toObject):
         if fromObject is not None and toObject is not None:
             if toObject.data != fromObject.data:
                 if toObject.type == fromObject.type:
                     toObject.data = fromObject.data
         return toObject
-        
