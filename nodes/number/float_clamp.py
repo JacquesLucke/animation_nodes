bdd3c37c25d634fe84bff5779a55139790feb220
Jacques Lucke <mail@jlucke.com>
2015-08-03 09:48:09 +0200

    cleanup clamp node

--- nodes/number/float_clamp.py
+++ nodes/number/float_clamp.py
@@ -1,31 +1,22 @@
 import bpy, random
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-
-class mn_FloatClamp(bpy.types.Node, AnimationNode):
+class FloatClamp(bpy.types.Node, AnimationNode):
     bl_idname = "mn_FloatClamp"
     bl_label = "Clamp"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = {  "Value" : "value",
+                    "Min" : "minValue",
+                    "Max" : "maxValue" }
+
+    outputNames = { "Value" : "value" }
+
+    def create(self):
         self.inputs.new("mn_FloatSocket", "Value")
         self.inputs.new("mn_FloatSocket", "Min").value = 0.0
         self.inputs.new("mn_FloatSocket", "Max").value = 1.0
         self.outputs.new("mn_FloatSocket", "Value")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Value" : "value",
-                "Min" : "minValue",
-                "Max" : "maxValue"}
-    def getOutputSocketNames(self):
-        return {"Value" : "value"}
 
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$value$ = min(max(%value%, %minValue%), %maxValue%)"
-        
-
