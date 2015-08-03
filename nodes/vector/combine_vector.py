849c66994c8a3319f55828ed83878f1046049580
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:36:35 +0200

    cleanup combine vector node

--- nodes/vector/combine_vector.py
+++ nodes/vector/combine_vector.py
@@ -1,30 +1,25 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
 
-class mn_CombineVector(bpy.types.Node, AnimationNode):
+class CombineVector(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CombineVector"
     bl_label = "Combine Vector"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "X" : "x",
+                   "Y" : "y",
+                   "Z" : "z" }
+
+    outputNames = { "Vector" : "vector" }
+
+    def create(self):
         self.inputs.new("mn_FloatSocket", "X")
         self.inputs.new("mn_FloatSocket", "Y")
         self.inputs.new("mn_FloatSocket", "Z")
         self.outputs.new("mn_VectorSocket", "Vector")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"X" : "x",
-                "Y" : "y",
-                "Z" : "z"}
-    def getOutputSocketNames(self):
-        return {"Vector" : "vector"}
 
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+    def getExecutionCode(self):
         return "$vector$ = mathutils.Vector((%x%, %y%, %z%))"
+        
     def getModuleList(self):
         return ["mathutils"]
