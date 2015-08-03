13b9f59cbece3894d48d9ce1c642d49df256e6fc
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:02:53 +0200

    cleanup integer socket

--- sockets/integer.py
+++ sockets/integer.py
@@ -1,5 +1,5 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
+from .. events import propertyChanged
 from .. base_types.socket import AnimationNodeSocket
 
 def getValue(self):
@@ -7,33 +7,35 @@ def getValue(self):
 def setValue(self, value):
     self["value"] = min(max(self.min, value), self.max)
 
-class mn_IntegerSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class IntegerSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_IntegerSocket"
     bl_label = "Integer Socket"
     dataType = "Integer"
     allowedInputTypes = ["Integer"]
     drawColor = (0.2, 0.2, 1.0, 1.0)
-    
-    value = bpy.props.IntProperty(default = 0, update = nodePropertyChanged, set = setValue, get = getValue)
+
+    value = bpy.props.IntProperty(default = 0,
+        set = setValue, get = getValue,
+        update = propertyChanged)
+
     showName = bpy.props.BoolProperty(default = True)
-    
+
     min = bpy.props.IntProperty(default = -10000000)
     max = bpy.props.IntProperty(default = 10000000)
-    
+
     def drawInput(self, layout, node, text):
         if not self.showName: text = ""
         layout.prop(self, "value", text = text)
-    
+
     def getValue(self):
         return self.value
-        
+
     def setStoreableValue(self, data):
         self.value = data
+        
     def getStoreableValue(self):
         return self.value
-        
+
     def setMinMax(self, min, max):
         self.min = min
         self.max = max
-        
-
