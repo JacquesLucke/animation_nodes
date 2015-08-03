403e955ff338c6fae32df6395f25a568fd97d7fb
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:39:18 +0200

    cleanup spline evaluation base

--- nodes/spline/spline_evaluation_base.py
+++ nodes/spline/spline_evaluation_base.py
@@ -1,10 +1,22 @@
 from bpy.props import *
 from ... mn_execution import nodePropertyChanged
+from ... events import propertyChanged
 
 parameterTypeItems = [
     ("RESOLUTION", "Resolution", ""),
     ("UNIFORM", "Uniform", "")]
 
 class SplineEvaluationBase:
-    parameterType = EnumProperty(name = "Parameter Type", default = "UNIFORM", items = parameterTypeItems, update = nodePropertyChanged)
-    resolution = IntProperty(name = "Resolution", default = 100, min = 2, update = nodePropertyChanged, description = "Increase to have a more accurate evaluation if the type is set to Length")
+
+    parameterType = EnumProperty(
+        name = "Parameter Type",
+        default = "UNIFORM",
+        items = parameterTypeItems,
+        update = propertyChanged)
+
+    resolution = IntProperty(
+        name = "Resolution",
+        min = 2, 
+        default = 100,
+        update = propertyChanged,
+        description = "Increase to have a more accurate evaluation if the type is set to Length")
