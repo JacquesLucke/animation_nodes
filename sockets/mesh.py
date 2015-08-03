8a8a8748d6a59ccf9c4d5dd2bd4807c5e2c2bcba
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:16:22 +0200

    cleanup mesh socket

--- sockets/mesh.py
+++ sockets/mesh.py
@@ -1,24 +1,25 @@
-import bpy, bmesh
-from .. mn_execution import nodePropertyChanged
+import bpy
+import bmesh
 from .. base_types.socket import AnimationNodeSocket
 
-class mn_MeshSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class MeshSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_MeshSocket"
     bl_label = "Mesh Socket"
     dataType = "Mesh"
     allowedInputTypes = ["Mesh"]
     drawColor = (0.1, 1.0, 0.1, 1)
-    
+
     def drawInput(self, layout, node, text):
         layout.label(text)
-        
+
     def getValue(self):
         return bmesh.new()
-        
+
     def setStoreableValue(self, data):
         pass
+
     def getStoreableValue(self):
         pass
-        
+
     def getCopyValueFunctionString(self):
         return "return value.copy()"
