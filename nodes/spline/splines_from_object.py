448713b5f1b66fbac11d4139ad993cf50f7efb0d
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:45:52 +0200

    cleanup splines from object node

--- nodes/spline/splines_from_object.py
+++ nodes/spline/splines_from_object.py
@@ -1,28 +1,27 @@
 import bpy
+from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 from ... data_structures.splines.from_blender import createSplinesFromBlenderObject
 
-class mn_SplinesFromObject(bpy.types.Node, AnimationNode):
+class SplinesFromObject(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SplinesFromObject"
     bl_label = "Splines from Object"
-    
-    useWorldTransform = bpy.props.BoolProperty(name = "Use World Transform", default = True, description = "Use the position in global space")
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Object" : "object" }
+    outputNames = { "Splines" : "splines" }
+
+    useWorldTransform = BoolProperty(
+        name = "Use World Transform",
+        description = "Use the position in global space",
+        default = True, update = propertyChanged)
+
+    def create(self):
         self.inputs.new("mn_ObjectSocket", "Object").showName = False
         self.outputs.new("mn_SplineListSocket", "Splines")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "useWorldTransform")
-        
-    def getInputSocketNames(self):
-        return {"Object" : "object"}
-
-    def getOutputSocketNames(self):
-        return {"Splines" : "splines"}
 
     def execute(self, object):
         splines = createSplinesFromBlenderObject(object)
