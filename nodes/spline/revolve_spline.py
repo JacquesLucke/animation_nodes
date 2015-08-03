7fac44a2b88cf14dde180030e4efc9d9f5635bfa
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:29:26 +0200

    cleanup revolve spline node

--- nodes/spline/revolve_spline.py
+++ nodes/spline/revolve_spline.py
@@ -1,21 +1,28 @@
 import bpy
 from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 from ... algorithms.mesh_generation.from_splines import revolveProfileAroundAxis
 
 projectionTypeItems = [
     ("PARAMETER", "Same Parameter", ""),
     ("PROJECT", "Project", "") ]
 
-class mn_RevolveSpline(bpy.types.Node, AnimationNode):
+class RevolveSpline(bpy.types.Node, AnimationNode):
     bl_idname = "mn_RevolveSpline"
     bl_label = "Revolve Spline"
-        
-    projectionType = EnumProperty(name = "Projection Type", default = "PROJECT", items = projectionTypeItems, update = nodePropertyChanged)
-        
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Axis" : "axis",
+                   "Profile" : "profile",
+                   "Spline Samples" : "splineSamples",
+                   "Surface Samples" : "surfaceSamples" }
+
+    outputNames = { "Vertices" : "vertices",
+                    "Polygons" : "polygons" }
+
+    projectionType = EnumProperty(name = "Projection Type", default = "PROJECT", items = projectionTypeItems, update = propertyChanged)
+
+    def create(self):
         self.inputs.new("mn_SplineSocket", "Axis")
         self.inputs.new("mn_SplineSocket", "Profile")
         socket = self.inputs.new("mn_IntegerSocket", "Spline Samples")
@@ -27,20 +34,9 @@ class mn_RevolveSpline(bpy.types.Node, AnimationNode):
         self.outputs.new("mn_VectorListSocket", "Vertices")
         self.outputs.new("mn_PolygonIndicesListSocket", "Polygons")
         self.width += 20
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "projectionType", text = "")
-
-    def getInputSocketNames(self):
-        return {"Axis" : "axis",
-                "Profile" : "profile",
-                "Spline Samples" : "splineSamples",
-                "Surface Samples" : "surfaceSamples"}
-
-    def getOutputSocketNames(self):
-        return {"Vertices" : "vertices",
-                "Polygons" : "polygons"}
 
     def execute(self, axis, profile, splineSamples, surfaceSamples):
         def canExecute():
@@ -49,10 +45,10 @@ class mn_RevolveSpline(bpy.types.Node, AnimationNode):
             if splineSamples < 2: return False
             if surfaceSamples < 3: return False
             return True
-            
+
         axis.update()
         profile.update()
-        
+
         if canExecute():
             vertices, polygons = revolveProfileAroundAxis(axis, profile, splineSamples, surfaceSamples, self.projectionType)
             return vertices, polygons
