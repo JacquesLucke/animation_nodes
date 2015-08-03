29ce54c90101887bcb1054f3ef388b6be99f9b78
Jacques Lucke <mail@jlucke.com>
2015-08-03 12:44:43 +0200

    cleanup particle info node

--- nodes/particles/particle_info.py
+++ nodes/particles/particle_info.py
@@ -1,16 +1,30 @@
-import bpy, random, mathutils
-from bpy.props import *
-from mathutils import Vector
+import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 
-class mn_ParticleInfo(bpy.types.Node, AnimationNode):
+class ParticleInfo(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ParticleInfo"
     bl_label = "Particle Info"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Particle" : "particle" }
+    outputNames = { "Location" : "location",
+                    "Rotation" : "rotation",
+                    "Age" : "age",
+                    "Velocity" : "velocity",
+                    "Angular Velocity" : "angularVelocity",
+                    "Size" : "size",
+                    "Alive State" : "aliveState",
+                    "Is Exist" : "isExist",
+                    "Is Visible" : "isVisible",
+                    "Lifetime" : "lifetime",
+                    "Birth Time" : "birthTime",
+                    "Die Time" : "dieTime",
+                    "Previous Location" : "previousLocation",
+                    "Previous Rotation" : "previousRotation",
+                    "Previous Velocity" : "previousVelocity",
+                    "Previous Angular Velocity" : "previousAngularVelocity" }
+
+    def create(self):
         self.inputs.new("mn_ParticleSocket", "Particle")
         self.outputs.new("mn_VectorSocket", "Location")
         self.outputs.new("mn_VectorSocket", "Rotation")
@@ -28,73 +42,49 @@ class mn_ParticleInfo(bpy.types.Node, AnimationNode):
         self.outputs.new("mn_VectorSocket", "Previous Rotation").hide = True
         self.outputs.new("mn_VectorSocket", "Previous Velocity").hide = True
         self.outputs.new("mn_VectorSocket", "Previous Angular Velocity").hide = True
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Particle" : "particle"}
-    def getOutputSocketNames(self):
-        return {"Location" : "location",
-                "Rotation" : "rotation",
-                "Age" : "age",
-                "Velocity" : "velocity",
-                "Angular Velocity" : "angularVelocity",
-                "Size" : "size",
-                "Alive State" : "aliveState",
-                "Is Exist" : "isExist",
-                "Is Visible" : "isVisible",
-                "Lifetime" : "lifetime",
-                "Birth Time" : "birthTime",
-                "Die Time" : "dieTime",
-                "Previous Location" : "previousLocation",
-                "Previous Rotation" : "previousRotation",
-                "Previous Velocity" : "previousVelocity",
-                "Previous Angular Velocity" : "previousAngularVelocity"}
-        
-    def useInLineExecution(self):
-        return True
-        
-    def getInLineExecutionString(self, outputUse):
+
+    def getExecutionCode(self, usedOutputs):
         codeLines = []
-        
+
         codeLines.append("if %particle%:")
-        if outputUse["Location"]: codeLines.append("    $location$ = %particle%.location")
-        if outputUse["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector(%particle%.rotation.to_euler())")
-        if outputUse["Velocity"]: codeLines.append("    $velocity$ = %particle%.velocity")
-        if outputUse["Angular Velocity"]: codeLines.append("    $angularVelocity$ = %particle%.angular_velocity")
-        if outputUse["Size"]: codeLines.append("    $size$ = %particle%.size")
-        if outputUse["Alive State"]: codeLines.append("    $aliveState$ = %particle%.alive_state")
-        if outputUse["Is Exist"]: codeLines.append("    $isExist$ = %particle%.is_exist")
-        if outputUse["Is Visible"]: codeLines.append("    $isVisible$ = %particle%.is_visible")
-        if outputUse["Lifetime"]: codeLines.append("    $lifetime$ = %particle%.lifetime")
-        if outputUse["Birth Time"]: codeLines.append("    $birthTime$ = %particle%.birth_time")
-        if outputUse["Die Time"]: codeLines.append("    $dieTime$ = %particle%.die_time")
-        if outputUse["Age"]: codeLines.append("    $age$ = max(0, scene.frame_current - %particle%.birth_time)")
-        if outputUse["Previous Location"]: codeLines.append("    $previousLocation$ = %particle%.prev_location")
-        if outputUse["Previous Rotation"]: codeLines.append("    $previousRotation$ = mathutils.Vector(%particle%.prev_rotation.to_euler())")
-        if outputUse["Previous Velocity"]: codeLines.append("    $previousVelocity$ = %particle%.prev_velocity")
-        if outputUse["Previous Angular Velocity"]: codeLines.append("    $previousAngularVelocity$ = %particle%.prev_angular_velocity")
+        if usedOutputs["Location"]: codeLines.append("    $location$ = %particle%.location")
+        if usedOutputs["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector(%particle%.rotation.to_euler())")
+        if usedOutputs["Velocity"]: codeLines.append("    $velocity$ = %particle%.velocity")
+        if usedOutputs["Angular Velocity"]: codeLines.append("    $angularVelocity$ = %particle%.angular_velocity")
+        if usedOutputs["Size"]: codeLines.append("    $size$ = %particle%.size")
+        if usedOutputs["Alive State"]: codeLines.append("    $aliveState$ = %particle%.alive_state")
+        if usedOutputs["Is Exist"]: codeLines.append("    $isExist$ = %particle%.is_exist")
+        if usedOutputs["Is Visible"]: codeLines.append("    $isVisible$ = %particle%.is_visible")
+        if usedOutputs["Lifetime"]: codeLines.append("    $lifetime$ = %particle%.lifetime")
+        if usedOutputs["Birth Time"]: codeLines.append("    $birthTime$ = %particle%.birth_time")
+        if usedOutputs["Die Time"]: codeLines.append("    $dieTime$ = %particle%.die_time")
+        if usedOutputs["Age"]: codeLines.append("    $age$ = max(0, scene.frame_current - %particle%.birth_time)")
+        if usedOutputs["Previous Location"]: codeLines.append("    $previousLocation$ = %particle%.prev_location")
+        if usedOutputs["Previous Rotation"]: codeLines.append("    $previousRotation$ = mathutils.Vector(%particle%.prev_rotation.to_euler())")
+        if usedOutputs["Previous Velocity"]: codeLines.append("    $previousVelocity$ = %particle%.prev_velocity")
+        if usedOutputs["Previous Angular Velocity"]: codeLines.append("    $previousAngularVelocity$ = %particle%.prev_angular_velocity")
         codeLines.append("    pass")
-        
+
         codeLines.append("else:")
-        if outputUse["Location"]: codeLines.append("    $location$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Velocity"]: codeLines.append("    $velocity$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Angular Velocity"]: codeLines.append("    $angularVelocity$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Size"]: codeLines.append("    $size$ = 1")
-        if outputUse["Alive State"]: codeLines.append("    $aliveState$ = 'DEAD'")
-        if outputUse["Is Exist"]: codeLines.append("    $isExist$ = False")
-        if outputUse["Is Visible"]: codeLines.append("    $isVisible$ = False")
-        if outputUse["Lifetime"]: codeLines.append("    $lifetime$ = 0")
-        if outputUse["Birth Time"]: codeLines.append("    $birthTime$ = 0")
-        if outputUse["Die Time"]: codeLines.append("    $dieTime$ = 0")
-        if outputUse["Age"]: codeLines.append("    $age$ = 0")
-        if outputUse["Previous Location"]: codeLines.append("    $previousLocation$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Previous Rotation"]: codeLines.append("    $previousRotation$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Previous Velocity"]: codeLines.append("    $previousVelocity$ = mathutils.Vector((0, 0, 0))")
-        if outputUse["Previous Angular Velocity"]: codeLines.append("    $previousAngularVelocity$ = mathutils.Vector((0, 0, 0))")
-        codeLines.append("    pass")        
-        
+        if usedOutputs["Location"]: codeLines.append("    $location$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Rotation"]: codeLines.append("    $rotation$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Velocity"]: codeLines.append("    $velocity$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Angular Velocity"]: codeLines.append("    $angularVelocity$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Size"]: codeLines.append("    $size$ = 1")
+        if usedOutputs["Alive State"]: codeLines.append("    $aliveState$ = 'DEAD'")
+        if usedOutputs["Is Exist"]: codeLines.append("    $isExist$ = False")
+        if usedOutputs["Is Visible"]: codeLines.append("    $isVisible$ = False")
+        if usedOutputs["Lifetime"]: codeLines.append("    $lifetime$ = 0")
+        if usedOutputs["Birth Time"]: codeLines.append("    $birthTime$ = 0")
+        if usedOutputs["Die Time"]: codeLines.append("    $dieTime$ = 0")
+        if usedOutputs["Age"]: codeLines.append("    $age$ = 0")
+        if usedOutputs["Previous Location"]: codeLines.append("    $previousLocation$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Previous Rotation"]: codeLines.append("    $previousRotation$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Previous Velocity"]: codeLines.append("    $previousVelocity$ = mathutils.Vector((0, 0, 0))")
+        if usedOutputs["Previous Angular Velocity"]: codeLines.append("    $previousAngularVelocity$ = mathutils.Vector((0, 0, 0))")
+        codeLines.append("    pass")
+
         return "\n".join(codeLines)
-        
+
     def getModuleList(self):
         return ["mathutils"]
