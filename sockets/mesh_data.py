6aaf282284455000b7a50fe9ed2f79717075f756
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:16:45 +0200

    cleanup mesh data socket

--- sockets/mesh_data.py
+++ sockets/mesh_data.py
@@ -1,23 +1,23 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
-from .. base_types.socket import AnimationNodeSocket
 from .. data_structures.mesh import MeshData
+from .. base_types.socket import AnimationNodeSocket
 
-class mn_MeshDataSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class MeshDataSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_MeshDataSocket"
     bl_label = "Mesh Data Socket"
     dataType = "Mesh Data"
     allowedInputTypes = ["Mesh Data"]
     drawColor = (0.3, 0.4, 0.18, 1)
-    
+
     def drawInput(self, layout, node, text):
         layout.label(text)
-        
+
     def getValue(self):
         return MeshData([], [], [])
-        
+
     def setStoreableValue(self, data):
         pass
+        
     def getStoreableValue(self):
         pass
 
