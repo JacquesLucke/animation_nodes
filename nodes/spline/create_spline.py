ae7bc3ab1490098ec8d08b5936eba674c9651b8f
Jacques Lucke <mail@jlucke.com>
2015-08-03 14:57:16 +0200

    cleanup create spline node

--- nodes/spline/create_spline.py
+++ nodes/spline/create_spline.py
@@ -1,36 +1,32 @@
 import bpy
 from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.splines.bezier_spline import BezierSpline
 from ... data_structures.splines.poly_spline import PolySpline
+from ... data_structures.splines.bezier_spline import BezierSpline
 
 splineTypeItems = [
     ("BEZIER", "Bezier", "Each control point has two handles"),
     ("POLY", "Poly", "Linear interpolation between the spline points")]
 
-class mn_CreateSpline(bpy.types.Node, AnimationNode):
+class CreateSpline(bpy.types.Node, AnimationNode):
     bl_idname = "mn_CreateSpline"
     bl_label = "Create Spline"
-    
-    splineType = EnumProperty(name = "Spline Type", items = splineTypeItems, update = nodePropertyChanged)
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Points" : "points",
+                   "Cyclic" : "cyclic" }
+
+    outputNames = { "Spline" : "spline" }
+
+    splineType = EnumProperty(name = "Spline Type", items = splineTypeItems, update = propertyChanged)
+
+    def create(self):
         self.inputs.new("mn_VectorListSocket", "Points")
         self.inputs.new("mn_BooleanSocket", "Cyclic").value = False
         self.outputs.new("mn_SplineSocket", "Spline")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "splineType", text = "")
-        
-    def getInputSocketNames(self):
-        return {"Points" : "points",
-                "Cyclic" : "cyclic"}
-
-    def getOutputSocketNames(self):
-        return {"Spline" : "spline"}
 
     def execute(self, points, cyclic):
         if self.splineType == "BEZIER": spline = BezierSpline()
