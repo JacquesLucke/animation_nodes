07942b7fb9c90501437642e32a19a19d3b3ae0cc
Jacques Lucke <mail@jlucke.com>
2015-08-03 14:51:15 +0200

    cleanup append point to spline node

--- nodes/spline/append_point_to_spline.py
+++ nodes/spline/append_point_to_spline.py
@@ -1,46 +1,40 @@
 import bpy
 from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from ... data_structures.splines.bezier_spline import BezierSpline
-from ... data_structures.splines.poly_spline import PolySpline
 
 pointTypeItems = [
     ("POINT", "Point", "Add a normal point to the spline"),
     ("BEZIER_POINT", "Bezier Point", "Add a point with handles")]
 
-class mn_AppendPointToSpline(bpy.types.Node, AnimationNode):
+class AppendPointToSpline(bpy.types.Node, AnimationNode):
     bl_idname = "mn_AppendPointToSpline"
     bl_label = "Append Point to Spline"
-    
+
+    inputNames = { "Spline" : "spline",
+                   "Point" : "point",
+                   "Left Handle" : "leftHandle",
+                   "Right Handle" : "rightHandle" }
+
+    outputNames = { "Spline" : "spline" }
+
     def settingChanged(self, context):
         self.inputs["Left Handle"].hide = self.pointType != "BEZIER_POINT"
         self.inputs["Right Handle"].hide = self.pointType != "BEZIER_POINT"
-        nodePropertyChanged(self, context)
-    
+        propertyChanged(self, context)
+
     pointType = EnumProperty(name = "Point Type", default = "POINT", items = pointTypeItems, update = settingChanged)
-    
-    def init(self, context):
-        forbidCompiling()
+
+    def create(self):
         self.inputs.new("mn_SplineSocket", "Spline")
         self.inputs.new("mn_VectorSocket", "Point")
         self.inputs.new("mn_VectorSocket", "Left Handle")
         self.inputs.new("mn_VectorSocket", "Right Handle")
         self.outputs.new("mn_SplineSocket", "Spline")
-        self.settingChanged(context)
-        allowCompiling()
-        
+        self.settingChanged(bpy.context)
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "pointType", text = "")
-        
-    def getInputSocketNames(self):
-        return {"Spline" : "spline",
-                "Point" : "point",
-                "Left Handle" : "leftHandle",
-                "Right Handle" : "rightHandle"}
-
-    def getOutputSocketNames(self):
-        return {"Spline" : "spline"}
 
     def execute(self, spline, point, leftHandle, rightHandle):
         if self.pointType == "BEZIER_POINT" and spline.type == "BEZIER":
