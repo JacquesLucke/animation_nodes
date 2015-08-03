2b8761a9806518d4dafc83ea24f0d385758eec81
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:41:43 +0200

    cleanup spline info node

--- nodes/spline/spline_info.py
+++ nodes/spline/spline_info.py
@@ -1,24 +1,19 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_SplineInfo(bpy.types.Node, AnimationNode):
+class SplineInfo(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SplineInfo"
     bl_label = "Spline Info"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Spline" : "spline" }
+
+    outputNames = { "Points" : "points",
+                    "Cyclic" : "cyclic" }
+
+    def create(self):
         self.inputs.new("mn_SplineSocket", "Spline").showName = False
         self.outputs.new("mn_VectorListSocket", "Points")
         self.outputs.new("mn_BooleanSocket", "Cyclic")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Spline" : "spline"}
-
-    def getOutputSocketNames(self):
-        return {"Points" : "points",
-                "Cyclic" : "cyclic"}
 
     def execute(self, spline):
         spline.update()
