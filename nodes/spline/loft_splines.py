d9f0a588e9c2a4f8c4fb958d379480a32ec4d2d9
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:18:50 +0200

    cleanup loft splines node

--- nodes/spline/loft_splines.py
+++ nodes/spline/loft_splines.py
@@ -1,33 +1,43 @@
 import bpy
 from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 from ... algorithms.mesh_generation.from_splines import loftSplines
 
 interpolationTypeItems = [
     ("LINEAR", "Linear", ""),
     ("BEZIER", "Bezier", "")]
-    
+
 sampleDistributionTypeItems = [
     ("RESOLUTION", "Resolution", ""),
-    ("UNIFORM", "Uniform", "")]    
-    
-class mn_LoftSplines(bpy.types.Node, AnimationNode):
+    ("UNIFORM", "Uniform", "")]
+
+class LoftSplines(bpy.types.Node, AnimationNode):
     bl_idname = "mn_LoftSplines"
     bl_label = "Loft Splines"
-    
+
+    inputNames = { "Splines" : "splines",
+                   "Spline Samples" : "splineSamples",
+                   "Surface Samples" : "surfaceSamples",
+                   "Cyclic" : "cyclic",
+                   "Smoothness" : "smoothness",
+                   "Start" : "start",
+                   "End" : "end" }
+
+    outputNames = { "Vertices" : "vertices",
+                    "Polygons" : "polygons" }
+
     def settingChanged(self, context):
         self.inputs["Smoothness"].hide = self.interpolationType != "BEZIER"
-        nodePropertyChanged(self, context)
-        
+        propertyChanged(self, context)
+
     interpolationType = EnumProperty(name = "Interpolation Type", default = "BEZIER", items = interpolationTypeItems, update = settingChanged)
-    
-    resolution = IntProperty(name = "Resolution", default = 100, min = 2, description = "Increase to have a more accurate evaluation", update = nodePropertyChanged)
-    splineDistributionType = EnumProperty(name = "Spline Distribution", default = "RESOLUTION", items = sampleDistributionTypeItems, update = nodePropertyChanged)
-    surfaceDistributionType = EnumProperty(name = "Surface Distribution", default = "RESOLUTION", items = sampleDistributionTypeItems, update = nodePropertyChanged)
-        
-    def init(self, context):
-        forbidCompiling()
+
+    resolution = IntProperty(name = "Resolution", default = 100, min = 2, description = "Increase to have a more accurate evaluation", update = propertyChanged)
+    splineDistributionType = EnumProperty(name = "Spline Distribution", default = "RESOLUTION", items = sampleDistributionTypeItems, update = propertyChanged)
+    surfaceDistributionType = EnumProperty(name = "Surface Distribution", default = "RESOLUTION", items = sampleDistributionTypeItems, update = propertyChanged)
+
+    def create(self):
         self.inputs.new("mn_SplineListSocket", "Splines")
         socket1 = self.inputs.new("mn_IntegerSocket", "Spline Samples")
         socket2 = self.inputs.new("mn_IntegerSocket", "Surface Samples")
@@ -45,30 +55,16 @@ class mn_LoftSplines(bpy.types.Node, AnimationNode):
         self.outputs.new("mn_VectorListSocket", "Vertices")
         self.outputs.new("mn_PolygonIndicesListSocket", "Polygons")
         self.width += 20
-        self.settingChanged(context)
-        allowCompiling()
-        
+        self.settingChanged(bpy.context)
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "interpolationType", text = "")
-        
+
     def draw_buttons_ext(self, context, layout):
         col = layout.column()
         col.prop(self, "splineDistributionType")
         col.prop(self, "surfaceDistributionType")
         col.prop(self, "resolution")
-
-    def getInputSocketNames(self):
-        return {"Splines" : "splines",
-                "Spline Samples" : "splineSamples",
-                "Surface Samples" : "surfaceSamples",
-                "Cyclic" : "cyclic",
-                "Smoothness" : "smoothness",
-                "Start" : "start",
-                "End" : "end"}
-
-    def getOutputSocketNames(self):
-        return {"Vertices" : "vertices",
-                "Polygons" : "polygons"}
 
     def execute(self, splines, splineSamples, surfaceSamples, cyclic, smoothness, start, end):
         def canExecute():
@@ -80,17 +76,17 @@ class mn_LoftSplines(bpy.types.Node, AnimationNode):
             isRealCyclic = cyclic and start == 0.0 and end == 1.0
             if isRealCyclic and surfaceSamples < 3: return False
             return True
-            
+
         for spline in splines:
             spline.update()
-        
+
         if canExecute():
-            vertices, polygons = loftSplines(splines, 
+            vertices, polygons = loftSplines(splines,
                                              splineSamples,
-                                             surfaceSamples, 
-                                             self.interpolationType,
-                                             cyclic,
-                                             smoothness,
+                                             surfaceSamples,
+                                             type = self.interpolationType,
+                                             cyclic = cyclic,
+                                             smoothness = smoothness,
                                              uniformConverterResolution = self.resolution,
                                              splineDistributionType = self.splineDistributionType,
                                              surfaceDistributionType = self.surfaceDistributionType,
