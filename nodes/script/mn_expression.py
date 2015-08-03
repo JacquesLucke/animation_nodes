7a1b350e7629b5471d0747d832917ab868ca8be2
Jacques Lucke <mail@jlucke.com>
2015-08-03 13:03:54 +0200

    cleanup expression node

--- nodes/script/mn_expression.py
+++ nodes/script/mn_expression.py
@@ -1,19 +1,31 @@
 import bpy
+from bpy.props import *
+from ... events import executionCodeChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
-from ... mn_utils import *
+from ... mn_utils import isValidCode, getRandomString
 
 defaultVariableNames = list("xyzwabcdefghijklmnopqrstuv")
 
-class mn_ExpressionNode(bpy.types.Node, AnimationNode):
+class ExpressionNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ExpressionNode"
     bl_label = "Expression"
-    
-    expression = bpy.props.StringProperty(default = "x", update = nodeTreeChanged, description = "Python Expression (math module is imported)")
-    isExpressionValid = bpy.props.BoolProperty(default = True)
-    
-    def init(self, context):
-        forbidCompiling()
+
+    @property
+    def inputNames(self):
+        inputSocketNames = {}
+        for socket in self.inputs:
+            if socket.name == "...":
+                inputSocketNames["..."] = "EMPTYSOCKET"
+            else:
+                inputSocketNames[socket.identifier] = socket.customName
+        return inputSocketNames
+
+    outputNames = { "Result" : "result" }
+
+    expression = StringProperty(default = "x", update = executionCodeChanged, description = "Python Expression (math module is imported)")
+    isExpressionValid = BoolProperty(default = True)
+
+    def create(self):
         socket = self.inputs.new("mn_GenericSocket", "x")
         socket.editableCustomName = True
         socket.customName = "x"
@@ -21,15 +33,13 @@ class mn_ExpressionNode(bpy.types.Node, AnimationNode):
         socket.removeable = True
         self.inputs.new("mn_EmptySocket", "...").passiveSocketType = "mn_GenericSocket"
         self.outputs.new("mn_GenericSocket", "Result")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "expression", text = "")
         if not self.isExpressionValid:
             layout.label("invalid expression", icon = "ERROR")
-        
+
     def update(self):
-        forbidCompiling()
         socket = self.inputs.get("...")
         if socket is not None:
             links = socket.links
@@ -44,18 +54,17 @@ class mn_ExpressionNode(bpy.types.Node, AnimationNode):
                 newSocket.customName = self.getNextCustomName()
                 self.inputs.new("mn_EmptySocket", "...").passiveSocketType = "mn_GenericSocket"
                 self.id_data.links.new(newSocket, fromSocket)
-        allowCompiling()
-        
+
     def getNextCustomName(self):
         for name in defaultVariableNames:
             if not self.isCustomNamesUsed(name): return name
         return getRandomString(5)
-            
+
     def isCustomNamesUsed(self, customName):
         for socket in self.inputs:
             if socket.customName == customName: return True
         return False
-        
+
     def getNotUsedSocketName(self):
         socketName = getRandomString(5)
         while self.isSocketNameUsed(socketName):
@@ -65,29 +74,12 @@ class mn_ExpressionNode(bpy.types.Node, AnimationNode):
         for socket in self.inputs:
             if socket.name == name or socket.identifier == name: return True
         return False
-        
-    def getInputSocketNames(self):
-        inputSocketNames = {}
-        for socket in self.inputs:
-            if socket.name == "...":
-                inputSocketNames["..."] = "EMPTYSOCKET"
-            else:
-                inputSocketNames[socket.identifier] = socket.customName
-        return inputSocketNames
-    def getOutputSocketNames(self):
-        return {"Result" : "result"}
-        
-    def useInLineExecution(self):
-        return True
-    def getModuleList(self):
-        return ["math", "re"]
-    def getInLineExecutionString(self, outputUse):
-        if not isValidCode(self.expression):
-            self.isExpressionValid = False
+
+    def getExecutionCode(self):
+        self.isExpressionValid = isValidCode(self.expression)
+        if not self.isExpressionValid:
             return "$result$ = None"
-        else:
-            self.isExpressionValid = True
-        
+
         expression = self.expression + " "
         customNames = self.getCustomNames()
         codeLine = ""
@@ -102,11 +94,13 @@ class mn_ExpressionNode(bpy.types.Node, AnimationNode):
                 currentWord = ""
                 codeLine += char
         return "try: $result$ = " + codeLine + "\nexcept: $result$ = None"
-        
+
+    def getModuleList(self):
+        return ["math", "re"]
+
     def getCustomNames(self):
         customNames = []
         for socket in self.inputs:
             if socket.name != "...":
                 customNames.append(socket.customName)
         return customNames
-        
