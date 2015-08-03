30fdd959f3c58a117e5af9b2f599e4c5bd42bf67
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:03:19 +0200

    cleanup integer list

--- sockets/integer_list.py
+++ sockets/integer_list.py
@@ -1,24 +1,24 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
 from .. base_types.socket import AnimationNodeSocket
 
-class mn_IntegerListSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class IntegerListSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_IntegerListSocket"
     bl_label = "Integer List Socket"
     dataType = "Integer List"
     allowedInputTypes = ["Integer List"]
     drawColor = (0.2, 0.0, 0.9, 1.0)
-    
+
     def drawInput(self, layout, node, text):
         layout.label(text)
-        
+
     def getValue(self):
         return []
-        
+
     def setStoreableValue(self, data):
         pass
+        
     def getStoreableValue(self):
         return []
-        
+
     def getCopyValueFunctionString(self):
         return "return value[:]"
