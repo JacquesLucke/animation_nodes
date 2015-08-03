2ce2ebd9005f57d81bb5be77de0de56f982fbb29
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:14:40 +0200

    cleanup interpolation socket

--- sockets/interpolation.py
+++ sockets/interpolation.py
@@ -1,46 +1,65 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
+from bpy.props import *
+from .. events import propertyChanged
 from .. base_types.socket import AnimationNodeSocket
-from .. algorithms.interpolation import *
+from .. algorithms.interpolation import (linear, expoEaseIn, expoEaseOut,
+                                    cubicEaseIn, cubicEaseOut, cubicEaseInOut,
+                                    backEaseIn, backEaseOut)
 
 topCategoryItems = [("LINEAR", "Linear", ""),
                     ("EXPONENTIAL", "Exponential", ""),
                     ("CUBIC", "Cubic", ""),
                     ("BACK", "Back", "")]
-                    
+
 exponentialCategoryItems = [("IN", "In", ""),
                             ("OUT", "Out", "")]
-                            
+
 cubicCategoryItems = [("IN", "In", ""),
                     ("OUT", "Out", ""),
                     ("INOUT", "In / Out", "")]
-                    
+
 backCategoryItems = [("IN", "In", ""),
                     ("OUT", "Out", "")]
 
-class mn_InterpolationSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class InterpolationSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_InterpolationSocket"
     bl_label = "Interpolation Socket"
     dataType = "Interpolation"
     allowedInputTypes = ["Interpolation"]
     drawColor = (0.7, 0.4, 0.3, 1)
-    
-    topCategory = bpy.props.EnumProperty(items = topCategoryItems, default = "LINEAR", update = nodePropertyChanged, name = "Category")
-    backCategory = bpy.props.EnumProperty(items = backCategoryItems, default = "OUT", update = nodePropertyChanged, name = "Back")
-    exponentialCategory = bpy.props.EnumProperty(items = exponentialCategoryItems, default = "OUT", update = nodePropertyChanged, name = "Exponential")
-    cubicCategory = bpy.props.EnumProperty(items = cubicCategoryItems, default = "OUT", update = nodePropertyChanged, name = "Cubic")
-    
-    showName = bpy.props.BoolProperty(default = True)
-    
+
+    topCategory = EnumProperty(
+        name = "Category", default = "LINEAR",
+        items = topCategoryItems, update = propertyChanged)
+
+    backCategory = EnumProperty(
+        name = "Back", default = "OUT",
+        items = backCategoryItems, update = propertyChanged)
+
+    exponentialCategory = EnumProperty(
+        name = "Exponential", default = "OUT",
+        items = exponentialCategoryItems, update = propertyChanged)
+
+    cubicCategory = EnumProperty(
+        name = "Cubic", default = "OUT",
+        items = cubicCategoryItems, update = propertyChanged)
+
+    showName = BoolProperty(default = True)
+
     def drawInput(self, layout, node, text):
         col = layout.column(align = True)
         if self.showName: col.label(text)
+
         row = col.row(align = True)
         row.prop(self, "topCategory", text = "")
-        if self.topCategory == "BACK": row.prop(self, "backCategory", text = "")
-        if self.topCategory == "EXPONENTIAL": row.prop(self, "exponentialCategory", text = "")
-        if self.topCategory == "CUBIC": row.prop(self, "cubicCategory", text = "")
-        
+
+        if self.topCategory == "BACK":
+            row.prop(self, "backCategory", text = "")
+        if self.topCategory == "EXPONENTIAL":
+            row.prop(self, "exponentialCategory", text = "")
+        if self.topCategory == "CUBIC":
+            row.prop(self, "cubicCategory", text = "")
+
     def getValue(self):
         if self.topCategory == "LINEAR": return (linear, None)
         if self.topCategory == "EXPONENTIAL":
@@ -54,14 +73,9 @@ class mn_InterpolationSocket(bpy.types.NodeSocket, AnimationNodeSocket):
             if self.backCategory == "IN": return (backEaseIn, 1.70158)
             if self.backCategory == "OUT": return (backEaseOut, 1.70158)
         return (linear, None)
-        
+
     def setStoreableValue(self, data):
         self.topCategory, self.backCategory, self.exponentialCategory, self.cubicCategory = data
+
     def getStoreableValue(self):
-        return (self.topCategory, self.backCategory, self.exponentialCategory, self.cubicCategory)
-        
-    def getCurrentMode(self):
-        for mode in interpolationEnum:
-            if mode.identifier == self.mode:
-                return mode
-        return None
+        return self.topCategory, self.backCategory, self.exponentialCategory, self.cubicCategory
