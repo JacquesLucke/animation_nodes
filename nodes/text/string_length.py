4c8f8d4d743846b26d303ec7489bd8db620cdf53
Jacques Lucke <mail@jlucke.com>
2015-08-03 16:56:11 +0200

    cleanup string length node

--- nodes/text/string_length.py
+++ nodes/text/string_length.py
@@ -1,21 +1,16 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_StringLengthNode(bpy.types.Node, AnimationNode):
+class StringLengthNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_StringLengthNode"
     bl_label = "Text Length"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Text" : "text" }
+    outputNames = { "Length" : "length" }
+
+    def create(self):
         self.inputs.new("mn_StringSocket", "Text")
         self.outputs.new("mn_IntegerSocket", "Length")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Text" : "text"}
-    def getOutputSocketNames(self):
-        return {"Length" : "length"}        
-        
-    def execute(self, text):
-        return len(text)
+
+    def getExecutionCode(self):
+        return "$length$ = len(%text%)"
