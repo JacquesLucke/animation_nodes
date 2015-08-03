ca80789474a95c6777b3b142d7c7d7c3e40adabb
Jacques Lucke <mail@jlucke.com>
2015-08-03 12:37:35 +0200

    cleanup filte particles node

--- nodes/particles/mn_filter_particles.py
+++ nodes/particles/mn_filter_particles.py
@@ -1,35 +1,31 @@
-import bpy, random, mathutils
+import bpy
 from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 
-class mn_FilterParticles(bpy.types.Node, AnimationNode):
+class FilterParticles(bpy.types.Node, AnimationNode):
     bl_idname = "mn_FilterParticles"
     bl_label = "Filter Particles"
-    
-    outputUnborn = BoolProperty(name = "Output Unborn Particles", default = False)
-    outputAlive = BoolProperty(name = "Output Alive Particles", default = True)
-    outputDying = BoolProperty(name = "Output Dying Particles", default = False)
-    outputDead = BoolProperty(name = "Output Dead Particles", default = False)
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Particles" : "particles" }
+    outputNames = { "Particles" : "filteredParticles" }
+
+    outputUnborn = BoolProperty(name = "Output Unborn Particles", default = False, update = propertyChanged)
+    outputAlive = BoolProperty(name = "Output Alive Particles", default = True, update = propertyChanged)
+    outputDying = BoolProperty(name = "Output Dying Particles", default = False, update = propertyChanged)
+    outputDead = BoolProperty(name = "Output Dead Particles", default = False, update = propertyChanged)
+
+    def create(self):
         self.inputs.new("mn_ParticleListSocket", "Particles")
         self.outputs.new("mn_ParticleListSocket", "Particles")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         col = layout.column()
         col.prop(self, "outputUnborn", text = "Unborn")
         col.prop(self, "outputAlive", text = "Alive")
         col.prop(self, "outputDying", text = "Dying")
         col.prop(self, "outputDead", text = "Dead")
-        
-    def getInputSocketNames(self):
-        return {"Particles" : "particles"}
-    def getOutputSocketNames(self):
-        return {"Particles" : "filteredParticles"}
 
     def execute(self, particles):
         filteredParticles = []
@@ -40,4 +36,3 @@ class mn_FilterParticles(bpy.types.Node, AnimationNode):
                 particle.alive_state == "DEAD" and self.outputDead:
                 filteredParticles.append(particle)
         return filteredParticles
-        
