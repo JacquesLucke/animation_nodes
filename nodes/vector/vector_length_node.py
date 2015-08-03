b8e3215c67740a42765ca26fb7bbceaab9d66457
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:54:31 +0200

    cleanup vector length node

--- nodes/vector/vector_length_node.py
+++ nodes/vector/vector_length_node.py
@@ -1,23 +1,17 @@
 import bpy
-import mathutils
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-class mn_VectorLengthNode(bpy.types.Node, AnimationNode):
+class VectorLengthNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_VectorLengthNode"
     bl_label = "Vector Length"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Vector" : "vector" }
+    outputNames = { "Length" : "length" }
+
+    def create(self):
         self.inputs.new("mn_VectorSocket", "Vector")
         self.outputs.new("mn_FloatSocket", "Length")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Vector" : "vector"}
-    def getOutputSocketNames(self):
-        return {"Length" : "length"}
-        
-    def execute(self, vector):
-        return vector.length
+
+    def getExecutionCode(self):
+        return "$length$ = %vector%.length"
