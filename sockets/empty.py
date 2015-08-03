960e012cc97715c1b08fbd63cfe32ddefc39d7af
Jacques Lucke <mail@jlucke.com>
2015-08-03 21:54:27 +0200

    cleanup empty socket

--- sockets/empty.py
+++ sockets/empty.py
@@ -1,23 +1,23 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
 from .. base_types.socket import AnimationNodeSocket
 
-class mn_EmptySocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class EmptySocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_EmptySocket"
     bl_label = "Empty Socket"
     dataType = "Empty"
     allowedInputTypes = ["all"]
     drawColor = (0.0, 0.0, 0.0, 0.0)
-    
+
     passiveSocketType = bpy.props.StringProperty(default = "")
-    
+
     def drawInput(self, layout, node, text):
         layout.label(text)
-        
+
     def getValue(self):
         return None
-        
+
     def setStoreableValue(self, data):
         pass
+
     def getStoreableValue(self):
         return None
