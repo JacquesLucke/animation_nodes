4e156a675762a6b20e0a8c5c1c3649630e3a7bff
Jacques Lucke <mail@jlucke.com>
2015-08-03 12:03:43 +0200

    cleanup name utils

--- utils/mn_name_utils.py
+++ utils/mn_name_utils.py
@@ -5,54 +5,37 @@ def toDataPath(name):
     return '["{}"]'.format(name)
 
 def getPossibleObjectName(name = "object"):
-    randomString = getRandomString(3)
-    counter = 1
-    while bpy.data.objects.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
-    
+    return getPossibleName(bpy.data.objects, name)
+
 def getPossibleCurveName(name = "curve"):
-    randomString = getRandomString(3)
-    counter = 1
-    while bpy.data.curves.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
+    return getPossibleName(bpy.data.curves, name)
 
 def getPossibleMeshName(name = "mesh"):
-    randomString = getRandomString(3)
-    counter = 1
-    while bpy.data.meshes.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
-    
+    return getPossibleName(bpy.data.meshes, name)
+
 def getPossibleCameraName(name = "camera"):
-    randomString = getRandomString(3)
-    counter = 1
-    while bpy.data.cameras.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
-    
+    return getPossibleName(bpy.data.cameras, name)
+
 def getPossibleLampName(name = "lamp"):
-    randomString = getRandomString(3)
-    counter = 1
-    while bpy.data.lamps.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
-    
+    return getPossibleName(bpy.data.lamp, name)
+
 def getPossibleNodeName(nodeTree, name = "node"):
-    randomString = getRandomString(3)
-    counter = 1
-    while nodeTree.nodes.get(name + randomString + str(counter)) is not None:
-        counter += 1
-    return name + randomString + str(counter)
-    
+    return getPossibleName(nodeTree.nodes, name)
+
 def getPossibleSocketName(node, name = "socket"):
+    while True:
+        name = getPossibleName(node.inputs, name)
+        if name not in node.outputs:
+            break
+    return name
+
+def getPossibleName(field, name = "element"):
     randomString = getRandomString(3)
     counter = 1
-    while node.inputs.get(name + randomString + str(counter)) or node.outputs.get(name + randomString + str(counter)):
+    while field.get(name + randomString + str(counter)):
         counter += 1
-    return name + randomString + str(counter)
-    
+    return name + randomString + str(counter)    
+
 def convertVariableNameToUI(sourceName):
     tempName = ""
 
@@ -73,7 +56,7 @@ def convertVariableNameToUI(sourceName):
             tempName += " "
         else:
             tempName += char
-            
+
     bindingWords = ["and", "of", "to", "from"]
     words = tempName.split()
     newName = ""
@@ -81,5 +64,5 @@ def convertVariableNameToUI(sourceName):
         if word.lower() in bindingWords:
             word = word.lower()
         newName += " " + word
-        
+
     return newName
