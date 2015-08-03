2d53b9ac53fc6f100ae2e3a75ca87763685139fd
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:48:12 +0200

    cleanup transform vector node

--- nodes/vector/transform_vector.py
+++ nodes/vector/transform_vector.py
@@ -1,28 +1,23 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodeTreeChanged, allowCompiling, forbidCompiling
 
-class mn_TransformVector(bpy.types.Node, AnimationNode):
+class TransformVector(bpy.types.Node, AnimationNode):
     bl_idname = "mn_TransformVector"
     bl_label = "Transform Vector"
     isDetermined = True
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Vector" : "vector",
+                   "Matrix" : "matrix" }
+
+    outputNames = { "Vector" : "vector" }
+
+    def create(self):
         self.inputs.new("mn_VectorSocket", "Vector")
         self.inputs.new("mn_MatrixSocket", "Matrix")
         self.outputs.new("mn_VectorSocket", "Vector")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Vector" : "vector", "Matrix" : "matrix"}
-    def getOutputSocketNames(self):
-        return {"Vector" : "vector"}
-        
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+
+    def getExecutionCode(self):
         return "$vector$ = %matrix% * mathutils.Vector(%vector%)"
+        
     def getModuleList(self):
         return ["mathutils"]
-
