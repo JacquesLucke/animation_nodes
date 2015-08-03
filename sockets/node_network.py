35b25aebef56329df8fe6a6f8974c4d762cbcd70
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:17:38 +0200

    cleanup node network socket

--- sockets/node_network.py
+++ sockets/node_network.py
@@ -1,21 +1,21 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
 from .. base_types.socket import AnimationNodeSocket
 
-class mn_NodeNetworkSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class NodeNetworkSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_NodeNetworkSocket"
     bl_label = "Node Network Socket"
     dataType = "Node Network"
     allowedInputTypes = ["all"]
     drawColor = (0.34, 0.25, 0.22, 1.0)
-    
+
     def drawInput(self, layout, node, text):
         layout.label(text)
-        
+
     def getValue(self):
         return None
-        
+
     def setStoreableValue(self, data):
         pass
+
     def getStoreableValue(self):
         return None
