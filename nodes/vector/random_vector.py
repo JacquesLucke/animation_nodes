cb86fbc2ad81d215765b489e778a5b7f2ef790cb
Jacques Lucke <mail@jlucke.com>
2015-08-03 17:43:03 +0200

    cleanup random vector node

--- nodes/vector/random_vector.py
+++ nodes/vector/random_vector.py
@@ -4,35 +4,32 @@ from ... base_types.node import AnimationNode
 from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 from mathutils import Vector
 
-class mn_RandomVectorNode(bpy.types.Node, AnimationNode):
+class RandomVectorNode(bpy.types.Node, AnimationNode):
     bl_idname = "mn_RandomVectorNode"
     bl_label = "Random Vector"
     isDetermined = True
-    
+
+    inputNames = { "Seed" : "seed",
+                   "Max Values" : "maxValues" }
+
+    outputNames = { "Vector" : "randomVector" }
+
     additionalSeed = bpy.props.IntProperty(update = nodePropertyChanged)
-    
-    def init(self, context):
-        forbidCompiling()
+
+    def create(self):
         self.inputs.new("mn_IntegerSocket", "Seed")
         self.inputs.new("mn_FloatSocket", "Max Values").value = 5.0
         self.outputs.new("mn_VectorSocket", "Vector")
-        allowCompiling()
-        
+
     def draw_buttons(self, context, layout):
         layout.prop(self, "additionalSeed", text = "Additional Seed")
-        
-    def getInputSocketNames(self):
-        return {"Seed" : "seed", "Max Values" : "maxValues"}
-    def getOutputSocketNames(self):
-        return {"Vector" : "random_vector"}
-        
+
     def execute(self, seed, maxValues):
         max = maxValues/2
         addSeed = 1193 * self.additionalSeed
         return Vector((getUniformRandom(seed + addSeed, -max, max),
                 getUniformRandom(seed + 754 + addSeed, -max, max),
                 getUniformRandom(seed + 2345 + addSeed, -max, max)))
-                
-    def copy(self, node):
-        self.additionalSeed = int(random.random()*1000)
-        
+
+    def duplicate(self, sourceNode):
+        self.additionalSeed = int(random.random() * 1000)
