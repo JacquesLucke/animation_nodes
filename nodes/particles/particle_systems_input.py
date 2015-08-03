078ba6db5cc7e42c72e603b9bde339457868a9a9
Jacques Lucke <mail@jlucke.com>
2015-08-03 12:50:00 +0200

    cleanup particle systems input node

--- nodes/particles/particle_systems_input.py
+++ nodes/particles/particle_systems_input.py
@@ -1,30 +1,23 @@
 import bpy, random, mathutils
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
-
-class mn_ParticleSystemsInput(bpy.types.Node, AnimationNode):
+class ParticleSystemsInput(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ParticleSystemsInput"
     bl_label = "Particle Systems Input"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Object" : "object" }
+
+    outputNames = { "Particle Systems" : "particleSystems",
+                    "Active" : "active"}
+
+    def create(self):
         self.inputs.new("mn_ObjectSocket", "Object").showName = False
         self.outputs.new("mn_ParticleSystemListSocket", "Particle Systems")
         self.outputs.new("mn_ParticleSystemSocket", "Active")
         self.width += 10
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Object" : "object"}
-    def getOutputSocketNames(self):
-        return {"Particle Systems" : "particleSystems",
-                "Active" : "active"}
 
     def execute(self, object):
         if not object: return [], None
         particleSystems = object.particle_systems
-        active = object.particle_systems.active
+        active = particleSystems.active
         return list(particleSystems), active
-        
-
