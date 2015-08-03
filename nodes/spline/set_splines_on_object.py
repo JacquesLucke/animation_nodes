a1d75994efc46974b319b6f93ea0757516e60312
Jacques Lucke <mail@jlucke.com>
2015-08-03 15:33:10 +0200

    cleanup set splines on object node

--- nodes/spline/set_splines_on_object.py
+++ nodes/spline/set_splines_on_object.py
@@ -1,27 +1,22 @@
 import bpy
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 from ... data_structures.splines.to_blender import setSplinesOnBlenderObject
 
-class mn_SetSplinesOnObject(bpy.types.Node, AnimationNode):
+class SetSplinesOnObject(bpy.types.Node, AnimationNode):
     bl_idname = "mn_SetSplinesOnObject"
     bl_label = "Set Splines on Object"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Object" : "object",
+                   "Splines" : "splines" }
+
+    outputNames = { "Object" : "object" }
+
+    def create(self):
         socket = self.inputs.new("mn_ObjectSocket", "Object")
         socket.showName = False
         socket.objectCreationType = "CURVE"
         self.inputs.new("mn_SplineListSocket", "Splines").showObjectInput = False
         self.outputs.new("mn_ObjectSocket", "Object")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Object" : "object",
-                "Splines" : "splines"}
-
-    def getOutputSocketNames(self):
-        return {"Object" : "object"}
 
     def execute(self, object, splines):
         setSplinesOnBlenderObject(object, splines)
