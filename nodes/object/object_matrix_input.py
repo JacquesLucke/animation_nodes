2bf870f37eedd2370a0180877e178b133704245e
Jacques Lucke <mail@jlucke.com>
2015-08-03 12:10:51 +0200

    cleanup object matrix input

--- nodes/object/object_matrix_input.py
+++ nodes/object/object_matrix_input.py
@@ -1,40 +1,32 @@
 import bpy
-from mathutils import *
 from ... base_types.node import AnimationNode
-from ... mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
 
 
-class mn_ObjectMatrixInput(bpy.types.Node, AnimationNode):
+class ObjectMatrixInput(bpy.types.Node, AnimationNode):
     bl_idname = "mn_ObjectMatrixInput"
     bl_label = "Object Matrix Input"
-    outputUseParameterName = "useOutput"
-    
-    def init(self, context):
-        forbidCompiling()
+
+    inputNames = { "Object" : "object" }
+
+    outputNames = { "Basis" : "basis",
+                    "Local" : "local",
+                    "Parent Inverse" : "parentInverse",
+                    "World" : "world" }
+
+    def create(self):
         self.inputs.new("mn_ObjectSocket", "Object").showName = False
         self.outputs.new("mn_MatrixSocket", "Basis")
         self.outputs.new("mn_MatrixSocket", "Local")
         self.outputs.new("mn_MatrixSocket", "Parent Inverse")
         self.outputs.new("mn_MatrixSocket", "World")
-        allowCompiling()
-        
-    def getInputSocketNames(self):
-        return {"Object" : "object"}
-    def getOutputSocketNames(self):
-        return {"Basis" : "basis",
-                "Local" : "local",
-                "Parent Inverse" : "parentInverse",
-                "World" : "world"}
-    
-    def useInLineExecution(self):
-        return True
-    def getInLineExecutionString(self, outputUse):
+
+    def getExecutionCode(self, usedOutputs):
         codeLines = []
         codeLines.append("try:")
-        if outputUse["Basis"]: codeLines.append("    $basis$ = %object%.matrix_basis")
-        if outputUse["Local"]: codeLines.append("    $local$ = %object%.matrix_local")
-        if outputUse["Parent Inverse"]: codeLines.append("    $parentInverse$ = %object%.matrix_parent_inverse")
-        if outputUse["World"]: codeLines.append("    $world$ = %object%.matrix_world")
+        if usedOutputs["Basis"]: codeLines.append("    $basis$ = %object%.matrix_basis")
+        if usedOutputs["Local"]: codeLines.append("    $local$ = %object%.matrix_local")
+        if usedOutputs["Parent Inverse"]: codeLines.append("    $parentInverse$ = %object%.matrix_parent_inverse")
+        if usedOutputs["World"]: codeLines.append("    $world$ = %object%.matrix_world")
         codeLines.append("    pass")
         codeLines.append("except:")
         codeLines.append("    $basis$ = mathutils.Matrix.Identity(4)")
@@ -42,6 +34,6 @@ class mn_ObjectMatrixInput(bpy.types.Node, AnimationNode):
         codeLines.append("    $parentInverse$ = mathutils.Matrix.Identity(4)")
         codeLines.append("    $world$ = mathutils.Matrix.Identity(4)")
         return "\n".join(codeLines)
-        
+
     def getModuleList(self):
         return ["mathutils"]
