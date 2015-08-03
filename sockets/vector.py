5b7b6a1a13f5c93060e3f71d2ef864689c26db52
Jacques Lucke <mail@jlucke.com>
2015-08-03 22:40:13 +0200

    cleanup vector socke

--- sockets/vector.py
+++ sockets/vector.py
@@ -1,18 +1,18 @@
 import bpy
-from .. mn_execution import nodePropertyChanged
-from .. base_types.socket import AnimationNodeSocket
 from mathutils import Vector
+from .. events import propertyChanged
+from .. base_types.socket import AnimationNodeSocket
 
-class mn_VectorSocket(bpy.types.NodeSocket, AnimationNodeSocket):
+class VectorSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "mn_VectorSocket"
     bl_label = "Vector Socket"
     dataType = "Vector"
     allowedInputTypes = ["Vector"]
     drawColor = (0.05, 0.05, 0.8, 0.7)
-    
-    value = bpy.props.FloatVectorProperty(default = [0, 0, 0], update = nodePropertyChanged)
+
+    value = bpy.props.FloatVectorProperty(default = [0, 0, 0], update = propertyChanged)
     showName = bpy.props.BoolProperty(default = True)
-    
+
     def drawInput(self, layout, node, text):
         col = layout.column(align = True)
         if self.showName: col.label(text)
@@ -20,14 +20,15 @@ class mn_VectorSocket(bpy.types.NodeSocket, AnimationNodeSocket):
         col.prop(self, "value", index = 1, text = "Y")
         col.prop(self, "value", index = 2, text = "Z")
         col.separator()
-        
+
     def getValue(self):
         return Vector(self.value)
-        
+
     def setStoreableValue(self, data):
         self.value = data
+        
     def getStoreableValue(self):
         return self.value[:]
-        
+
     def getCopyValueFunctionString(self):
         return "return value.copy()"
