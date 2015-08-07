592da272ff8bd745fa75091f45850e7b2bb434b9
Jacques Lucke <mail@jlucke.com>
2015-08-07 13:15:05 +0200

    cleanup

--- nodes/list/utils_list_input_operators.py
+++ nodes/list/utils_list_input_operators.py
@@ -2,18 +2,6 @@ import bpy
 from bpy.props import *
 from ... utils.nodes import getNode
 
-class an_NewPropertyListNode(bpy.types.Operator):
-    bl_idname = "an.new_property_to_list_node"
-    bl_label = "New String Property to String List Node"
-
-    nodeTreeName = StringProperty()
-    nodeName = StringProperty()
-
-    def execute(self, context):
-        node = getNode(self.nodeTreeName, self.nodeName)
-        node.addItemToList()
-        return {'FINISHED'}
-
 class an_RemovePropertyFromListNode(bpy.types.Operator):
     bl_idname = "an.remove_property_from_list_node"
     bl_label = "Remove String Property from String List Node"
