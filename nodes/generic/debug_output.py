a28e78ef6fc93b577ac45db07e58fa96342ab019
Jacques Lucke <mail@jlucke.com>
2015-08-24 15:37:18 +0200

    rewrite debug node

--- nodes/generic/debug_output.py
+++ nodes/generic/debug_output.py
@@ -2,66 +2,54 @@ import bpy
 from bpy.props import *
 from ... base_types.node import AnimationNode
 
-debugTextBlockName = "AN Debug"
+textBlockName = "AN Debug"
+debugDataCache = {}
 
-class DebugOutputNode(bpy.types.Node, AnimationNode):
-    bl_idname = "an_DebugOutputNode"
-    bl_label = "Debug Output"
+class DebugNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_DebugNode"
+    bl_label = "Debug"
 
-    # just a random sequence
-    lineSeparator = "je2c4nw"
-
-    printDebugString = BoolProperty(default = False, name = "Print in Console")
-    writeToTextBlock = BoolProperty(default = False, name = "Write to Text Block")
-
-    showIterableInRows = BoolProperty(default = False, description = "Display individual elements in rows")
-    startRow = IntProperty(default = 0, name = "Start Rows", min = 0)
-    maxRowAmount = IntProperty(default = 10, name = "Maximum Rows", min = 1)
-
-    debugOutputString = StringProperty(default = "")
+    printData = BoolProperty(default = False, name = "Print to Console")
+    writeData = BoolProperty(default = False, name = "Write into Text Block")
 
     def create(self):
-        self.inputs.new("an_GenericSocket", "Data", "data")
-        self.bl_width_max = 10000
+        self.newInput()
 
-    def draw(self, layout):
-        if self.showIterableInRows:
-            elements = self.debugOutputString.split(self.lineSeparator)
-            if self.startRow > 0: layout.label("--- {} elements above ---".format(min(self.startRow, len(elements))))
-            col = layout.column(align = True)
-            endRow = self.startRow + self.maxRowAmount
-            for i, element in enumerate(elements):
-                if self.startRow <= i < endRow:
-                    col.label("{}: {}".format(i, element))
-            if endRow < len(elements): layout.label("--- {} elements below ---".format(len(elements) - endRow))
-        else:
-            layout.label(self.debugOutputString)
+    def newInput(self):
+        socket = self.inputs.new("an_GenericSocket", "Data", "data")
+        socket.removeable = True
+        socket.moveable = True
+        socket["dataWidth"] = 10
 
     def drawAdvanced(self, layout):
-        col = layout.column()
-        col.prop(self, "showIterableInRows", text = "Show in Rows")
-        subcol = col.column(align = True)
-        subcol.active = self.showIterableInRows
-        subcol.prop(self, "startRow")
-        subcol.prop(self, "maxRowAmount")
+        self.functionOperator(layout, "newInput", text = "New Input", icon = "PLUS")
+        layout.prop(self, "printData")
+        layout.prop(self, "writeData")
 
-        layout.prop(self, "printDebugString")
-        layout.prop(self, "writeToTextBlock")
+        col = layout.column(align = True)
+        for i, socket in enumerate(self.inputs):
+            col.prop(socket, '["dataWidth"]', text = "Width " + str(i + 1))
 
-    def execute(self, data):
-        if self.showIterableInRows and hasattr(data, "__iter__"):
-            self.debugOutputString = self.lineSeparator.join([str(element) for element in data])
-        else:
-            self.debugOutputString = str(data)
-        if self.printDebugString: print(str(data))
-        if self.writeToTextBlock:
-            text = bpy.data.texts.get(debugTextBlockName)
-            text.write(str(data))
-            text.write("\n")
+    @property
+    def inputNames(self):
+        return {socket.identifier : "data_" + str(i) for i, socket in enumerate(self.inputs)}
 
+    def getExecutionCode(self):
+        names = ["data_" + str(i) for i in range(len(self.inputs))]
+        return "self.writeDebugCache([{}])".format(", ".join(names))
 
-def clearDebugTextBlock():
-    text = bpy.data.texts.get(debugTextBlockName)
-    if text is None:
-        text = bpy.data.texts.new(debugTextBlockName)
-    text.clear()
+    def writeDebugCache(self, dataList):
+        texts = []
+        for data, socket in zip(dataList, self.inputs):
+            if isinstance(data, float): text = str(round(data, 5))
+            else: text = str(data)
+            socket.name = text
+            texts.append(text.rjust(socket["dataWidth"]))
+        if self.printData: print(" ".join(texts))
+        if self.writeData:
+            textBlock = bpy.data.texts[textBlockName]
+            textBlock.write(" ".join(texts))
+            textBlock.write("\n")
+
+def clearDebugData():
+    bpy.data.texts[textBlockName].clear()
