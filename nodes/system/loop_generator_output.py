9412d8334d67f6fe13f3038ff765ccbe54d2fb8e
Jacques Lucke <mail@jlucke.com>
2015-08-25 10:21:01 +0200

    loop generator output ui

---
+++ nodes/system/loop_generator_output.py
@@ -0,0 +1,51 @@
+import bpy
+from bpy.props import *
+from ... base_types.node import AnimationNode
+from ... sockets.info import toIdName, toBaseDataType
+from ... tree_info import keepNodeLinks
+
+addTypeItems = [
+    ("APPEND", "Append", "Add one element to the output list", "NONE", 0),
+    ("EXTEND", "Extend", "Add a custom amount of elements to the output list", "NONE", 1) ]
+
+class LoopGeneratorOutput(bpy.types.Node, AnimationNode):
+    bl_idname = "an_LoopGeneratorOutput"
+    bl_label = "Loop Generator Output"
+
+    def dataTypeChanged(self, context):
+        self.generateSockets()
+
+    def nameChanged(self, context):
+        self.label = self.outputName
+
+    listDataType = StringProperty(update = dataTypeChanged)
+    addType = EnumProperty(name = "Add Type", items = addTypeItems, update = dataTypeChanged)
+    outputName = StringProperty(name = "Generator Name", update = nameChanged)
+
+    def create(self):
+        self.listDataType = "Vector List"
+        self.outputName = "Vector List"
+
+    def drawAdvanced(self, layout):
+        layout.prop(self, "outputName", text = "Name")
+        layout.prop(self, "addType")
+        self.functionOperator(layout, "chooseNewGeneratorType", text = "Change Type")
+
+    def chooseNewGeneratorType(self):
+        self.chooseSocketDataType("setListDataType", socketGroup = "LIST")
+
+    def setListDataType(self, dataType):
+        self.listDataType = dataType
+
+    @keepNodeLinks
+    def generateSockets(self):
+        self.inputs.clear()
+
+        socket = self.inputs.new("an_BooleanSocket", "Activate")
+        socket.value = True
+        socket.hide = True
+
+        if self.addType == "APPEND": dataType = toBaseDataType(self.listDataType)
+        elif self.addType == "EXTEND": dataType = self.listDataType
+        socket = self.inputs.new(toIdName(dataType), dataType)
+        socket.display.nameOnly = True
