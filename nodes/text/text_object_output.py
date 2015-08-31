5008ab361cbd6b6adb54f75340a72afc3540f871
Jacques Lucke <mail@jlucke.com>
2015-08-31 20:26:07 +0200

    implement bevel depth, bevel resolution and alignment in text object output

--- nodes/text/text_object_output.py
+++ nodes/text/text_object_output.py
@@ -1,5 +1,6 @@
 import bpy
 from bpy.props import *
+from ... utils.layout import writeText
 from ... events import executionCodeChanged
 from ... base_types.node import AnimationNode
 
@@ -7,13 +8,17 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TextObjectOutputNode"
     bl_label = "Text Object Output"
 
+    errorMessage = StringProperty()
+
     def create(self):
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
 
         self.inputs.new("an_StringSocket", "Text", "text")
-        self.inputs.new("an_FloatSocket", "Extrude", "extrude").setMinMax(0.0, 1e9)
-        self.inputs.new("an_FloatSocket", "Shear", "shear").setMinMax(-1.0, 1.0)
         self.inputs.new("an_FloatSocket", "Size", "size").value = 1.0
+        self.inputs.new("an_FloatSocket", "Extrude", "extrude")
+        self.inputs.new("an_FloatSocket", "Shear", "shear")
+        self.inputs.new("an_FloatSocket", "Bevel Depth", "bevelDepth")
+        self.inputs.new("an_IntegerSocket", "Bevel Resolution", "bevelResolution")
 
         self.inputs.new("an_FloatSocket", "Letter Spacing", "letterSpacing").value = 1.0
         self.inputs.new("an_FloatSocket", "Word Spacing", "wordSpacing").value = 1.0
@@ -21,6 +26,7 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
 
         self.inputs.new("an_FloatSocket", "X Offset", "xOffset")
         self.inputs.new("an_FloatSocket", "Y Offset", "yOffset")
+        self.inputs.new("an_StringSocket", "Align", "align")
 
         for socket in self.inputs[1:]:
             socket.defaultDrawType = "TEXT_ONLY"
@@ -28,9 +34,15 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
             socket.hide = True
 
         self.outputs.new("an_ObjectSocket", "Object", "object")
+        self.width = 170
+
+    def draw(self, layout):
+        if self.errorMessage != "":
+            writeText(layout, self.errorMessage, width = 25, icon = "ERROR")
 
     def drawAdvanced(self, layout):
         layout.label("This node uses only linked inputs!", icon = "INFO")
+        writeText(layout, "Possible values for 'Align' are 'LEFT', 'CENTER', 'RIGHT', 'JUSTIFY' and 'FLUSH'")
 
     def getExecutionCode(self):
         isLinked = self.getLinkedInputsDict()
@@ -39,9 +51,11 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
         lines.append("    textObject = object.data")
 
         if isLinked["text"]: lines.append("    textObject.body = text")
+        if isLinked["size"]: lines.append("    textObject.size = size")
         if isLinked["extrude"]: lines.append("    textObject.extrude = extrude")
         if isLinked["shear"]: lines.append("    textObject.shear = shear")
-        if isLinked["size"]: lines.append("    textObject.size = size")
+        if isLinked["bevelDepth"]: lines.append("    textObject.bevel_depth = bevelDepth")
+        if isLinked["bevelResolution"]: lines.append("    textObject.bevel_resolution = bevelResolution")
 
         if isLinked["letterSpacing"]: lines.append("    textObject.space_character = letterSpacing")
         if isLinked["wordSpacing"]: lines.append("    textObject.space_word = wordSpacing")
@@ -49,7 +63,15 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
 
         if isLinked["xOffset"]: lines.append("    textObject.offset_x = xOffset")
         if isLinked["yOffset"]: lines.append("    textObject.offset_y = yOffset")
+        if isLinked["align"]: lines.append("    self.setAlignment(textObject, align)")
 
         lines.append("    pass")
 
         return lines
+
+    def setAlignment(self, textObject, align):
+        if align in ("LEFT", "CENTER", "RIGHT", "JUSTIFY", "FLUSH"):
+            textObject.align = align
+            self.errorMessage = ""
+        else:
+            self.errorMessage = "The align type is invalid. Look in the advanced panels to see the possibilities."
--- utils/layout.py
+++ utils/layout.py
@@ -1,3 +1,5 @@
+import textwrap
+
 def splitAlignment(layout):
     row = layout.row()
     left = row.row()
@@ -5,3 +7,12 @@ def splitAlignment(layout):
     right = row.row()
     right.alignment = "RIGHT"
     return left, right
+
+def writeText(layout, text, width = 30, icon = "NONE"):
+    col = layout.column(align = True)
+    col.scale_y = 0.85
+    prefix = " "
+    for line in textwrap.wrap(text, width):
+        col.label(prefix + line, icon = icon)
+        if icon != "NONE": prefix = "     "
+        icon = "NONE"
