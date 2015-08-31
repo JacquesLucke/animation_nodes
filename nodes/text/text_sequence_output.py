ffcb2d3ab628ea66aeaa15e9c5f934c23446ce8e
Jacques Lucke <mail@jlucke.com>
2015-08-31 20:36:28 +0200

    fix text sequence output node

--- nodes/text/text_sequence_output.py
+++ nodes/text/text_sequence_output.py
@@ -1,32 +1,56 @@
 import bpy
+from bpy.props import *
+from ... utils.layout import writeText
 from ... base_types.node import AnimationNode
 
 class TextSequenceOutputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TextSequenceOutputNode"
     bl_label = "Text Sequence Output"
 
+    errorMessage = StringProperty()
+
     def create(self):
         self.inputs.new("an_SequenceSocket", "Sequence", "sequence").defaultDrawType = "PROPERTY_ONLY"
         self.inputs.new("an_StringSocket", "Text", "text")
-        self.inputs.new("an_IntegerSocket", "Size", "size").number = 200
+        self.inputs.new("an_IntegerSocket", "Size", "size").value = 200
         self.inputs.new("an_BooleanSocket", "Shadow", "shadow").value = False
-        socket = self.inputs.new("an_StringSocket", "Align", "align")
-        socket.useEnum = True
-        socket.setEnumItems([("CENTER", "Center"), ("LEFT", "Left"), ("RIGHT", "Right")])
-        socket.string = "CENTER"
-        self.inputs.new("an_FloatSocket", "X Location", "xLocation").number = 0.5
-        self.inputs.new("an_FloatSocket", "Y Location", "yLocation").number = 0.0
-        self.outputs.new("an_SequenceSocket", "Sequence", "outSequence")
+        self.inputs.new("an_StringSocket", "Align", "align")
+        self.inputs.new("an_FloatSocket", "X Location", "xLocation").value = 0.5
+        self.inputs.new("an_FloatSocket", "Y Location", "yLocation").value = 0.0
+        self.outputs.new("an_SequenceSocket", "Sequence", "sequence")
 
-    def execute(self, sequence, text, size, shadow, align, xLocation, yLocation):
-        if getattr(sequence, "type", "") != "TEXT": return sequence
+        for socket in self.inputs[1:]:
+            socket.defaultDrawType = "TEXT_ONLY"
+        for socket in self.inputs[4:]:
+            socket.hide = True
 
-        sequence.text = text
-        sequence.font_size = size
-        sequence.use_shadow = shadow
-        sequence.location[0] = xLocation
-        sequence.location[1] = yLocation
-        if align in ("CENTER", "LEFT", "RIGHT"):
+    def draw(self, layout):
+        if self.errorMessage != "":
+            writeText(layout, self.errorMessage, width = 25, icon = "ERROR")
+
+    def drawAdvanced(self, layout):
+        layout.label("This node uses only linked inputs!", icon = "INFO")
+        writeText(layout, "Possible values for 'Align' are 'LEFT', 'CENTER' and 'RIGHT'")
+
+    def getExecutionCode(self):
+        isLinked = self.getLinkedInputsDict()
+        lines = []
+        lines.append("if getattr(sequence, 'type', '') == 'TEXT':")
+
+        if isLinked["text"]: lines.append("    sequence.text = text")
+        if isLinked["size"]: lines.append("    sequence.font_size = size")
+        if isLinked["shadow"]: lines.append("    sequence.use_shadow = shadow")
+        if isLinked["align"]: lines.append("    self.setAlignment(sequence, align)")
+        if isLinked["xLocation"]: lines.append("    sequence.location[0] = xLocation")
+        if isLinked["yLocation"]: lines.append("    sequence.location[1] = yLocation")
+
+        lines.append("    pass")
+
+        return lines
+
+    def setAlignment(self, sequence, align):
+        if align in ("LEFT", "CENTER", "RIGHT"):
             sequence.align = align
-
-        return sequence
+            self.errorMessage = ""
+        else:
+            self.errorMessage = "The align type is invalid. Look in the advanced panels to see the possibilities."
