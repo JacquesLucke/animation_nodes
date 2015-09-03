2758f19ade4b006817533a1d912503ed261adb75
Jacques Lucke <mail@jlucke.com>
2015-09-03 10:40:06 +0200

    make sockets for useWorldSpace and useModifiers in object mesh data node

--- nodes/mesh/object_mesh_data.py
+++ nodes/mesh/object_mesh_data.py
@@ -7,23 +7,13 @@ class ObjectMeshDataNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ObjectMeshDataNode"
     bl_label = "Object Mesh Data"
 
-    useWorldSpace = BoolProperty(name = "Use World Space", default = True, update = propertyChanged,
-        description = "Use real vertex locations (should be turned of when the vertices will be transformed later anyway)")
-
-    useModifiers = BoolProperty(name = "Use Modifiers", default = False, update = propertyChanged,
-        description = "Apply modifiers before importing the data")
-
-
-
     def create(self):
-        self.inputs.new("an_ObjectSocket", "Object", "object")
+        self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
+        self.inputs.new("an_BooleanSocket", "Use World Space", "useWorldSpace").value = True
+        self.inputs.new("an_BooleanSocket", "Use Modifiers", "useModifiers").value = False
         self.outputs.new("an_VectorListSocket", "Vertices", "vertices")
         self.outputs.new("an_EdgeIndicesListSocket", "Edge Indices", "edgeIndices")
         self.outputs.new("an_PolygonIndicesListSocket", "Polygon Indices", "polygonIndices")
-
-    def draw(self, layout):
-        layout.prop(self, "useModifiers")
-        layout.prop(self, "useWorldSpace")
 
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
@@ -33,27 +23,27 @@ class ObjectMeshDataNode(bpy.types.Node, AnimationNode):
 
         lines = []
         lines.append("if getattr(object, 'type', '') == 'MESH':")
-        lines.append("    mesh = self.getMesh(object)")
-        if isLinked["vertices"]: lines.append("    vertices = self.getVertices(mesh, object)")
+        lines.append("    mesh = self.getMesh(object, useModifiers)")
+        if isLinked["vertices"]: lines.append("    vertices = self.getVertices(mesh, object, useWorldSpace)")
         if isLinked["edgeIndices"]: lines.append("    edgeIndices = self.getEdges(mesh)")
         if isLinked["polygonIndices"]: lines.append("    polygonIndices = self.getPolygons(mesh)")
-        lines.append("    self.clearMesh(mesh)")
+        lines.append("    self.clearMesh(mesh, useModifiers)")
         lines.append("else: vertices, edgeIndices, polygonIndices = [], [], []")
         return lines
 
 
-    def getMesh(self, object):
-        if self.useModifiers:
+    def getMesh(self, object, useModifiers):
+        if useModifiers:
             settings = "RENDER" if isRendering() else "PREVIEW"
             return object.to_mesh(scene = bpy.context.scene, apply_modifiers = True, settings = settings)
         return object.data
 
-    def clearMesh(self, mesh):
-        if self.useModifiers: bpy.data.meshes.remove(mesh)
+    def clearMesh(self, mesh, useModifiers):
+        if useModifiers: bpy.data.meshes.remove(mesh)
 
 
-    def getVertices(self, mesh, object):
-        if self.useWorldSpace:
+    def getVertices(self, mesh, object, useWorldSpace):
+        if useWorldSpace:
             matrix = object.matrix_world
             return [matrix * v.co for v in mesh.vertices]
         else:
