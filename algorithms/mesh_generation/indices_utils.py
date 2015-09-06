14e15268910a2c73e539ffd87965337245c7a801
Jacques Lucke <mail@jlucke.com>
2015-09-06 11:57:30 +0200

    grid mesh node

--- algorithms/mesh_generation/indices_utils.py
+++ algorithms/mesh_generation/indices_utils.py
@@ -10,7 +10,17 @@ def gridQuadPolygonIndices(xDivisions, yDivisions):
         for j in range(i, i + yDivisions - 1):
             polygons.append((j, j + 1, j + yDivisions + 1, j + yDivisions))
     return polygons
-    
+
+def gridQuadEdgeIndices(xDivisions, yDivisions):
+    edges = []
+    for i in range((xDivisions - 1) * yDivisions):
+        edges.append((i, i + yDivisions))
+    for i in range(yDivisions - 1):
+        for j in range(xDivisions):
+            firstIndex = i + j * yDivisions
+            edges.append((firstIndex, firstIndex + 1))
+    return edges
+
 def gridEndEdgesQuadPolygonIndices(xDivisions, yDivisions):
     polygons = []
     for i in range(0, (xDivisions - 1) * yDivisions, yDivisions):
