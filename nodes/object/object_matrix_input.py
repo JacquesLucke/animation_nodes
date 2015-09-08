8714127a1bf241de3ce7115ec5b91acfc1c6b87d
Jacques Lucke <mail@jlucke.com>
2015-09-08 14:01:54 +0200

    fix

--- nodes/object/object_matrix_input.py
+++ nodes/object/object_matrix_input.py
@@ -14,9 +14,9 @@ class ObjectMatrixInputNode(bpy.types.Node, AnimationNode):
         self.outputs.new("an_MatrixSocket", "World", "world")
 
     def getExecutionCode(self):
-        if len(self.isLinked) == 0: return ""
 
         isLinked = self.getLinkedOutputsDict()
+        if not any(isLinked.values()): return ""
         lines = []
         lines.append("if object is None:")
         if isLinked["basis"]: lines.append("    basis = mathutils.Matrix.Identity(4)")
