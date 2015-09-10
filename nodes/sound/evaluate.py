df6d6b733c1ce777c59d5e03c504ab2c43360092
Jacques Lucke <mail@jlucke.com>
2015-09-10 11:51:01 +0200

    offset frame evaluation

--- nodes/sound/evaluate.py
+++ nodes/sound/evaluate.py
@@ -1,15 +1,32 @@
 import bpy
+from bpy.props import *
+from ... events import propertyChanged
 from ... base_types.node import AnimationNode
+
+frameTypes = [
+    ("OFFSET", "Offset", ""),
+    ("ABSOLUTE", "Absolute", "") ]
 
 class EvaluateSoundNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_EvaluateSoundNode"
     bl_label = "Evaluate Sound"
 
+    def frameTypeChanged(self, context):
+        self.inputs["Frame"].hide = False
+        propertyChanged()
+
+    frameType = EnumProperty(
+        name = "Frame Type", default = "OFFSET",
+        items = frameTypes, update = frameTypeChanged)
+
     def create(self):
         self.width = 175
         self.inputs.new("an_SoundSocket", "Sound", "sound")
-        self.inputs.new("an_FloatSocket", "Frame", "frame")
+        self.inputs.new("an_FloatSocket", "Frame", "frame").hide = True
         self.outputs.new("an_FloatSocket", "Strength", "strength")
+
+    def draw(self, layout):
+        layout.prop(self, "frameType", text = "Type")
 
     def drawAdvanced(self, layout):
         layout.prop(bpy.context.scene, "sync_mode")
@@ -17,4 +34,6 @@ class EvaluateSoundNode(bpy.types.Node, AnimationNode):
     def execute(self, sound, frame):
         if sound is None: return 0
         if sound.type != "Single": return 0
+        if self.frameType == "OFFSET":
+            frame += bpy.context.scene.frame_current
         return sound.evaluate(int(frame))
