5c95c943b69dc5aa6ff5a3e0c6267de76fd154f0
Jacques Lucke <mail@jlucke.com>
2015-09-10 20:05:13 +0200

    fix and cleanup mesh data on object node

--- nodes/mesh/set_mesh_data_on_object.py
+++ nodes/mesh/set_mesh_data_on_object.py
@@ -1,10 +1,16 @@
 import bpy
 import bmesh
+import itertools
+from bpy.props import *
 from ... base_types.node import AnimationNode
 
 class SetMeshDataOnObjectNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SetMeshDataOnObjectNode"
     bl_label = "Set Mesh Data on Object"
+
+    errorMessage = StringProperty()
+    checkIndices = BoolProperty(name = "Check Indices", default = True,
+        description = "Check that the highest edge or polygon index is below the vertex amount")
 
     def create(self):
         socket = self.inputs.new("an_ObjectSocket", "Object", "object")
@@ -13,12 +19,30 @@ class SetMeshDataOnObjectNode(bpy.types.Node, AnimationNode):
         self.inputs.new("an_MeshDataSocket", "Mesh Data", "meshData")
         self.outputs.new("an_ObjectSocket", "Object", "object")
 
+    def draw(self, layout):
+        if self.errorMessage != "":
+            layout.label(self.errorMessage, icon = "ERROR")
+
+    def drawAdvanced(self, layout):
+        layout.prop(self, "checkIndices")
+
     def execute(self, object, meshData):
         if object is None: return object
-        if object.type == "MESH":
-            if object.mode != "OBJECT":
-                bpy.ops.object.mode_set(mode = "OBJECT")
-            if object.mode == "OBJECT":
-                bmesh.new().to_mesh(object.data)
-                object.data.from_pydata(meshData.vertices, meshData.edges, meshData.polygons)
+        if object.type != "MESH": return object
+        if object.mode != "OBJECT": return object
+        bmesh.new().to_mesh(object.data)
+
+        vertices, edges, polygons = meshData.vertices, meshData.edges, meshData.polygons
+        if self.checkIndices:
+            if not self.areIndicesValid(vertices, edges, polygons):
+                self.errorMessage = "Indices are invalid"
+                return object
+                
+        object.data.from_pydata(vertices, edges, polygons)
+        self.errorMessage = ""
         return object
+
+    def areIndicesValid(self, vertices, edges, polygons):
+        maxEdgeIndex = max(itertools.chain([-1], *edges))
+        maxPolygonIndex = max(itertools.chain([-1], *polygons))
+        return max(maxEdgeIndex, maxPolygonIndex) < len(vertices)
