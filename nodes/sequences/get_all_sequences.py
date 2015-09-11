c554be27a19c88750bbad1c5670e2a8dd4fdabe8
Jacques Lucke <mail@jlucke.com>
2015-09-11 14:28:07 +0200

    sequences from scene

--- nodes/sequences/get_all_sequences.py
+++ nodes/sequences/get_all_sequences.py
@@ -3,11 +3,12 @@ from ... base_types.node import AnimationNode
 
 class GetAllSequencesNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_GetAllSequencesNode"
-    bl_label = "Get all Sequences"
+    bl_label = "Get All Sequences"
 
     def create(self):
+        self.inputs.new("an_SceneSocket", "Scene", "scene").hide = True
         self.outputs.new("an_SequenceListSocket", "Sequences", "sequences")
 
     def getExecutionCode(self):
-        return ("editor = bpy.context.scene.sequence_editor",
-                "sequences = list(editor.sequences) if editor else []")
+        return ("editor = scene.sequence_editor if scene else None",
+                "sequences = getattr(editor, 'sequences', [])")
