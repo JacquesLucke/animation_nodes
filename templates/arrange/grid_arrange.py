e0be7ee376c9da1c25b60bb87cf2a6c0b756d7ba
Jacques Lucke <mail@jlucke.com>
2015-09-12 19:38:33 +0200

    cleanup

--- templates/arrange/grid_arrange.py
+++ templates/arrange/grid_arrange.py
@@ -7,26 +7,26 @@ class GridArrangeObjectsTemplate(bpy.types.Operator, Template):
     bl_label = "Grid Arrange Objects"
 
     def insert(self):
-        xDivisionsNode = self.newNode('an_DataInputNode', x = 0, y = 0, label = 'X Divisions')
+        xDivisionsNode = self.newNode("an_DataInputNode", x = 0, y = 0, label = "X Divisions")
         xDivisionsNode.assignedType = "Integer"
         xDivisionsNode.inputs[0].value = 5
 
-        yDivisionsNode = self.newNode('an_DataInputNode', x = 0, y = -140, label = 'Y Divisions')
+        yDivisionsNode = self.newNode("an_DataInputNode", x = 0, y = -140, label = "Y Divisions")
         yDivisionsNode.assignedType = "Integer"
         yDivisionsNode.inputs[0].value = 5
 
-        gridMeshNode = self.newNode('an_GridMeshNode', x = 300, y = -100)
-        calcAmountNode = self.newNode('an_FloatMathNode', x = 300, y = 100)
-        toIntegerNode = self.newNode('an_FloatToIntegerNode', x = 500, y = 100)
-        objectInstancerNode = self.newNode('an_ObjectInstancerNode', x = 700, y = 100)
+        gridMeshNode = self.newNode("an_GridMeshNode", x = 300, y = -100)
+        calcAmountNode = self.newNode("an_FloatMathNode", x = 300, y = 100)
+        toIntegerNode = self.newNode("an_FloatToIntegerNode", x = 500, y = 100)
+        objectInstancerNode = self.newNode("an_ObjectInstancerNode", x = 700, y = 100)
 
-        invokeSubprogramNode = self.newNode('an_InvokeSubprogramNode', x = 950, y = -25)
+        invokeSubprogramNode = self.newNode("an_InvokeSubprogramNode", x = 950, y = -25)
 
-        loopInputNode = self.newNode('an_LoopInputNode', x = 550, y = -260)
+        loopInputNode = self.newNode("an_LoopInputNode", x = 550, y = -260)
         loopInputNode.newIterator("Object List")
         loopInputNode.newIterator("Vector List")
 
-        transformsOutputNode = self.newNode('an_ObjectTransformsOutputNode', x = 850, y = -215)
+        transformsOutputNode = self.newNode("an_ObjectTransformsOutputNode", x = 850, y = -215)
         transformsOutputNode.useLocation = [True] * 3
 
         invokeSubprogramNode.subprogramIdentifier = loopInputNode.identifier
