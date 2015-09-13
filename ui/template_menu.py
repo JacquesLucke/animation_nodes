1d6f11db8be7a6231b2a21fe88ede51425f28e95
Jacques Lucke <mail@jlucke.com>
2015-09-13 15:43:29 +0200

    network from particles template

--- ui/template_menu.py
+++ ui/template_menu.py
@@ -9,6 +9,7 @@ class TemplatesMenu(bpy.types.Menu):
         layout.operator("an.empty_subprogram_template")
         layout.operator("an.grid_arrange_objects_template")
         layout.operator("an.random_vertices_offset_template")
+        layout.operator("an.network_from_particles_template")
 
 class TemplatesMenuInHeader(bpy.types.Header):
     bl_idname = "an_templates_menu_in_header"
