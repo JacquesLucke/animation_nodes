04e1bbcaae5d6a65996e94e83a342254b24a1568
Jacques Lucke <mail@jlucke.com>
2015-09-13 10:43:24 +0200

    profile updating

--- operators/profiling.py
+++ operators/profiling.py
@@ -1,5 +1,6 @@
 import bpy
 import cProfile
+from .. update import updateEverything
 
 class ProfileMainUnitExecution(bpy.types.Operator):
     bl_idname = "an.profile_main_unit_execution"
@@ -14,4 +15,14 @@ class ProfileMainUnitExecution(bpy.types.Operator):
     def execute(self, context):
         d = {"context" : context}
         cProfile.runctx("context.space_data.edit_tree.execute()", d, d, sort = "cumtime")
+        return {"FINISHED"}
+
+class ProfileUpdating(bpy.types.Operator):
+    bl_idname = "an.profile_updating"
+    bl_label = "Profile Updating"
+    bl_description = ""
+
+    def execute(self, context):
+        d = {"update" : updateEverything}
+        cProfile.runctx("update()", d, d, sort = "cumtime")
         return {"FINISHED"}
