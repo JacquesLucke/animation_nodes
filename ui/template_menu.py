17142b318ef354b617b64612d038d39df4678375
Jacques Lucke <mail@jlucke.com>
2015-09-13 23:03:56 +0200

    transform individual polygons template

--- ui/template_menu.py
+++ ui/template_menu.py
@@ -10,6 +10,7 @@ class TemplatesMenu(bpy.types.Menu):
         layout.operator("an.grid_arrange_objects_template")
         layout.operator("an.random_vertices_offset_template")
         layout.operator("an.network_from_particles_template")
+        layout.operator("an.transform_individual_polygons_template")
 
 class TemplatesMenuInHeader(bpy.types.Header):
     bl_idname = "an_templates_menu_in_header"
