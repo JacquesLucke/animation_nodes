d828be2e035bb71db8b4e0a14d1dd0034dab10b2
Jacques Lucke <mail@jlucke.com>
2015-09-15 20:07:54 +0200

    speedup object attribute input node

--- nodes/object/object_attribute_input.py
+++ nodes/object/object_attribute_input.py
@@ -1,20 +1,45 @@
 import bpy
+from bpy.props import *
+from ... utils.code import isCodeValid
+from ... events import executionCodeChanged
 from ... base_types.node import AnimationNode
 
 class ObjectAttributeInputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ObjectAttributeInputNode"
     bl_label = "Object Attribute Input"
 
+    attribute = StringProperty(name = "Attribute", default = "",
+        update = executionCodeChanged)
+
+    errorMessage = StringProperty()
+
     def create(self):
+        self.width = 160
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
-        self.inputs.new("an_StringSocket", "Path", "path")
         self.outputs.new("an_GenericSocket", "Value", "value")
 
-    def execute(self, object, path):
-        try:
-            if path.startswith("["):
-                return eval("object" + path)
-            else:
-                return eval("object." + path)
-        except:
-            return None
+    def draw(self, layout):
+        layout.prop(self, "attribute", text = "")
+        if self.errorMessage != "":
+            layout.label(self.errorMessage, icon = "ERROR")
+
+    def getExecutionCode(self):
+        code = self.evaluationExpression
+
+        if not isCodeValid(code):
+            self.errorMessage = "Invalid Syntax"
+            yield "value = None"
+            return
+        else: self.errorMessage = ""
+
+        yield "try:"
+        yield "    self.errorMessage = ''"
+        yield "    " + code
+        yield "except:"
+        yield "    if object: self.errorMessage = 'Attribute not found'"
+        yield "    value = None"
+
+    @property
+    def evaluationExpression(self):
+        if self.attribute.startswith("["): return "value = object" + self.attribute
+        else: return "value = object." + self.attribute
