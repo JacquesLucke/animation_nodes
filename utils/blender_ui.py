c5f7ded84f5fbc1688e6b8c3359c70a015c7df47
Jacques Lucke <mail@jlucke.com>
2015-09-17 23:46:55 +0200

    don't render while viewport rendering is active

--- utils/blender_ui.py
+++ utils/blender_ui.py
@@ -1,5 +1,15 @@
 import bpy
 
+def iterActiveSpacesByType(type):
+    for space in iterActiveSpaces():
+        if space.type == type:
+            yield space
+
+def iterActiveSpaces():
+    for area in iterAreas():
+        yield area.spaces.active
+
+
 def getAreaWithType(type):
     for area in iterAreasByType(type):
         return area
@@ -10,6 +20,11 @@ def iterAreasByType(type):
             yield area
 
 def iterAreas():
-    for screen in bpy.data.screens:
+    for screen in iterActiveScreens():
         for area in screen.areas:
             yield area
+
+def iterActiveScreens():
+    for windowManager in bpy.data.window_managers:
+        for window in windowManager.windows:
+            yield window.screen
