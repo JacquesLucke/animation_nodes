33587fa177aabc4bd94eefcfa5ffdb8e8e058200
Jacques Lucke <mail@jlucke.com>
2015-09-18 11:48:31 +0200

    transform object and update object matrices nodes

---
+++ nodes/object/utility_nodes/update_matrices.py
@@ -0,0 +1,14 @@
+import bpy
+from .... base_types.node import AnimationNode
+
+class UpdateObjectMatricesNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_UpdateObjectMatricesNode"
+    bl_label = "Update Object Matrices"
+
+    def create(self):
+        self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
+        self.outputs.new("an_ObjectSocket", "Object", "object")
+
+    def getExecutionCode(self):
+        yield "if object:"
+        yield "    object.matrix_world = animation_nodes.utils.math.composeMatrix(object.location, object.rotation_euler, object.scale)"
