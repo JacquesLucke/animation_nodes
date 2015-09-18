a483442d64a050a5c88bd6cfd894f2f287df81fd
Jacques Lucke <mail@jlucke.com>
2015-09-18 11:24:51 +0200

    more search results for the math node

--- nodes/number/float_math.py
+++ nodes/number/float_math.py
@@ -7,57 +7,69 @@ from ... base_types.node import AnimationNode
 
 
 operationItems = [
-    ("ADD", "Add", "A + B"),
-    ("SUBTRACT", "Subtract", "A - B"),
-    ("MULITPLY", "Multiply", "A * B"),
-    ("DIVIDE", "Divide", "A / B"),
-    ("SINE", "Sine", "sin A"),
-    ("COSINE", "Cosine", "cos A"),
-    ("TANGENT", "Tangent", "tan A"),
-    ("ARCSINE", "Arcsine", "asin A"),
-    ("ARCCOSINE", "Arccosine", "acos A"),
-    ("ARCTANGENT", "Arctangent", "atan A"),
-    ("POWER", "Power", "A ** B"),
-    ("LOGARITHM", "Logarithm", "A log B"),
-    ("MINIMUM", "Minimum", "min A B"),
-    ("MAXIMUM", "Maximum", "max A B"),
-    ("ROUND", "Round", "A round B"),
-    ("LESSTHAN", "Less Than", "A < B"),
-    ("GREATHERTHAN", "Greather Than", "A > B"),
-    ("MODULO", "Modulo", "A % B"),
-    ("ABSOLUTE", "Absolute", "abs A"),
-    ("FLOOR", "Floor", "floor A"),
-    ("CEILING", "Ceiling", "ceil A"),
-    ("SQRT", "Square Root", "sqrt A"),
-    ("INVERT", "Inverted", "- A"),
-    ("RECIPROCAL", "Reciprocal", "1 / A")]
+    ("ADD", "Add", "A + B", "", 0),
+    ("SUBTRACT", "Subtract", "A - B", "", 1),
+    ("MULTIPLY", "Multiply", "A * B", "", 2),
+    ("DIVIDE", "Divide", "A / B", "", 3),
+    ("SINE", "Sine", "sin A", "", 4),
+    ("COSINE", "Cosine", "cos A", "", 5),
+    ("TANGENT", "Tangent", "tan A", "", 6),
+    ("ARCSINE", "Arcsine", "asin A", "", 7),
+    ("ARCCOSINE", "Arccosine", "acos A", "", 8),
+    ("ARCTANGENT", "Arctangent", "atan A", "", 9),
+    ("POWER", "Power", "A ** B", "", 10),
+    ("LOGARITHM", "Logarithm", "A log B", "", 11),
+    ("MINIMUM", "Minimum", "min A B", "", 12),
+    ("MAXIMUM", "Maximum", "max A B", "", 13),
+    ("ROUND", "Round", "A round B", "", 14),
+    ("MODULO", "Modulo", "A % B", "", 15),
+    ("ABSOLUTE", "Absolute", "abs A", "", 16),
+    ("FLOOR", "Floor", "floor A", "", 17),
+    ("CEILING", "Ceiling", "ceil A", "", 18),
+    ("SQRT", "Square Root", "sqrt A", "", 19),
+    ("INVERT", "Invert", "- A", "", 20),
+    ("RECIPROCAL", "Reciprocal", "1 / A", "", 21)]
 
 singleInputOperations = ("SINE", "COSINE", "TANGENT", "ARCSINE",
     "ARCCOSINE", "ARCTANGENT", "ABSOLUTE", "FLOOR", "CEILING", "SQRT", "INVERT", "RECIPROCAL")
+
+searchItems = {
+    "Add Numbers" : "ADD",
+    "Subtract Numbers" : "SUBTRACT",
+    "Multiply Numbers" : "MULTIPLY",
+    "Divide Numbers" : "DIVIDE",
+    "Invert Number" : "INVERT",
+    "Reciprocal Number" : "RECIPROCAL" }
 
 
 class FloatMathNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_FloatMathNode"
     bl_label = "Math"
 
+    @classmethod
+    def getSearchTags(cls):
+        tags = []
+        for name, operation in searchItems.items():
+            tags.append((name, {"operation" : repr(operation)}))
+        return tags
+
     def operationChanged(self, context):
         for item in operationItems:
             if item[0] == self.operation:
-                self.labelOperation = item[2]
+                self.operationLabel = item[2]
         self.inputs[1].hide = self.operation in singleInputOperations
         executionCodeChanged()
 
     def outputIntegerChanged(self, context):
         self.recreateOutputSocket()
 
-    operation = EnumProperty(name = "Operation", default = "MULITPLY",
+    operation = EnumProperty(name = "Operation", default = "MULTIPLY",
         items = operationItems, update = operationChanged)
 
     outputInteger = BoolProperty(name = "Output Integer", default = False,
         update = outputIntegerChanged)
 
-    labelOperation = StringProperty(name = "Operation Label", default = "A * B")
-    labelOutputType = StringProperty(name = "Output Type Label", default = "Float")
+    operationLabel = StringProperty(name = "Operation Label", default = "A * B")
 
     def create(self):
         self.inputs.new("an_FloatSocket", "A", "a")
@@ -68,7 +80,7 @@ class FloatMathNode(bpy.types.Node, AnimationNode):
         layout.prop(self, "operation", text = "")
 
     def drawLabel(self):
-        return self.labelOperation + " (" + self.labelOutputType + ")"
+        return "{} ({})".format(self.operationLabel, "Integer" if self.outputInteger else "Float")
 
     def edit(self):
         targets = self.outputs[0].dataTargets
@@ -83,7 +95,6 @@ class FloatMathNode(bpy.types.Node, AnimationNode):
     def recreateOutputSocket(self):
         idName = "an_IntegerSocket" if self.outputInteger else "an_FloatSocket"
         if self.outputs[0].bl_idname == idName: return
-        self.labelOutputType = "Integer" if self.outputInteger else "Float"
         self._recreateOutputSocket(idName)
 
     @keepNodeLinks
@@ -105,7 +116,8 @@ class FloatMathNode(bpy.types.Node, AnimationNode):
         if op == "ARCCOSINE": yield "result = math.acos(min(max(a, -1), 1))"
         if op == "ARCTANGENT": yield "result = math.atan(a)"
         if op == "POWER": yield "result = math.pow(a, b) if a >= 0 or int(b) == b else 0"
-        if op == "LOGARITHM": yield from ("if b <= 0 or b == 1: result = math.log(a)",
+        if op == "LOGARITHM": yield from ("if a <= 0: result = 0",
+                                          "elif b <= 0 or b == 1: result = math.log(a)",
                                           "else: result = math.log(a, b)")
         if op == "MINIMUM": yield "result = min(a, b)"
         if op == "MAXIMUM": yield "result = max(a, b)"
