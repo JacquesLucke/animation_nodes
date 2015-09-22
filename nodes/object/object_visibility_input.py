bd66414879d092c64edb12c72c5e019ccdd7de49
og76 <ovidiu.grigorescu@gmail.com>
2015-09-22 15:26:41 +0300

    corrections

--- nodes/object/object_visibility_input.py
+++ nodes/object/object_visibility_input.py
@@ -1,11 +1,10 @@
 import bpy
 from ... base_types.node import AnimationNode
 
-class an_ObjectVisibilityInput(bpy.types.Node, AnimationNode):
-    bl_idname = "an_ObjectVisibilityInput"
+class ObjectVisibilityInputNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_ObjectVisibilityInputNode"
     bl_label = "Object Visibility Input"
-    outputUseParameterName = "useOutput"
-    
+
     def create(self):
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
         self.outputs.new("an_BooleanSocket", "Hide", "hide")
@@ -20,7 +19,6 @@ class an_ObjectVisibilityInput(bpy.types.Node, AnimationNode):
         isLinked = self.getLinkedOutputsDict()
         if not any(isLinked.values()): return ""
         lines = []
-        lines.append("hide, hideSelect, hideRender, showName, showAxis, showXray = None, None, None, None, None, None")
         lines.append("if object is not None:")
         if isLinked["hide"]: lines.append("    hide = object.hide")
         if isLinked["hideSelect"]: lines.append("    hideSelect = object.hide_select")
@@ -28,5 +26,6 @@ class an_ObjectVisibilityInput(bpy.types.Node, AnimationNode):
         if isLinked["showName"]: lines.append("    showName = object.show_name")
         if isLinked["showAxis"]: lines.append("    showAxis = object.show_axis")
         if isLinked["showXray"]: lines.append("    showXray = object.show_x_ray")
-        
+        lines.append("else: hide = hideSelect = hideRender = showName = showAxis = showXray = None")
+
         return lines
