5c0bc297fb4be0273f43b6ce4ac6c9b1fd0f7a2b
og76 <ovidiu.grigorescu@gmail.com>
2015-09-22 14:57:13 +0300

    no all scene, no names out
    and other little stuff
    I need the description in the case sensitive prop to show if mouse hover,
    the name does not and I don't use it anyway, the button is just icon

--- nodes/object/object_name_filter.py
+++ nodes/object/object_name_filter.py
@@ -2,19 +2,17 @@ import bpy
 from ... events import executionCodeChanged
 from ... base_types.node import AnimationNode
 
-class an_ObjectNameFilterNode(bpy.types.Node, AnimationNode):
+class ObjectNameFilterNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ObjectNameFilterNode"
     bl_label = "Object Name Filter"
     
     def checkedPropertiesChanged(self, context):
-        self.updateSocketVisibility()
         executionCodeChanged()
     
-    useAllInScene = bpy.props.BoolProperty(default = False, description = "Use All Objects In Scene instead of socket", update = checkedPropertiesChanged)
     useCaseSensitive = bpy.props.BoolProperty(default = False, description = "Use Case Sensitive", update = checkedPropertiesChanged)
     
-    filterType = bpy.props.EnumProperty(items = [("STARTS WITH", "Starts With", "All Objects with names starting with"),
-                                                 ("ENDS WITH", "Ends With", "All Objects with names ending with")],
+    filterType = bpy.props.EnumProperty(items = [("STARTS_WITH", "Starts With", "All Objects with names starting with"),
+                                                 ("ENDS_WITH", "Ends With", "All Objects with names ending with")],
                                         update = checkedPropertiesChanged, default = "STARTS WITH")
     
     def create(self):
@@ -23,21 +21,13 @@ class an_ObjectNameFilterNode(bpy.types.Node, AnimationNode):
         self.inputs.new("an_ObjectListSocket", "Objects", "objects")
         self.inputs.new("an_StringSocket", "Name", "name")
         self.outputs.new("an_ObjectListSocket", "Objects", "objects")
-        self.outputs.new("an_StringListSocket", "Names", "names").hide = True
-        self.updateSocketVisibility()
-        
+
     def draw(self, layout):
         col = layout.column(align = True)
         row = col.row(align = True)
         row.prop(self, "filterType", text = "Type", expand = True)
-
-        row = col.row(align = True)
-        row.prop(self, "useAllInScene", text = "All Scene Objects", icon = "SCENE_DATA")
         row.prop(self, "useCaseSensitive", text = "", icon = "FONTPREVIEW")
         
-    def updateSocketVisibility(self):
-        self.inputs["Objects"].hide = self.useAllInScene
-
     def getExecutionCode(self):
 
         isLinked = self.getLinkedOutputsDict()
@@ -45,22 +35,20 @@ class an_ObjectNameFilterNode(bpy.types.Node, AnimationNode):
         
         lines = []
         lines.append("obList = []")
-        if self.useAllInScene: lines.append("obList = bpy.context.scene.objects")
-        else: lines.append("if objects is not None: obList = objects")
+        lines.append("if objects is not None: obList = objects")
     
-        lines.append("filteredObjects, filteredNames = [], []")
+        lines.append("filteredObjects = []")
         if self.useCaseSensitive: 
-            if self.filterType == "STARTS WITH": 
+            if self.filterType == "STARTS_WITH": 
                 lines.append("filteredObjects = [ob for ob in obList if ob.name.startswith(str(name))]")
-            if self.filterType == "ENDS WITH": 
+            if self.filterType == "ENDS_WITH": 
                 lines.append("filteredObjects = [ob for ob in obList if ob.name.endswith(str(name))]")
         else: 
-            if self.filterType == "STARTS WITH": 
+            if self.filterType == "STARTS_WITH": 
                 lines.append("filteredObjects = [ob for ob in obList if ob.name.lower().startswith(str.lower(name))]")
-            if self.filterType == "ENDS WITH": 
+            if self.filterType == "ENDS_WITH": 
                 lines.append("filteredObjects = [ob for ob in obList if ob.name.lower().endswith(str.lower(name))]")
         
-        lines.append("filteredNames = [ob.name for ob in filteredObjects]")
-        lines.append("objects, names = filteredObjects, filteredNames")
+        lines.append("objects = filteredObjects")
         
         return lines
