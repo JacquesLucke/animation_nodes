b11a31fe0b87aab151ca9ee41edf4ae4d6a937aa
Jacques Lucke <mail@jlucke.com>
2015-09-27 17:25:26 +0200

    color input -> Choose Color node with alpha

--- nodes/color/choose_color.py
+++ nodes/color/choose_color.py
@@ -3,20 +3,21 @@ from bpy.props import *
 from ... events import propertyChanged
 from ... base_types.node import AnimationNode
 
-class ColorInputNode(bpy.types.Node, AnimationNode):
-    bl_idname = "an_ColorInputNode"
-    bl_label = "Color Input"
+class ChooseColorNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_ChooseColorNode"
+    bl_label = "Choose Color"
 
     colorProperty = FloatVectorProperty(
         default = [0.5, 0.5, 0.5], subtype = "COLOR",
         soft_min = 0.0, soft_max = 1.0, update = propertyChanged)
 
     def create(self):
+        self.inputs.new("an_FloatSocket", "Alpha", "alpha").value = 1.0
         self.outputs.new("an_ColorSocket", "Color", "color")
 
     def draw(self, layout):
         layout.template_color_picker(self, "colorProperty", value_slider = True)
 
-    def execute(self):
+    def execute(self, alpha):
         color = self.colorProperty
-        return [color[0], color[1], color[2], 1.0]
+        return [color[0], color[1], color[2], alpha]
