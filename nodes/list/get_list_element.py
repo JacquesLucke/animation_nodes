b06bb8ae1443d815dfdc10b4623674a510dd3307
Jacques Lucke <mail@jlucke.com>
2015-09-27 18:46:39 +0200

    hide fall-back input by default: close #212

--- nodes/list/get_list_element.py
+++ nodes/list/get_list_element.py
@@ -65,5 +65,5 @@ class GetListElementNode(bpy.types.Node, AnimationNode):
         self.outputs.clear()
         self.inputs.new(self.listIdName, "List", "list")
         self.inputs.new("an_IntegerSocket", "Index", "index")
-        self.inputs.new(self.baseIdName, "Fallback", "fallback")
+        self.inputs.new(self.baseIdName, "Fallback", "fallback").hide = True
         self.outputs.new(self.baseIdName, "Element", "element")
