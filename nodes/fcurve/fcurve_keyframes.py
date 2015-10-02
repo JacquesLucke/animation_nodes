edb78eba22c9ff06f8bfd6a832afc0dfa71b09cf
og76 <o.g.blender@gmail.com>
2015-10-02 04:31:59 +0300

    present keyframes of an fcurve as frame and value (lists)
    we need to know keyframe pos and value
    I chose 2 float lists instead of 1 list of pairs so that
    -we access the value directly not with yet another get el
    -we don't add other sockets just for this (and not using generic)
    -sometimes we just need the frame (ex start and end for the range...)

---
+++ nodes/fcurve/fcurve_keyframes.py
@@ -0,0 +1,21 @@
+import bpy
+from ... base_types.node import AnimationNode
+
+class FCurveKeyframesNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_FCurveKeyframesNode"
+    bl_label = "FCurve Keyframes"
+
+    def create(self):
+        self.inputs.new("an_FCurveSocket", "FCurve", "fCurve")
+        self.outputs.new("an_FloatListSocket", "Keyframes Frames", "keyframesFrames")
+        self.outputs.new("an_FloatListSocket", "Keyframes Values", "keyframesValues")
+
+    def getExecutionCode(self):
+        isLinked = self.getLinkedOutputsDict()
+        if not any(isLinked.values()): return
+
+        yield "if fCurve is not None:"
+        yield "    keyframesFrames = keyframesValues = []"
+        yield "    for point in fCurve.keyframe_points:"
+        if isLinked["keyframesFrames"]: yield " " * 8 + "keyframesFrames.append(point.co[0])"
+        if isLinked["keyframesValues"]: yield " " * 8 + "keyframesValues.append(point.co[1])"
