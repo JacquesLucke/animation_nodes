894f3eff954ec19ca107071a5ee1ad4fcec06eb8
Jacques Lucke <mail@jlucke.com>
2015-10-06 12:14:07 +0200

    use center in transform object node

--- nodes/object/utility_nodes/transform_object.py
+++ nodes/object/utility_nodes/transform_object.py
@@ -1,14 +1,30 @@
 import bpy
+from bpy.props import *
+from mathutils import Matrix
+from .... events import propertyChanged
 from .... base_types.node import AnimationNode
 
 class TransformObjectNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TransformObjectNode"
     bl_label = "Transform Object"
 
+    useCenter = BoolProperty(name = "Use Center", default = True,
+        description = "Use the object location as origin", update = propertyChanged)
+
     def create(self):
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
         self.inputs.new("an_MatrixSocket", "Matrix", "matrix")
         self.outputs.new("an_ObjectSocket", "Object", "object")
 
-    def getExecutionCode(self):
-        return "if object: object.matrix_world = matrix * object.matrix_world"
+    def draw(self, layout):
+        layout.prop(self, "useCenter")
+
+    def execute(self, object, matrix):
+        if object is None: return None
+        if self.useCenter:
+            offset = Matrix.Translation(object.location)
+            transformation = offset * matrix * offset.inverted()
+        else:
+            transformation = matrix
+        object.matrix_world = transformation * object.matrix_world
+        return object
--- templates/animation/fcurve_animation_offset.py
+++ templates/animation/fcurve_animation_offset.py
@@ -17,9 +17,9 @@ class FCurveAnimationOffsetTemplate(bpy.types.Operator, Template):
 
         timeInfoNode = self.newNode('an_TimeInfoNode', x = 0, y = -200)
         additionalOffsetNode = self.newNode('an_TranslationMatrixNode', x = 0, y = -325)
-        additionalOffsetNode.inputs[0].value = [2, 0, 2]
-        transformationOffsetNode = self.newNode('an_ComposeMatrixNode', x = 220, y = -325)
-        transformationOffsetNode.inputs[0].value = [3, 0, 0]
+        additionalOffsetNode.inputs[0].value = [4, 3, 0]
+        individualOffsetNode = self.newNode('an_ComposeMatrixNode', x = 220, y = -325)
+        individualOffsetNode.inputs[0].value = [3, 0, 0]
         delayTimeNode = self.newNode('an_DelayTimeNode', x = 220, y = -200)
         objectInstancer = self.newNode('an_ObjectInstancerNode', x = 220, y = 150)
         objectInstancer.copyObjectProperties = True
@@ -36,7 +36,7 @@ class FCurveAnimationOffsetTemplate(bpy.types.Operator, Template):
         animationOffsetGroupInputNode.newParameter('Float', name = 'Frame')
         animationOffsetGroupInputNode.newParameter('Float', name = 'Time Offset').value = 10
         animationOffsetGroupInputNode.newParameter('Matrix', name = 'Additional Offset')
-        animationOffsetGroupInputNode.newParameter('Matrix', name = 'Transformation Offset')
+        animationOffsetGroupInputNode.newParameter('Matrix', name = 'Individual Offset')
 
         fCurvesFromObjectNode = self.newNode('an_FCurvesFromObjectNode', x = 250, y = -775)
         invokeObjectLoopNode = self.newNode('an_InvokeSubprogramNode', x = 470, y = -745)
@@ -54,21 +54,23 @@ class FCurveAnimationOffsetTemplate(bpy.types.Operator, Template):
         objectLoopInputNode.newParameter('Float', name = 'Frame')
         objectLoopInputNode.newParameter('Float', name = 'Time Offset')
         objectLoopInputNode.newParameter('Matrix', name = 'Additional Offset')
-        objectLoopInputNode.newParameter('Matrix', name = 'Transformation Offset')
+        objectLoopInputNode.newParameter('Matrix', name = 'Individual Offset')
         accumulatedTransformationSocket = objectLoopInputNode.newParameter('Matrix', name = 'Accumulated Transformation')
         accumulatedTransformationSocket.loop.useAsInput = False
 
         multiplyIndexAndOffsetNode = self.newNode('an_FloatMathNode', x = 350, y = -1200)
         subtractOffsetFromFrameNode = self.newNode('an_FloatMathNode', x = 550, y = -1200)
         subtractOffsetFromFrameNode.operation = "SUBTRACT"
-        calculateAccumulatedMatrixNode = self.newNode('an_MatrixMathNode', x = 350, y = -1530)
-        reassignAccumulatedMatrixNode = self.newNode('an_ReassignLoopParameterNode', x = 540, y = -1575)
+        calculateAccumulatedMatrixNode = self.newNode('an_MatrixMathNode', x = 350, y = -1580)
+        reassignAccumulatedMatrixNode = self.newNode('an_ReassignLoopParameterNode', x = 540, y = -1580)
         copyTransformsNode = self.newNode('an_CopyTransformsNode', x = 550, y = -1340)
         copyTransformsNode.width = 140
         invokeCopyFCurvesNode = self.newNode('an_InvokeSubprogramNode', x = 760, y = -1200)
         updateMatrixNode = self.newNode('an_UpdateObjectMatricesNode', x = 1030, y = -1330)
         localTransformNode = self.newNode('an_TransformObjectNode', x = 1230, y = -1370)
+        localTransformNode.useCenter = True
         additionalTransformNode = self.newNode('an_TransformObjectNode', x = 1450, y = -1415)
+        additionalTransformNode.useCenter = False
 
         # Copy FCurves Loop
 
@@ -106,7 +108,7 @@ class FCurveAnimationOffsetTemplate(bpy.types.Operator, Template):
         self.newLink(objectInstancer.outputs[0], invokeAnimationOffsetNode.inputs[1])
         self.newLink(delayTimeNode.outputs[0], invokeAnimationOffsetNode.inputs[2])
         self.newLink(additionalOffsetNode.outputs[0], invokeAnimationOffsetNode.inputs[4])
-        self.newLink(transformationOffsetNode.outputs[0], invokeAnimationOffsetNode.inputs[5])
+        self.newLink(individualOffsetNode.outputs[0], invokeAnimationOffsetNode.inputs[5])
 
         # Animation Offset Group
 
@@ -131,9 +133,9 @@ class FCurveAnimationOffsetTemplate(bpy.types.Operator, Template):
         self.newLink(objectLoopInputNode.outputs[8], additionalTransformNode.inputs[1])
         self.newLink(objectLoopInputNode.outputs[9], calculateAccumulatedMatrixNode.inputs[0])
         self.newLink(objectLoopInputNode.outputs[10], calculateAccumulatedMatrixNode.inputs[1])
+        self.newLink(objectLoopInputNode.outputs[10], localTransformNode.inputs[1])
         self.newLink(multiplyIndexAndOffsetNode.outputs[0], subtractOffsetFromFrameNode.inputs[1])
         self.newLink(subtractOffsetFromFrameNode.outputs[0], invokeCopyFCurvesNode.inputs[2])
-        self.newLink(calculateAccumulatedMatrixNode.outputs[0], localTransformNode.inputs[1])
         self.newLink(calculateAccumulatedMatrixNode.outputs[0], reassignAccumulatedMatrixNode.inputs[0])
         self.newLink(copyTransformsNode.outputs[0], invokeCopyFCurvesNode.inputs[1])
         self.newLink(invokeCopyFCurvesNode.outputs[0], updateMatrixNode.inputs[0])
