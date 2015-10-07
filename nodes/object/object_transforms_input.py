cf41cb38bd392b9ae1ab3841a6cd25e8ce42c31d
Jacques Lucke <mail@jlucke.com>
2015-10-07 13:38:10 +0200

    use euler socket in transforms in/out nodes #239

--- nodes/object/object_transforms_input.py
+++ nodes/object/object_transforms_input.py
@@ -30,7 +30,7 @@ class ObjectTransformsInputNode(bpy.types.Node, AnimationNode):
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
         self.inputs.new("an_FloatSocket", "Frame", "frame").hide = True
         self.outputs.new("an_VectorSocket", "Location", "location")
-        self.outputs.new("an_VectorSocket", "Rotation", "rotation")
+        self.outputs.new("an_EulerSocket", "Rotation", "rotation")
         self.outputs.new("an_VectorSocket", "Scale", "scale")
         self.outputs.new("an_QuaternionSocket", "Quaternion", "quaternion").hide = True
 
@@ -51,20 +51,20 @@ class ObjectTransformsInputNode(bpy.types.Node, AnimationNode):
         yield "try:"
         if self.useCurrentTransforms:
             if isLinked["location"]: yield "    location = object.location"
-            if isLinked["rotation"]: yield "    rotation = mathutils.Vector(object.rotation_euler)"
+            if isLinked["rotation"]: yield "    rotation = object.rotation_euler"
             if isLinked["scale"]:    yield "    scale = object.scale"
             if isLinked["quaternion"]: yield "    quaternion = object.rotation_quaternion"
         else:
             if self.frameType == "OFFSET": yield "    evaluationFrame = frame + self.nodeTree.scene.frame_current_final"
             else: yield "    evaluationFrame = frame"
             if isLinked["location"]: yield "    location = mathutils.Vector(animation_nodes.utils.fcurve.getArrayValueAtFrame(object, 'location', evaluationFrame))"
-            if isLinked["rotation"]: yield "    rotation = mathutils.Vector(animation_nodes.utils.fcurve.getArrayValueAtFrame(object, 'rotation_euler', evaluationFrame))"
+            if isLinked["rotation"]: yield "    rotation = mathutils.Euler(animation_nodes.utils.fcurve.getArrayValueAtFrame(object, 'rotation_euler', evaluationFrame))"
             if isLinked["scale"]:    yield "    scale = mathutils.Vector(animation_nodes.utils.fcurve.getArrayValueAtFrame(object, 'scale', evaluationFrame))"
             if isLinked["quaternion"]:    yield "    quaternion = mathutils.Quaternion(animation_nodes.utils.fcurve.getArrayValueAtFrame(object, 'rotation_quaternion', evaluationFrame, arraySize = 4))"
 
         yield "except:"
         yield "    location = mathutils.Vector((0, 0, 0))"
-        yield "    rotation = mathutils.Vector((0, 0, 0))"
+        yield "    rotation = mathutils.Euler((0, 0, 0))"
         yield "    scale = mathutils.Vector((0, 0, 0))"
         yield "    quaternion = mathutils.Quaternion((1, 0, 0, 0))"
 
--- nodes/object/object_transforms_output.py
+++ nodes/object/object_transforms_output.py
@@ -18,7 +18,7 @@ class an_ObjectTransformsOutputNode(bpy.types.Node, AnimationNode):
     def create(self):
         self.inputs.new("an_ObjectSocket", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
         self.inputs.new("an_VectorSocket", "Location", "location")
-        self.inputs.new("an_VectorSocket", "Rotation", "rotation")
+        self.inputs.new("an_EulerSocket", "Rotation", "rotation")
         self.inputs.new("an_VectorSocket", "Scale", "scale").value = (1, 1, 1)
         self.outputs.new("an_ObjectSocket", "Object", "object")
         self.updateSocketVisibility()
