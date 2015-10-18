2e5d445b81f1ea9fc31f484e39086bdb66da0c77
Jacques Lucke <mail@jlucke.com>
2015-10-18 19:10:16 +0200

    minor speedup

--- base_types/socket.py
+++ base_types/socket.py
@@ -191,6 +191,7 @@ class AnimationNodeSocket:
         else: return self.nodeTree.links.new(self, socket)
 
     def removeLinks(self):
+        if not self.is_linked: return
         tree = self.nodeTree
         for link in self.links:
             tree.links.remove(link)
