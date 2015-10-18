8451d31f29f7ddfc7c94eff32baa5a971bea60b9
Jacques Lucke <mail@jlucke.com>
2015-10-18 18:26:40 +0200

    version bump and remove warning

--- __init__.py
+++ __init__.py
@@ -23,11 +23,11 @@ bl_info = {
     "name":        "Animation Nodes",
     "description": "Node system for more flexible animations.",
     "author":      "Jacques Lucke",
-    "version":     (0, 1, 0),
+    "version":     (1, 0, 0),
     "blender":     (2, 7, 6),
     "location":    "Node Editor",
     "category":    "Node",
-    "warning":     "Stable, but some things may change in the future."
+    "warning":     ""
     }
 
 
