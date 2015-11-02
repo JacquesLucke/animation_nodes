86b0e814f6012c8c739b37a752fc4a5dfe706115
Jacques Lucke <mail@jlucke.com>
2015-11-02 23:14:02 +0100

    cleanup node link conversion code

--- node_link_conversion.py
+++ node_link_conversion.py
@@ -38,11 +38,32 @@ class LinkCorrection:
     # subclasses need a check and insert function
     pass
 
-class ConvertNumberToEuler(LinkCorrection):
+class SimpleConvert(LinkCorrection):
+    rules = {
+        ("Boolean", "Integer") : "an_BooleanToIntegerNode",
+        ("Boolean", "Float") : "an_BooleanToIntegerNode",
+        ("Float", "Integer") : "an_FloatToIntegerNode",
+        ("Vector", "Matrix") : "an_TranslationMatrixNode",
+        ("Text Block", "String") : "an_TextBlockReaderNode",
+        ("Vector", "Float") : "an_SeparateVectorNode",
+        ("Float", "Vector") : "an_CombineVectorNode",
+        ("Integer", "Vector") : "an_CombineVectorNode",
+        ("Integer List", "Polygon Indices") : "an_CreatePolygonIndicesNode",
+        ("Polygon Indices List", "Edge Indices List") : "an_EdgesOfPolygonsNode",
+        ("Vector List", "Mesh Data") : "an_CombineMeshDataNode",
+        ("Mesh Data", "Vector List") : "an_SeparateMeshDataNode",
+        ("Mesh Data", "BMesh") : "an_CreateBMeshFromMeshData",
+        ("Particle System", "Particle List") : "an_GetParticlesNode",
+        ("Integer", "Euler") : "an_CombineEulerNode",
+        ("Float", "Euler") : "an_CombineEulerNode",
+        ("Euler", "Float") : "an_SeparateEulerNode"
+    }
+
     def check(self, origin, target):
-        return origin.dataType in ["Integer", "Float"] and target.dataType == "Euler"
+        return (origin.dataType, target.dataType) in self.rules
     def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_CombineEulerNode", origin, target)
+        nodeIdName = self.rules[(dataOrigin.dataType, target.dataType)]
+        node = insertLinkedNode(nodeTree, nodeIdName, origin, target)
 
 class ConvertVectorToEuler(LinkCorrection):
     def check(self, origin, target):
@@ -80,22 +101,6 @@ class ConvertQuaternionToEuler(LinkCorrection):
         node.inputs[0].linkWith(origin)
         node.outputs[0].linkWith(target)
 
-class ConvertParticleSystemToParticle(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Particle System" and target.dataType == "Particle"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        systemInfo, listElement = insertNodes(nodeTree, ["an_GetParticlesNode", "an_GetListElementNode"], origin, target)
-        listElement.generateSockets(listIdName = "an_ParticleListSocketNode")
-        nodeTree.links.new(systemInfo.inputs[0], origin)
-        nodeTree.links.new(listElement.inputs[0], systemInfo.outputs[0])
-        nodeTree.links.new(listElement.outputs[0], target)
-
-class ConvertParticleSystemToParticles(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Particle System" and target.dataType == "Particle List"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_GetParticlesNode", origin, target)
-
 class ConvertListToElement(LinkCorrection):
     def check(self, origin, target):
         return toBaseIdName(origin.bl_idname) == target.bl_idname
@@ -112,35 +117,6 @@ class ConvertElementToList(LinkCorrection):
         node.assignBaseDataType(origin.dataType, inputAmount = 1)
         insertBasicLinking(nodeTree, origin, node, target)
 
-class ConvertMeshDataToMesh(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Mesh Data" and target.dataType == "BMesh"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_CreateBMeshFromMeshData", origin, target)
-
-class ConvertMeshDataToVertexLocations(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Mesh Data" and target.dataType == "Vector List"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_SeparateMeshDataNode", origin, target)
-
-class ConvertVertexLocationsToMeshData(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Vector List" and target.dataType == "Mesh Data"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_CombineMeshDataNode", origin, target)
-
-class ConvertPolygonListIndicesToEdgeListIndices(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Polygon Indices List" and target.dataType == "Edge Indices List"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_EdgesOfPolygonsNode", origin, target)
-
-class ConvertIntegerListToPolygonIndices(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Integer List" and target.dataType == "Polygon Indices"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_CreatePolygonIndicesNode", origin, target)
 
 class ConvertSeparatedMeshDataToBMesh(LinkCorrection):
     separatedMeshDataTypes = ["Vector List", "Edge Indices List", "Polygon Indices List"]
@@ -152,53 +128,17 @@ class ConvertSeparatedMeshDataToBMesh(LinkCorrection):
         nodeTree.links.new(toMesh.inputs[0], toMeshData.outputs[0])
         nodeTree.links.new(toMesh.outputs[0], target)
 
-class ConvertNumberToVector(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType in ["Integer", "Float"] and target.dataType == "Vector"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_CombineVectorNode", origin, target)
-
-class ConvertVectorToNumber(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Vector" and target.dataType == "Float"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_SeparateVectorNode", origin, target)
-
-class ConvertTextBlockToString(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Text Block" and target.dataType == "String"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_TextBlockReaderNode", origin, target)
-
-class ConvertVectorToMatrix(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Vector" and target.dataType == "Matrix"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        insertLinkedNode(nodeTree, "an_TranslationMatrixNode", origin, target)
-
 class ConvertListToLength(LinkCorrection):
     def check(self, origin, target):
         return "List" in origin.dataType and target.dataType == "Integer"
     def insert(self, nodeTree, origin, target, dataOrigin):
         insertLinkedNode(nodeTree, "an_GetListLengthNode", origin, target)
 
-class ConverFloatToInteger(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Float" and target.dataType == "Integer"
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        node = insertLinkedNode(nodeTree, "an_FloatToIntegerNode", origin, target)
-
 class ConvertToString(LinkCorrection):
     def check(self, origin, target):
         return target.dataType == "String"
     def insert(self, nodeTree, origin, target, dataOrigin):
         node = insertLinkedNode(nodeTree, "an_ConvertToStringNode", origin, target)
-
-class ConvertBooleanToInteger(LinkCorrection):
-    def check(self, origin, target):
-        return origin.dataType == "Boolean" and target.dataType in ("Integer", "Float")
-    def insert(self, nodeTree, origin, target, dataOrigin):
-        node = insertLinkedNode(nodeTree, "an_BooleanToIntegerNode", origin, target)
 
 class ConvertFromGenericList(LinkCorrection):
     def check(self, origin, target):
@@ -244,28 +184,14 @@ def getSocketCenter(socket1, socket2):
     return (socket1.node.viewLocation + socket2.node.viewLocation) / 2
 
 linkCorrectors = [
-    ConvertNumberToEuler(),
     ConvertVectorToEuler(),
     ConvertEulerToVector(),
     ConvertEulerToQuaternion(),
     ConvertQuaternionToEuler(),
-    ConvertParticleSystemToParticle(),
-    ConvertParticleSystemToParticles(),
     ConvertListToElement(),
     ConvertElementToList(),
-    ConvertMeshDataToMesh(),
-    ConvertMeshDataToVertexLocations(),
-    ConvertVertexLocationsToMeshData(),
-    ConvertPolygonListIndicesToEdgeListIndices(),
-    ConvertIntegerListToPolygonIndices(),
-    ConvertSeparatedMeshDataToBMesh(),
-    ConvertNumberToVector(),
-    ConvertVectorToNumber(),
-    ConvertTextBlockToString(),
-    ConvertVectorToMatrix(),
 	ConvertListToLength(),
-    ConverFloatToInteger(),
+    SimpleConvert(),
     ConvertToString(),
-    ConvertBooleanToInteger(),
     ConvertFromGenericList(),
     ConvertFromGeneric() ]
