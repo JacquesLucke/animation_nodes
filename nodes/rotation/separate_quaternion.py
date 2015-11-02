d7001b990f39be9e884a8993798310ad3fd72696
og76 <ovidiu.grigorescu@gmail.com>
2015-11-02 22:34:25 +0200

    Create separate_quaternion.py
    also no mathutils

---
+++ nodes/rotation/separate_quaternion.py
@@ -0,0 +1,16 @@
+import bpy
+from ... base_types.node import AnimationNode
+
+class SeparateQuaternionNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_SeparateQuaternionNode"
+    bl_label = "Separate Quaternion"
+
+    def create(self):
+        self.inputs.new("an_QuaternionSocket", "Quaternion", "quaternion")
+        self.outputs.new("an_FloatSocket", "W", "w")
+        self.outputs.new("an_FloatSocket", "X", "x")
+        self.outputs.new("an_FloatSocket", "Y", "y")
+        self.outputs.new("an_FloatSocket", "Z", "z")
+        
+    def getExecutionCode(self):
+        return "w, x, y, z = quaternion"
