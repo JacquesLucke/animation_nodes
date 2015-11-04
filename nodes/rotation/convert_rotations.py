3250656e207b99ea11d4c4c5f1fe93c63e55ce9a
og76 <ovidiu.grigorescu@gmail.com>
2015-11-04 22:52:25 +0200

    Update convert_rotations.py
    adding axis angle / quaternion

--- nodes/rotation/convert_rotations.py
+++ nodes/rotation/convert_rotations.py
@@ -10,7 +10,9 @@ conversionTypeItems = [
     ("QUATERNION_TO_MATRIX", "Quaternion to Matrix", "", "NONE", 2),
     ("MATRIX_TO_QUATERNION", "Matrix to Quaternion", "", "NONE", 3),
     ("EULER_TO_MATRIX", "Euler to Matrix", "", "NONE", 4),
-    ("MATRIX_TO_EULER", "Matrix to Euler", "", "NONE", 5)]
+    ("MATRIX_TO_EULER", "Matrix to Euler", "", "NONE", 5),
+    ("QUATERNION_TO_AXIS_ANGLE", "Quaternion to Axis Angle", "", "NONE", 6),
+    ("AXIS_ANGLE_TO_QUATERNION", "Axis Angle to Quaternion", "", "NONE", 7) ]
 
 class ConvertRotationsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ConvertRotationsNode"
@@ -25,6 +27,7 @@ class ConvertRotationsNode(bpy.types.Node, AnimationNode):
 
     conversionType = EnumProperty(name = "Conversion Type", default = "QUATERNION_TO_EULER",
         items = conversionTypeItems, update = conversionTypeChanged)
+    useDegree = BoolProperty(name = "Use Degree", default = False, update = executionCodeChanged)
 
     def create(self):
         self.width = 160
@@ -32,6 +35,7 @@ class ConvertRotationsNode(bpy.types.Node, AnimationNode):
 
     def draw(self, layout):
         layout.prop(self, "conversionType", text = "")
+        if "ANGLE" in self.conversionType: layout.prop(self, "useDegree")
 
     def drawLabel(self):
         for item in conversionTypeItems:
@@ -53,8 +57,15 @@ class ConvertRotationsNode(bpy.types.Node, AnimationNode):
         if self.conversionType == "MATRIX_TO_EULER":
             return "euler = matrix.to_euler('XYZ')"
 
+        if self.conversionType == "QUATERNION_TO_AXIS_ANGLE":
+            if self.useDegree: return "axis, angle = quaternion.axis, math.degrees(quaternion.angle)"
+            else: return "axis, angle = quaternion.to_axis_angle()"
+        if self.conversionType == "AXIS_ANGLE_TO_QUATERNION":
+            if self.useDegree: return "quaternion = mathutils.Quaternion(axis, math.radians(angle))"
+            else: return "quaternion = mathutils.Quaternion(axis, angle)"
+
     def getUsedModules(self):
-        return ["mathutils"]
+        return ["mathutils", "math"]
 
     @keepNodeLinks
     def createSockets(self):
@@ -81,5 +92,14 @@ class ConvertRotationsNode(bpy.types.Node, AnimationNode):
         if self.conversionType == "MATRIX_TO_EULER":
             self.inputs.new("an_MatrixSocket", "Matrix", "matrix")
             self.outputs.new("an_EulerSocket", "Euler", "euler")
+
+        if self.conversionType == "QUATERNION_TO_AXIS_ANGLE":
+            self.inputs.new("an_QuaternionSocket", "Quaternion", "quaternion")
+            self.outputs.new("an_VectorSocket", "Axis", "axis")
+            self.outputs.new("an_FloatSocket", "Angle", "angle")
+        if self.conversionType == "AXIS_ANGLE_TO_QUATERNION":
+            self.inputs.new("an_VectorSocket", "Axis", "axis")
+            self.inputs.new("an_FloatSocket", "Angle", "angle")
+            self.outputs.new("an_QuaternionSocket", "Quaternion", "quaternion")
 
         self.inputs[0].defaultDrawType = "PREFER_PROPERTY"
