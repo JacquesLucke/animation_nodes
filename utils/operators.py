9346788e6a53c3db49b9248d36c053428ccb4b02
Jacques Lucke <mail@jlucke.com>
2015-11-08 15:38:14 +0100

    allow arguments in dynamically defined operators

--- utils/operators.py
+++ utils/operators.py
@@ -1,25 +1,42 @@
 import bpy
+import inspect
+from bpy.props import *
 
 createdOperators = []
 
-def makeOperator(idName, label):
+def makeOperator(idName, label, arguments = []):
     def makeOperatorDecorator(function):
-        operator = getOperatorForFunction(function, idName, label)
+        operator = getOperatorForFunction(function, idName, label, arguments)
         bpy.utils.register_class(operator)
         createdOperators.append(operator)
         return function
     return makeOperatorDecorator
 
-def getOperatorForFunction(function, idName, label):
+def getOperatorForFunction(function, idName, label, arguments = []):
     def execute(self, context):
-        function()
+        parameters = list(iterParameterNamesAndDefaults(function))
+        function(*[getattr(self, name) for name, _ in parameters])
         return {"FINISHED"}
 
     operator = type(idName, (bpy.types.Operator, ), {
         "bl_idname" : idName,
         "bl_label" : label,
         "execute" : execute })
+
+    parameters = list(iterParameterNamesAndDefaults(function))
+    for argument, (name, default) in zip(arguments, parameters):
+        if argument == "Int": propertyType = IntProperty
+        if argument == "String": propertyType = StringProperty
+
+        if default is None: setattr(operator, name, propertyType())
+        else: setattr(operator, name, propertyType(default = default))
+
     return operator
+
+def iterParameterNamesAndDefaults(function):
+    for parameter in inspect.signature(function).parameters.values():
+        default = parameter.default if isinstance(parameter.default, (int, float, str)) else None
+        yield (parameter.name, default)
 
 def register():
     for operator in createdOperators:
