d4ef8531349e060181812acf911f1b7ab1265886
Jacques Lucke <mail@jlucke.com>
2015-11-13 15:03:58 +0100

    show less decimals in debug drawer node

--- utils/pretty_strings.py
+++ utils/pretty_strings.py
@@ -1,11 +1,11 @@
 def formatVector(vector):
-    return "V({:>8.4f}, {:>8.4f}, {:>8.4f})".format(*vector)
+    return "V({:>7.3f}, {:>7.3f}, {:>7.3f})".format(*vector)
 
 def formatEuler(euler):
-    return "E({:>8.4f}, {:>8.4f}, {:>8.4f})".format(*euler)
+    return "E({:>7.3f}, {:>7.3f}, {:>7.3f})".format(*euler)
 
 def formatQuaternion(quaternion):
-    return "Q({:>7.4f}, {:>7.4f}, {:>7.4f}, {:>7.4f})".format(*quaternion)
+    return "Q({:>7.3f}, {:>7.3f}, {:>7.3f}, {:>7.3f})".format(*quaternion)
 
 def formatFloat(number):
     return "{:>10.5f}".format(number)
