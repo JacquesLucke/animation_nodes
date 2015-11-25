6ca829df24079910243e2835ffc9afba172824e5
Jacques Lucke <mail@jlucke.com>
2015-11-25 20:24:28 +0100

    Create invoke node with W-pie-menu #340

--- ui/node_pie.py
+++ ui/node_pie.py
@@ -1,6 +1,7 @@
 import bpy
 from .. sockets.info import isList
 from .. utils.blender_ui import PieMenuHelper
+from .. nodes.system.subprogram_base import SubprogramBaseNode
 
 '''
                 ###############
@@ -35,10 +36,24 @@ class ContextPie(bpy.types.Menu, PieMenuHelper):
         else: layout.operator("an.insert_debug_node_template_operator", text = "Debug")
 
     def drawRight(self, layout):
+        col = layout.column(align = False)
+        self.insertInvokeNodeTemplate(col)
+        self.insertLoopTemplate(col)
+
+    def insertInvokeNodeTemplate(self, layout):
+        col = layout.column(align = True)
+        if getattr(self.activeNode, "isSubprogramNode", False):
+            props = col.operator("an.insert_invoke_node_template_operator",
+                text = "Create Invoke Node", icon = "GROUP_VERTEX")
+            props.subprogramIdentifier = self.activeNode.identifier
+
+    def insertLoopTemplate(self, layout):
         col = layout.column(align = True)
         for socket in self.activeNode.outputs:
             if isList(socket.bl_idname):
-                props = col.operator("an.insert_loop_for_iteration_template", text = "Loop through {}".format(repr(socket.getDisplayedName())))
+                props = col.operator("an.insert_loop_for_iteration_template",
+                    text = "Loop through {}".format(repr(socket.getDisplayedName())),
+                    icon = "MOD_ARRAY")
                 props.nodeIdentifier = self.activeNode.identifier
                 props.socketIndex = socket.index
 
