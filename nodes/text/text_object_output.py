9fe7668814b5897f94405cf5808e062c578ca493
Jacques Lucke <mail@jlucke.com>
2016-01-14 21:09:51 +0100

    set bold, italic and bold italic font using the Text Object Output node #354

--- nodes/text/text_object_output.py
+++ nodes/text/text_object_output.py
@@ -29,6 +29,9 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
         self.inputs.new("an_StringSocket", "Align", "align").value = "CENTER"
 
         self.inputs.new("an_FontSocket", "Font", "font")
+        self.inputs.new("an_FontSocket", "Bold Font", "fontBold")
+        self.inputs.new("an_FontSocket", "Italic Font", "fontItalic")
+        self.inputs.new("an_FontSocket", "Bold Italic Font", "fontBoldItalic")
 
         for socket in self.inputs[1:]:
             socket.useIsUsedProperty = True
@@ -67,6 +70,9 @@ class TextObjectOutputNode(bpy.types.Node, AnimationNode):
         if s["Align"].isUsed:               yield "    self.setAlignment(textObject, align)"
 
         if s["Font"].isUsed:                yield "    textObject.font = font"
+        if s["Bold Font"].isUsed:           yield "    textObject.font_bold = fontBold"
+        if s["Italic Font"].isUsed:         yield "    textObject.font_italic = fontItalic"
+        if s["Bold Italic Font"].isUsed:    yield "    textObject.font_bold_italic = fontBoldItalic"
 
     def setAlignment(self, textObject, align):
         if align in ("LEFT", "CENTER", "RIGHT", "JUSTIFY", "FLUSH"):
