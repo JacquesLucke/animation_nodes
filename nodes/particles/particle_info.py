71cb71edd93c4d8f40553880e3d3a97cbb122552
og76 <ovidiu.grigorescu@gmail.com>
2016-02-10 22:42:40 +0200

    euler and hair all
    euler and hair all

--- nodes/particles/particle_info.py
+++ nodes/particles/particle_info.py
@@ -8,9 +8,9 @@ class ParticleInfoNode(bpy.types.Node, AnimationNode):
     def create(self):
         self.inputs.new("an_ParticleSocket", "Particle", "particle")
         self.outputs.new("an_VectorSocket", "Location", "location")
-        self.outputs.new("an_VectorSocket", "Rotation", "rotation")
+        self.outputs.new("an_EulerSocket", "Rotation", "rotation")
         self.outputs.new("an_VectorSocket", "Velocity", "velocity").hide = True
-        self.outputs.new("an_VectorSocket", "Angular Velocity", "angularVelocity").hide = True
+        self.outputs.new("an_EulerSocket", "Angular Velocity", "angularVelocity").hide = True
         self.outputs.new("an_FloatSocket", "Size", "size").hide = True
         self.outputs.new("an_StringSocket", "Alive State", "aliveState").hide = True
         self.outputs.new("an_BooleanSocket", "Is Exist", "isExist").hide = True
@@ -19,9 +19,14 @@ class ParticleInfoNode(bpy.types.Node, AnimationNode):
         self.outputs.new("an_FloatSocket", "Birth Time", "birthTime").hide = True
         self.outputs.new("an_FloatSocket", "Die Time", "dieTime").hide = True
         self.outputs.new("an_VectorSocket", "Previous Location", "previousLocation").hide = True
-        self.outputs.new("an_VectorSocket", "Previous Rotation", "previousRotation").hide = True
+        self.outputs.new("an_EulerSocket", "Previous Rotation", "previousRotation").hide = True
         self.outputs.new("an_VectorSocket", "Previous Velocity", "previousVelocity").hide = True
-        self.outputs.new("an_VectorSocket", "Previous Angular Velocity", "previousAngularVelocity").hide = True
+        self.outputs.new("an_EulerSocket", "Previous Angular Velocity", "previousAngularVelocity").hide = True
+        
+        self.outputs.new("an_FloatListSocket", "Hair Time", "hairTime").hide = True
+        self.outputs.new("an_FloatListSocket", "Hair Weight", "hairWeight").hide = True
+        self.outputs.new("an_VectorListSocket", "Hair Points", "hairPoints").hide = True
+        self.outputs.new("an_VectorListSocket", "Hair Points Local", "hairPointsLocal").hide = True
 
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
@@ -30,9 +35,9 @@ class ParticleInfoNode(bpy.types.Node, AnimationNode):
 
         lines.append("if particle:")
         if isLinked["location"]: lines.append("    location = particle.location")
-        if isLinked["rotation"]: lines.append("    rotation = mathutils.Vector(particle.rotation.to_euler())")
+        if isLinked["rotation"]: lines.append("    rotation = particle.rotation.to_euler()")
         if isLinked["velocity"]: lines.append("    velocity = particle.velocity")
-        if isLinked["angularVelocity"]: lines.append("    angularVelocity = particle.angular_velocity")
+        if isLinked["angularVelocity"]: lines.append("    angularVelocity = mathutils.Euler(particle.angular_velocity)")
         if isLinked["size"]: lines.append("    size = particle.size")
         if isLinked["aliveState"]: lines.append("    aliveState = particle.alive_state")
         if isLinked["isExist"]: lines.append("    isExist = particle.is_exist")
@@ -41,16 +46,27 @@ class ParticleInfoNode(bpy.types.Node, AnimationNode):
         if isLinked["birthTime"]: lines.append("    birthTime = particle.birth_time")
         if isLinked["dieTime"]: lines.append("    dieTime = particle.die_time")
         if isLinked["previousLocation"]: lines.append("    previousLocation = particle.prev_location")
-        if isLinked["previousRotation"]: lines.append("    previousRotation = mathutils.Vector(particle.prev_rotation.to_euler())")
+        if isLinked["previousRotation"]: lines.append("    previousRotation = particle.prev_rotation.to_euler()")
         if isLinked["previousVelocity"]: lines.append("    previousVelocity = particle.prev_velocity")
-        if isLinked["previousAngularVelocity"]: lines.append("    previousAngularVelocity = particle.prev_angular_velocity")
+        if isLinked["previousAngularVelocity"]: lines.append("    previousAngularVelocity = mathutils.Euler(particle.prev_angular_velocity)")
+        
+        hair = ["hairTime", "hairWeight", "hairPoints", "hairPointsLocal"]
+        if any([isLinked[item] for item in hair]):
+            lines.append("    " + ", ".join(hair) + " = [], [], [] ,[]")
+            lines.append("    if particle.hair_keys:")
+            lines.append(" " * 8 + "for key in particle.hair_keys:")
+            if isLinked["hairTime"]:        lines.append(" " * 12 + "hairTime.append(key.time)")
+            if isLinked["hairWeight"]:      lines.append(" " * 12 + "hairWeight.append(key.weight)")
+            if isLinked["hairPoints"]:      lines.append(" " * 12 + "hairPoints.append(key.co)")
+            if isLinked["hairPointsLocal"]: lines.append(" " * 12 + "hairPointsLocal.append(key.co_local)")
+            
         lines.append("    pass")
 
         lines.append("else:")
         if isLinked["location"]: lines.append("    location = mathutils.Vector((0, 0, 0))")
-        if isLinked["rotation"]: lines.append("    rotation = mathutils.Vector((0, 0, 0))")
+        if isLinked["rotation"]: lines.append("    rotation = mathutils.Euler((0, 0, 0))")
         if isLinked["velocity"]: lines.append("    velocity = mathutils.Vector((0, 0, 0))")
-        if isLinked["angularVelocity"]: lines.append("    angularVelocity = mathutils.Vector((0, 0, 0))")
+        if isLinked["angularVelocity"]: lines.append("    angularVelocity = mathutils.Euler((0, 0, 0))")
         if isLinked["size"]: lines.append("    size = 1")
         if isLinked["aliveState"]: lines.append("    aliveState = 'DEAD'")
         if isLinked["isExist"]: lines.append("    isExist = False")
@@ -59,9 +75,14 @@ class ParticleInfoNode(bpy.types.Node, AnimationNode):
         if isLinked["birthTime"]: lines.append("    birthTime = 0")
         if isLinked["dieTime"]: lines.append("    dieTime = 0")
         if isLinked["previousLocation"]: lines.append("    previousLocation = mathutils.Vector((0, 0, 0))")
-        if isLinked["previousRotation"]: lines.append("    previousRotation = mathutils.Vector((0, 0, 0))")
+        if isLinked["previousRotation"]: lines.append("    previousRotation = mathutils.Euler((0, 0, 0))")
         if isLinked["previousVelocity"]: lines.append("    previousVelocity = mathutils.Vector((0, 0, 0))")
-        if isLinked["previousAngularVelocity"]: lines.append("    previousAngularVelocity = mathutils.Vector((0, 0, 0))")
+        if isLinked["previousAngularVelocity"]: lines.append("    previousAngularVelocity = mathutils.Euler((0, 0, 0))")
+        
+        if isLinked["hairTime"]:        lines.append("    hairTime = []")
+        if isLinked["hairWeight"]:      lines.append("    hairWeight = []")
+        if isLinked["hairPoints"]:      lines.append("    hairPoints = []")
+        if isLinked["hairPointsLocal"]: lines.append("    hairPointsLocal = []")
         lines.append("    pass")
 
         return lines
