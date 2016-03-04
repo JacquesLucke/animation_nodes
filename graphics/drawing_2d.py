70a126b0ad56a1d0c21fe1dd415bb42cbd414678
Jacques Lucke <mail@jlucke.com>
2016-03-04 14:52:42 +0100

    initial statistics modal

---
+++ graphics/drawing_2d.py
@@ -0,0 +1,43 @@
+import blf
+from bgl import *
+
+dpi = 72
+
+def set_drawing_dpi(new_dpi):
+    global dpi
+    dpi = new_dpi
+
+def draw_horizontal_line(x, y, length, color, width):
+    draw_line(x, y, x + length, y, color, width)
+
+def draw_line(x1, y1, x2, y2, color, width):
+    glLineWidth(width)
+    glColor4f(*color)
+    glEnable(GL_BLEND)
+    glBegin(GL_LINES)
+    glVertex2f(x1, y1)
+    glVertex2f(x2, y2)
+    glEnd()
+    glLineWidth(1)
+
+def draw_boolean(state, x, y, size = 12, alpha = 1):
+    if state:
+        draw_text("ON", x, y, align = "LEFT", size = size,
+                  color = (0.8, 1, 0.8, alpha))
+    else:
+        draw_text("OFF", x, y, align = "LEFT", size = size,
+                  color = (1, 0.8, 0.8, alpha))
+
+def draw_text(text, x, y, align = "LEFT", size = 12, color = (1, 1, 1, 1)):
+    font = 0
+    blf.size(font, size, int(dpi))
+    glColor4f(*color)
+
+    if align == "LEFT":
+        blf.position(font, x, y, 0)
+    else:
+        width, height = blf.dimensions(font, text)
+        if align == "RIGHT":
+            blf.position(font, x - width, y, 0)
+
+    blf.draw(font, text)
