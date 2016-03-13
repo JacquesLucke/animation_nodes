35090a3840171e082984728ea9c3d2485b15b878
Jacques Lucke <mail@jlucke.com>
2016-03-13 16:05:19 +0100

    distance vector to plane node

---
+++ nodes/vector/distance_vector_to_plane.py
@@ -0,0 +1,18 @@
+import bpy
+from ... base_types.node import AnimationNode
+
+class DistanceVectorToPlaneNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_DistanceVectorToPlaneNode"
+    bl_label = "Distance Vector to Plane"
+
+    def create(self):
+        self.inputs.new("an_VectorSocket", "Vector", "vector")
+        self.inputs.new("an_VectorSocket", "Point on Plane", "planePoint")
+        self.inputs.new("an_VectorSocket", "Plane Normal", "planeNormal").value = (0, 0, 1)
+        self.outputs.new("an_FloatSocket", "Distance", "distance")
+
+    def getExecutionCode(self):
+        return "distance = mathutils.geometry.distance_point_to_plane(vector, planePoint, planeNormal)"
+
+    def getUsedModules(self):
+        return ["mathutils"]
