c26143f52f69465ac40f7e4171e8ef049740b981
Jacques Lucke <mail@jlucke.com>
2016-03-13 16:05:05 +0100

    cleanup

--- data_structures/splines/utils.py
+++ data_structures/splines/utils.py
@@ -2,5 +2,5 @@ def findNearestParameterOnLine(linePosition, lineDirection, point):
     directionLength = lineDirection.length
     lineDirection = lineDirection.normalized()
     if directionLength == 0: return 0
-    parameter = (lineDirection.dot(point - linePosition)) / directionLength # can cause division by zero
+    parameter = (lineDirection.dot(point - linePosition)) / directionLength
     return parameter
