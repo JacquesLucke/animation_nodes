9b523b6d1b3a89fcafb996e281de99d4d9940d4c
Jacques Lucke <mail@jlucke.com>
2016-03-13 17:18:00 +0100

    speedup setting splines on curve objects

--- data_structures/splines/to_blender.py
+++ data_structures/splines/to_blender.py
@@ -1,9 +1,10 @@
+from itertools import chain
 from mathutils import Vector
 
 def setSplinesOnBlenderObject(object, splines):
     if object is None: return
     if object.type != "CURVE": return
-    
+
     bSplines = object.data.splines
     bSplines.clear()
     for spline in splines:
@@ -11,28 +12,32 @@ def setSplinesOnBlenderObject(object, splines):
             appendBezierSpline(bSplines, spline)
         if spline.type == "POLY":
             appendPolySpline(bSplines, spline)
-            
-            
+
+
 def appendBezierSpline(bSplines, spline):
     bSpline = bSplines.new("BEZIER")
     bSpline.use_cyclic_u = spline.isCyclic
-    
+
     # one point is already there
     bSpline.bezier_points.add(len(spline.points) - 1)
-    
-    for i, point in enumerate(spline.points):
-        bPoint = bSpline.bezier_points[i]
-        bPoint.co = point.location
-        bPoint.handle_left = point.leftHandle
-        bPoint.handle_right = point.rightHandle
-        
-        
+
+    locations = tuple(chain.from_iterable(point.location for point in spline.points))
+    leftHandles = tuple(chain.from_iterable(point.leftHandle for point in spline.points))
+    rightHandles = tuple(chain.from_iterable(point.rightHandle for point in spline.points))
+
+    bSpline.bezier_points.foreach_set("co", locations)
+    bSpline.bezier_points.foreach_set("handle_left", leftHandles)
+    bSpline.bezier_points.foreach_set("handle_right", rightHandles)
+
+
 def appendPolySpline(bSplines, spline):
     bSpline = bSplines.new("POLY")
     bSpline.use_cyclic_u = spline.isCyclic
-    
+
     # one point is already there
     bSpline.points.add(len(spline.points) - 1)
-    
-    for i, point in enumerate(spline.points):
-        bSpline.points[i].co = Vector((list(point) + [0]))
+
+    # points of poly splines have 4 values
+    nullList = [0]
+    locations = tuple(chain.from_iterable(list(point) + nullList for point in spline.points))
+    bSpline.points.foreach_set("co", locations)
