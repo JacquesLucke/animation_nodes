ab339564cf2c1bd7d40173dd630245d72a342d31
Jacques Lucke <mail@jlucke.com>
2016-03-15 11:57:06 +0100

    bake no higher frequencies than 20000
    The bake sound to fcurve operator can give completely wrong results if
    you do so

--- nodes/sound/bake.py
+++ nodes/sound/bake.py
@@ -25,8 +25,8 @@ class SoundBakeNode(bpy.types.Node, AnimationNode):
     activeBakeDataIndex = IntProperty()
     activeEqualizerDataIndex = IntProperty()
 
-    low = IntProperty(name = "Low", default = 0, min = 0)
-    high = IntProperty(name = "High", default = 20000, min = 0)
+    low = IntProperty(name = "Low", default = 0, min = 0, max = 20000)
+    high = IntProperty(name = "High", default = 20000, min = 0, max = 20000)
     attack = FloatProperty(name = "Attack", default = 0.005, precision = 3)
     release = FloatProperty(name = "Release", default = 0.2, precision = 3)
 
