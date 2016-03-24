dab7798d0e0db2ebaa7e515d19dcc0d4403e6d23
Jacques Lucke <mail@jlucke.com>
2016-03-24 19:51:43 +0100

    improve writeText function to allow empty lines

--- utils/layout.py
+++ utils/layout.py
@@ -13,7 +13,14 @@ def writeText(layout, text, width = 30, icon = "NONE"):
     col.scale_y = 0.85
     prefix = " "
     for paragraph in text.split("\n"):
-        for line in textwrap.wrap(paragraph, width):
+        lines = textwrap.wrap(paragraph, width)
+
+        if len(lines) == 0:
+            subcol = col.column()
+            subcol.scale_y = 0.3
+            subcol.label("")
+
+        for line in lines:
             col.label(prefix + line, icon = icon)
             if icon != "NONE": prefix = "     "
             icon = "NONE"
