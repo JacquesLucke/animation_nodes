35920690c85bc30ea9080265507beff1b9b4691d
Jacques Lucke <mail@jlucke.com>
2016-03-24 13:27:56 +0100

    minor cleanup of Project Point on Plane node
    I renamed 'int' to 'intersection' because 'int' is a builtin function of
    python. Overwriting it can interfere with other nodes

--- nodes/geometry/project_point_on_plane.py
+++ nodes/geometry/project_point_on_plane.py
@@ -10,24 +10,25 @@ class ProjectPointOnPlaneNode(bpy.types.Node, AnimationNode):
     def create(self):
         self.width = 170
         self.inputs.new("an_VectorSocket", "Point", "point")
-        self.inputs.new("an_VectorSocket", "Plane Point", "planePoint").value = (1, 0, 0)
+        self.inputs.new("an_VectorSocket", "Plane Point", "planePoint").value = (0, 0, 0)
         self.inputs.new("an_VectorSocket", "Plane Normal", "planeNormal").value = (0, 0, 1)
-        
+
         self.outputs.new("an_VectorSocket", "Projection", "projection")
-        self.outputs.new("an_FloatSocket", "Signed Distance to Plane", "distance")
-        
+        self.outputs.new("an_FloatSocket", "Signed Distance", "distance")
+
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
-        if not any(isLinked.values()): return ""
-        
-        if any(isLinked.values()):yield "plane_co, plane_no = " + getPlane()
+        if not any(isLinked.values()):
+            return
+
+        yield "plane_co = planePoint"
+        yield "plane_no = planeNormal if planeNormal.length_squared != 0 else mathutils.Vector((0, 0, 1))"
+
         if isLinked["projection"]:
-            yield "int = mathutils.geometry.intersect_line_plane(point, point + plane_no, plane_co, plane_no, False)"
-            yield "projection = mathutils.Vector((0, 0, 0)) if int is None else int"
-        if isLinked["distance"]: yield "distance = mathutils.geometry.distance_point_to_plane(point, plane_co, plane_no)"
- 
+            yield "intersection = mathutils.geometry.intersect_line_plane(point, point + plane_no, plane_co, plane_no, False)"
+            yield "projection = mathutils.Vector((0, 0, 0)) if intersection is None else intersection"
+        if isLinked["distance"]:
+            yield "distance = mathutils.geometry.distance_point_to_plane(point, plane_co, plane_no)"
+
     def getUsedModules(self):
         return ["mathutils"]
-
-def getPlane():
-    return "planePoint, planeNormal if planeNormal.length_squared != 0 else mathutils.Vector((0, 0, 1))"
