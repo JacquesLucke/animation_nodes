767c3b009d01f062890d1b06041b40e53d2a0cab
Jacques Lucke <mail@jlucke.com>
2016-03-26 16:43:32 +0100

    compose matrix speed through inlining #446

--- utils/math.py
+++ utils/math.py
@@ -1,17 +1,18 @@
 from mathutils import Matrix, Euler
 
 def composeMatrix(location, rotation, scale):
-    matrix = (rotation.to_matrix() * scaleMatrix_3x3(scale)).to_4x4()
+    # Scale
+    scaleMatrix = Matrix.Identity(3)
+    scaleMatrix[0][0] = scale[0]
+    scaleMatrix[1][1] = scale[1]
+    scaleMatrix[2][2] = scale[2]
+    # Rotation
+    matrix = (rotation.to_matrix() * scaleMatrix).to_4x4()
+    # Translation
     matrix[0][3] = location[0]
     matrix[1][3] = location[1]
     matrix[2][3] = location[2]
-    return matrix
 
-def scaleMatrix_3x3(scale):
-    matrix = Matrix.Identity(3)
-    matrix[0][0] = scale[0]
-    matrix[1][1] = scale[1]
-    matrix[2][2] = scale[2]
     return matrix
 
 def extractRotation(matrix):
