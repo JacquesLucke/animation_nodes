54c7be9068f8e5af4acd6fa05c14f6ba0f7a9839
og76 <o.g.blender@gmail.com>
2016-03-28 03:28:51 +0300

    corrected 1 append

--- nodes/list/append_list.py
+++ nodes/list/append_list.py
@@ -1,8 +1,8 @@
 import bpy
 from bpy.props import *
-from ... tree_info import keepNodeLinks
+from ... tree_info import keepNodeState
 from ... base_types.node import AnimationNode
-from ... sockets.info import getBaseDataTypeItemsCallback, toIdName, toListIdName, isBase, toBaseDataType
+from ... sockets.info import getBaseDataTypeItemsCallback, toIdName, toListIdName, isBase, toBaseDataType, isLimitedList, toGeneralListIdName
 
 class AppendListNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_AppendListNode"
@@ -25,18 +25,28 @@ class AppendListNode(bpy.types.Node, AnimationNode):
             socketGroup = "LIST", text = "Change Type", icon = "TRIA_RIGHT")
 
     def getExecutionCode(self):
-        return ("list.append(element)")
+        if self.isTuple(): yield "inList = list(inList)"
+        yield "inList.append(element)"
 
     def edit(self):
         baseDataType = self.getWantedDataType()
         self.assignType(baseDataType)
+        
+    def isTuple(self):
+        listInput = self.inputs["List"].dataOrigin
+        if listInput is not None:
+            if isLimitedList(listInput.bl_idname): return True
+        return False
 
     def getWantedDataType(self):
         listInput = self.inputs["List"].dataOrigin
         elementInput = self.inputs["Element"].dataOrigin
         listOutputs = self.outputs["List"].dataTargets
 
-        if listInput is not None: return toBaseDataType(listInput.bl_idname)
+        if listInput is not None: 
+            idName = listInput.bl_idname
+            if isLimitedList(idName): idName = toGeneralListIdName(idName)
+            return toBaseDataType(idName)
         if elementInput is not None: return elementInput.dataType
         if len(listOutputs) == 1: return toBaseDataType(listOutputs[0].bl_idname)
         return self.inputs["Element"].dataType
@@ -49,10 +59,10 @@ class AppendListNode(bpy.types.Node, AnimationNode):
         if baseDataType == self.assignedType: return
         self.assignedType = baseDataType
 
-    @keepNodeLinks
+    @keepNodeState
     def generateSockets(self):
         self.inputs.clear()
         self.outputs.clear()
-        self.inputs.new(self.listIdName, "List", "list").dataIsModified  = True
+        self.inputs.new(self.listIdName, "List", "inList").dataIsModified  = True
         self.inputs.new(self.baseIdName, "Element", "element").dataIsModified = True
-        self.outputs.new(self.listIdName, "List", "list")
+        self.outputs.new(self.listIdName, "List", "inList")
