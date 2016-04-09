0f764138d7da4966461239c45986ee631151f66b
Jacques Lucke <mail@jlucke.com>
2016-04-09 13:59:58 +0200

    minor fixes to fill list node

--- nodes/list/fill_list.py
+++ nodes/list/fill_list.py
@@ -1,10 +1,9 @@
 import bpy
 from bpy.props import *
-from ... events import executionCodeChanged
 from ... tree_info import keepNodeState
+from ... events import executionCodeChanged
 from ... base_types.node import AnimationNode
-from ... sockets.info import (getBaseDataTypeItemsCallback, getSocketClassFromIdName,
-                                toIdName, toListIdName, isBase, toBaseDataType)
+from ... sockets.info import toIdName, toListIdName, isBase, toBaseDataType
 
 fillModeItems = [
     ("LEFT", "Left", "", "TRIA_LEFT", 0),
@@ -39,14 +38,13 @@ class FillListNode(bpy.types.Node, AnimationNode):
 
     def getExecutionCode(self):
         yield ("fillList = [{} for i in range(max(length - len(inList), 0))]"
-                .format(self.getCopyString(self.inputs["Fill Element"], "fill")) )
+                .format(self.getCopyString(self.inputs["Element"], "fillElement")))
         if self.fillMode == "LEFT": yield "outList = fillList + inList"
         if self.fillMode == "RIGHT": yield "outList = inList + fillList"
 
     def getCopyString(self, fromSocket, variableString):
-        if not fromSocket.isCopyable: return variableString
+        if not fromSocket.isCopyable(): return variableString
         return fromSocket.getCopyExpression().replace("value", variableString)
-
 
     def edit(self):
         baseDataType = self.getWantedDataType()
@@ -54,13 +52,13 @@ class FillListNode(bpy.types.Node, AnimationNode):
 
     def getWantedDataType(self):
         listInput = self.inputs["List"].dataOrigin
-        elementInput = self.inputs["Fill Element"].dataOrigin
+        elementInput = self.inputs["Element"].dataOrigin
         listOutputs = self.outputs["List"].dataTargets
 
         if listInput is not None: return toBaseDataType(listInput.bl_idname)
         if elementInput is not None: return elementInput.dataType
         if len(listOutputs) == 1: return toBaseDataType(listOutputs[0].bl_idname)
-        return self.inputs["Fill Element"].dataType
+        return self.inputs["Element"].dataType
 
     def assignListDataType(self, listDataType):
         self.assignType(toBaseDataType(listDataType))
@@ -69,14 +67,13 @@ class FillListNode(bpy.types.Node, AnimationNode):
         if not isBase(baseDataType): return
         if baseDataType == self.assignedType: return
         self.assignedType = baseDataType
-        
+
     @keepNodeState
     def generateSockets(self):
         self.inputs.clear()
         self.outputs.clear()
-        
+
         self.inputs.new("an_IntegerSocket", "Length", "length")
         self.inputs.new(self.listIdName, "List", "inList").dataIsModified = True
-        self.inputs.new(self.baseIdName, "Fill Element", "fill").dataIsModified = True
+        self.inputs.new(self.baseIdName, "Element", "fillElement").dataIsModified = True
         self.outputs.new(self.listIdName, "List", "outList")
-
