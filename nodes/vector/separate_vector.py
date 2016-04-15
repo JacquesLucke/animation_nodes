cd76c74a69dda05b493170f9e32a14aa2d80228f
Jacques Lucke <mail@jlucke.com>
2016-04-15 13:24:48 +0200

    speedup Separate Vector node

--- nodes/vector/separate_vector.py
+++ nodes/vector/separate_vector.py
@@ -12,4 +12,7 @@ class SeparateVectorNode(bpy.types.Node, AnimationNode):
         self.newOutput("Float", "Z", "z")
 
     def getExecutionCode(self):
-        return "x, y, z = vector.xyz"
+        isLinked = self.getLinkedOutputsDict()
+        if isLinked["x"]: yield "x = vector[0]"
+        if isLinked["y"]: yield "y = vector[1]"
+        if isLinked["z"]: yield "z = vector[2]"
