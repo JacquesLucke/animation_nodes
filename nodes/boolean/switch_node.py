7bd87fbc99c73829b348d54146dd7af55c70c4c9
Jacques Lucke <mail@jlucke.com>
2016-04-17 23:55:57 +0200

    cleanup

--- nodes/boolean/switch_node.py
+++ nodes/boolean/switch_node.py
@@ -35,8 +35,9 @@ class SwitchNode(bpy.types.Node, AnimationNode):
         self.assignedType = dataType
 
     def getExecutionCode(self):
-        return ("output = ifTrue if condition else ifFalse",
-                "other = ifFalse if condition else ifTrue")
+        isLinked = self.getLinkedOutputsDict()
+        if isLinked["output"]: yield "output = ifTrue if condition else ifFalse"
+        if isLinked["other"]:  yield "other = ifFalse if condition else ifTrue"
 
     @keepNodeState
     def generateSockets(self):
