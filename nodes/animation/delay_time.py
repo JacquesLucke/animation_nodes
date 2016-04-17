84c11dc4bae5f85b15c0cfc9a65fc34065fe3859
Jacques Lucke <mail@jlucke.com>
2016-04-18 00:42:48 +0200

    Dynamic label for Delay Time node #486

--- nodes/animation/delay_time.py
+++ nodes/animation/delay_time.py
@@ -4,11 +4,20 @@ from ... base_types.node import AnimationNode
 class DelayTimeNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_DelayTimeNode"
     bl_label = "Delay Time"
+    dynamicLabelType = "HIDDEN_ONLY"
 
     def create(self):
         self.newInput("Float", "Time", "time")
         self.newInput("Float", "Delay", "delay", value = 10)
         self.newOutput("Float", "Time", "outTime")
+
+    def drawLabel(self):
+        delaySocket = self.inputs["Delay"]
+        if delaySocket.isUnlinked:
+            value = delaySocket.value
+            if value == int(value): return "Delay " + str(int(value)) + " Frames"
+            else: return "Delay " + str(round(value, 2)) + " Frames"
+        else: return "Delay Time"
 
     def getExecutionCode(self):
         return "outTime = time - delay"
