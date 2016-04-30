4e37689c80f547f1adea8def4908b6c38c3d101e
Jacques Lucke <mail@jlucke.com>
2016-04-30 11:04:18 +0200

    more options for Slice List node

--- nodes/list/slice_list.py
+++ nodes/list/slice_list.py
@@ -5,39 +5,62 @@ from ... base_types.node import AnimationNode
 from ... sockets.info import isBase, toBaseDataType, toListDataType
 
 sliceEndType = [
-    ("END_INDEX", "End Index", "", "NONE", 0),
-    ("OUTPUT_LENGTH", "Output Length", "", "NONE", 1)]
+    ("END_INDEX", "Index", "", "NONE", 0),
+    ("OUTPUT_LENGTH", "Length", "", "NONE", 1)]
 
 class SliceListNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SliceListNode"
     bl_label = "Slice List"
+    bl_width_default = 170
 
-    def assignedTypeChanged(self, context):
+    def settingChanged(self, context):
         self.generateSockets()
 
-    assignedType = StringProperty(update = assignedTypeChanged)
+    assignedType = StringProperty(update = settingChanged)
 
-    def sliceEndTypeChanged(self, context):
-        self.generateSockets()
+    useStart = BoolProperty(name = "Start", default = True,
+        update = settingChanged)
+    useEnd = BoolProperty(name = "End", default = True,
+        update = settingChanged)
+    useStep = BoolProperty(name = "Step", default = False,
+        update = settingChanged)
 
     sliceEndType = EnumProperty(name = "Slice Type", default = "END_INDEX",
-        items = sliceEndType, update = sliceEndTypeChanged)
+        items = sliceEndType, update = settingChanged)
 
     def create(self):
         self.assignedType = "Float"
 
+    def draw(self, layout):
+        col = layout.column()
+        row = col.row(align = True)
+        row.prop(self, "useStart", expand = True, icon = "TRIA_RIGHT")
+        row.prop(self, "useEnd", expand = True, icon = "TRIA_LEFT")
+        row.prop(self, "useStep", expand = True, icon = "ARROW_LEFTRIGHT")
+        if self.useEnd:
+            col.prop(self, "sliceEndType", text = "End")
+
     def drawAdvanced(self, layout):
-        layout.prop(self, "sliceEndType")
         self.invokeSocketTypeChooser(layout, "assignListDataType",
             socketGroup = "LIST", text = "Change Type", icon = "TRIA_RIGHT")
 
     def getExecutionCode(self):
-        yield "_step = 1 if step == 0 else step"
-        if self.sliceEndType == "END_INDEX":
-            yield "slicedList = list[start:end:_step]"
-        elif self.sliceEndType == "OUTPUT_LENGTH":
-            yield "endIndex = max(0, start + _step * length)"
-            yield "slicedList = list[start:endIndex:_step]"
+        if self.useStart: yield "_start = start"
+        else:             yield "_start = 0"
+
+        if self.useStep: yield "_step = 1 if step == 0 else step"
+        else:            yield "_step = 1"
+
+        if self.useEnd:
+            if self.sliceEndType == "END_INDEX":
+                yield "_end = end"
+            elif self.sliceEndType == "OUTPUT_LENGTH":
+                yield "_end = max(0, _start + _step * length)"
+
+        elements = ["_start" if self.useStart else "",
+                    "_end" if self.useEnd else "",
+                    "_step" if self.useStep else ""]
+        yield "slicedList = list[{}]".format(":".join(elements))
 
     def edit(self):
         baseDataType = self.getWantedDataType()
@@ -66,10 +89,13 @@ class SliceListNode(bpy.types.Node, AnimationNode):
 
         listDataType = toListDataType(self.assignedType)
         self.newInput(listDataType, "List", "list", dataIsModified  = True)
-        self.newInput("Integer", "Start", "start")
-        if self.sliceEndType == "END_INDEX":
-            self.newInput("Integer", "End", "end")
-        elif self.sliceEndType == "OUTPUT_LENGTH":
-            self.newInput("Integer", "Length", "length")
-        self.newInput("Integer", "Step", "step", value = 1, hide = True)
+        if self.useStart:
+            self.newInput("Integer", "Start", "start")
+        if self.useEnd:
+            if self.sliceEndType == "END_INDEX":
+                self.newInput("Integer", "End", "end")
+            elif self.sliceEndType == "OUTPUT_LENGTH":
+                self.newInput("Integer", "Length", "length")
+        if self.useStep:
+            self.newInput("Integer", "Step", "step", value = 1)
         self.newOutput(listDataType, "List", "slicedList")
