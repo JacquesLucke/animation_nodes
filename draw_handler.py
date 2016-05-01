97430eeb1459feaac86d3db9a37353c46b4dbb5b
Jacques Lucke <mail@jlucke.com>
2016-05-01 13:53:26 +0200

    draw execution time in the top left

--- draw_handler.py
+++ draw_handler.py
@@ -1,5 +1,6 @@
 import bpy
 from . preferences import getExecutionCodeType
+from . ui.node_editor_hud import drawNodeEditorHud
 from . execution.measurements import drawMeasurementResults
 from . nodes.generic.debug_drawer import drawDebugTextBoxes
 from . nodes.interpolation.debug import drawInterpolationPreviews
@@ -9,6 +10,7 @@ def drawNodeEditor():
     drawInterpolationPreviews()
     if getExecutionCodeType() == "MEASURE":
         drawMeasurementResults()
+    drawNodeEditorHud()
 
 _nodeDrawHandler = None
 def register():
