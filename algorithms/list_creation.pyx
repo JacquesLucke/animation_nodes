5999aa3283946085e67e55504943574fb86d5271
Jacques Lucke <mail@jlucke.com>
2016-05-07 22:08:03 +0200

    create integer range

--- algorithms/list_creation.pyx
+++ algorithms/list_creation.pyx
@@ -1,8 +1,15 @@
-from .. data_structures.lists.base_lists cimport DoubleList, FloatList
+from .. data_structures.lists.base_lists cimport DoubleList, FloatList, LongLongList
 
-def fromRange(long amount, double start, double step):
+def createDoubleListRange(long amount, double start, double step):
     cdef DoubleList newList = DoubleList(max(0, amount))
     cdef long i
     for i in range(max(0, amount)):
         newList.data[i] = start + i * step
+    return newList
+
+def createLongLongListRange(long amount, double start, double step):
+    cdef LongLongList newList = LongLongList(max(0, amount))
+    cdef long i
+    for i in range(max(0, amount)):
+        newList.data[i] = <long long>(start + i * step)
     return newList
