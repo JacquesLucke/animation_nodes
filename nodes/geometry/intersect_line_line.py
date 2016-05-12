0f978912cd9f9ce2176625c68c0b0ee8f4911bf7
og76 <o.g.blender@gmail.com>
2016-05-12 09:32:09 +0300

    corrections

--- nodes/geometry/intersect_line_line.py
+++ nodes/geometry/intersect_line_line.py
@@ -8,32 +8,32 @@ class IntersectLineLineNode(bpy.types.Node, AnimationNode):
     searchTags = ["Closest Points on 2 Lines"]
         
     def create(self):
-        self.newInput("Vector", "Line 1 Start", "line1Start").value = [-2, 0, 0]
-        self.newInput("Vector", "Line 1 End", "line1End").value = [2, 0, 0]
-        self.newInput("Vector", "Line 2 Start", "line2Start").value = [1, -1, 0]
-        self.newInput("Vector", "Line 2 End", "line2End").value = [1, 1, 0]
+        self.newInput("Vector", "Line 1 Start", "lineStart1")
+        self.newInput("Vector", "Line 1 End", "lineEnd1", value = [0, 1, 0])
+        self.newInput("Vector", "Line 2 Start", "lineStart2")
+        self.newInput("Vector", "Line 2 End", "lineEnd2", value = [0, 0, 1])
         
-        self.newOutput("Vector", "Intersection (Closest) 1", "int1")
-        self.newOutput("Vector", "Intersection (Closest) 2", "int2")
+        self.newOutput("Vector", "Closest On Line 1", "closest1")
+        self.newOutput("Vector", "Closest On Line 2", "closest2")
         self.newOutput("Boolean", "Is Valid", "isValid")
         
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
         if not any(isLinked.values()): return ""
     
-        intersection1  = isLinked["int1"]
-        intersection2  = isLinked["int2"]
+        intersection1  = isLinked["closest1"]
+        intersection2  = isLinked["closest2"]
         isValid = isLinked["isValid"]
         
-        yield "int = mathutils.geometry.intersect_line_line(line1Start, line1End, line2Start, line2End)"
+        yield "closestPoints = mathutils.geometry.intersect_line_line(lineStart1, lineEnd1, lineStart2, lineEnd2)"
         
-        yield "if int is None:"
-        if intersection1 : yield "    int1 = mathutils.Vector((0, 0, 0))"
-        if intersection2 : yield "    int2 = mathutils.Vector((0, 0, 0))"
+        yield "if closestPoints is None:"
+        if intersection1 : yield "    closest1 = mathutils.Vector((0, 0, 0))"
+        if intersection2 : yield "    closest2 = mathutils.Vector((0, 0, 0))"
         if isValid: yield "    isValid = False"
         yield "else:"
-        if intersection1 : yield "    int1 = int[0]"
-        if intersection2 : yield "    int2 = int[1]"
+        if intersection1 : yield "    closest1 = closestPoints[0]"
+        if intersection2 : yield "    closest2 = closestPoints[1]"
         if isValid: yield "    isValid = True"
     
     def getUsedModules(self):
