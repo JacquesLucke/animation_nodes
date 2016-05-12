bb4b840e44af33dcfaca948794eb2eb429d4424b
og76 <o.g.blender@gmail.com>
2016-05-12 11:49:31 +0300

    corrections

--- nodes/geometry/intersect_sphere_sphere.py
+++ nodes/geometry/intersect_sphere_sphere.py
@@ -4,18 +4,18 @@ from ... base_types.node import AnimationNode
 class IntersectSphereSphereNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_IntersectSphereSphereNode"
     bl_label = "Intersect Sphere Sphere"
+    bl_width_default = 160
 
     def create(self):
-        self.width = 160
-        self.inputs.new("an_VectorSocket", "Sphere 1 Center", "c1")
-        self.inputs.new("an_FloatSocket", "Sphere 1 Radius", "r1").value = 1
-        self.inputs.new("an_VectorSocket", "Sphere 2 Center", "c2").value = (0, 0, 1)
-        self.inputs.new("an_FloatSocket", "Sphere 2 Radius", "r2").value = 1
+        self.newInput("Vector", "Sphere 1 Center", "center1")
+        self.newInput("Float", "Sphere 1 Radius", "radius1", value = 1)
+        self.newInput("Vector", "Sphere 2 Center", "center2", value = (0, 0, 1))
+        self.newInput("Float", "Sphere 2 Radius", "radius2", value = 1)
         
-        self.outputs.new("an_VectorSocket", "Circle Center", "center")
-        self.outputs.new("an_VectorSocket", "Circle Normal", "normal")
-        self.outputs.new("an_FloatSocket", "Circle Radius", "radius")
-        self.outputs.new("an_BooleanSocket", "Is Valid", "isValid").hide = True
+        self.newOutput("Vector", "Circle Center", "center")
+        self.newOutput("Vector", "Circle Normal", "normal")
+        self.newOutput("Float", "Circle Radius", "radius")
+        self.newOutput("Boolean", "Is Valid", "isValid", hide = True)
         
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
@@ -25,28 +25,26 @@ class IntersectSphereSphereNode(bpy.types.Node, AnimationNode):
         normal  = isLinked["normal"]
         radius  = isLinked["radius"]
         isValid = isLinked["isValid"]
-        zero = "mathutils.Vector((0,0,0))"
         
-        yield "if c1 == c2: "
-        if center : yield "    center =" + zero
-        if normal : yield "    normal =" + zero
+        yield "if center1 == center2: "
+        if center : yield "    center = mathutils.Vector((0,0,0))"
+        if normal : yield "    normal = mathutils.Vector((0,0,0))"
         if radius : yield "    radius = 0"
         if isValid: yield "    isValid = False"
+    
         yield "else:"
-        yield "    dif = (c2 - c1)"
+        yield "    dif = (center2 - center1)"
         yield "    dist = dif.length"
-        yield "    _, intx = mathutils.geometry.intersect_sphere_sphere_2d((0, 0),r1, (dist, 0), r2)"
+        yield "    _, intx = mathutils.geometry.intersect_sphere_sphere_2d( (0, 0), radius1, (dist, 0), radius2)"
 
         yield "    if intx is not None:"
-        if center or radius: 
-                    yield "        L, radius = intx[0], intx[1] * 2"
-        if center : yield "        center = c1.lerp(c2, L/dist)"
+        if center : yield "        center = center1.lerp(center2, intx[0]/dist)"
         if normal : yield "        normal = dif.normalized()"
+        if radius : yield "        radius = intx[1] * 2"
         if isValid: yield "        isValid = True"
-
         yield "    else:"
-        if center : yield "        center =" + zero
-        if normal : yield "        normal =" + zero
+        if center : yield "        center = mathutils.Vector((0,0,0))"
+        if normal : yield "        normal = mathutils.Vector((0,0,0))"
         if radius : yield "        radius = 0"
         if isValid: yield "        isValid = False"
         
