654c8c0b068f243b07a70bb392f3c35e24464e6b
og76 <o.g.blender@gmail.com>
2016-05-12 10:12:15 +0300

    corrections

--- nodes/geometry/intersect_line_sphere.py
+++ nodes/geometry/intersect_line_sphere.py
@@ -13,14 +13,14 @@ class IntersectLineSphereNode(bpy.types.Node, AnimationNode):
         
     def create(self):
         self.newInput("Vector", "Line Start", "lineStart")
-        self.newInput("Vector", "Line End", "lineEnd").value = (0, 0, 1)
+        self.newInput("Vector", "Line End", "lineEnd", value = (0, 0, 1))
         self.newInput("Vector", "Sphere Center", "center")
-        self.newInput("Vector", "Sphere Radius", "radius").value = 1
+        self.newInput("Float", "Sphere Radius", "radius", value = 1)
         
         self.newOutput("Vector", "Intersection 1", "intersection1")
         self.newOutput("Vector", "Intersection 2", "intersection2")
-        self.newOutput("Boolean", "Is Valid 1", "isValid1").hide = True
-        self.newOutput("Boolean", "Is Valid 2", "isValid2").hide = True
+        self.newOutput("Boolean", "Is Valid 1", "isValid1", hide = True)
+        self.newOutput("Boolean", "Is Valid 2", "isValid2", hide = True)
         
     def draw(self, layout):
         layout.prop(self, "clip")
@@ -41,11 +41,11 @@ class IntersectLineSphereNode(bpy.types.Node, AnimationNode):
         if isValid2: yield "    isValid2 = False"
         
         yield "else:"
-        yield "    int1, int2 = mathutils.geometry.intersect_line_sphere(lineStart, lineEnd, center, radius, self.clip)"
+        yield "    inters1, inters2 = mathutils.geometry.intersect_line_sphere(lineStart, lineEnd, center, radius, self.clip)"
         if intersection1 or isValid1:
-            yield "    intersection1, isValid1 = (int1, True) if int1 != None else (center, False)"
+            yield "    intersection1, isValid1 = (inters1, True) if inters1 != None else (center, False)"
         if intersection2 or isValid2:
-            yield "    intersection2, isValid2 = (int2, True) if int2 != None else (center, False)"
+            yield "    intersection2, isValid2 = (inters2, True) if inters2 != None else (center, False)"
 
     def getUsedModules(self):
         return ["mathutils"]
