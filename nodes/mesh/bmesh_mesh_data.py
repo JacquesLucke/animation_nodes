244c5c5ee7d42da982b0203619ba0117fa9af19c
Jacques Lucke <mail@jlucke.com>
2016-05-14 11:55:37 +0200

    remove Vertex and Polygon support

--- nodes/mesh/bmesh_mesh_data.py
+++ nodes/mesh/bmesh_mesh_data.py
@@ -1,16 +1,10 @@
 import bpy
 from bpy.props import *
-from ... events import propertyChanged
 from ... base_types.node import AnimationNode
-from ... data_structures.mesh import Polygon, Vertex
 
 class BMeshMeshDataNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_BMeshMeshDataNode"
     bl_label = "BMesh Mesh Data"
-
-    centerWeighted = BoolProperty(name = "Weigthed Center", default = False,
-        description = "Center weigthed by edges length instead of normal median",
-        update = propertyChanged)
 
     def create(self):
         self.newInput("BMesh", "BMesh", "bm")
@@ -18,11 +12,6 @@ class BMeshMeshDataNode(bpy.types.Node, AnimationNode):
         self.newOutput("Vector List", "Vertex Locations", "vertexLocations")
         self.newOutput("Edge Indices List", "Edge Indices", "edgeIndices")
         self.newOutput("Polygon Indices List", "Polygon Indices", "polygonIndices")
-        self.newOutput("Vertex List", "Vertices", "vertices")
-        self.newOutput("Polygon List", "Polygons", "polygons")
-
-    def drawAdvanced(self, layout):
-        layout.prop(self, "centerWeighted")
 
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
@@ -35,12 +24,8 @@ class BMeshMeshDataNode(bpy.types.Node, AnimationNode):
             yield "    edgeIndices = self.getEdgeIndices(bm)"
         if isLinked["polygonIndices"]:
             yield "    polygonIndices = self.getPolygonIndices(bm)"
-        if isLinked["vertices"]:
-            yield "    vertices = self.getVertices(bm)"
-        if isLinked["polygons"]:
-            yield "    polygons = self.getPolygons(bm, self.centerWeighted)"
 
-        yield "else: vertexLocations, edgeIndices, polygonIndices, vertices, polygons = [], [], [], [], []"
+        yield "else: vertexLocations, edgeIndices, polygonIndices = [], [], []"
 
 
     def getVertexLocations(self, BMesh):
@@ -51,9 +36,3 @@ class BMeshMeshDataNode(bpy.types.Node, AnimationNode):
 
     def getPolygonIndices(self, BMesh):
         return [tuple(v.index for v in face.verts) for face in BMesh.faces]
-
-    def getVertices(self, BMesh):
-        return [Vertex.fromBMeshVert(vert) for vert in BMesh.verts]
-
-    def getPolygons(self, BMesh, centerWeighted):
-        return [Polygon.fromBMeshFace(face, centerWeighted) for face in BMesh.faces]
