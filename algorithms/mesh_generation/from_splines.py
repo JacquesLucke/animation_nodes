839ac722cfa3063b5663d8eff4cb70e2b965fbe3
Jacques Lucke <mail@jlucke.com>
2016-06-30 20:16:00 +0200

    cleanup loft splines algorithm + grid mesh indices generation; fix #573

--- algorithms/mesh_generation/from_splines.py
+++ algorithms/mesh_generation/from_splines.py
@@ -1,51 +1,63 @@
-from ... data_structures.splines.bezier_spline import BezierSpline
-from ... data_structures.splines.poly_spline import PolySpline
-from . indices_utils import gridQuadPolygonIndices, tubeQuadPolygonIndices
 from . basic_shapes import tubeVertices
+from . indices_utils import GridMeshIndices
+from ... data_structures.splines import BezierSpline, PolySpline
 
 # Loft
 ###################################
 
-def loftSplines(splines, 
+def loftSplines(splines,
                 nSplineSamples, nSurfaceSamples, *,
-                type = "LINEAR", cyclic = False, smoothness = 1,
+                type = "LINEAR", cyclic = False, smoothness = 0.333,
                 uniformConverterResolution = 100, splineDistributionType = "RESOLUTION", surfaceDistributionType = "RESOLUTION",
                 startSurfaceParameter = 0.0, endSurfaceParameter = 1.0):
+    assert len(splines) >= 2
+    assert nSplineSamples >= 2
+    assert nSurfaceSamples >= 2
 
-    vertices = []
+    def calculateVertices():
+        vertices = []
+        for samples in zip(*sampleInputSplines()):
+            spline = createSurfaceSpline(samples)
+            vertices.extend(sampleSurfaceSpline(spline))
+        return vertices
 
-    isRealCyclic = cyclic and startSurfaceParameter <= 0.0 and endSurfaceParameter >= 1.0
+    def sampleInputSplines():
+        if splineDistributionType == "RESOLUTION":
+            return [spline.getSamples(nSplineSamples)
+                    for spline in splines]
+        elif splineDistributionType == "UNIFORM":
+            return [spline.getUniformSamples(nSplineSamples, resolution = uniformConverterResolution)
+                    for spline in splines]
 
-    if splineDistributionType == "RESOLUTION":
-        samples = [spline.getSamples(nSplineSamples) for spline in splines]
-    elif splineDistributionType == "UNIFORM":
-        samples = [spline.getUniformSamples(nSplineSamples, resolution = uniformConverterResolution) for spline in splines]
-
-    for points in zip(*samples):
+    def createSurfaceSpline(samples):
         if type == "BEZIER":
-            spline = BezierSpline.fromLocations(points)
+            spline = BezierSpline.fromLocations(samples)
             spline.isCyclic = cyclic
             spline.calculateSmoothHandles(smoothness)
         elif type == "LINEAR":
-            spline = PolySpline.fromLocations(points)
+            spline = PolySpline.fromLocations(samples)
             spline.isCyclic = cyclic
         spline.update()
+        return spline
 
-        amount = nSurfaceSamples + int(isRealCyclic)
-
+    def sampleSurfaceSpline(spline):
         if surfaceDistributionType == "RESOLUTION":
-            vertices.extend(spline.getSamples(amount, start = startSurfaceParameter, end = endSurfaceParameter))
+            return spline.getSamples(nSurfaceSamples,
+                start = startSurfaceParameter, end = endSurfaceParameter)
         elif surfaceDistributionType == "UNIFORM":
-            vertices.extend(spline.getUniformSamples(amount, resolution = uniformConverterResolution, start = startSurfaceParameter, end = endSurfaceParameter))
+            return spline.getUniformSamples(nSurfaceSamples,
+                resolution = uniformConverterResolution,
+                start = startSurfaceParameter, end = endSurfaceParameter)
 
-        if isRealCyclic: del vertices[-1]
+    def calculatePolygonIndices():
+        allSplinesCyclic = all(spline.isCyclic for spline in splines)
+        isRealCyclic = cyclic and startSurfaceParameter <= 0.0 and endSurfaceParameter >= 1.0
+        return GridMeshIndices.quadPolygons(
+                   nSplineSamples, nSurfaceSamples,
+                   joinHorizontal = allSplinesCyclic,
+                   joinVertical = isRealCyclic)
 
-    if isRealCyclic:
-        polygons = tubeQuadPolygonIndices(nSplineSamples, nSurfaceSamples)
-    else:
-        polygons = gridQuadPolygonIndices(nSplineSamples, nSurfaceSamples)
-
-    return vertices, polygons
+    return calculateVertices(), calculatePolygonIndices()
 
 
 # Revolve
@@ -66,6 +78,6 @@ def revolveProfileAroundAxis(axis, profile, nSplineSamples, nSurfaceSamples, typ
             tangents.append(tangent)
 
     vertices = tubeVertices(axisSamples, profileSamples, tangents, nSurfaceSamples)
-    polygons = tubeQuadPolygonIndices(nSplineSamples, nSurfaceSamples)
+    polygons = GridMeshIndices.quadPolygons(nSplineSamples, nSurfaceSamples, joinVertical = True)
 
     return vertices, polygons
--- algorithms/mesh_generation/indices_utils.py
+++ algorithms/mesh_generation/indices_utils.py
@@ -1,28 +1,50 @@
-def tubeQuadPolygonIndices(xDivisions, yDivisions):
-    polygons = []
-    polygons.extend(gridQuadPolygonIndices(xDivisions, yDivisions))
-    polygons.extend(gridEndEdgesQuadPolygonIndices(xDivisions, yDivisions))
-    return polygons
+class GridMeshIndices:
+    @classmethod
+    def quadPolygons(cls, xDivisions, yDivisions, joinHorizontal = False, joinVertical = False):
+        polygons = []
+        polygons.extend(cls.innerQuadPolygons(xDivisions, yDivisions))
+        if joinHorizontal:
+            polygons.extend(cls.joinHorizontalEdgesQuadPolygons(xDivisions, yDivisions))
+        if joinVertical:
+            polygons.extend(cls.joinVerticalEdgesQuadPolygons(xDivisions, yDivisions))
+        if joinHorizontal and joinVertical:
+            polygons.append(cls.joinCornersWithQuad(xDivisions, yDivisions))
+        return polygons
 
-def gridQuadPolygonIndices(xDivisions, yDivisions):
-    polygons = []
-    for i in range(0, (xDivisions - 1) * yDivisions, yDivisions):
-        for j in range(i, i + yDivisions - 1):
-            polygons.append((j, j + 1, j + yDivisions + 1, j + yDivisions))
-    return polygons
+    @staticmethod
+    def innerQuadPolygons(xDivisions, yDivisions):
+        polygons = []
+        for i in range(0, (xDivisions - 1) * yDivisions, yDivisions):
+            for j in range(i, i + yDivisions - 1):
+                polygons.append((j, j + 1, j + yDivisions + 1, j + yDivisions))
+        return polygons
 
-def gridQuadEdgeIndices(xDivisions, yDivisions):
-    edges = []
-    for i in range((xDivisions - 1) * yDivisions):
-        edges.append((i, i + yDivisions))
-    for i in range(yDivisions - 1):
-        for j in range(xDivisions):
-            firstIndex = i + j * yDivisions
-            edges.append((firstIndex, firstIndex + 1))
-    return edges
+    @staticmethod
+    def joinHorizontalEdgesQuadPolygons(xDivisions, yDivisions):
+        polygons = []
+        offset = yDivisions * (xDivisions - 1)
+        for i in range(yDivisions - 1):
+            polygons.append((i, i + offset, i + offset + 1, i + 1))
+        return polygons
 
-def gridEndEdgesQuadPolygonIndices(xDivisions, yDivisions):
-    polygons = []
-    for i in range(0, (xDivisions - 1) * yDivisions, yDivisions):
-        polygons.append((i, i + yDivisions, i + 2 * yDivisions - 1, i + yDivisions - 1))
-    return polygons
+    @staticmethod
+    def joinVerticalEdgesQuadPolygons(xDivisions, yDivisions):
+        polygons = []
+        for i in range(0, (xDivisions - 1) * yDivisions, yDivisions):
+            polygons.append((i, i + yDivisions, i + 2 * yDivisions - 1, i + yDivisions - 1))
+        return polygons
+
+    @staticmethod
+    def joinCornersWithQuad(xDivisions, yDivisions):
+        return (0, yDivisions - 1, yDivisions * xDivisions - 1, yDivisions * (xDivisions - 1))
+
+    @staticmethod
+    def innerQuadEdges(xDivisions, yDivisions):
+        edges = []
+        for i in range((xDivisions - 1) * yDivisions):
+            edges.append((i, i + yDivisions))
+        for i in range(yDivisions - 1):
+            for j in range(xDivisions):
+                firstIndex = i + j * yDivisions
+                edges.append((firstIndex, firstIndex + 1))
+        return edges
