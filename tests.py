2dc91ce25c9693b9bc3f0d8d0b7f26d88c4a475b
Jacques Lucke <mail@jlucke.com>
2016-07-13 22:57:38 +0200

    implement very basic unit test system

---
+++ tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+def runTests():
+    print("\n" * 5)
+    allTests = unittest.TestLoader().discover("animation_nodes", pattern = "test*")
+    unittest.TextTestRunner(verbosity = 2).run(allTests)
+    print("\n" * 5)
