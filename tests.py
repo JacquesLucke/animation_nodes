da5f3f2815d7e16b93bc4cd15a26515cf71b0b8d
Jacques Lucke <mail@jlucke.com>
2016-07-14 18:30:49 +0200

    cleanup

--- tests.py
+++ tests.py
@@ -3,5 +3,5 @@ import unittest
 def runTests():
     print("\n" * 5)
     allTests = unittest.TestLoader().discover("animation_nodes", pattern = "test*")
-    unittest.TextTestRunner(verbosity = 2).run(allTests)
+    unittest.TextTestRunner(verbosity = 1).run(allTests)
     print("\n" * 5)
