82fbfd508a054bbfd4d1d2ec1b2270330eb43933
Jacques Lucke <mail@jlucke.com>
2016-08-06 19:35:57 +0200

    implement matrix list socket

--- sockets/matrix.py
+++ sockets/matrix.py
@@ -1,5 +1,6 @@
 import bpy
 from mathutils import Matrix
+from .. data_structures import Matrix4x4List
 from .. base_types.socket import AnimationNodeSocket, ListSocket
 
 class MatrixSocket(bpy.types.NodeSocket, AnimationNodeSocket):
@@ -39,13 +40,32 @@ class MatrixListSocket(bpy.types.NodeSocket, ListSocket, AnimationNodeSocket):
     comparable = False
 
     @classmethod
+    def getDefaultValue(cls):
+        return Matrix4x4List()
+
+    @classmethod
+    def getDefaultValueCode(cls):
+        return "Matrix4x4List()"
+
+    @classmethod
     def getCopyExpression(cls):
-        return "[element.copy() for element in value]"
+        return "value.copy()"
+
+    @classmethod
+    def getFromValuesCode(cls):
+        return "Matrix4x4List.fromValues(value)"
+
+    @classmethod
+    def getJoinListsCode(cls):
+        return "Matrix4x4List.join(value)"
+
+    @classmethod
+    def getReverseCode(cls):
+        return "value.reversed()"
 
     @classmethod
     def correctValue(cls, value):
-        if isinstance(value, list):
-            if all(isinstance(element, Matrix) for element in value):
-                return value, 0
-        try: return [Matrix(element) for element in value], 1
+        if isinstance(value, Matrix4x4List):
+            return value, 0
+        try: return Matrix4x4List.fromValues(value), 1
         except: return cls.getDefaultValue(), 2
