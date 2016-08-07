87d3b436cc8aec0afc2ebe00563ea5071ace3a8c
Jacques Lucke <mail@jlucke.com>
2016-08-07 18:28:43 +0200

    tests

--- data_structures/lists/test_polygon_indices_list.py
+++ data_structures/lists/test_polygon_indices_list.py
@@ -20,3 +20,23 @@ class TestCopy(TestCase):
         self.assertEquals(len(copy), 2)
         self.assertEquals(copy[0], (1, 2, 3))
         self.assertEquals(copy[1], (4, 5, 6, 7))
+
+class TestReversed(TestCase):
+    def testNormal(self):
+        self.list = PolygonIndicesList.fromValues([
+            (1, 2, 3), (4, 5, 6, 7), (8, 9, 0)])
+        newList = self.list.reversed()
+        self.assertEquals(newList[0], (8, 9, 0))
+        self.assertEquals(newList[1], (4, 5, 6, 7))
+        self.assertEquals(newList[2], (1, 2, 3))
+
+class TestJoin(TestCase):
+    def testNormal(self):
+        l1 = PolygonIndicesList([(1, 2, 3), (4, 5, 6, 7)])
+        l2 = PolygonIndicesList([(2, 4, 7, 8), (1, 2, 3, 4)])
+        l3 = PolygonIndicesList([(6, 5, 4), (2, 3, 4, 5, 6)])
+        newList = PolygonIndicesList.join(l1, l2, l3)
+        self.assertEquals(len(newList), 6)
+        self.assertEquals(newList[0], (1, 2, 3))
+        self.assertEquals(newList[3], (1, 2, 3, 4))
+        self.assertEquals(newList[5], (2, 3, 4, 5, 6))
