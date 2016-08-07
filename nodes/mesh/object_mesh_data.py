14e5c9b299cd621613f3caa74bd9611556c43eb8
Jacques Lucke <mail@jlucke.com>
2016-08-07 14:06:56 +0200

    cleanup

--- nodes/mesh/object_mesh_data.py
+++ nodes/mesh/object_mesh_data.py
@@ -1,7 +1,6 @@
 import bpy
 from bpy.props import *
 from ... events import isRendering
-from ... utils.math import extractRotation
 from ... base_types.node import AnimationNode
 from ... math.list_operations import transformVector3DList
 from ... data_structures import Vector3DList, EdgeIndicesList
