bc9c9288984c67838830e5ea40c15f0b48270297
Jacques Lucke <mail@jlucke.com>
2016-08-09 11:40:15 +0200

    test get polyspline samples

--- data_structures/splines/test_poly_spline.py
+++ data_structures/splines/test_poly_spline.py
@@ -152,7 +152,76 @@ class TestEvaluateTangent(TestCase):
         testEqual(self, self.spline.evaluateTangent(0.8), (-2, -10, -8))
         testEqual(self, self.spline.evaluateTangent(1.0), (-2, -10, -8))
 
+class TestGetSamples(TestCase):
+    def setUp(self):
+        self.spline = PolySpline()
+        self.spline.appendPoint((0, 0, 0))
+        self.spline.appendPoint((4, 0, 0))
+        self.spline.appendPoint((4, 2, 0))
+        self.spline.appendPoint((10, 2, 10))
+
+    def testNormal(self):
+        samples = self.spline.getSamples(4)
+        self.assertEqual(len(samples), 4)
+        testEqual(self, samples[0], (0, 0, 0))
+        testEqual(self, samples[1], (4, 0, 0))
+        testEqual(self, samples[2], (4, 2, 0))
+        testEqual(self, samples[3], (10, 2, 10))
+
+    def testZeroSamples(self):
+        samples = self.spline.getSamples(0)
+        self.assertEqual(len(samples), 0)
+
+    def testOneSample(self):
+        samples = self.spline.getSamples(1)
+        self.assertEqual(len(samples), 1)
+        testEqual(self, samples[0], (4, 1, 0))
+
+    def testCyclicSimple(self):
+        self.spline.cyclic = True
+        samples = self.spline.getSamples(2)
+        testEqual(self, samples[0], (0, 0, 0))
+        testEqual(self, samples[1], (4, 2, 0))
+
+    def testStartEnd(self):
+        samples = self.spline.getSamples(5, start = 1/3, end = 2/3)
+        testEqual(self, samples[0], (4, 0.0, 0))
+        testEqual(self, samples[1], (4, 0.5, 0))
+        testEqual(self, samples[2], (4, 1.0, 0))
+        testEqual(self, samples[3], (4, 1.5, 0))
+        testEqual(self, samples[4], (4, 2.0, 0))
+
+    def testSwitchedParameters(self):
+        samples = self.spline.getSamples(4, start = 1, end = 0.5)
+        testEqual(self, samples[0], (10, 2, 10))
+        testEqual(self, samples[1], (7, 2, 5))
+        testEqual(self, samples[2], (4, 2, 0))
+        testEqual(self, samples[3], (4, 1, 0))
+
+    def testNegativeAmount(self):
+        with self.assertRaises(ValueError):
+            self.spline.getSamples(-1)
+
+    def testParametersNotInRange(self):
+        with self.assertRaises(ValueError):
+            self.spline.getSamples(5, start = -0.5)
+        with self.assertRaises(ValueError):
+            self.spline.getSamples(5, start = 1.5)
+        with self.assertRaises(ValueError):
+            self.spline.getSamples(5, end = -0.5)
+        with self.assertRaises(ValueError):
+            self.spline.getSamples(5, end = 1.5)
+
+    def testNotEvaluableSpline(self):
+        spline = PolySpline()
+        with self.assertRaises(Exception):
+            spline.getSamples(5)
+        spline.appendPoint((0, 0, 0))
+        with self.assertRaises(Exception):
+            spline.getSamples(5)
+
+
 def testEqual(testCase, vector1, vector2):
-    testCase.assertAlmostEqual(vector1[0], vector2[0])
-    testCase.assertAlmostEqual(vector1[1], vector2[1])
-    testCase.assertAlmostEqual(vector1[2], vector2[2])
+    testCase.assertAlmostEqual(vector1[0], vector2[0], places = 5)
+    testCase.assertAlmostEqual(vector1[1], vector2[1], places = 5)
+    testCase.assertAlmostEqual(vector1[2], vector2[2], places = 5)
