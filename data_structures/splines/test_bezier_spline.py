bfa1fd49c004c4e61fc6172d5a1fd402f6a7ffc6
Jacques Lucke <mail@jlucke.com>
2016-08-10 18:58:57 +0200

    evaluate tangent test

--- data_structures/splines/test_bezier_spline.py
+++ data_structures/splines/test_bezier_spline.py
@@ -26,6 +26,24 @@ class TestEvaluate(TestCase):
         testEqual(self, self.spline.evaluate(0.5), (1, 0, 0))
         testEqual(self, self.spline.evaluate(0.75), (0.875, -1.375, 0))
 
+class TestEvaluateTangent(TestCase):
+    def setUp(self):
+        self.spline = BezierSpline()
+        self.spline.appendPoint((-1, 0, 0), (-1, 1, 0), (-1, -1, 0))
+        self.spline.appendPoint((1, 0, 0), (1, 1, 0), (1, -1, 0))
+        self.spline.appendPoint((0, -2, 0), (1, -2, 0), (-1, -2, 0))
+
+    def testStart(self):
+        testEqual(self, self.spline.evaluateTangent(0), (0, -3, 0))
+
+    def testEnd(self):
+        testEqual(self, self.spline.evaluateTangent(1), (-3, 0, 0))
+
+    def testNormal(self):
+        testEqual(self, self.spline.evaluateTangent(0.25), (3, 1.5, 0))
+        testEqual(self, self.spline.evaluateTangent(0.5), (0, -3, 0))
+        testEqual(self, self.spline.evaluateTangent(0.75), (-0.75, -2.25, 0))
+
 def testEqual(testCase, vector1, vector2):
     testCase.assertAlmostEqual(vector1[0], vector2[0], places = 5)
     testCase.assertAlmostEqual(vector1[1], vector2[1], places = 5)
