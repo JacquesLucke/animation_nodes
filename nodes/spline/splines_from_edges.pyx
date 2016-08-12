fe3385698a165a1de7b95e0add0d20dfb0ba4001
Jacques Lucke <mail@jlucke.com>
2016-08-12 23:03:58 +0200

    speedup splines from edges node

---
+++ nodes/spline/splines_from_edges.pyx
@@ -0,0 +1,35 @@
+import bpy
+from ... base_types.node import AnimationNode
+from ... data_structures cimport PolySpline, Vector3DList, EdgeIndicesList
+from ... math.ctypes cimport Vector3
+
+class SplinesFromEdgesNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_SplinesFromEdgesNode"
+    bl_label = "Splines from Edges"
+
+    def create(self):
+        self.newInput("Vector List", "Vertices", "vertices", dataIsModified = True)
+        self.newInput("Edge Indices List", "Edge Indices", "edgeIndices")
+        self.newOutput("Spline List", "Splines", "splines")
+
+    def execute(self, Vector3DList vertices, EdgeIndicesList edgeIndices):
+        if vertices.getLength() == 0 or edgeIndices.getLength() == 0: return []
+        
+        cdef long highestIndex = edgeIndices.base.getMaxValue()
+        if highestIndex >= vertices.getLength(): return []
+
+        cdef:
+            long i
+            list splines = []
+            Vector3DList edgeVertices
+            Vector3* _vertices = <Vector3*>vertices.base.data
+            Vector3* _edgeVertices
+
+        for i in range(edgeIndices.getLength()):
+            edgeVertices = Vector3DList.__new__(Vector3DList, length = 2)
+            _edgeVertices = <Vector3*>edgeVertices.base.data
+            _edgeVertices[0] = _vertices[edgeIndices.base.data[2 * i + 0]]
+            _edgeVertices[1] = _vertices[edgeIndices.base.data[2 * i + 1]]
+            splines.append(PolySpline.__new__(PolySpline, edgeVertices))
+
+        return splines
