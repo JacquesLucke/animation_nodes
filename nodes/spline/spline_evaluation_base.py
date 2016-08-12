a5c34060ad314585cf70b223c2bbb8ff19d12f95
Jacques Lucke <mail@jlucke.com>
2016-08-12 12:43:03 +0200

    better default

--- nodes/spline/spline_evaluation_base.py
+++ nodes/spline/spline_evaluation_base.py
@@ -20,6 +20,6 @@ class SplineEvaluationBase:
     resolution = IntProperty(
         name = "Resolution",
         min = 2,
-        default = 100,
+        default = 200,
         update = propertyChanged,
         description = "Increase to have a more accurate evaluation if the type is set to Length")
