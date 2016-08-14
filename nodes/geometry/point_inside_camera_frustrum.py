0abd6217196955556f791770802f20c8be7b4902
s-leger <stephen@3dservices.ch>
2016-08-14 18:43:07 +0200

    Fix according comments

--- nodes/geometry/point_inside_camera_frustrum.py
+++ nodes/geometry/point_inside_camera_frustrum.py
@@ -10,13 +10,13 @@ class PointInCameraFrustrum(bpy.types.Node, AnimationNode):
     def create(self):
         self.newInput("Scene", "Scene", "scene", hide = True)
         self.newInput("Object", "Camera", "camera")
-        self.newInput("Vector", "3d Point", "point", value=(0, 0, 0))
-        self.newInput("Float", "Threshold % (positive = interior)", "threshold", value = 0.0)
+        self.newInput("Vector", "Point", "point", value=(0, 0, 0))
+        self.newInput("Float", "Threshold", "threshold", value = 0.0)
         
         self.newOutput("Float", "Image u", "u")
         self.newOutput("Float", "Image v", "v")
         self.newOutput("Float", "Z depth", "z")
-        self.newOutput("Boolean", "Is in frustrum", "visible")
+        self.newOutput("Boolean", "Visible", "visible")
         
     def getExecutionCode(self):
         yield "u, v, z, visible = self.isInFrustrum(scene, camera, point, threshold)"
@@ -25,18 +25,16 @@ class PointInCameraFrustrum(bpy.types.Node, AnimationNode):
         return ["bpy_extras"]
     
     def isInFrustrum(self, scene, camera, point, threshold):
-        if camera is None:
-            return 0, 0, 0, False, 0
-        if camera.type != 'CAMERA':
-            return 0, 0, 0, False, 0
+        if getattr(camera, "type", "") != 'CAMERA':
+            return 0, 0, 0, False
         co = bpy_extras.object_utils.world_to_camera_view(scene, camera, point)
-        cs, ce = camera.data.clip_start, camera.data.clip_end
+        clipStart, clipEnd = camera.data.clip_start, camera.data.clip_end
         th = threshold / 100
         if th > 0.5:
             th = 0.5
         limit = 0.5 - th
-        u = co.x
-        v = co.y
-        z = co.z
-        visible = (0.0 + th < u < 1.0 - th and 0.0 + th < v < 1.0 - th and cs < z <  ce)
+        u, y, z = co.xyz
+        visible = ( 0.0 + th < u < 1.0 - th and 
+                    0.0 + th < v < 1.0 - th and 
+                    clipStart < z <  clipEnd)
         return u, v, z, visible
