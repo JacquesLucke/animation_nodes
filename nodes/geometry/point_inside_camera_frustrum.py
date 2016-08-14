7a6cc06977b4896392be12082b29b7aa02bbe492
s-leger <stephen@3dservices.ch>
2016-08-14 17:49:34 +0200

    Update point_inside_camera_frustrum.py

--- nodes/geometry/point_inside_camera_frustrum.py
+++ nodes/geometry/point_inside_camera_frustrum.py
@@ -3,48 +3,40 @@ from bpy.props import *
 from ... base_types.node import AnimationNode
 import bpy_extras
 
-class PointInsideCameraFrustrum(bpy.types.Node, AnimationNode):
-    bl_idname = "an_PointInsideCameraFrustrum"
-    bl_label = "Point inside Camera Frustrum"
+class PointInCameraFrustrum(bpy.types.Node, AnimationNode):
+    bl_idname = "an_PointInCameraFrustrum"
+    bl_label = "Point in Camera Frustrum"
     
     def create(self):
         self.newInput("Scene", "scene", "scene", hide = True)
         self.newInput("Object", "Camera", "camera")
         self.newInput("Vector", "3d Point", "point", value=(0, 0, 0))
         self.newInput("Float", "Threshold % (positive = interior)", "threshold", value = 0.0)
-        self.newInput("Float", "Fade Area %", "fadearea", value = 10.0)
         
         self.newOutput("Float", "Image u", "u")
         self.newOutput("Float", "Image v", "v")
-        self.newOutput("Float", "Fade", "fade")
+        self.newOutput("Float", "Z depth", "z")
         self.newOutput("Boolean", "Is in frustrum", "visible")
         
     def getExecutionCode(self):
-        yield "u, v, visible, fade = self.isInFrustrum(scene, camera, point, threshold, fadearea)"
+        yield "u, v, z, visible = self.isInFrustrum(scene, camera, point, threshold)"
             
     def getUsedModules(self):
         return ["bpy_extras"]
     
-    def isInFrustrum(self, scene, camera, point, threshold, fadearea):
+    def isInFrustrum(self, scene, camera, point, threshold):
         if camera is None:
-            return 0, 0, False, 0
+            return 0, 0, 0, False, 0
         if camera.type != 'CAMERA':
-            return 0, 0, False, 0
+            return 0, 0, 0, False, 0
         co = bpy_extras.object_utils.world_to_camera_view(scene, camera, point)
         cs, ce = camera.data.clip_start, camera.data.clip_end
-        th, fa = threshold / 100, fadearea / 100
-        limit = 0.5 - th
+        th = threshold / 100
         if th > 0.5:
             th = 0.5
+        limit = 0.5 - th
         u = co.x
         v = co.y
-        visible = (0.0 + th < co.x < 1.0 - th and 0.0 + th < co.y < 1.0 - th and cs < co.z <  ce)
-        fu = (limit - abs(u - 0.5)) / fa
-        fv = (limit - abs(v - 0.5)) / fa 
-        if fu > fv:
-            fade = fv
-        else:
-            fade = fu
-        if fade > 1:
-            fade = 1
-        return u, v, visible, fade
+        z = co.z
+        visible = (0.0 + th < u < 1.0 - th and 0.0 + th < v < 1.0 - th and cs < z <  ce)
+        return u, v, z, visible
