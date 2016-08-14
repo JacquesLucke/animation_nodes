616e4b19115ea228076b97aedbeae6df5ed118dd
Jacques Lucke <mail@jlucke.com>
2016-08-14 16:11:27 +0200

    naming fix

--- nodes/object/utility_nodes/get_active_camera.py
+++ nodes/object/utility_nodes/get_active_camera.py
@@ -4,9 +4,9 @@ from .... base_types.node import AnimationNode
 class GetActiveCameraNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_GetActiveCameraNode"
     bl_label = "Get Active Camera"
-    
+
     def create(self):
-       self.newInput("Scene", "scene", "scene", hide = True)
+       self.newInput("Scene", "Scene", "scene", hide = True)
        self.newOutput("Object", "Active Camera", "activeCamera")
 
     def execute(self, scene):
