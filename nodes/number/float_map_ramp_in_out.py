032011e65ee03c295368f235e54f618f4c3e4f00
s-leger <stephen@3dservices.ch>
2016-08-15 20:13:32 +0200

    Rework
    code using same basis as range
    massive variables improvement
    no need for clampInput to make interpolation as range is checked
    use interpolation

--- nodes/number/float_map_ramp_in_out.py
+++ nodes/number/float_map_ramp_in_out.py
@@ -1,5 +1,6 @@
 import bpy
 from bpy.props import *
+from ... tree_info import keepNodeState
 from ... base_types.node import AnimationNode
 
 class FloatMapRampInOutNode(bpy.types.Node, AnimationNode):
@@ -18,27 +19,51 @@ class FloatMapRampInOutNode(bpy.types.Node, AnimationNode):
     """
     bl_idname = "an_FloatMapRampInOutNode"
     bl_label = "Map Ramp In-Out"
+    bl_width_default = 170
+
+    def settingChanged(self, context):
+        self.recreateInputs()
+        
+    clampInput = BoolProperty(name = "Clamp Input", default = True,
+        description = "The input will be between Input Min and Input Max",
+        update = settingChanged)
+     
+    useInterpolation = BoolProperty(name = "Use Interpolation", default = False,
+        description = "Don't use the normal linear interpolation between Min and Max",
+        update = settingChanged)
     
     def create(self):
-        self.newInput("Float", "Value", "val", value = 0)
-        self.newInput("Boolean", "Clamp", "clamp", value = True)
-        self.newInput("Float", "Input Min", "imin", value = 0)
-        self.newInput("Float", "Input Max", "imax", value = 1)
-        self.newInput("Float", "Output Min", "omin", value = 0)
-        self.newInput("Float", "Output Max", "omax", value = 1)
+        self.recreateInputs()
+        self.newOutput("Float", "Value", "newValue")
+        
+    @keepNodeState
+    def recreateInputs(self):
+        self.inputs.clear()
+        self.newInput("Float", "Value", "value", value = 0)
+        self.newInput("Float", "Input Min", "inMin", value = 0)
+        self.newInput("Float", "Input Max", "inMax", value = 1)
+        self.newInput("Float", "Output Min", "outMin", value = 0)
+        self.newInput("Float", "Output Max", "outMax", value = 1)
         self.newInput("Float", "Size", "size", value = 0.1)
         self.newInput("Float", "Offset", "offset", value = 0.0)
-        self.newOutput("Float", "Value", "output")
-        
-    def execute(self, val, clamp, imin, imax, omin, omax, size, offset):
-        if imin == imax or size == 0:
-            return val
-        delta_i = imax-imin
-        delta_o = omax-omin
-        offset = min(offset, 0.5)
-        interval_input = abs(((val-imin) / delta_i) - 0.5)
-        interval_ramp = 0.5 - offset
-        ramp = (interval_ramp - interval_input) / size
-        if interval_input < interval_ramp or clamp:
-            ramp = max(0.0, min(ramp, 1.0))
-        return omin + ramp * delta_o
+        if self.useInterpolation:
+            self.newInput("Interpolation", "Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
+    
+    def draw(self, layout):
+        col = layout.column(align = True)
+        col.prop(self, "useInterpolation")   
+        col.prop(self, "clampInput")   
+    
+    def getExecutionCode(self):
+        yield "if inMin == inMax or size == 0: newValue = 0"
+        yield "else:"
+        yield "    x = ((0.5 - min(offset, 0.5)) - abs(((value - inMin) / (inMax - inMin)) - 0.5)) / size"
+        if self.clampInput:
+            yield "    x = max(0.0, min(x, 1.0))"
+        yield "    if 0 < x < 1:"
+        if self.useInterpolation:
+            yield "        newValue = outMin + interpolation(x) * (outMax - outMin)"
+        else:
+            yield "        newValue = outMin + x * (outMax - outMin)"
+        yield "    else:"
+        yield "        newValue = outMin + x * (outMax - outMin)"
