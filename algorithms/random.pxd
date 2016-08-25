52e99f6d64a7ddf1d70889396e76c33f838201f3
Jacques Lucke <mail@jlucke.com>
2016-08-25 21:11:34 +0200

    random falloff node

--- algorithms/random.pxd
+++ algorithms/random.pxd
@@ -1,1 +1,2 @@
+cdef double uniformRandomNumber(int x, double min, double max)
 cdef double randomNumber(int x)
---
+++ nodes/falloff/random_falloff.pyx
@@ -0,0 +1,32 @@
+import bpy
+from ... data_structures cimport FalloffBase
+from ... base_types.node import AnimationNode
+from ... algorithms.random cimport uniformRandomNumber
+
+class RandomFalloffNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_RandomFalloffNode"
+    bl_label = "Random Falloff"
+
+    def create(self):
+        self.newInput("Integer", "Seed", "seed")
+        self.newInput("Float", "Min", "minValue", value = 0).setRange(0, 1)
+        self.newInput("Float", "Max", "maxValue", value = 1).setRange(0, 1)
+        self.newOutput("Falloff", "Falloff", "falloff")
+
+    def execute(self, seed, minValue, maxValue):
+        return RandomFalloff(seed % 0x7fffffff, minValue, maxValue)
+
+
+cdef class RandomFalloff(FalloffBase):
+    cdef:
+        int seed
+        double minValue, maxValue
+
+    def __cinit__(self, int seed, double minValue, double maxValue):
+        self.seed = (seed * 534523) % 0x7fffffff
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.dataType = "All"
+
+    cdef double evaluate(self, void* object, long index):
+        return uniformRandomNumber((self.seed + index) % 0x7fffffff, self.minValue, self.maxValue)
