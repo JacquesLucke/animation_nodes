68a1e13f11910b42e9908178efcd6153ebdfe957
Jacques Lucke <mail@jlucke.com>
2016-08-26 14:43:21 +0200

    interpolate falloff node

---
+++ nodes/falloff/interpolate_falloff.pyx
@@ -0,0 +1,31 @@
+import bpy
+from ... base_types.node import AnimationNode
+from ... data_structures cimport CompoundFalloff, Falloff, InterpolationBase
+
+class InterpolateFalloffNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_InterpolateFalloffNode"
+    bl_label = "Interpolate Falloff"
+
+    def create(self):
+        self.newInput("Falloff", "Falloff", "falloff")
+        self.newInput("Interpolation", "Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
+        self.newOutput("Falloff", "Falloff", "falloff")
+
+    def execute(self, falloff, interpolation):
+        return InterpolateFalloff(falloff, interpolation)
+
+
+cdef class InterpolateFalloff(CompoundFalloff):
+    cdef:
+        Falloff falloff
+        InterpolationBase interpolation
+
+    def __cinit__(self, Falloff falloff, InterpolationBase interpolation):
+        self.falloff = falloff
+        self.interpolation = interpolation
+
+    cdef list getDependencies(self):
+        return [self.falloff]
+
+    cdef double evaluate(self, double* dependencyResults):
+        return self.interpolation.evaluate(min(max(dependencyResults[0], 0), 1))
