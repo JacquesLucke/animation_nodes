0b82c39a24da6d5e786a1a739d67134dee3d128f
Jacques Lucke <mail@jlucke.com>
2016-08-26 14:14:07 +0200

    more options in distance falloff node

--- nodes/falloff/distance_falloff.pyx
+++ nodes/falloff/distance_falloff.pyx
@@ -3,24 +3,33 @@ from ... data_structures cimport BaseFalloff
 from ... base_types.node import AnimationNode
 from ... math cimport Vector3, toVector3, distanceVec3
 
-class DistanceFalloffNode(bpy.types.Node, AnimationNode):
-    bl_idname = "an_DistanceFalloffNode"
-    bl_label = "Distance Falloff"
+class PointDistanceFalloffNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_PointDistanceFalloffNode"
+    bl_label = "Point Distance Falloff"
 
     def create(self):
         self.newInput("Vector", "Origin", "origin")
+        self.newInput("Float", "Min Distance", "minDistance")
+        self.newInput("Float", "Max Distance", "maxDistance")
         self.newOutput("Falloff", "Falloff", "falloff")
 
-    def execute(self, origin):
-        return DistanceFalloff(origin)
+    def execute(self, origin, minDistance, maxDistance):
+        return PointDistanceFalloff(origin, minDistance, maxDistance)
 
 
-cdef class DistanceFalloff(BaseFalloff):
-    cdef Vector3 origin
+cdef class PointDistanceFalloff(BaseFalloff):
+    cdef:
+        Vector3 origin
+        double minDistance, maxDistance
+        double factor
 
-    def __cinit__(self, vector):
+    def __cinit__(self, vector, minDistance, maxDistance):
         toVector3(&self.origin, vector)
+        if minDistance == maxDistance: minDistance -= 0.00001
+        self.factor = 1 / (maxDistance - minDistance)
+        self.minDistance = minDistance
+        self.maxDistance = maxDistance
         self.dataType = "Location"
-        
+
     cdef double evaluate(self, void* object, long index):
-        return distanceVec3(&self.origin, <Vector3*>object)
+        return (distanceVec3(&self.origin, <Vector3*>object) - self.minDistance) * self.factor
