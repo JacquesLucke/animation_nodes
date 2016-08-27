71aa447c3cb0e6a650f31571b85436c96ecf3f20
Jacques Lucke <mail@jlucke.com>
2016-08-27 14:57:52 +0200

    restructure math functions

--- math/base_operations.pxd
+++ math/base_operations.pxd
@@ -1,14 +1,3 @@
 from . cimport Vector3, Matrix4
 
-cdef float lengthVec3(Vector3* v)
-cdef void scaleVec3(Vector3* v, float factor)
-cdef void addVec3(Vector3* target, Vector3* a, Vector3* b)
-cdef void subVec3(Vector3* target, Vector3* a, Vector3* b)
-cdef void mixVec3(Vector3* target, Vector3* a, Vector3* b, float factor)
-cdef void crossVec3(Vector3* result, Vector3* a, Vector3* b)
-cdef float dotVec3(Vector3* a, Vector3* b)
-
-cdef void normalizeVec3(Vector3* v)
-cdef float distanceVec3(Vector3* a, Vector3* b)
-cdef float distanceSquaredVec3(Vector3* a, Vector3* b)
 cdef void transformVec3(Vector3* target, Vector3* v, Matrix4* m)
--- math/base_operations.pyx
+++ math/base_operations.pyx
@@ -1,53 +1,3 @@
-from libc.math cimport sqrt
-
-cdef void scaleVec3(Vector3* v, float factor):
-    v.x *= factor
-    v.y *= factor
-    v.z *= factor
-
-cdef float lengthVec3(Vector3* v):
-    return sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2)
-
-cdef void addVec3(Vector3* target, Vector3* a, Vector3* b):
-    target.x = a.x + b.x
-    target.y = a.y + b.y
-    target.z = a.z + b.z
-
-cdef void subVec3(Vector3* target, Vector3* a, Vector3* b):
-    target.x = a.x - b.x
-    target.y = a.y - b.y
-    target.z = a.z - b.z
-
-cdef void mixVec3(Vector3* target, Vector3* a, Vector3* b, float factor):
-    cdef float newX, newY, newZ
-    newX = a.x * (1 - factor) + b.x * factor
-    newY = a.y * (1 - factor) + b.y * factor
-    newZ = a.z * (1 - factor) + b.z * factor
-    target.x = newX
-    target.y = newY
-    target.z = newZ
-
-cdef void normalizeVec3(Vector3* v):
-    cdef float length = sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2)
-    if length != 0:
-        v.x /= length
-        v.y /= length
-        v.z /= length
-    else:
-        v.x = 0
-        v.y = 0
-        v.z = 0
-
-cdef float distanceVec3(Vector3* a, Vector3* b):
-    return sqrt(distanceSquaredVec3(a, b))
-
-cdef float distanceSquaredVec3(Vector3* a, Vector3* b):
-    cdef:
-        float diff1 = (a.x - b.x)
-        float diff2 = (a.y - b.y)
-        float diff3 = (a.z - b.z)
-    return diff1 * diff1 + diff2 * diff2 + diff3 * diff3
-
 cdef void transformVec3(Vector3* target, Vector3* v, Matrix4* m):
     cdef float newX, newY, newZ
     newX = v.x * m.a11 + v.y * m.a12 + v.z * m.a13 + m.a14
@@ -56,11 +6,3 @@ cdef void transformVec3(Vector3* target, Vector3* v, Matrix4* m):
     target.x = newX
     target.y = newY
     target.z = newZ
-
-cdef float dotVec3(Vector3* a, Vector3* b):
-    return a.x * b.x + a.y * b.y + a.z * b.z
-
-cdef void crossVec3(Vector3* result, Vector3* a, Vector3* b):
-    result.x = a.y * b.z - a.z * b.y
-    result.y = a.z * b.x - a.x * b.z
-    result.z = a.x * b.y - a.y * b.x
