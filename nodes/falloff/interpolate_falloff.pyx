7ad048899344cfb6c4dadbf4b0094f4553a6c5ec
Jacques Lucke <mail@jlucke.com>
2016-08-28 18:42:48 +0200

    fix

--- nodes/falloff/interpolate_falloff.pyx
+++ nodes/falloff/interpolate_falloff.pyx
@@ -7,9 +7,9 @@ class InterpolateFalloffNode(bpy.types.Node, AnimationNode):
     bl_label = "Interpolate Falloff"
 
     def create(self):
-        self.newInput("Falloff", "Falloff", "falloff")
+        self.newInput("Falloff", "Falloff", "inFalloff")
         self.newInput("Interpolation", "Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
-        self.newOutput("Falloff", "Falloff", "falloff")
+        self.newOutput("Falloff", "Falloff", "outFalloff")
 
     def execute(self, falloff, interpolation):
         return InterpolateFalloff(falloff, interpolation)
