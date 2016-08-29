7d09996d529cc7c4701de6750a2b19a8c81f828c
Jacques Lucke <mail@jlucke.com>
2016-08-29 10:36:18 +0200

    implement minValue and maxValue into PointDistanceFalloff

--- nodes/falloff/point_distance_falloff.pyx
+++ nodes/falloff/point_distance_falloff.pyx
@@ -11,28 +11,35 @@ class PointDistanceFalloffNode(bpy.types.Node, AnimationNode):
         self.newInput("Vector", "Origin", "origin")
         self.newInput("Float", "Min Distance", "minDistance")
         self.newInput("Float", "Max Distance", "maxDistance", value = 5)
+        self.newInput("Float", "Min Value", "minValue", hide = True, value = 0).setRange(0, 1)
+        self.newInput("Float", "Max Value", "maxValue", hide = True, value = 1).setRange(0, 1)
         self.newOutput("Falloff", "Falloff", "falloff")
 
-    def execute(self, origin, minDistance, maxDistance):
-        return PointDistanceFalloff(origin, minDistance, maxDistance)
+    def execute(self, origin, minDistance, maxDistance, minValue, maxValue):
+        return PointDistanceFalloff(origin, minDistance, maxDistance, minValue, maxValue)
 
 
 cdef class PointDistanceFalloff(BaseFalloff):
     cdef:
         Vector3 origin
-        double factor, minDistance, maxDistance
+        double factor
+        double minDistance, maxDistance
+        double minValue, maxValue
 
-    def __cinit__(self, vector, double minDistance, double maxDistance):
+    def __cinit__(self, vector, double minDistance, double maxDistance,
+                                double minValue, double maxValue):
         if minDistance == maxDistance: minDistance -= 0.00001
-        self.factor = 1 / (maxDistance - minDistance)
+        self.factor = (maxValue - minValue) / (maxDistance - minDistance)
         self.minDistance = minDistance
         self.maxDistance = maxDistance
+        self.minValue = minValue
+        self.maxValue = maxValue
         toVector3(&self.origin, vector)
         self.dataType = "Location"
-        self.clamped = True
+        self.clamped = 0 <= min(minValue, maxValue) <= max(minValue, maxValue) <= 1
 
     cdef double evaluate(self, void* value, long index):
         cdef double distance = distanceVec3(&self.origin, <Vector3*>value)
-        if distance <= self.minDistance: return 1
-        if distance <= self.maxDistance: return 1 - (distance - self.minDistance) * self.factor
-        return 0
+        if distance <= self.minDistance: return self.maxValue
+        if distance <= self.maxDistance: return self.maxValue - (distance - self.minDistance) * self.factor
+        return self.minValue
