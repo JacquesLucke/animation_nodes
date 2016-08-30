9e40583dcf685fae4f3377c827e1c74b12459835
Jacques Lucke <mail@jlucke.com>
2016-08-30 22:29:00 +0200

    implement 4x4 matrix list

--- math/conversion.pyx
+++ math/conversion.pyx
@@ -1,6 +1,8 @@
 from mathutils import Vector, Matrix
 
 cdef toMatrix4(Matrix4* m, value):
+    if not (len(value.rows) == len(value.rows[0]) == 4):
+        raise TypeError("element is not a 4x4 matrix")
     m.a11 = value[0][0]
     m.a12 = value[0][1]
     m.a13 = value[0][2]
@@ -28,6 +30,8 @@ cdef toPyMatrix(Matrix4* m):
                    (m.a41, m.a42, m.a43, m.a44)))
 
 cdef toVector3(Vector3* v, value):
+    if len(value) != 3:
+        raise TypeError("element is not a 3D vector")
     v.x = value[0]
     v.y = value[1]
     v.z = value[2]
