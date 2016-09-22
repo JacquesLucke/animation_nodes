cf48ce3a7cf5e2641377312dd5e25da53a71e72e
Jacques Lucke <mail@jlucke.com>
2016-09-22 13:17:34 +0200

    update shift list node

--- nodes/list/shift_list.py
+++ nodes/list/shift_list.py
@@ -1,50 +1,28 @@
 import bpy
 from bpy.props import *
 from ... sockets.info import isList
-from ... tree_info import keepNodeState
-from ... base_types import AnimationNode
+from ... base_types import AnimationNode, UpdateAssignedListDataType
 
 class ShiftListNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ShiftListNode"
     bl_label = "Shift List"
 
-    def assignedTypeChanged(self, context):
-        self.generateSockets()
-
-    assignedType = StringProperty(update = assignedTypeChanged)
+    assignedType = StringProperty(update = AnimationNode.updateSockets, default = "Float List")
 
     def create(self):
-        self.assignedType = "Object List"
+        listDataType = self.assignedType
+
+        self.newInput(listDataType, "List", "inList", dataIsModified = True)
+        self.newInput("Integer", "Amount", "amount")
+        self.newOutput(listDataType, "Shifted List", "shiftedList")
+
+        self.newSocketEffect(UpdateAssignedListDataType("assignedType", "LIST",
+            [(self.inputs[0], "LIST"),
+             (self.outputs[0], "LIST")]
+        ))
 
     def getExecutionCode(self):
         yield "if len(inList) == 0: shiftedList = self.outputs[0].getDefaultValue()"
         yield "else:"
         yield "    shiftAmount = amount % len(inList)"
         yield "    shiftedList = inList[-shiftAmount:] + inList[:-shiftAmount]"
-
-    def edit(self):
-        listDataType = self.getWantedDataType()
-        self.assignType(listDataType)
-
-    def getWantedDataType(self):
-        listInput = self.inputs[0].dataOrigin
-        listOutputs = self.outputs[0].dataTargets
-
-        if listInput is not None: return listInput.dataType
-        if len(listOutputs) == 1: return listOutputs[0].dataType
-        return self.inputs[0].dataType
-
-    def assignType(self, listDataType):
-        if not isList(listDataType): return
-        if listDataType == self.assignedType: return
-        self.assignedType = listDataType
-
-    @keepNodeState
-    def generateSockets(self):
-        self.inputs.clear()
-        self.outputs.clear()
-
-        listDataType = self.assignedType
-        self.newInput(listDataType, "List", "inList", dataIsModified = True)
-        self.newInput("Integer", "Amount", "amount")
-        self.newOutput(listDataType, "Shifted List", "shiftedList")
