7f31dc8ed76a3e9b163f946a90fbfdbf40dc67d3
Jacques Lucke <mail@jlucke.com>
2016-09-22 13:07:23 +0200

    update reverse list node

--- nodes/list/reverse_list.py
+++ nodes/list/reverse_list.py
@@ -2,45 +2,29 @@ import bpy
 from bpy.props import *
 from ... sockets.info import isList
 from ... tree_info import keepNodeLinks
-from ... base_types import AnimationNode
+from ... base_types import AnimationNode, UpdateAssignedListDataType
 
 class ReverseListNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ReverseListNode"
     bl_label = "Reverse List"
 
-    def assignedTypeChanged(self, context):
-        self.generateSockets()
-
-    assignedType = StringProperty(update = assignedTypeChanged)
+    assignedType = StringProperty(update = AnimationNode.updateSockets, default = "Float List")
 
     def create(self):
-        self.assignedType = "Object List"
+        listDataType = self.assignedType
+        self.newInput(listDataType, "List", "inList", dataIsModified = True)
+        self.newOutput(listDataType, "Reversed List", "reversedList")
+
+        self.newSocketEffect(UpdateAssignedListDataType("assignedType", "LIST",
+            [(self.inputs[0], "LIST"),
+             (self.outputs[0], "LIST")]
+        ))
 
     def getExecutionCode(self):
         reverseCode = self.inputs[0].getReverseCode().replace("value", "inList")
         return "reversedList = " + reverseCode
 
-    def edit(self):
-        listDataType = self.getWantedDataType()
-        self.assignType(listDataType)
-
-    def getWantedDataType(self):
-        listInput = self.inputs[0].dataOrigin
-        listOutputs = self.outputs[0].dataTargets
-
-        if listInput is not None: return listInput.dataType
-        if len(listOutputs) == 1: return listOutputs[0].dataType
-        return self.inputs[0].dataType
-
     def assignType(self, listDataType):
         if not isList(listDataType): return
         if listDataType == self.assignedType: return
         self.assignedType = listDataType
-
-    @keepNodeLinks
-    def generateSockets(self):
-        self.inputs.clear()
-        self.outputs.clear()
-        listDataType = self.assignedType
-        self.newInput(listDataType, "List", "inList", dataIsModified = True)
-        self.newOutput(listDataType, "Reversed List", "reversedList")
