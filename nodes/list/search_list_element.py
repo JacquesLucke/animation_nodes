eb7693dc179428923445d57bbdb584420ae0c14f
Jacques Lucke <mail@jlucke.com>
2016-09-22 13:10:44 +0200

    update search list element node

--- nodes/list/search_list_element.py
+++ nodes/list/search_list_element.py
@@ -2,23 +2,30 @@ import bpy
 from bpy.props import *
 from ... tree_info import keepNodeLinks
 from ... events import executionCodeChanged
-from ... base_types import AnimationNode
 from ... sockets.info import isBase, toBaseDataType, toListDataType
+from ... base_types import AnimationNode, UpdateAssignedListDataType
 
 class SearchListElementNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SearchListElementNode"
     bl_label = "Search List Element"
 
-    def assignedTypeChanged(self, context):
-        self.generateSockets()
-
-    assignedType = StringProperty(update = assignedTypeChanged)
+    assignedType = StringProperty(update = AnimationNode.updateSockets, default = "Float")
 
     def create(self):
-        self.assignedType = "Float"
+        baseDataType = self.assignedType
+        listDataType = toListDataType(self.assignedType)
+
+        self.newInput(listDataType, "List", "list", dataIsModified  = True)
+        self.newInput(baseDataType, "Search", "search", dataIsModified = True)
+
         self.newOutput("an_IntegerSocket", "First Index", "firstIndex")
         self.newOutput("an_IntegerListSocket", "All Indices", "allIndices")
         self.newOutput("an_IntegerSocket", "Occurrences", "occurrences")
+
+        self.newSocketEffect(UpdateAssignedListDataType("assignedType", "BASE",
+            [(self.inputs[0], "LIST"),
+             (self.inputs[1], "BASE")]
+        ))
 
     def drawAdvanced(self, layout):
         self.invokeSocketTypeChooser(layout, "assignListDataType",
@@ -39,18 +46,6 @@ class SearchListElementNode(bpy.types.Node, AnimationNode):
             if isLinked["occurrences"]:
                 yield "occurrences = list.count(search)"
 
-    def edit(self):
-        baseDataType = self.getWantedDataType()
-        self.assignType(baseDataType)
-
-    def getWantedDataType(self):
-        listInput = self.inputs["List"].dataOrigin
-        elementInput = self.inputs["Search"].dataOrigin
-
-        if listInput is not None: return toBaseDataType(listInput.bl_idname)
-        if elementInput is not None: return elementInput.dataType
-        return self.inputs["Search"].dataType
-
     def assignListDataType(self, listDataType):
         self.assignType(toBaseDataType(listDataType))
 
@@ -58,11 +53,3 @@ class SearchListElementNode(bpy.types.Node, AnimationNode):
         if not isBase(baseDataType): return
         if baseDataType == self.assignedType: return
         self.assignedType = baseDataType
-
-    @keepNodeLinks
-    def generateSockets(self):
-        self.inputs.clear()
-        baseDataType = self.assignedType
-        listDataType = toListDataType(self.assignedType)
-        self.newInput(listDataType, "List", "list", dataIsModified  = True)
-        self.newInput(baseDataType, "Search", "search", dataIsModified = True)
