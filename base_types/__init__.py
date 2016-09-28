1a13481f386433739dfb10c60a5adf02512768b2
Jacques Lucke <mail@jlucke.com>
2016-09-28 21:48:21 +0200

    cleanup

--- base_types/__init__.py
+++ base_types/__init__.py
@@ -2,7 +2,6 @@ from . template import Template
 from . node import AnimationNode
 from . node_tree import AnimationNodeTree
 from . socket import AnimationNodeSocket, ListSocket
-from . dynamic_socket_set import DynamicSocketSet
 from . socket_effects import (AutoSelectFloatOrInteger,
                               AutoSelectListDataType,
                               AutoSelectDataType,
