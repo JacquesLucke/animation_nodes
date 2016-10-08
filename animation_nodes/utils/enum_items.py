ca0f591101f0d2e0f85bca6a2692924b3bfdeeae
Jacques Lucke <mail@jlucke.com>
2016-10-09 01:44:39 +0200

    cleanup

--- animation_nodes/utils/enum_items.py
+++ animation_nodes/utils/enum_items.py
@@ -1,24 +1,9 @@
-from . hash import hashStringToNumber as hashText
-
-# itemData is a list of dicts
-# the dicts have at least an 'id' key
-def enumItemsFromDicts(itemData):
-    items = []
-    for data in itemData:
-        if "value" not in data: raise Exception("'value' key is missing")
-        if "name" not in data: data["name"] = data["value"]
-        if "description" not in data: data["description"] = ""
-        if "icon" not in data: data["icon"] = "NONE"
-        number = hashText(data["id"]) if "id" in data else hashText(data["value"])
-        items.append((data["value"], data["name"], data["description"], data["icon"], number))
-    if len(items) == 0:
-        items = [("NONE", "NONE", "")]
-    return items
+from .. algorithms.hashing import strToEnumItemID
 
 def enumItemsFromList(itemData):
     items = []
     for element in itemData:
-        items.append((element, element, "", "NONE", hashText(element)))
+        items.append((element, element, "", "NONE", strToEnumItemID(element)))
     if len(items) == 0:
         items = [("NONE", "NONE", "")]
     return items
