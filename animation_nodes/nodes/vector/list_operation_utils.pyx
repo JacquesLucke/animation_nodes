0d2b8ec1ea8dce12053b1c34db7dd8b8806c1eae
Jacques Lucke <mail@jlucke.com>
2016-10-12 01:34:09 +0200

    vectorize Separate Vector node

--- animation_nodes/nodes/vector/list_operation_utils.pyx
+++ animation_nodes/nodes/vector/list_operation_utils.pyx
@@ -2,10 +2,25 @@ from ... data_structures cimport DoubleList, Vector3DList
 
 def combineDoubleListsToVectorList(DoubleList x, DoubleList y, DoubleList z):
     assert x.length == y.length == z.length
-    cdef Vector3DList output = Vector3DList(length = len(x))
+    cdef Vector3DList output = Vector3DList(length = x.length)
     cdef long i
-    for i in range(len(output)):
+    for i in range(output.length):
         output.data[i].x = x.data[i]
         output.data[i].y = y.data[i]
         output.data[i].z = z.data[i]
+    return output
+
+def getAxisListOfVectorList(Vector3DList myList, str axis):
+    assert axis in "xyz"
+    cdef DoubleList output = DoubleList(length = myList.length)
+    cdef long i
+    if axis == "x":
+        for i in range(output.length):
+            output.data[i] = myList.data[i].x
+    elif axis == "y":
+        for i in range(output.length):
+            output.data[i] = myList.data[i].y
+    elif axis == "z":
+        for i in range(output.length):
+            output.data[i] = myList.data[i].z
     return output
--- animation_nodes/nodes/vector/separate_vector.py
+++ animation_nodes/nodes/vector/separate_vector.py
@@ -1,18 +1,39 @@
 import bpy
-from ... base_types import AnimationNode
+from bpy.props import *
+from . list_operation_utils import getAxisListOfVectorList
+from ... base_types import AnimationNode, AutoSelectVectorization
 
 class SeparateVectorNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SeparateVectorNode"
     bl_label = "Separate Vector"
 
+    useList = BoolProperty(default = False, update = AnimationNode.updateSockets)
+
     def create(self):
-        self.newInput("Vector", "Vector", "vector")
-        self.newOutput("Float", "X", "x")
-        self.newOutput("Float", "Y", "y")
-        self.newOutput("Float", "Z", "z")
+        self.newInputGroup(self.useList,
+            ("Vector", "Vector", "vector"),
+            ("Vector List", "Vectors", "vectors"))
+
+        for axis in "XYZ":
+            self.newOutputGroup(self.useList,
+                ("Float", axis, axis.lower()),
+                ("Float List", axis, axis.lower()))
+
+        vectorization = AutoSelectVectorization()
+        vectorization.input(self, "useList", self.inputs[0])
+        vectorization.output(self, "useList", self.outputs[0])
+        vectorization.output(self, "useList", self.outputs[1])
+        vectorization.output(self, "useList", self.outputs[2])
+        self.newSocketEffect(vectorization)
 
     def getExecutionCode(self):
         isLinked = self.getLinkedOutputsDict()
-        if isLinked["x"]: yield "x = vector[0]"
-        if isLinked["y"]: yield "y = vector[1]"
-        if isLinked["z"]: yield "z = vector[2]"
+        for i, axis in enumerate("xyz"):
+            if isLinked[axis]:
+                if self.useList:
+                    yield "{0} = self.getAxisList(vectors, '{0}')".format(axis)
+                else:
+                    yield "{} = vector[{}]".format(axis, i)
+
+    def getAxisList(self, vectors, axis):
+        return getAxisListOfVectorList(vectors, axis)
