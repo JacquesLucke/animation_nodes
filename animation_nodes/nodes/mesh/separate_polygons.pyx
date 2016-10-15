8dbabc67ae8a5316031f5212b7b91c6cbe945f5b
Jacques Lucke <mail@jlucke.com>
2016-10-15 19:24:38 +0200

    cleanup Separate Polygons node

--- animation_nodes/nodes/mesh/separate_polygons.pyx
+++ animation_nodes/nodes/mesh/separate_polygons.pyx
@@ -1,4 +1,5 @@
 import bpy
+from ... math cimport Vector3
 from ... base_types.node import AnimationNode
 from ... data_structures cimport Vector3DList, PolygonIndicesList
 from libc.string cimport memcpy
@@ -15,25 +16,33 @@ class SeparatePolygonsNode(bpy.types.Node, AnimationNode):
         self.newOutput("Polygon Indices List", "Polygon Indices", "outPolygonIndices")
 
     def execute(self, Vector3DList oldVertices, PolygonIndicesList oldPolygons):
-        cdef:
-            Vector3DList newVertices
-            PolygonIndicesList newPolygons
+        return separatePolygons(oldVertices, oldPolygons)
 
-        newVertices = Vector3DList(length = oldPolygons.indices.length)
-        newPolygons = PolygonIndicesList(indicesAmount = oldPolygons.indices.length,
-                                         polygonAmount = oldPolygons.getLength())
+def separatePolygons(Vector3DList oldVertices, PolygonIndicesList oldPolygons):
+    cdef Vector3DList newVertices
+    cdef PolygonIndicesList newPolygons
 
-        memcpy(newPolygons.polyStarts.data,
-               oldPolygons.polyStarts.data,
-               oldPolygons.polyStarts.length * oldPolygons.polyStarts.getElementSize())
+    newVertices = Vector3DList(length = oldPolygons.indices.length)
+    newPolygons = PolygonIndicesList(indicesAmount = oldPolygons.indices.length,
+                                     polygonAmount = oldPolygons.getLength())
 
-        memcpy(newPolygons.polyLengths.data,
-               oldPolygons.polyLengths.data,
-               oldPolygons.polyLengths.length * oldPolygons.polyLengths.getElementSize())
+    memcpy(newPolygons.polyStarts.data,
+           oldPolygons.polyStarts.data,
+           oldPolygons.polyStarts.length * oldPolygons.polyStarts.getElementSize())
 
-        cdef long i
-        for i in range(oldPolygons.indices.length):
-            newPolygons.indices.data[i] = i
-            newVertices.data[i] = oldVertices.data[oldPolygons.indices.data[i]]
+    memcpy(newPolygons.polyLengths.data,
+           oldPolygons.polyLengths.data,
+           oldPolygons.polyLengths.length * oldPolygons.polyLengths.getElementSize())
 
-        return newVertices, newPolygons
+    cdef:
+        long i
+        Vector3* _oldVertices = oldVertices.data
+        Vector3* _newVertices = newVertices.data
+        unsigned int* _oldIndices = oldPolygons.indices.data
+        unsigned int* _newIndices = newPolygons.indices.data
+
+    for i in range(oldPolygons.indices.length):
+        _newIndices[i] = i
+        _newVertices[i] = _oldVertices[_oldIndices[i]]
+
+    return newVertices, newPolygons
