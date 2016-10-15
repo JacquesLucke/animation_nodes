f0271bfdff9e67abff527ab94900e857a939f2c0
Jacques Lucke <mail@jlucke.com>
2016-10-15 19:58:49 +0200

    cleanup Transform Polygons node

--- animation_nodes/nodes/mesh/transform_polygons.pyx
+++ animation_nodes/nodes/mesh/transform_polygons.pyx
@@ -1,4 +1,5 @@
 import bpy
+from bpy.props import *
 from ... base_types.node import AnimationNode
 from ... data_structures cimport Vector3DList, PolygonIndicesList, Matrix4x4List
 from ... math cimport transformVec3AsPoint_InPlace, Matrix4
@@ -7,6 +8,8 @@ class TransformPolygonsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TransformPolygonsNode"
     bl_label = "Transform Polygons"
 
+    errorMessage = StringProperty()
+
     def create(self):
         self.newInput("Vector List", "Vertices", "vertices", dataIsModified = True)
         self.newInput("Polygon Indices List", "Polygon Indices", "polygonIndices")
@@ -14,19 +17,33 @@ class TransformPolygonsNode(bpy.types.Node, AnimationNode):
 
         self.newOutput("Vector List", "Vertices", "vertices")
 
+    def draw(self, layout):
+        if self.errorMessage != "":
+            layout.label(self.errorMessage, icon = "ERROR")
+
     def execute(self, Vector3DList vertices, PolygonIndicesList polygons, Matrix4x4List matrices):
-        cdef:
-            Matrix4* matrix
-            long i, j
-            long start, length
-            long index
+        self.errorMessage = ""
+        if len(polygons) != 0 and polygons.getMaxIndex() >= len(vertices):
+            self.errorMessage = "Invalid polygon indices"
+            return vertices
+        if len(polygons) != len(matrices):
+            self.errorMessage = "Different amount of polygons and matrices"
+            return vertices
 
-        for i in range(matrices.length):
-            matrix = matrices.data + i
-            start = polygons.polyStarts.data[i]
-            length = polygons.polyLengths.data[i]
-            for j in range(length):
-                index = polygons.indices.data[start + j]
-                transformVec3AsPoint_InPlace(vertices.data + polygons.indices.data[index], matrix)
-
+        transformPolygons(vertices, polygons, matrices)
         return vertices
+
+def transformPolygons(Vector3DList vertices, PolygonIndicesList polygons, Matrix4x4List matrices):
+    cdef:
+        Matrix4* matrix
+        long i, j
+        long start, length
+        long index
+
+    for i in range(matrices.length):
+        matrix = matrices.data + i
+        start = polygons.polyStarts.data[i]
+        length = polygons.polyLengths.data[i]
+        for j in range(length):
+            index = polygons.indices.data[start + j]
+            transformVec3AsPoint_InPlace(vertices.data + polygons.indices.data[index], matrix)
