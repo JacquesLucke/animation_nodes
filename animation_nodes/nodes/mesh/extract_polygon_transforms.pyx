95d8c063ba624d52dec83891ef72341a0b0c2ce2
Jacques Lucke <mail@jlucke.com>
2016-10-15 20:08:46 +0200

    cleanup Extract Polygon Transforms node

--- animation_nodes/nodes/mesh/extract_polygon_transforms.pyx
+++ animation_nodes/nodes/mesh/extract_polygon_transforms.pyx
@@ -1,4 +1,5 @@
 import bpy
+from bpy.props import *
 from ... base_types.node import AnimationNode
 from ... data_structures cimport Vector3DList, PolygonIndicesList, Matrix4x4List
 from ... math cimport Vector3, setTranslationMatrix, scaleVec3, scaleVec3, toPyVector3
@@ -7,6 +8,8 @@ class ExtractPolygonTransformsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ExtractPolygonTransformsNode"
     bl_label = "Extract Polygon Transforms"
 
+    errorMessage = StringProperty()
+
     def create(self):
         self.newInput("Vector List", "Vertices", "vertices")
         self.newInput("Polygon Indices List", "Polygon Indices", "polygonIndices")
@@ -14,23 +17,35 @@ class ExtractPolygonTransformsNode(bpy.types.Node, AnimationNode):
         self.newOutput("Matrix List", "Transforms", "transforms")
         self.newOutput("Matrix List", "Inverted Transforms", "invertedTransforms")
 
+    def draw(self, layout):
+        if self.errorMessage != "":
+            layout.label(self.errorMessage, icon = "ERROR")
+
     def execute(self, Vector3DList vertices, PolygonIndicesList polygons):
-        cdef:
-            Matrix4x4List transforms = Matrix4x4List(length = polygons.getLength())
-            Matrix4x4List invertedTransforms = Matrix4x4List(length = polygons.getLength())
-            Vector3 center, invertedCenter
-            long i, j
-            long start, length
+        self.errorMessage = ""
+        if len(polygons) == 0 or polygons.getMaxIndex() < len(vertices):
+            return extractPolygonTransforms(vertices, polygons)
+        else:
+            self.errorMessage = "Invalid polygon indices"
+            return Matrix4x4List(), Matrix4x4List()
 
-        # TODO check indices
+def extractPolygonTransforms(Vector3DList vertices, PolygonIndicesList polygons):
+    cdef:
+        Matrix4x4List transforms = Matrix4x4List(length = polygons.getLength())
+        Matrix4x4List invertedTransforms = Matrix4x4List(length = polygons.getLength())
+        Vector3 center, invertedCenter
+        long i, j
+        long start, length
 
-        for i in range(transforms.length):
-            calcPolygonCenter(&center, vertices, polygons, i)
-            setTranslationMatrix(transforms.data + i, &center)
-            scaleVec3(&invertedCenter, &center, -1)
-            setTranslationMatrix(invertedTransforms.data + i, &invertedCenter)
+    # TODO check indices
 
-        return transforms, invertedTransforms
+    for i in range(transforms.length):
+        calcPolygonCenter(&center, vertices, polygons, i)
+        setTranslationMatrix(transforms.data + i, &center)
+        scaleVec3(&invertedCenter, &center, -1)
+        setTranslationMatrix(invertedTransforms.data + i, &invertedCenter)
+
+    return transforms, invertedTransforms
 
 cdef calcPolygonCenter(Vector3* target, Vector3DList vertices, PolygonIndicesList polygons, long index):
     cdef:
