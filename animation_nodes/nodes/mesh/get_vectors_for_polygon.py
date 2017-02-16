000395c385ad1a02b239c42a41a34bf994c0d93a
Jacques Lucke <mail@jlucke.com>
2017-02-17 00:20:43 +0100

    rename to Get Vectors for Polygon (because it also works for normals etc)

--- animation_nodes/nodes/mesh/get_vectors_for_polygon.py
+++ animation_nodes/nodes/mesh/get_vectors_for_polygon.py
@@ -2,17 +2,17 @@ import bpy
 from bpy.props import *
 from ... base_types.node import AnimationNode
 
-class GetPolygonVerticesNode(bpy.types.Node, AnimationNode):
-    bl_idname = "an_GetPolygonVerticesNode"
-    bl_label = "Get Polygon Vertices"
-    bl_width_default = 160
+class GetVectorsForPolygonNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_GetVectorsForPolygonNode"
+    bl_label = "Get Vectors for Polygon"
+    bl_width_default = 170
 
     errorMessage = StringProperty()
 
     def create(self):
-        self.newInput("Vector List", "All Vertices", "allVertices")
         self.newInput("Polygon Indices", "Polygon Indices", "polygonIndices")
-        self.newOutput("Vector List", "Polygon Vertices", "polygonVertices")
+        self.newInput("Vector List", "All Vectors", "allVectors")
+        self.newOutput("Vector List", "Polygon Vectors", "polygonVectors")
 
     def draw(self, layout):
         if self.errorMessage != "":
@@ -21,7 +21,7 @@ class GetPolygonVerticesNode(bpy.types.Node, AnimationNode):
     def getExecutionCode(self):
         yield "try:"
         yield "    self.errorMessage = ''"
-        yield "    polygonVertices = allVertices[polygonIndices]"
+        yield "    polygonVectors = allVectors[polygonIndices]"
         yield "except IndexError:"
         yield "    self.errorMessage = 'Invalid Indices'"
-        yield "    polygonVertices = Vector3DList()"
+        yield "    polygonVectors = Vector3DList()"
