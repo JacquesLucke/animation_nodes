3d3015a530540807278e07dbc45586ab77be5a9d
Jacques Lucke <mail@jlucke.com>
2017-02-16 21:23:23 +0100

    fix interpolation from fcurve node

--- animation_nodes/nodes/interpolation/from_fcurve.py
+++ animation_nodes/nodes/interpolation/from_fcurve.py
@@ -11,11 +11,11 @@ class InterpolationFromFCurveNode(bpy.types.Node, AnimationNode):
         self.newOutput("Interpolation", "Interpolation", "interpolation")
 
     def execute(self, fCurve):
-        if fCurve is None: return Linear
+        if fCurve is None: return Linear()
         startX, endX = fCurve.range()
-        if startX == endX: return Linear
+        if startX == endX: return Linear()
         startY = fCurve.evaluate(startX)
         endY = fCurve.evaluate(endX)
-        if startY == endY: return Linear
+        if startY == endY: return Linear()
 
         return FCurveMapping(fCurve, startX, endX - startX, -startY, 1 / (endY - startY))
