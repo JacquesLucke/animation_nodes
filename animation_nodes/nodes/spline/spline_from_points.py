7a930d26196683e2b16086bbdb04b18767c9aa46
Jacques Lucke <mail@jlucke.com>
2017-03-14 11:53:38 +0100

    create bezier spline from points

--- animation_nodes/nodes/spline/spline_from_points.py
+++ animation_nodes/nodes/spline/spline_from_points.py
@@ -6,27 +6,45 @@ from ... data_structures.splines.poly_spline import PolySpline
 from ... data_structures.splines.bezier_spline import BezierSpline
 
 splineTypeItems = [
-    ("BEZIER", "Bezier", "Each control point has two handles"),
-    ("POLY", "Poly", "Linear interpolation between the spline points")]
+    ("BEZIER", "Bezier", "Each control point has two handles", "CURVE_BEZCURVE", 0),
+    ("POLY", "Poly", "Linear interpolation between the spline points", "NOCURVE", 1)
+]
 
 class SplineFromPointsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SplineFromPointsNode"
     bl_label = "Spline from Points"
 
     splineType = EnumProperty(name = "Spline Type", default = "BEZIER",
-        items = splineTypeItems, update = propertyChanged)
+        items = splineTypeItems, update = AnimationNode.refresh)
 
     def create(self):
-        self.newInput("Vector List", "Points", "points")
+        self.newInput("Vector List", "Points", "points", dataIsModified = True)
+        if self.splineType == "BEZIER":
+            self.newInput("Vector List", "Left Handles", "leftHandles", dataIsModified = True)
+            self.newInput("Vector List", "Right Handles", "rightHandles", dataIsModified = True)
         self.newInput("Boolean", "Cyclic", "cyclic", value = False)
         self.newOutput("Spline", "Spline", "spline")
 
     def draw(self, layout):
         layout.prop(self, "splineType", text = "")
 
-    def execute(self, points, cyclic):
+    def getExecutionFunctionName(self):
         if self.splineType == "BEZIER":
-            spline = BezierSpline(points, points.copy(), points.copy(), cyclic)
+            return "execute_Bezier"
         elif self.splineType == "POLY":
-            spline = PolySpline(points, cyclic)
-        return spline
+            return "execute_Poly"
+
+    def execute_Bezier(self, points, leftHandles, rightHandles, cyclic):
+        self.correctHandlesListIfNecessary(points, leftHandles)
+        self.correctHandlesListIfNecessary(points, rightHandles)
+        print(len(points), len(leftHandles), len(rightHandles))
+        return BezierSpline(points, leftHandles, rightHandles, cyclic)
+
+    def correctHandlesListIfNecessary(self, points, handles):
+        if len(points) < len(handles):
+            del handles[len(points):]
+        elif len(points) > len(handles):
+            handles += points[len(handles):]
+
+    def execute_Poly(self, points, cyclic):
+        return PolySpline(points, cyclic)
