408b0d6788046a4e9b1a5c8458019141a4aa711b
Jacques Lucke <mail@jlucke.com>
2017-03-14 11:42:23 +0100

    extend Spline Info node

--- animation_nodes/nodes/spline/spline_info.py
+++ animation_nodes/nodes/spline/spline_info.py
@@ -1,13 +1,41 @@
 import bpy
+from bpy.props import *
 from ... base_types import AnimationNode
+from ... data_structures import Vector3DList
+
+splineTypeItems = [
+    ("BEZIER", "Bezier", "Each control point has two handles", "CURVE_BEZCURVE", 0),
+    ("POLY", "Poly", "Linear interpolation between the spline points", "NOCURVE", 1)
+]
 
 class SplineInfoNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SplineInfoNode"
     bl_label = "Spline Info"
 
-    def create(self):
-        self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY")
-        self.newOutput("Boolean", "Cyclic", "cyclic")
+    splineType = EnumProperty(name = "Spline Type", default = "BEZIER",
+        items = splineTypeItems, update = AnimationNode.refresh)
 
-    def execute(self, spline):
-        return spline.cyclic
+    def create(self):
+        self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY", dataIsModified = True)
+        self.newOutput("Boolean", "Cyclic", "cyclic")
+        self.newOutput("Vector List", "Points", "points")
+        if self.splineType == "BEZIER":
+            self.newOutput("Vector List", "Left Handles", "leftHandles")
+            self.newOutput("Vector List", "Right Handles", "rightHandles")
+
+    def draw(self, layout):
+        layout.prop(self, "splineType", text = "")
+
+    def getExecutionFunctionName(self):
+        if self.splineType == "BEZIER":
+            return "execute_Bezier"
+        elif self.splineType == "POLY":
+            return "execute_Poly"
+
+    def execute_Bezier(self, spline):
+        if spline.type == "BEZIER":
+            return spline.cyclic, spline.points, spline.leftHandles, spline.rightHandles
+        return spline.cyclic, spline.points, Vector3DList(), Vector3DList()
+
+    def execute_Poly(self, spline):
+        return spline.cyclic, spline.points
