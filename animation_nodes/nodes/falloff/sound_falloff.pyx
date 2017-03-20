d550fd5d0e60dcf5da5095a87080799689aaade7
Jacques Lucke <mail@jlucke.com>
2017-03-20 18:34:15 +0100

    implement more frequency sound falloffs

--- animation_nodes/nodes/falloff/sound_falloff.pyx
+++ animation_nodes/nodes/falloff/sound_falloff.pyx
@@ -18,6 +18,13 @@ spectrumFalloffTypeItems = [
     ("INDEX_FREQUENCY", "Index Frequency", "NONE", 0)
 ]
 
+indexFrequencyExtensionTypeItems = [
+    ("LOOP", "Loop", "", "NONE", 0),
+    ("MIRROR", "Mirror", "", "NONE", 1),
+    ("EXTEND_LAST", "Extend Last", "NONE", 2),
+    ("EXTEND_ZERO", "Extend Zero", "NONE", 3)
+]
+
 class SoundFalloffNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SoundFalloffNode"
     bl_label = "Sound Falloff"
@@ -30,6 +37,9 @@ class SoundFalloffNode(bpy.types.Node, AnimationNode):
 
     spectrumFalloffType = EnumProperty(name = "Spectrum Falloff Type", default = "INDEX_FREQUENCY",
         items = spectrumFalloffTypeItems, update = AnimationNode.refresh)
+
+    indexFrequencyExtensionType = EnumProperty(name = "Index Frequency Extension Type", default = "LOOP",
+        items = indexFrequencyExtensionTypeItems, update = AnimationNode.refresh)
 
     useCurrentFrame = BoolProperty(name = "Use Current Frame", default = True,
         update = AnimationNode.refresh)
@@ -44,8 +54,11 @@ class SoundFalloffNode(bpy.types.Node, AnimationNode):
                 self.newInput("Integer", "Offset", "offset", value = 1, minValue = 0)
         elif self.soundType == "SPECTRUM":
             if self.spectrumFalloffType == "INDEX_FREQUENCY":
-                self.newInput("Integer", "Length", "length", value = 10, minValue = 1)
-                self.newInput("Interpolation", "Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
+                self.newInput("Float", "Length", "length", value = 10, minValue = 1)
+                self.newInput("Float", "Offset", "offset")
+                self.newInput("Interpolation", "Interpolation", "interpolation",
+                    defaultDrawType = "PROPERTY_ONLY", category = "QUADRATIC",
+                    easeIn = True, easeOut = True)
 
         self.newOutput("Falloff", "Falloff", "falloff")
 
@@ -56,6 +69,8 @@ class SoundFalloffNode(bpy.types.Node, AnimationNode):
             col.prop(self, "averageFalloffType", text = "")
         else:
             col.prop(self, "spectrumFalloffType", text = "")
+            if self.spectrumFalloffType == "INDEX_FREQUENCY":
+                col.prop(self, "indexFrequencyExtensionType", text = "")
 
     def getExecutionCode(self):
         yield "if sound is not None and sound.type == self.soundType:"
@@ -67,7 +82,7 @@ class SoundFalloffNode(bpy.types.Node, AnimationNode):
                 yield "    falloff = self.execute_Average_IndexOffset(sound, _frame, offset)"
         elif self.soundType == "SPECTRUM":
             if self.spectrumFalloffType == "INDEX_FREQUENCY":
-                yield "    falloff = self.execute_Spectrum_IndexFrequency(sound, _frame, length, interpolation)"
+                yield "    falloff = self.execute_Spectrum_IndexFrequency(sound, _frame, length, offset, interpolation)"
 
         yield "else: falloff = self.getConstantFalloff(0)"
 
@@ -77,9 +92,10 @@ class SoundFalloffNode(bpy.types.Node, AnimationNode):
     def execute_Average_IndexOffset(self, sound, frame, offset):
         return Average_IndexOffset_SoundFalloff(sound, frame, offset)
 
-    def execute_Spectrum_IndexFrequency(self, sound, frame, length, interpolation):
+    def execute_Spectrum_IndexFrequency(self, sound, frame, length, offset, interpolation):
+        type = self.indexFrequencyExtensionType
         myList = DoubleList.fromValues(sound.evaluate(frame))
-        return DoubleListMixFalloff(myList, length, interpolation)
+        return InterpolateDoubleListFalloff.construct(type, myList, length, offset, interpolation)
 
 cdef class Average_IndexOffset_SoundFalloff(BaseFalloff):
     cdef:
@@ -96,31 +112,56 @@ cdef class Average_IndexOffset_SoundFalloff(BaseFalloff):
     cdef double evaluate(self, void *object, long index):
         return self.sound.evaluate(self.frame - index * self.offsetInverse)
 
-cdef class DoubleListMixFalloff(BaseFalloff):
+cdef class InterpolateDoubleListFalloff(BaseFalloff):
     cdef:
         DoubleList myList
         Interpolation interpolation
-        long myLength
-        long length
+        double length, offset
 
-    def __cinit__(self, DoubleList myList, length, Interpolation interpolation):
+    @classmethod
+    def construct(cls, extensionMode, DoubleList myList, length, offset, interpolation):
         if len(myList) == 0:
-            raise ValueError("list must not be empty")
+            return ConstantFalloff(0)
+        if len(myList) == 1 or length == 0:
+            return ConstantFalloff(myList[0])
+
+        if extensionMode == "LOOP":
+            return Loop_InterpolateDoubleListFalloff(myList, length, offset, interpolation)
+        elif extensionMode == "MIRROR":
+            return Loop_InterpolateDoubleListFalloff(myList + myList.reversed(), length * 2, offset * 2, interpolation)
+        elif extensionMode == "EXTEND_LAST":
+            return Extend_InterpolateDoubleListFalloff(myList, length, offset, interpolation)
+        elif extensionMode == "EXTEND_ZERO":
+            return Extend_InterpolateDoubleListFalloff([0] + myList + [0], length + 2, offset + 1, interpolation)
+
+    # should only be called from subclasses
+    def __cinit__(self, DoubleList myList, double length, double offset, Interpolation interpolation):
         self.myList = myList
-        self.myLength = len(myList)
-        self.length = min(max(length, 1), LONG_MAX)
+        self.length = length
+        self.offset = offset
         self.interpolation = interpolation
         self.dataType = "All"
         self.clamped = False
 
-    cdef double evaluate(self, void *object, long index):
-        index = index % self.length
-        cdef float pos = <float>index / <float>self.length * <float>self.myLength
-        cdef long indexBefore = <int>pos
-        cdef float influence = self.interpolation.evaluate(pos - <float>indexBefore)
+    cdef evaluatePosition(self, double x):
+        cdef long indexBefore = <int>x
+        cdef float influence = self.interpolation.evaluate(x - <float>indexBefore)
         cdef long indexAfter
-        if indexBefore < self.myLength - 1:
+        if indexBefore < self.myList.length - 1:
             indexAfter = indexBefore + 1
         else:
             indexAfter = indexBefore
         return self.myList.data[indexBefore] * (1 - influence) + self.myList.data[indexAfter] * influence
+
+cdef class Loop_InterpolateDoubleListFalloff(InterpolateDoubleListFalloff):
+    cdef double evaluate(self, void *object, long _index):
+        cdef double index = (<double>_index + self.offset) % self.length
+        cdef double x = index / self.length * self.myList.length
+        return self.evaluatePosition(x)
+
+cdef class Extend_InterpolateDoubleListFalloff(InterpolateDoubleListFalloff):
+    cdef double evaluate(self, void *object, long _index):
+        cdef double index = <double>_index + self.offset
+        index = min(max(index, 0), self.length - 1)
+        cdef double x = index / self.length * self.myList.length
+        return self.evaluatePosition(x)
