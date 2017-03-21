5460b4a490906a1e1e561d832cc658007a7d46d4
Jacques Lucke <mail@jlucke.com>
2017-03-21 20:40:02 +0100

    implement text id key from text body operator

--- animation_nodes/id_keys/data_types/text_type.py
+++ animation_nodes/id_keys/data_types/text_type.py
@@ -1,5 +1,20 @@
+import bpy
 from . base import SingleIDKeyDataType
+from ... utils.operators import makeOperator
 
 class TextDataType(SingleIDKeyDataType):
     identifier = "Text"
     default = ""
+
+    @classmethod
+    def drawExtras(cls, layout, object, name):
+        props = layout.operator("an.id_keys_from_text_body", icon = "OUTLINER_DATA_FONT")
+        props.name = name
+
+@makeOperator("an.id_keys_from_text_body", "From Text Body", arguments = ["String"])
+def idKeyFromSelectionOrder(name):
+    for object in bpy.context.selected_objects:
+        if object.type == "FONT":
+            object.id_keys.set("Text", name, object.data.body)
+        else:
+            object.id_keys.set("Text", name, "")
