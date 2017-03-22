bf7d5b553401605fa4c770661f2613fcd48cdbff
Jacques Lucke <mail@jlucke.com>
2017-03-22 18:48:31 +0100

    implement offset for ID Key from Selection Order operation

--- animation_nodes/id_keys/data_types/text_type.py
+++ animation_nodes/id_keys/data_types/text_type.py
@@ -11,7 +11,8 @@ class TextDataType(SingleIDKeyDataType):
         props = layout.operator("an.id_keys_from_text_body", icon = "OUTLINER_DATA_FONT")
         props.name = name
 
-@makeOperator("an.id_keys_from_text_body", "From Text Body", arguments = ["String"])
+@makeOperator("an.id_keys_from_text_body", "From Text Body", arguments = ["String"],
+              description = "Assign text ID Keys based on text body.")
 def idKeyFromSelectionOrder(name):
     for object in bpy.context.selected_objects:
         if object.type == "FONT":
