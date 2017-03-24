05d3490046990200db88416ee5ab5fd23701585a
Jacques Lucke <mail@jlucke.com>
2017-03-24 14:08:51 +0100

    cleanup

--- animation_nodes/base_types/__init__.py
+++ animation_nodes/base_types/__init__.py
@@ -3,6 +3,7 @@ from . node import AnimationNode
 from . node_tree import AnimationNodeTree
 from . socket import AnimationNodeSocket
 from . list_sockets import ListSocket, PythonListSocket, CListSocket
-from . socket_effects import (AutoSelectListDataType,
-                              AutoSelectDataType,
-                              AutoSelectVectorization)
+
+from . effects.socket_effects import (AutoSelectListDataType,
+                                      AutoSelectDataType,
+                                      AutoSelectVectorization)
