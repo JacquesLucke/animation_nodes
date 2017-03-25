9b863220b5f7f27cf144c04248cb2e98f045ef33
Jacques Lucke <mail@jlucke.com>
2017-03-25 19:44:42 +0100

    create Falloffs that can mix multiple falloffs together

--- animation_nodes/nodes/falloff/mix_falloffs.pyx
+++ animation_nodes/nodes/falloff/mix_falloffs.pyx
@@ -1,6 +1,7 @@
 import bpy
 from bpy.props import *
 from ... base_types import AnimationNode
+from . constant_falloff import ConstantFalloff
 from ... data_structures cimport CompoundFalloff, Falloff
 
 mixTypeItems = [
@@ -18,34 +19,58 @@ class MixFalloffsNode(bpy.types.Node, AnimationNode):
     mixType = EnumProperty(name = "Mix Type", items = mixTypeItems,
         default = "MAX", update = AnimationNode.refresh)
 
+    mixFalloffList = BoolProperty(name = "Mix Falloff List", default = False,
+        update = AnimationNode.refresh)
+
     def create(self):
-        self.newInput("Falloff", "A", "a")
-        self.newInput("Falloff", "B", "b")
-        if self.mixType in useFactorTypes:
-            self.newInput("Float", "Factor", "factor", value = 1).setRange(0, 1)
+        if self.mixFalloffList:
+            self.newInput("Falloff List", "Falloffs", "falloffs")
+        else:
+            self.newInput("Falloff", "A", "a")
+            self.newInput("Falloff", "B", "b")
+            if self.mixType in useFactorTypes:
+                self.newInput("Float", "Factor", "factor", value = 1).setRange(0, 1)
         self.newOutput("Falloff", "Falloff", "falloff")
 
     def draw(self, layout):
-        layout.prop(self, "mixType", text = "")
+        row = layout.row(align = True)
+        row.prop(self, "mixType", text = "")
+        row.prop(self, "mixFalloffList", text = "", icon = "LINENUMBERS_ON")
 
     def getExecutionFunctionName(self):
-        if self.mixType in useFactorTypes:
-            return "execute_WithFactor"
+        if self.mixFalloffList:
+            return "execute_List"
         else:
-            return "execute_WithoutFactor"
+            if self.mixType in useFactorTypes:
+                return "execute_WithFactor"
+            else:
+                return "execute_WithoutFactor"
+
+    def execute_List(self, falloffs):
+        if len(falloffs) == 0:
+            return ConstantFalloff(0)
+
+        if self.mixType == "ADD":
+            return AddFalloffs(falloffs)
+        elif self.mixType == "MULTIPLY":
+            return MultiplyFalloffs(falloffs)
+        elif self.mixType == "MIN":
+            return MinFalloffs(falloffs)
+        elif self.mixType == "MAX":
+            return MaxFalloffs(falloffs)
 
     def execute_WithFactor(self, a, b, factor):
         if self.mixType == "ADD":
-            return AddFalloffs(a, b, factor)
+            return AddTwoFalloffs(a, b, factor)
         raise Exception("should not happen")
 
     def execute_WithoutFactor(self, a, b):
         if self.mixType == "MULTIPLY":
-            return MultiplyFalloffs(a, b)
+            return MultiplyTwoFalloffs(a, b)
         elif self.mixType == "MIN":
-            return MinFalloffs(a, b)
+            return MinTwoFalloffs(a, b)
         elif self.mixType == "MAX":
-            return MaxFalloffs(a, b)
+            return MaxTwoFalloffs(a, b)
         raise Exception("should not happen")
 
 
@@ -62,18 +87,66 @@ cdef class MixTwoFalloffsBase(CompoundFalloff):
     cdef list getDependencies(self):
         return [self.a, self.b]
 
-cdef class AddFalloffs(MixTwoFalloffsBase):
-    cdef double evaluate(self, double* dependencyResults):
+cdef class AddTwoFalloffs(MixTwoFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
         return dependencyResults[0] + self.factor * dependencyResults[1]
 
-cdef class MultiplyFalloffs(MixTwoFalloffsBase):
-    cdef double evaluate(self, double* dependencyResults):
+cdef class MultiplyTwoFalloffs(MixTwoFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
         return dependencyResults[0] * dependencyResults[1]
 
-cdef class MinFalloffs(MixTwoFalloffsBase):
-    cdef double evaluate(self, double* dependencyResults):
+cdef class MinTwoFalloffs(MixTwoFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
         return min(dependencyResults[0], dependencyResults[1])
 
-cdef class MaxFalloffs(MixTwoFalloffsBase):
-    cdef double evaluate(self, double* dependencyResults):
+cdef class MaxTwoFalloffs(MixTwoFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
         return max(dependencyResults[0], dependencyResults[1])
+
+
+cdef class MixFalloffsBase(CompoundFalloff):
+    cdef list falloffs
+    cdef int amount
+
+    def __init__(self, list falloffs not None):
+        self.falloffs = falloffs
+        self.amount = len(falloffs)
+        if self.amount == 0:
+            raise Exception("at least one falloff required")
+
+    cdef list getDependencies(self):
+        return self.falloffs
+
+cdef class AddFalloffs(MixFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
+        cdef int i
+        cdef double sum = 0
+        for i in range(self.amount):
+            sum += dependencyResults[i]
+        return sum
+
+cdef class MultiplyFalloffs(MixFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
+        cdef int i
+        cdef double product = 1
+        for i in range(self.amount):
+            product *= dependencyResults[i]
+        return product
+
+cdef class MinFalloffs(MixFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
+        cdef int i
+        cdef double minValue = dependencyResults[0]
+        for i in range(1, self.amount):
+            if dependencyResults[i] < minValue:
+                minValue = dependencyResults[i]
+        return minValue
+
+cdef class MaxFalloffs(MixFalloffsBase):
+    cdef double evaluate(self, double *dependencyResults):
+        cdef int i
+        cdef double maxValue = dependencyResults[0]
+        for i in range(1, self.amount):
+            if dependencyResults[i] > maxValue:
+                maxValue = dependencyResults[i]
+        return maxValue
