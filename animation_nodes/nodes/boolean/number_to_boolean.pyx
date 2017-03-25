c34aa3e8b4da916c9ed6ef2579ff9af0e95cd7c6
Jacques Lucke <mail@jlucke.com>
2017-03-25 12:34:28 +0100

    cleanup Number to Boolean node

--- animation_nodes/nodes/boolean/number_to_boolean.pyx
+++ animation_nodes/nodes/boolean/number_to_boolean.pyx
@@ -1,27 +1,20 @@
 import bpy
 from bpy.props import *
+from ... base_types import VectorizedNode
 from ... data_structures cimport DoubleList, BooleanList
-from ... base_types import AnimationNode, AutoSelectVectorization
 
-class NumberToBooleanNode(bpy.types.Node, AnimationNode):
+class NumberToBooleanNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_NumberToBooleanNode"
     bl_label = "Number to Boolean"
 
-    useList = BoolProperty(update = AnimationNode.refresh)
+    useList = VectorizedNode.newVectorizeProperty()
 
-    def create(self):
-        self.newInputGroup(self.useList,
-            ("Float", "Number", "number"),
-            ("Float List", "Numbers", "numbers"))
+    def createVectorized(self):
+        self.newVectorizedInput("Float", "useList",
+            ("Number", "number"), ("Numbers", "numbers"))
 
-        self.newOutputGroup(self.useList,
-            ("Boolean", "Boolean", "boolean"),
-            ("Boolean List", "Booleans", "booleans"))
-
-        vectorization = AutoSelectVectorization()
-        vectorization.input(self, "useList", self.inputs[0])
-        vectorization.output(self, "useList", self.outputs[0])
-        self.newSocketEffect(vectorization)
+        self.newVectorizedOutput("Boolean", "useList",
+            ("Boolean", "boolean"), ("Booleans", "booleans"))
 
     def getExecutionCode(self):
         if self.useList:
