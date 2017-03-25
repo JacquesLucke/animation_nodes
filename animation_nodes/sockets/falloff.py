2ebcaa9efe85f5e7770be72ac435a6567a9ce776
Jacques Lucke <mail@jlucke.com>
2017-03-25 19:42:44 +0100

    implement Falloff List socket

--- animation_nodes/sockets/falloff.py
+++ animation_nodes/sockets/falloff.py
@@ -1,9 +1,9 @@
 import bpy
 from bpy.props import *
 from .. events import propertyChanged
-from .. data_structures import BaseFalloff, CompoundFalloff
-from .. base_types import AnimationNodeSocket
+from .. data_structures import Falloff
 from .. nodes.falloff.constant_falloff import ConstantFalloff
+from .. base_types import AnimationNodeSocket, PythonListSocket
 
 class FalloffSocket(bpy.types.NodeSocket, AnimationNodeSocket):
     bl_idname = "an_FalloffSocket"
@@ -34,6 +34,28 @@ class FalloffSocket(bpy.types.NodeSocket, AnimationNodeSocket):
 
     @classmethod
     def correctValue(cls, value):
-        if isinstance(value, BaseFalloff) or isinstance(value, CompoundFalloff):
+        if isinstance(value, Falloff):
             return value, 0
+        return cls.getDefaultValue(), 2
+
+
+class FalloffListSocket(bpy.types.NodeSocket, PythonListSocket):
+    bl_idname = "an_FalloffListSocket"
+    bl_label = "Falloff List Socket"
+    dataType = "Falloff List"
+    baseDataType = "Falloff"
+    allowedInputTypes = ["Falloff List"]
+    drawColor = (0.32, 1, 0.18, 0.5)
+    storable = False
+    comparable = False
+
+    @classmethod
+    def getCopyExpression(cls):
+        return "value[:]"
+
+    @classmethod
+    def correctValue(cls, value):
+        if isinstance(value, list):
+            if all(isinstance(element, Falloff) for element in value):
+                return value, 0
         return cls.getDefaultValue(), 2
