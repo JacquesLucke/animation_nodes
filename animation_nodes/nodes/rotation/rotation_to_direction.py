838f19790f4c1a8c565ee8a1693ed2007b3de549
Jacques Lucke <mail@jlucke.com>
2017-03-25 13:38:30 +0100

    cleanup Rotation to Direction node

--- animation_nodes/nodes/rotation/rotation_to_direction.py
+++ animation_nodes/nodes/rotation/rotation_to_direction.py
@@ -1,34 +1,29 @@
 import bpy
 from bpy.props import *
 from ... events import propertyChanged
-from ... base_types import AnimationNode, AutoSelectVectorization
+from ... base_types import VectorizedNode
 
-directionAxisItems = [(axis, axis, "") for axis in ("X", "Y", "Z", "-X", "-Y", "-Z")]
+directionAxisItems = [(axis, axis, "", "", i)
+                      for i, axis in enumerate(("X", "Y", "Z", "-X", "-Y", "-Z"))]
 
-class RotationToDirectionNode(bpy.types.Node, AnimationNode):
+class RotationToDirectionNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_RotationToDirectionNode"
     bl_label = "Rotation to Direction"
     bl_width_default = 160
 
-    useRotationList = BoolProperty(update = AnimationNode.refresh)
+    useRotationList = VectorizedNode.newVectorizeProperty()
 
-    directionAxis = EnumProperty(items = directionAxisItems, update = propertyChanged, default = "Z")
+    directionAxis = EnumProperty(name = "Direction Axis", default = "Z",
+        items = directionAxisItems, update = propertyChanged)
 
-    def create(self):
-        self.newInputGroup(self.useRotationList,
-            ("Euler", "Rotation", "rotation"),
-            ("Euler List", "Rotations", "rotations"))
+    def createVectorized(self):
+        self.newVectorizedInput("Euler", "useRotationList",
+            ("Rotation", "rotation"), ("Rotations", "rotations"))
 
         self.newInput("Float", "Length", "length", value = 1)
 
-        self.newOutputGroup(self.useRotationList,
-            ("Vector", "Direction", "direction"),
-            ("Vector List", "Directions", "directions"))
-
-        vectorization = AutoSelectVectorization()
-        vectorization.input(self, "useRotationList", self.inputs[0])
-        vectorization.output(self, "useRotationList", self.outputs[0])
-        self.newSocketEffect(vectorization)
+        self.newVectorizedOutput("Vector", "useRotationList",
+            ("Direction", "direction"), ("Directions", "directions"))
 
     def draw(self, layout):
         layout.prop(self, "directionAxis", expand = True)
