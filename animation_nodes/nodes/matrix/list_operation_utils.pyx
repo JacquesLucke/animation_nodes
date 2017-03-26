40bf74585e6c025e9f21106fb69be78b20cabc5e
Jacques Lucke <mail@jlucke.com>
2017-03-26 23:24:15 +0200

    vectorize Decompose Matrix node

--- animation_nodes/nodes/matrix/list_operation_utils.pyx
+++ animation_nodes/nodes/matrix/list_operation_utils.pyx
@@ -1,7 +1,10 @@
 from ... data_structures cimport (Vector3DList, EulerList, Matrix4x4List,
                                   CListMock)
 
-from ... math cimport setTranslationRotationScaleMatrix, Vector3, Euler3
+from ... math cimport Vector3, Euler3, Matrix4, setTranslationRotationScaleMatrix
+from ... math import matrix4x4ListToEulerList
+
+from libc.math cimport sqrt
 
 def composeMatrices(translations, rotations, scales):
     cdef:
@@ -20,3 +23,33 @@ def composeMatrices(translations, rotations, scales):
             <Vector3*>_scales.get(i))
 
     return matrices
+
+def extractMatrixTranslations(Matrix4x4List matrices):
+    cdef Vector3DList translations = Vector3DList(length = len(matrices))
+    cdef Matrix4 *_matrices = matrices.data
+    cdef Vector3 *_translations = translations.data
+    cdef Py_ssize_t i
+
+    for i in range(len(translations)):
+        _translations[i].x = _matrices[i].a14
+        _translations[i].y = _matrices[i].a24
+        _translations[i].z = _matrices[i].a34
+
+    return translations
+
+def extractMatrixRotations(Matrix4x4List matrices):
+    return matrix4x4ListToEulerList(matrices)
+
+def extractMatrixScales(Matrix4x4List matrices):
+    cdef Vector3DList scales = Vector3DList(length = len(matrices))
+    cdef Py_ssize_t i
+
+    for i in range(len(scales)):
+        scaleFromMatrix(scales.data + i, matrices.data + i)
+
+    return scales
+
+cdef void scaleFromMatrix(Vector3 *scale, Matrix4 *matrix):
+    scale.x = sqrt(matrix.a11 * matrix.a11 + matrix.a21 * matrix.a21 + matrix.a31 * matrix.a31)
+    scale.y = sqrt(matrix.a12 * matrix.a12 + matrix.a22 * matrix.a22 + matrix.a32 * matrix.a32)
+    scale.z = sqrt(matrix.a13 * matrix.a13 + matrix.a23 * matrix.a23 + matrix.a33 * matrix.a33)
