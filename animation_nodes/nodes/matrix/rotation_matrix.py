38a865a120598da8704f0e447fd22342738405ad
Jacques Lucke <mail@jlucke.com>
2017-03-29 22:47:35 +0200

    vectorize Rotation Matrix node

--- animation_nodes/nodes/matrix/rotation_matrix.py
+++ animation_nodes/nodes/matrix/rotation_matrix.py
@@ -1,6 +1,7 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
+from . list_operation_utils import createAxisRotations, createRotationsFromEulers
 
 axisItems = [
     ("X", "X", "", "", 0),
@@ -8,23 +9,41 @@ axisItems = [
     ("Z", "Z", "", "", 2),
     ("ALL", "All", "", "", 3) ]
 
-class RotationMatrixNode(bpy.types.Node, AnimationNode):
+class RotationMatrixNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_RotationMatrixNode"
     bl_label = "Rotation Matrix"
 
     axis = EnumProperty(default = "X", items = axisItems,
-        update = AnimationNode.refresh)
+        update = VectorizedNode.refresh)
+
+    useList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
         socketType = "Euler" if self.axis == "ALL" else "Float"
-        self.newInput(socketType, "Angle", "angle")
-        self.newOutput("Matrix", "Matrix", "matrix")
+        self.newVectorizedInput(socketType, "useList",
+            ("Angle", "angle"), ("Angles", "angles"))
+
+        self.newVectorizedOutput("Matrix", "useList",
+            ("Matrix", "matrix"), ("Matrices", "matrices"))
 
     def draw(self, layout):
         layout.prop(self, "axis", expand = True)
+
+    def getExecutionFunctionName(self):
+        if self.useList:
+            if self.axis == "ALL":
+                return "execute_List_All"
+            else:
+                return "execute_List_Axis"
 
     def getExecutionCode(self):
         if len(self.axis) == 1:
             return "matrix = Matrix.Rotation(angle, 4, '{}')".format(self.axis)
         else:
             return "matrix = angle.to_matrix(); matrix.resize_4x4()"
+
+    def execute_List_All(self, rotations):
+        return createRotationsFromEulers(rotations)
+
+    def execute_List_Axis(self, angles):
+        return createAxisRotations(angles, self.axis)
