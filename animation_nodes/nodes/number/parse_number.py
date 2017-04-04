67ff64e5797b7db506c83db264d5c3714964aa1c
Jacques Lucke <mail@jlucke.com>
2017-04-04 20:39:32 +0200

    vectorize Parse Number node

--- animation_nodes/nodes/number/parse_number.py
+++ animation_nodes/nodes/number/parse_number.py
@@ -1,25 +1,36 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
 
-class ParseNumberNode(bpy.types.Node, AnimationNode):
+class ParseNumberNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_ParseNumberNode"
     bl_label = "Parse Number"
 
     parsingSuccessfull = BoolProperty()
+    useList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
-        self.newInput("Text", "Text", "text")
-        self.newOutput("Float", "Number", "number")
+        self.newVectorizedInput("Text", "useList",
+            ("Text", "text", dict(value = "0")), ("Texts", "texts"))
+        self.newVectorizedOutput("Float", "useList",
+            ("Number", "number"), ("Numbers", "numbers"))
 
     def draw(self, layout):
         if not self.parsingSuccessfull:
             layout.label("Parsing Error", icon = "ERROR")
 
     def getExecutionCode(self):
-        yield "try:"
-        yield "    number = float(text)"
-        yield "    self.parsingSuccessfull = True"
-        yield "except:"
-        yield "    number = 0"
-        yield "    self.parsingSuccessfull = False"
+        if self.useList:
+            yield "try:"
+            yield "    numbers = DoubleList.fromValues(float(text) for text in texts)"
+            yield "    self.parsingSuccessfull = True"
+            yield "except:"
+            yield "    numbers = DoubleList()"
+            yield "    self.parsingSuccessfull = False"
+        else:
+            yield "try:"
+            yield "    number = float(text)"
+            yield "    self.parsingSuccessfull = True"
+            yield "except:"
+            yield "    number = 0"
+            yield "    self.parsingSuccessfull = False"
