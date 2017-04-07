599c4beb84e91e8f9914adce1bc8aa26a92b35b7
Jacques Lucke <mail@jlucke.com>
2017-04-08 00:11:12 +0200

    vectorize Is Inside Volume node

--- animation_nodes/nodes/bvh_tree/is_inside_volume.py
+++ animation_nodes/nodes/bvh_tree/is_inside_volume.py
@@ -1,7 +1,7 @@
 import bpy
 from random import random
 from mathutils import Vector
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
 
 # in some cases multiple tests have to done
 # to reduce the probability for errors
@@ -9,16 +9,27 @@ direction1 = Vector((random(), random(), random())).normalized()
 direction2 = Vector((random(), random(), random())).normalized()
 direction3 = Vector((random(), random(), random())).normalized()
 
-class IsInsideVolumeBVHTreeNode(bpy.types.Node, AnimationNode):
+class IsInsideVolumeBVHTreeNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_IsInsideVolumeBVHTreeNode"
     bl_label = "Is Inside Volume"
+    autoVectorizeExecution = True
+
+    useVectorList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
         self.newInput("BVHTree", "BVHTree", "bvhTree")
-        self.newInput("Vector", "Vector", "vector", defaultDrawType = "PROPERTY_ONLY")
-        self.newOutput("Boolean", "Is Inside", "isInside")
 
-    def execute(self, bvhTree, vector):
+        self.newVectorizedInput("Vector", "useVectorList",
+            ("Vector", "vector", dict(defaultDrawType = "PROPERTY_ONLY")),
+            ("Vectors", "vectors", dict(defaultDrawType = "PROPERTY_ONLY")))
+
+        self.newVectorizedOutput("Boolean", "useVectorList",
+            ("Is Inside", "isInside"), ("Are Inside", "areInside"))
+
+    def getExecutionCode(self):
+        return "isInside = self.execute_Single(bvhTree, vector)"
+
+    def execute_Single(self, bvhTree, vector):
         hits1 = self.countHits(bvhTree, vector, direction1)
         if hits1 == 0: return False
         if hits1 == 1: return True
