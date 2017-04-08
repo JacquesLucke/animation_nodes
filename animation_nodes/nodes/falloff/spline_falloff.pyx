98624197ccc3cef79e94573ff6c36c60ffcbe46e
Jacques Lucke <mail@jlucke.com>
2017-04-08 22:40:46 +0200

    vectorize Spline Falloff node

--- animation_nodes/nodes/falloff/spline_falloff.pyx
+++ animation_nodes/nodes/falloff/spline_falloff.pyx
@@ -1,12 +1,20 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
+from ... events import propertyChanged
 from ... math cimport Vector3, distanceVec3
-from . constant_falloff import ConstantFalloff
-from . interpolate_falloff import InterpolateFalloff
 from ... data_structures cimport Spline, PolySpline, BaseFalloff
 
-class SplineFalloffNode(bpy.types.Node, AnimationNode):
+from . mix_falloffs import MixFalloffs
+from . constant_falloff import ConstantFalloff
+from . interpolate_falloff import InterpolateFalloff
+
+mixListTypeItems = [
+    ("MAX", "Max", "", "NONE", 0),
+    ("ADD", "Add", "", "NONE", 1)
+]
+
+class SplineFalloffNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_SplineFalloffNode"
     bl_label = "Spline Falloff"
     bl_width_default = 160
@@ -14,10 +22,16 @@ class SplineFalloffNode(bpy.types.Node, AnimationNode):
     resolution = IntProperty(name = "Resolution", default = 5, min = 2,
         description = "Poly spline segments per bezier spline segments")
 
+    mixListType = EnumProperty(name = "Mix List Type", default = "MAX",
+        items = mixListTypeItems, update = propertyChanged)
+
+    useSplineList = VectorizedNode.newVectorizeProperty()
+
     def create(self):
-        self.newInput("Spline", "Spline", "spline",
-            defaultDrawType = "PROPERTY_ONLY",
-            dataIsModified = True)
+        socketProps = dict(defaultDrawType = "PROPERTY_ONLY", dataIsModified = True)
+        self.newVectorizedInput("Spline", "useSplineList",
+            ("Spline", "spline", socketProps),
+            ("Splines", "splines", socketProps))
         self.newInput("Float", "Distance", "distance", value = 0)
         self.newInput("Float", "Width", "width", value = 1, minValue = 0)
         self.newInput("Interpolation", "Interpolation", "interpolation",
@@ -25,10 +39,37 @@ class SplineFalloffNode(bpy.types.Node, AnimationNode):
 
         self.newOutput("Falloff", "Falloff", "falloff")
 
+    def draw(self, layout):
+        if self.useSplineList:
+            layout.prop(self, "mixListType", text = "")
+
     def drawAdvanced(self, layout):
         layout.prop(self, "resolution")
 
-    def execute(self, spline, distance, width, interpolation):
+    def getExecutionFunctionName(self):
+        if self.useSplineList:
+            return "execute_List"
+        else:
+            return "execute_Single"
+
+    def execute_Single(self, spline, distance, width, interpolation):
+        falloff = self.falloffFromSpline(spline, distance, width)
+        return InterpolateFalloff(falloff, interpolation)
+
+    def execute_List(self, splines, distance, width, interpolation):
+        falloffs = []
+        for spline in splines:
+            if spline.isEvaluable():
+                falloffs.append(self.falloffFromSpline(spline, distance, width))
+
+        if self.mixListType == "ADD":
+            interpolatedFalloffs = [InterpolateFalloff(f, interpolation) for f in falloffs]
+            return MixFalloffs(interpolatedFalloffs, "ADD", default = 0)
+        elif self.mixListType == "MAX":
+            falloff = MixFalloffs(falloffs, "MAX", default = 0)
+            return InterpolateFalloff(falloff, interpolation)
+
+    def falloffFromSpline(self, spline, distance, width):
         if not spline.isEvaluable():
             return ConstantFalloff(0)
 
@@ -38,8 +79,7 @@ class SplineFalloffNode(bpy.types.Node, AnimationNode):
             falloffSpline = PolySpline(spline.getSamples(self.resolution * (len(spline.points) - 1)))
             falloffSpline.cyclic = spline.cyclic
 
-        falloff = SplineFalloff(falloffSpline, distance, width)
-        return InterpolateFalloff(falloff, interpolation)
+        return SplineFalloff(falloffSpline, distance, width)
 
 
 cdef class SplineFalloff(BaseFalloff):
