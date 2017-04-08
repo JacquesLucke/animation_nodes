dc363145284ec47e78484093c6afb4ad8bb71a09
Jacques Lucke <mail@jlucke.com>
2017-04-08 23:53:17 +0200

    vectorize Text Length node

--- animation_nodes/nodes/text/text_length.py
+++ animation_nodes/nodes/text/text_length.py
@@ -1,13 +1,19 @@
 import bpy
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
 
-class TextLengthNode(bpy.types.Node, AnimationNode):
+class TextLengthNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_TextLengthNode"
     bl_label = "Text Length"
+    autoVectorizeExecution = True
+
+    useTextList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
-        self.newInput("Text", "Text", "text")
-        self.newOutput("Integer", "Length", "length")
+        self.newVectorizedInput("Text", "useTextList",
+            ("Text", "text"), ("Texts", "texts"))
+
+        self.newVectorizedOutput("Integer", "useTextList",
+            ("Length", "length"), ("Lengths", "lengths"))
 
     def getExecutionCode(self):
         return "length = len(text)"
