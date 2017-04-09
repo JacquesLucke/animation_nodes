b0edb73ee1f8117f30ebfb12db6ecae778a02a0d
Jacques Lucke <mail@jlucke.com>
2017-04-09 20:19:59 +0200

    vectorize Convert Euler and Vector node

--- animation_nodes/nodes/rotation/convert_euler_and_vector.py
+++ animation_nodes/nodes/rotation/convert_euler_and_vector.py
@@ -1,13 +1,14 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
 from ... events import executionCodeChanged
+from . list_utils import vectorsToEulers, eulersToVectors
 
 conversionTypeItems = [
     ("VECTOR_TO_EULER", "Vector to Euler", "", "NONE", 0),
     ("EULER_TO_VECTOR", "Euler to Vector", "", "NONE", 1)]
 
-class ConvertVectorAndEulerNode(bpy.types.Node, AnimationNode):
+class ConvertVectorAndEulerNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_ConvertVectorAndEulerNode"
     bl_label = "Convert Vector and Euler"
     dynamicLabelType = "ALWAYS"
@@ -19,15 +20,21 @@ class ConvertVectorAndEulerNode(bpy.types.Node, AnimationNode):
         update = executionCodeChanged)
 
     conversionType = EnumProperty(name = "Conversion Type", default = "VECTOR_TO_EULER",
-        update = AnimationNode.refresh, items = conversionTypeItems)
+        update = VectorizedNode.refresh, items = conversionTypeItems)
+
+    useList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
         if self.conversionType == "VECTOR_TO_EULER":
-            self.newInput("Vector", "Vector", "vector")
-            self.newOutput("Euler", "Euler", "euler")
+            self.newVectorizedInput("Vector", "useList",
+                ("Vector", "vector"), ("Vectors", "vectors"))
+            self.newVectorizedOutput("Euler", "useList",
+                ("Euler", "euler"), ("Eulers", "eulers"))
         if self.conversionType == "EULER_TO_VECTOR":
-            self.newInput("Euler", "Euler", "euler")
-            self.newOutput("Vector", "Vector", "vector")
+            self.newVectorizedInput("Euler", "useList",
+                ("Euler", "euler"), ("Eulers", "eulers"))
+            self.newVectorizedOutput("Vector", "useList",
+                ("Vector", "vector"), ("Vectors", "vectors"))
         self.inputs[0].defaultDrawType = "PROPERTY_ONLY"
 
     def draw(self, layout):
@@ -39,12 +46,24 @@ class ConvertVectorAndEulerNode(bpy.types.Node, AnimationNode):
             if self.conversionType == item[0]: return item[1]
 
     def getExecutionCode(self):
-        if self.conversionType == "VECTOR_TO_EULER":
-            if self.useDegree: return "euler = Euler(vector / 180 * math.pi, 'XYZ')"
-            else: return "euler = Euler(vector, 'XYZ')"
-        elif self.conversionType == "EULER_TO_VECTOR":
-            if self.useDegree: return "vector = Vector(euler) * 180 / math.pi"
-            else: return "vector = Vector(euler)"
+        if self.useList:
+            if self.conversionType == "VECTOR_TO_EULER":
+                return "eulers = self.vectorsToEulers(vectors)"
+            elif self.conversionType == "EULER_TO_VECTOR":
+                return "vectors = self.eulersToVectors(eulers)"
+        else:
+            if self.conversionType == "VECTOR_TO_EULER":
+                if self.useDegree: return "euler = Euler(vector / 180 * math.pi, 'XYZ')"
+                else: return "euler = Euler(vector, 'XYZ')"
+            elif self.conversionType == "EULER_TO_VECTOR":
+                if self.useDegree: return "vector = Vector(euler) * 180 / math.pi"
+                else: return "vector = Vector(euler)"
+
+    def vectorsToEulers(self, vectors):
+        return vectorsToEulers(vectors, self.useDegree)
+
+    def eulersToVectors(self, eulers):
+        return eulersToVectors(eulers, self.useDegree)
 
     def getUsedModules(self):
         return ["math"]
