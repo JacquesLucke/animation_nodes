01fb72bc6dd11edf48b6ea00f92bbfb2a8d74b56
Jacques Lucke <mail@jlucke.com>
2017-04-23 17:51:38 +0200

    copy text id key to text body

--- animation_nodes/id_keys/data_types/text_type.py
+++ animation_nodes/id_keys/data_types/text_type.py
@@ -11,9 +11,16 @@ class TextDataType(SingleIDKeyDataType):
         props = layout.operator("an.id_keys_from_text_body", icon = "OUTLINER_DATA_FONT")
         props.name = name
 
+    @classmethod
+    def drawCopyMenu(cls, layout, object, name):
+        props = layout.operator("an.copy_id_key_to_attribute", text = "to Text Body")
+        props.dataType = "Text"
+        props.propertyName = name
+        props.attribute = "data.body"
+
 @makeOperator("an.id_keys_from_text_body", "From Text Body", arguments = ["String"],
               description = "Assign text ID Keys based on text body.")
-def idKeyFromSelectionOrder(name):
+def idKeyFromTextBody(name):
     for object in bpy.context.selected_objects:
         if object.type == "FONT":
             object.id_keys.set("Text", name, object.data.body)
