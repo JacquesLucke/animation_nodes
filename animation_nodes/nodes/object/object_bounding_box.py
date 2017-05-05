827b0e7a6d94335d7af23c6a7855b27a9d3c7982
Jacques Lucke <mail@jlucke.com>
2017-05-05 22:53:57 +0200

    correct normals of bounding box polygons

--- animation_nodes/nodes/object/object_bounding_box.py
+++ animation_nodes/nodes/object/object_bounding_box.py
@@ -36,6 +36,6 @@ edges = EdgeIndicesList.fromValues(
 )
 
 polygons = PolygonIndicesList.fromValues(
-    [(0, 1, 2, 3), (4, 5, 6, 7), (0, 3, 7, 4),
-     (0, 1, 5, 4), (1, 2, 6, 5), (2, 3, 7, 6)]
+    [(0, 1, 2, 3), (7, 6, 5, 4), (0, 3, 7, 4),
+     (4, 5, 1, 0), (5, 6, 2, 1), (6, 7, 3, 2)]
 )
