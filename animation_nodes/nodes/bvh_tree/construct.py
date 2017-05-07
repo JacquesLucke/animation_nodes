471512359bb40448f773ac620f31ab280beb7b03
Jacques Lucke <mail@jlucke.com>
2017-05-07 14:09:14 +0200

    construct BVHTree directly from object

--- animation_nodes/nodes/bvh_tree/construct.py
+++ animation_nodes/nodes/bvh_tree/construct.py
@@ -4,8 +4,9 @@ from mathutils.bvhtree import BVHTree
 from ... base_types import AnimationNode
 
 sourceTypeItems = [
-    ("MESH_DATA", "Mesh Data", "", "", 0),
-    ("BMESH", "BMesh", "", "", 1) ]
+    ("MESH_DATA", "Mesh Data", "", "NONE", 0),
+    ("BMESH", "BMesh", "", "NONE", 1),
+    ("OBJECT", "Object", "", "NONE", 2) ]
 
 class ConstructBVHTreeNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ConstructBVHTreeNode"
@@ -21,26 +22,44 @@ class ConstructBVHTreeNode(bpy.types.Node, AnimationNode):
             self.newInput("Polygon Indices List", "Polygon Indices", "polygonsIndices")
         elif self.sourceType == "BMESH":
             self.newInput("BMesh", "BMesh", "bm")
+        elif self.sourceType == "OBJECT":
+            self.newInput("Object", "Object", "object", defaultDrawType = "PROPERTY_ONLY")
+
         self.newInput("Float", "Epsilon", "epsilon", hide = True, minValue = 0)
         self.newOutput("BVHTree", "BVHTree", "bvhTree")
 
     def draw(self, layout):
         layout.prop(self, "sourceType", text = "Source")
 
-    def getExecutionCode(self):
+    def getExecutionFunctionName(self):
         if self.sourceType == "MESH_DATA":
-            return "bvhTree = self.fromMeshData(vectorList, polygonsIndices, max(epsilon, 0))"
+            return "execute_MeshData"
         elif self.sourceType == "BMESH":
-            return "bvhTree = self.fromBMesh(bm, max(epsilon, 0))"
+            return "execute_BMesh"
+        elif self.sourceType == "OBJECT":
+            return "execute_Object"
 
-    def getUsedModules(self):
-        return ["mathutils"]
+    def execute_MeshData(self, vectorList, polygonsIndices, epsilon):
+        if len(polygonsIndices) == 0:
+            return self.getFallbackBVHTree()
 
-    def fromMeshData(self, vectorList, polygonsIndices, epsilon):
-        if len(polygonsIndices) > 0:
-            if 0 <= polygonsIndices.getMinIndex() <= polygonsIndices.getMaxIndex() < len(vectorList):
-                return BVHTree.FromPolygons(vectorList, polygonsIndices, epsilon = epsilon)
-        return BVHTree.FromPolygons([], [], epsilon = epsilon)
+        if 0 <= polygonsIndices.getMinIndex() <= polygonsIndices.getMaxIndex() < len(vectorList):
+            return BVHTree.FromPolygons(vectorList, polygonsIndices, epsilon = max(epsilon, 0))
 
-    def fromBMesh(self, bm, epsilon):
-        return BVHTree.FromBMesh(bm, epsilon = epsilon)
+    def execute_BMesh(self, bm, epsilon):
+        return BVHTree.FromBMesh(bm, epsilon = max(epsilon, 0))
+
+    def execute_Object(self, object, epsilon):
+        if object is None:
+            return self.getFallbackBVHTree()
+        if object.type != "MESH":
+            return self.getFallbackBVHTree()
+
+        mesh = object.data
+        vertices = mesh.an.getVertices()
+        vertices.transform(object.matrix_world)
+        polygons = mesh.an.getPolygonIndices()
+        return self.execute_MeshData(vertices, polygons, epsilon)
+
+    def getFallbackBVHTree(self):
+        return self.outputs[0].getDefaultValue()
