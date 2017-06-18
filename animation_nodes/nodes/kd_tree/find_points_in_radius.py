0640cd81fc1212f37827208518381d278c89304a
Jacques Lucke <mail@jlucke.com>
2017-06-18 21:56:14 +0200

    fix Find Points in Radius node

--- animation_nodes/nodes/kd_tree/find_points_in_radius.py
+++ animation_nodes/nodes/kd_tree/find_points_in_radius.py
@@ -9,13 +9,15 @@ class FindPointsInRadiusInKDTreeNode(bpy.types.Node, AnimationNode):
         self.newInput("KDTree", "KDTree", "kdTree")
         self.newInput("Float", "Radius", "radius", value = 5, minValue = 0)
         self.newInput("Vector", "Vector", "searchVector", defaultDrawType = "PROPERTY_ONLY")
-        
+
         self.newOutput("an_VectorListSocket", "Vectors", "nearestVectors")
         self.newOutput("an_FloatListSocket", "Distances", "distances")
         self.newOutput("an_IntegerListSocket", "Indices", "indices")
 
     def getExecutionCode(self):
-        yield "nearestVectors, distances, indices = [], [], []"
+        yield "nearestVectors = Vector3DList()"
+        yield "distances = DoubleList()"
+        yield "indices = LongList()"
         yield "for vector, index, distance in kdTree.find_range(searchVector, max(radius, 0)):"
         yield "    nearestVectors.append(vector)"
         yield "    indices.append(index)"
