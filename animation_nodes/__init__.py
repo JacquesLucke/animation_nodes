cc84018077404852172f59ed5749dc82a93ca62b
Jacques Lucke <mail@jlucke.com>
2017-07-12 19:59:12 +0200

    Take info from compilation_info.py file to show better error messages

--- animation_nodes/__init__.py
+++ animation_nodes/__init__.py
@@ -35,12 +35,17 @@ bl_info = {
 # Test Environment
 ##################################
 
+import os
+import sys
 import traceback
-from os import listdir
-from os.path import dirname
-addonsDirectory = dirname(dirname(__file__))
+from os.path import dirname, join, abspath
+currentDirectory = dirname(abspath(__file__))
+addonsDirectory = dirname(currentDirectory)
+compilationInfoPath = join(currentDirectory, "compilation_info.json")
+
+
 counter = 0
-for name in listdir(addonsDirectory):
+for name in os.listdir(addonsDirectory):
     name = name.lower()
     if "animation" in name and "nodes" in name and not "test" in name:
         counter += 1
@@ -84,24 +89,60 @@ if getBlenderVersion() < (2, 76, 0):
     raise Exception(message)
 
 
+if not os.path.isfile(compilationInfoPath):
+    message = ("\n\n"
+        "This is just the source code of Animation Nodes, not a compiled build.\n"
+        "Please download a build for your operating system instead.\n"
+        "Don't forget to remove this version at first.")
+    raise Exception(message)
+
+import json
+with open(compilationInfoPath) as f:
+    compilation_info = json.load(f)
+
+if compilation_info["sys.platform"] != sys.platform:
+    message = ("\n\n"
+        "This Animation Nodes build is for another OS.\n\n"
+        "You are using: {}\n"
+        "This build is for: {}\n\n"
+        "Please download a build for your operating system."
+        ).format(sys.platform, compilation_info["sys.platform"])
+    raise Exception(message)
+
+currentPythonVersion = tuple(sys.version_info[:3])
+addonPythonVersion = tuple(compilation_info["sys.version_info"][:3])
+
+if currentPythonVersion[:2] != addonPythonVersion[:2]:
+    message = ("\n\n"
+        "There is a Python version mismatch.\n\n"
+        "Your Blender build uses: {}\n"
+        "Animation Nodes has been compiled for: {}\n\n"
+        "You have three options:\n"
+        "  1. Try make Blender use another Python version.\n"
+        "     (Blender 2.78/2.79 officially uses Python 3.5.x)\n"
+        "  2. Compile Animation Nodes yourself using the correct Python version.\n"
+        "     (Look in the developer manual for more information)\n"
+        "  3. Create an issue on Github and ask if someone can create a build for you."
+        ).format(currentPythonVersion, addonPythonVersion)
+    raise Exception(message)
+
+
 try: from . import test_compile
 except: traceback.print_exc()
 
 if "test_compile" not in globals():
     message = ("\n\n"
         "This build does not work at the moment.\n"
-        "  1. Make sure that you are using compiled version of AN.\n"
-        "     (Not a direct download of the repository from Github)\n"
-        "  2. Check if the build you downloaded has been compiled for the OS\n"
+        "  1. Check if the build you downloaded has been compiled for the OS\n"
         "     you are using. If not, download another one.\n"
-        "  3. If you are on windows you can try to install a library called\n"
+        "  2. If you are on windows you can try to install a library called\n"
         "     'Visual C++ 2015 Redistributable'. Should be easy to find using\n"
         "     your search engine of choice.\n"
-        "  4. It is possible that you have a build for the correct platform\n"
+        "  3. It is possible that you have a build for the correct platform\n"
         "     but it still does not work. We experienced this mainly on linux.\n"
         "     Try to find another build for your platform.\n"
         "     If it still does not work, you have to compile AN yourself.\n"
-        "  5. Make a bug report on Github and give as much information\n"
+        "  4. Make a bug report on Github and give as much information\n"
         "     as you can. Specifically the full error message, your OS, version, ...")
     raise Exception(message)
 
