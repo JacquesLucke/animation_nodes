f525e66c6c05e6e8921f403a832f46d5f23a81a2
Jacques Lucke <mail@jlucke.com>
2017-07-13 14:50:35 +0200

    vectorize Shade Object Smooth node #730

--- animation_nodes/nodes/mesh/shade_object_smooth.py
+++ animation_nodes/nodes/mesh/shade_object_smooth.py
@@ -1,16 +1,29 @@
 import bpy
-from ... base_types import AnimationNode
+from ... base_types import VectorizedNode
 
-class ShadeObjectSmooth(bpy.types.Node, AnimationNode):
+class ShadeObjectSmooth(bpy.types.Node, VectorizedNode):
     bl_idname = "an_ShadeObjectSmoothNode"
     bl_label = "Shade Object Smooth"
+    autoVectorizeExecution = True
+
+    useObjectList = VectorizedNode.newVectorizeProperty()
+    useSmoothList = VectorizedNode.newVectorizeProperty()
 
     def create(self):
-        self.newInput("Object", "Object", "object").defaultDrawType = "PROPERTY_ONLY"
-        self.newInput("Boolean", "Smooth", "smooth")
-        self.newOutput("Object", "Object", "object")
+        self.newVectorizedInput("Object", "useObjectList",
+            ("Object", "object", dict(defaultDrawType = "PROPERTY_ONLY")),
+            ("Objects", "objects"))
 
-    def execute(self, object, smooth):
+        self.newVectorizedInput("Boolean", "useSmoothList",
+            ("Smooth", "smooth"), ("Smooth", "smooth"))
+
+        self.newVectorizedOutput("Object", "useObjectList",
+            ("Object", "object"), ("Objects", "objects"))
+
+    def getExecutionCode(self):
+        return "object = self.execute_Single(object, smooth)"
+
+    def execute_Single(self, object, smooth):
         if getattr(object, "type", "") == "MESH":
             mesh = object.data
             if len(mesh.polygons) > 0:
