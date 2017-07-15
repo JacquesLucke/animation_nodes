7102b98422f37ae9ca317cbec7269b2f800d1dd4
Jacques Lucke <mail@jlucke.com>
2017-07-15 11:30:31 +0200

    version bump

--- animation_nodes/__init__.py
+++ animation_nodes/__init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "name":        "Animation Nodes",
     "description": "Node based visual scripting system designed for motion graphics in Blender.",
     "author":      "Jacques Lucke",
-    "version":     (2, 0, 3),
+    "version":     (2, 0, 4),
     "blender":     (2, 78, 0),
     "location":    "Node Editor",
     "category":    "Node",
