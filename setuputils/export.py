1b97a03272530cf5135fe2b3280c31b83e293fb1
Jacques Lucke <mail@jlucke.com>
2017-07-31 16:45:19 +0200

    cleanup

--- setuputils/export.py
+++ setuputils/export.py
@@ -18,23 +18,24 @@ def execute_Export(addonDirectory, exportPath):
 
 def execute_ExportC(addonDirectory, exportCPath, exportCSetupPath):
     removeFile(exportCPath)
+    topdir = "animation_nodes_c"
 
     with zipfile.ZipFile(exportCPath, "w", zipfile.ZIP_DEFLATED) as zipFile:
         for relativePath in iterRelativeExportCFiles(addonDirectory):
             absolutePath = os.path.join(addonDirectory, relativePath)
-            zipFile.write(absolutePath, os.path.join("animation_nodes", relativePath))
+            zipFile.write(absolutePath, os.path.join(topdir, "animation_nodes", relativePath))
 
         for path in iterPathsWithExtension(currentDirectory, ".py"):
             relativePath = os.path.relpath(path, currentDirectory)
-            zipFile.write(path, os.path.join("setuputils", relativePath))
+            zipFile.write(path, os.path.join(topdir, "setuputils", relativePath))
 
         setupInfo = getPlatformSummary()
         tmpFileName = "tmp.json"
         writeJsonFile(tmpFileName, setupInfo)
-        zipFile.write(tmpFileName, "setup_info.json")
+        zipFile.write(tmpFileName, os.path.join(topdir, "setup_info.json"))
         removeFile(tmpFileName)
 
-        zipFile.write(exportCSetupPath, "setup.py")
+        zipFile.write(exportCSetupPath, os.path.join(topdir, "setup.py"))
 
     print("Exported C Build:")
     print("    " + exportCPath)
