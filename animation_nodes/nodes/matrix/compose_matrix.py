8d765ed9f011218ac00aab82189faf71a9c3dd86
Jacques Lucke <mail@jlucke.com>
2017-08-01 14:23:13 +0200

    revectorize Compose Matrix node using Virtual Lists

--- animation_nodes/nodes/matrix/compose_matrix.py
+++ animation_nodes/nodes/matrix/compose_matrix.py
@@ -1,6 +1,8 @@
 import bpy
 from bpy.props import *
+from . c_utils import composeMatrices
 from ... base_types import VectorizedNode
+from ... data_structures import VirtualList, VirtualVector3DList, VirtualEulerList
 
 class ComposeMatrixNode(bpy.types.Node, VectorizedNode):
     bl_idname = "an_ComposeMatrixNode"
@@ -32,5 +34,8 @@ class ComposeMatrixNode(bpy.types.Node, VectorizedNode):
         yield "matrix = animation_nodes.utils.math.composeMatrix(translation, rotation, scale)"
 
     def execute_List(self, translations, rotations, scales):
-        from . c_utils import composeMatrices
-        return composeMatrices(translations, rotations, scales)
+        _translations = VirtualVector3DList.fromListOrElement(translations, (0, 0, 0))
+        _rotations = VirtualEulerList.fromListOrElement(rotations, (0, 0, 0))
+        _scales = VirtualVector3DList.fromListOrElement(scales, (1, 1, 1))
+        length = VirtualList.getMaxLength(_translations, _rotations, _scales)
+        return composeMatrices(length, _translations, _rotations, _scales)
