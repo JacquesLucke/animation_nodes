7d7f1fb9effaa88a3a5c6d0391b94552800d772b
Jacques Lucke <mail@jlucke.com>
2017-08-03 11:20:55 +0200

    use Socket Template in Switch Node

--- animation_nodes/nodes/boolean/switch_node.py
+++ animation_nodes/nodes/boolean/switch_node.py
@@ -1,32 +1,30 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode, AutoSelectDataType
+from ... base_types import AnimationNode, DataTypeSelectorSocket
 
 class SwitchNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SwitchNode"
     bl_label = "Switch"
 
-    assignedType = StringProperty(update = AnimationNode.refresh, default = "Float")
+    assignedType = DataTypeSelectorSocket.newProperty(default = "Float")
 
     def create(self):
         self.newInput("an_BooleanSocket", "Condition", "condition")
-        self.newInput(self.assignedType, "If True", "ifTrue")
-        self.newInput(self.assignedType, "If False", "ifFalse")
-        self.newOutput(self.assignedType, "Output", "output")
-        self.newOutput(self.assignedType, "Other", "other", hide = True)
 
-        self.newSocketEffect(AutoSelectDataType("assignedType",
-            [self.inputs[1], self.inputs[2],
-             self.outputs[0], self.outputs[1]]
-        ))
+        self.newInput(DataTypeSelectorSocket("If True", "ifTrue", "assignedType"))
+        self.newInput(DataTypeSelectorSocket("If False", "ifFalse", "assignedType"))
+
+        self.newOutput(DataTypeSelectorSocket("Output", "output", "assignedType"))
+        self.newOutput(DataTypeSelectorSocket("Other", "other", "assignedType"), hide = True)
 
     def drawAdvanced(self, layout):
         self.invokeSelector(layout, "DATA_TYPE", "assignType",
             text = "Change Type", icon = "TRIA_RIGHT")
 
     def assignType(self, dataType):
-        if dataType == self.assignedType: return
-        self.assignedType = dataType
+        if self.assignedType != dataType:
+            self.assignedType = dataType
+            self.refresh()
 
     def getExecutionCode(self, required):
         if "output" in required: yield "output = ifTrue if condition else ifFalse"
