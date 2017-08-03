bbb82c8ad1e913dc3d5fb6e4abbf019f5faaef16
Jacques Lucke <mail@jlucke.com>
2017-08-03 11:05:22 +0200

    use DataTypeSelectorSocket in Compare node

--- animation_nodes/nodes/boolean/compare.py
+++ animation_nodes/nodes/boolean/compare.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy.props import *
-from ... base_types import AnimationNode, AutoSelectDataType
+from ... base_types import AnimationNode, DataTypeSelectorSocket
 
 compare_types = ["A = B", "A != B", "A < B", "A <= B", "A > B", "A >= B", "A is B","A is None"]
 compare_types_items = [(t, t, "") for t in compare_types]
@@ -12,28 +12,24 @@ class CompareNode(bpy.types.Node, AnimationNode):
     bl_label = "Compare"
     dynamicLabelType = "HIDDEN_ONLY"
 
-    assignedType = StringProperty(update = AnimationNode.refresh, default = "Integer")
+    assignedType = DataTypeSelectorSocket.newProperty(default = "Integer")
 
     compareType = EnumProperty(name = "Compare Type",
         items = compare_types_items, update = AnimationNode.refresh)
 
     def create(self):
-        self.newInput(self.assignedType, "A", "a")
+        self.newInput(DataTypeSelectorSocket("A", "a", "assignedType"))
         if self.compareType != "A is None":
-            self.newInput(self.assignedType, "B", "b")
-        self.newOutput("an_BooleanSocket", "Result", "result")
+            self.newInput(DataTypeSelectorSocket("B", "b", "assignedType"))
 
-        self.newSocketEffect(AutoSelectDataType("assignedType",
-            [self.inputs[0],
-             self.inputs[1] if len(self.inputs) == 2 else None]
-        ))
+        self.newOutput("Boolean", "Result", "result")
 
     def draw(self, layout):
         layout.prop(self, "compareType", text = "Type")
 
     def drawLabel(self):
         label = self.compareType
-        if self.assignedType in numericLabelTypes:
+        if self.assignedType in numericLabelTypes and len(self.inputs) == 2:
             if getattr(self.socketA, "isUnlinked", False):
                 label = label.replace("A", str(round(self.socketA.value, 4)))
             if getattr(self.socketB, "isUnlinked", False):
@@ -57,8 +53,9 @@ class CompareNode(bpy.types.Node, AnimationNode):
         return "result = False"
 
     def assignType(self, dataType):
-        if self.assignedType == dataType: return
-        self.assignedType = dataType
+        if self.assignedType != dataType:
+            self.assignedType = dataType
+            self.refresh()
 
     @property
     def socketA(self):
