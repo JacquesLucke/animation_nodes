3fd35e04ad0146d4d8a2c410e6094e219ab6c1e8
Jacques Lucke <mail@jlucke.com>
2017-08-28 10:28:01 +0200

    fix preferences class name for Blender 2.79

--- animation_nodes/__init__.py
+++ animation_nodes/__init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "name":        "Animation Nodes",
     "description": "Node based visual scripting system designed for motion graphics in Blender.",
     "author":      "Jacques Lucke",
-    "version":     (2, 0, 4),
+    "version":     (2, 0, 5),
     "blender":     (2, 78, 0),
     "location":    "Node Editor",
     "category":    "Node",
