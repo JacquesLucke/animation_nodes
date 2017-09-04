613af2dcffe6735da97698a5f50f21e274b21e17
Jacques Lucke <mail@jlucke.com>
2017-09-04 12:21:07 +0200

    vectorize Vector Angle node

--- animation_nodes/nodes/vector/vector_angle.py
+++ animation_nodes/nodes/vector/vector_angle.py
@@ -1,15 +1,27 @@
 import bpy
-from ... base_types import AnimationNode
+from ... base_types import AnimationNode, VectorizedSocket
 
 class VectorAngleNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_VectorAngleNode"
     bl_label = "Vector Angle"
+    codeEffects = [VectorizedSocket.CodeEffect]
+
+    useListA = VectorizedSocket.newProperty()
+    useListB = VectorizedSocket.newProperty()
 
     def create(self):
-        self.newInput("Vector", "A", "a", value = [1, 0, 0])
-        self.newInput("Vector", "B", "b", value = [0, 0, 1])
-        self.newOutput("Float", "Angle", "angle")
-        self.newOutput("Quaternion", "Rotation Difference", "rotationDifference")
+        self.newInput(VectorizedSocket("Vector", "useListA",
+            ("A", "a", dict(value = (1, 0, 0))), ("A", "a")))
+
+        self.newInput(VectorizedSocket("Vector", "useListB",
+            ("B", "b", dict(value = (0, 0, 1))), ("B", "b")))
+
+        self.newOutput(VectorizedSocket("Float", ["useListA", "useListB"],
+            ("Angle", "angle"), ("Angles", "angles")))
+
+        self.newOutput(VectorizedSocket("Quaternion", ["useListA", "useListB"],
+            ("Rotation Difference", "rotationDifference"),
+            ("Rotation Differences", "rotationDifferences")))
 
     def getExecutionCode(self, required):
         if "angle" in required:
