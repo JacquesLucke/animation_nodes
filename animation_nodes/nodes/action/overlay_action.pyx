1273bb82969145cf7616e0a55f14b337c68e4783
Jacques Lucke <mail@jlucke.com>
2017-09-23 10:48:57 +0200

    unbounded action support for Overlay Action node

--- animation_nodes/nodes/action/overlay_action.pyx
+++ animation_nodes/nodes/action/overlay_action.pyx
@@ -3,7 +3,8 @@ from ... base_types import AnimationNode
 from ... data_structures cimport (
     FloatList,
     Action, ActionEvaluator,
-    BoundedAction, BoundedActionEvaluator
+    BoundedAction, BoundedActionEvaluator,
+    UnboundedAction, UnboundedActionEvaluator
 )
 
 class OverlayActionNode(bpy.types.Node, AnimationNode):
@@ -17,25 +18,30 @@ class OverlayActionNode(bpy.types.Node, AnimationNode):
 
     def execute(self, base, overlay):
         if base is None:
-            return None
+            return overlay
         if overlay is None:
             return base
-        return OverlayAction(base, overlay)
+        if isinstance(base, BoundedAction):
+            return BoundedOverlayAction(base, overlay)
+        elif isinstance(base, UnboundedAction):
+            return UnboundedOverlayAction(base, overlay)
 
 
-cdef class OverlayAction(BoundedAction):
+cdef class BoundedOverlayAction(BoundedAction):
     cdef BoundedAction base
     cdef Action overlay
+    cdef set channels
 
     def __cinit__(self, BoundedAction base, Action overlay):
         self.base = base
         self.overlay = overlay
+        self.channels = base.getChannelSet() | overlay.getChannelSet()
 
     cdef set getChannelSet(self):
-        return self.base.getChannelSet()
+        return self.channels
 
     cdef BoundedActionEvaluator getEvaluator_Limited(self, list channels):
-        base = self.base.getEvaluator_Limited(channels)
+        base = self.base.getEvaluator(channels)
         overlay = self.overlay.getEvaluator(channels)
         return OverlayActionEvaluator(base, overlay)
 
@@ -66,3 +72,41 @@ cdef class OverlayActionEvaluator(BoundedActionEvaluator):
 
     cpdef float getLength(self, Py_ssize_t index):
         return self.base.getLength(index)
+
+
+cdef class UnboundedOverlayAction(UnboundedAction):
+    cdef UnboundedAction base
+    cdef Action overlay
+    cdef set channels
+
+    def __cinit__(self, UnboundedAction base, Action overlay):
+        self.base = base
+        self.overlay = overlay
+        self.channels = base.getChannelSet() | overlay.getChannelSet()
+
+    cdef set getChannelSet(self):
+        return self.channels
+
+    cdef UnboundedActionEvaluator getEvaluator_Limited(self, list channels):
+        base = self.base.getEvaluator(channels)
+        overlay = self.overlay.getEvaluator(channels)
+        return UnboundedOverlayActionEvaluator(base, overlay)
+
+cdef class UnboundedOverlayActionEvaluator(UnboundedActionEvaluator):
+    cdef UnboundedActionEvaluator base
+    cdef ActionEvaluator overlay
+    cdef FloatList overlayTarget
+
+    def __cinit__(self, UnboundedActionEvaluator base, ActionEvaluator overlay):
+        self.base = base
+        self.overlay = overlay
+        self.overlayTarget = FloatList(length = overlay.channelAmount)
+        self.channelAmount = base.channelAmount
+
+    cdef void evaluate(self, float frame, Py_ssize_t index, float *target):
+        self.base.evaluate(frame, index, target)
+        self.overlay.evaluate(frame, index, self.overlayTarget.data)
+
+        cdef Py_ssize_t i
+        for i in range(self.channelAmount):
+            target[i] += self.overlayTarget.data[i]
