55f6bb99dfecc52ad5d6eed95d295c8a3d29d67f
Jacques Lucke <mail@jlucke.com>
2017-10-04 16:16:04 +0200

    change functionality of the Transform Polygons node

--- animation_nodes/nodes/mesh/transform_polygons.py
+++ animation_nodes/nodes/mesh/transform_polygons.py
@@ -1,25 +1,35 @@
 import bpy
 from ... base_types import AnimationNode
-from . c_utils import transformPolygons
+from .. matrix.transformation_base_node import MatrixTransformationBase
+from . c_utils import transformPolygons, separatePolygons, extractPolygonTransforms
+from ... data_structures import Mesh, EdgeIndicesList
+from ... data_structures.meshes.mesh_data import createValidEdgesList
 
-class TransformPolygonsNode(bpy.types.Node, AnimationNode):
+class TransformPolygonsNode(bpy.types.Node, AnimationNode, MatrixTransformationBase):
     bl_idname = "an_TransformPolygonsNode"
     bl_label = "Transform Polygons"
     errorHandlingType = "EXCEPTION"
 
     def create(self):
-        self.newInput("Vector List", "Vertices", "vertices", dataIsModified = True)
-        self.newInput("Polygon Indices List", "Polygon Indices", "polygonIndices")
-        self.newInput("Matrix List", "Matrices", "matrices")
+        self.newInput("Mesh", "Mesh", "inMesh")
+        self.createMatrixTransformationInputs(useMatrixList = True)
+        self.newOutput("Mesh", "Mesh", "outMesh")
 
-        self.newOutput("Vector List", "Vertices", "vertices")
-        self.newOutput("Polygon Indices List", "Polygon Indices", "polygonIndices")
+    def draw(self, layout):
+        self.draw_MatrixTransformationProperties(layout)
 
-    def execute(self, vertices, polygons, matrices):
-        if len(polygons) != 0 and polygons.getMaxIndex() >= len(vertices):
-            self.raiseErrorMessage("Invalid polygon indices")
-        if len(polygons) != len(matrices):
-            self.raiseErrorMessage("Different amount of polygons and matrices")
+    def drawAdvanced(self, layout):
+        self.drawAdvanced_MatrixTransformationProperties(layout)
 
-        transformPolygons(vertices, polygons, matrices)
-        return vertices, polygons
+    def execute(self, mesh, *transformationArgs):
+        newVertices, newPolygons = separatePolygons(mesh.vertices, mesh.polygons)
+        transforms, invertedTransforms = extractPolygonTransforms(newVertices, newPolygons, calcInverted = True)
+        transformPolygons(newVertices, newPolygons, invertedTransforms)
+        newTransforms = self.transformMatrices(transforms, transformationArgs)
+        transformPolygons(newVertices, newPolygons, newTransforms)
+        newEdges = createValidEdgesList(EdgeIndicesList(), newPolygons)
+        return Mesh(newVertices, newEdges, newPolygons)
+
+    def transformMatrices(self, matrices, args):
+        name = self.getMatrixTransformationFunctionName(useMatrixList = True)
+        return getattr(self, name)(matrices, *args)
