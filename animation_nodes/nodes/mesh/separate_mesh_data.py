3c4dd59ddee3974054862a6b8a9175bd2f6ba276
Jacques Lucke <mail@jlucke.com>
2017-10-05 12:29:39 +0200

    cleanup

--- animation_nodes/nodes/mesh/separate_mesh_data.py
+++ animation_nodes/nodes/mesh/separate_mesh_data.py
@@ -6,7 +6,7 @@ class SeparateMeshNode(bpy.types.Node, AnimationNode):
     bl_label = "Separate Mesh"
 
     def create(self):
-        self.newInput("Mesh", "Mesh", "meshData").dataIsModified = True
+        self.newInput("Mesh", "Mesh", "meshData", dataIsModified = True)
         self.newOutput("Vector List", "Vertex Locations", "vertexLocations")
         self.newOutput("Edge Indices List", "Edges Indices", "edgesIndices")
         self.newOutput("Polygon Indices List", "Polygons Indices", "polygonsIndices")
