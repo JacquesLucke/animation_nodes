5867e3c8ec9e05343bb6500d4d0b7044a18443e1
OmarSquircleArt <omar.squircleart@gmail.com>
2018-07-14 14:18:56 +0200

    General Refactor Of The Algorithm.
    Co-Authored-By: Jacques Lucke <mail@jlucke.com>

--- animation_nodes/nodes/spline/splines_from_edges_utils.pyx
+++ animation_nodes/nodes/spline/splines_from_edges_utils.pyx
@@ -22,51 +22,74 @@ def splinesFromBranches(Vector3DList vertices, EdgeIndicesList edges, VirtualDou
         start += neighboursAmounts.data[i]
 
     # Keep track of how many index is in each group of neighbours at each iteration.
-    cdef IntegerList filledSpaces = IntegerList.fromValue(0, length = verticesAmount)
+    cdef IntegerList usedSlots = IntegerList.fromValue(0, length = verticesAmount)
 
     # Compute the indices of neighbouring vertices of each vertex.
     cdef unsigned int v1, v2
     cdef IntegerList neighbours = IntegerList(length = edgesAmount * 2)
     for i in range(edgesAmount):
         v1, v2 = edges.data[i].v1, edges.data[i].v2
-        neighbours.data[neighboursStarts.data[v1] + filledSpaces.data[v1]] = v2
-        filledSpaces.data[v1] += 1
+        neighbours.data[neighboursStarts.data[v1] + usedSlots.data[v1]] = v2
+        usedSlots.data[v1] += 1
 
-        neighbours.data[neighboursStarts.data[v2] + filledSpaces.data[v2]] = v1
-        filledSpaces.data[v2] += 1
+        neighbours.data[neighboursStarts.data[v2] + usedSlots.data[v2]] = v1
+        usedSlots.data[v2] += 1
 
     # Generate Splines.
     cdef list splines = []
     cdef PolySpline spline
     cdef FloatList splineRadii
     cdef Vector3DList splineVertices
-    cdef int currentVertex, nextVertex
-    for i in range(verticesAmount):
-        if neighboursAmounts.data[i] == 2: continue
-        for j in range(neighboursAmounts.data[i]):
-            currentVertex = i
-            nextVertex = neighbours.data[neighboursStarts.data[i] + j]
-            if (filledSpaces.data[nextVertex] == neighboursAmounts.data[nextVertex] or
-                                                neighboursAmounts.data[nextVertex] != 2):
-                splineVertices = Vector3DList.__new__(Vector3DList)
-                splineRadii = FloatList.__new__(FloatList)
+    cdef IntegerList unusedEdges = usedSlots
+    del usedSlots
+
+    cdef int startVertex, lastVertex, currentVertex, nextVertex
+    cdef int offset, n1, n2
+
+    for startVertex in range(verticesAmount):
+        # is part of another spline
+        if neighboursAmounts.data[startVertex] == 2:
+            continue
+
+        # has no unused outgoing edges
+        if unusedEdges.data[startVertex] == 0:
+            continue
+
+        for j in range(neighboursAmounts.data[startVertex]):
+            currentVertex = startVertex
+            nextVertex = neighbours.data[neighboursStarts.data[currentVertex] + j]
+
+            # check if this neighbour can be connected
+            if unusedEdges.data[nextVertex] == 0:
+                continue
+
+            splineVertices = Vector3DList.__new__(Vector3DList)
+            splineRadii = FloatList.__new__(FloatList)
+
+            splineVertices.append_LowLevel(vertices.data[currentVertex])
+            splineRadii.append_LowLevel(radii.get(currentVertex))
+
+            # follow branch until the next non bipolar vertex
+            while True:
+                lastVertex = currentVertex
+                currentVertex = nextVertex
 
                 splineVertices.append_LowLevel(vertices.data[currentVertex])
                 splineRadii.append_LowLevel(radii.get(currentVertex))
-                filledSpaces.data[currentVertex] -= 1
-                while True:
-                    if neighbours.data[neighboursStarts.data[nextVertex]] == currentVertex:
-                        currentVertex = nextVertex
-                        nextVertex = neighbours.data[neighboursStarts.data[nextVertex] + 1]
-                    else:
-                        currentVertex = nextVertex
-                        nextVertex = neighbours.data[neighboursStarts.data[nextVertex]]
-                    splineVertices.append_LowLevel(vertices.data[currentVertex])
-                    splineRadii.append_LowLevel(radii.get(currentVertex))
-                    filledSpaces.data[currentVertex] -= 1
-                    if neighboursAmounts.data[currentVertex] != 2:
-                        break
-                splines.append(PolySpline.__new__(PolySpline, splineVertices, splineRadii))
+                unusedEdges.data[lastVertex] -= 1
+                unusedEdges.data[currentVertex] -= 1
+
+                # stop at another non bipolar vertex
+                if neighboursAmounts.data[currentVertex] != 2:
+                    break
+
+                # choose next vertex to be the one we are not coming from
+                offset = neighboursStarts.data[currentVertex]
+                n1 = neighbours.data[offset + 0]
+                n2 = neighbours.data[offset + 1]
+                nextVertex = n1 if lastVertex != n1 else n2
+
+            splines.append(PolySpline.__new__(PolySpline, splineVertices, splineRadii))
     return splines
 
 def splinesFromEdges(Vector3DList vertices, EdgeIndicesList edges, VirtualDoubleList radii,
