ecce0e78808b15f3bd7e9ff209852555bc31915b
Omar Ahmad <25300994+OmarSquircleArt@users.noreply.github.com>
2018-07-14 15:08:05 +0200

    New Algorithm For The Splines From Edges Node. (#922)
    * Added Vertex Neighbours Code.
    
    * Added Initial Implementation for connectedSplinesFromEdges function.
    
    * Added Initial Implementation For New Splines From Edges Node.
    
    * Added Radii Option For Per Branch Spline From Edges Node.
    
    * Initialize Lists Using __new__ And Cdef Float List.
    
    * Fix Double Non Bipolar Vertices Spline Bug.
    
    * String Input For Radius Type And Follow Convention For Algorithm.
    
    * Added Exceptions If The Edge Indices Is Invaild.
    
    * Use While Loop Instead Of For With Break Condition.
    
    * Added Error Handling For Splines From Edges Node.
    
    * String To Boolean And Fix Error Handling.
    
    * Refactor splinesFromBranches Inner Loop.
    
    * Changed To AN Lists Instead Of Dynamically Allocating Memory.
    
    * Use data[i] Instead Of Direct Indexing.
    
    * Use append_LowLevel Function Instead Of Full Mememory Allocation.
    
    * Avoid Going Into Spline Loop If No Branches Exist.
    
    * Refactored Inner Loop.
    
    * Refactor Inner Loop Again.
    
    * Remove nonBipolarVertices List.
    
    * General Refactor Of The Algorithm.
    
    Co-Authored-By: Jacques Lucke <mail@jlucke.com>
    
    * Improve Grammer And Formatting.
    
    * Remove Mesh Data Structure.
    
    * Remove Unwanted Variables.

--- animation_nodes/nodes/spline/c_utils.pyx
+++ animation_nodes/nodes/spline/c_utils.pyx
@@ -1,44 +1,10 @@
 from ... data_structures.meshes.mesh_data import calculateCrossProducts
 from .. mesh.c_utils import matricesFromNormalizedAxisData
 from ... data_structures cimport (
-    DoubleList, Vector3DList, EdgeIndicesList, FloatList,
-    PolySpline, Spline, Matrix4x4List, VirtualFloatList
+    Vector3DList, EdgeIndicesList, FloatList,
+    Spline, Matrix4x4List, VirtualFloatList
 )
 from ... math cimport scaleMatrix3x3Part
-
-def splinesFromEdges(Vector3DList vertices, EdgeIndicesList edges, DoubleList radii, str radiusType):
-    if edges.length == 0: return []
-    if edges.getMaxIndex() >= vertices.length:
-        raise Exception("Invalid edge indices")
-    if radiusType == "EDGE":
-        if edges.length != radii.length:
-            raise Exception("wrong radius amount")
-    elif radiusType == "VERTEX":
-        if vertices.length != radii.length:
-            raise Exception("wrong radius amount")
-
-    cdef:
-        long i
-        list splines = []
-        FloatList edgeRadii
-        Vector3DList edgeVertices
-        bint radiusPerVertex = radiusType == "VERTEX"
-
-    for i in range(edges.length):
-        edgeVertices = Vector3DList.__new__(Vector3DList, length = 2)
-        edgeVertices.data[0] = vertices.data[edges.data[i].v1]
-        edgeVertices.data[1] = vertices.data[edges.data[i].v2]
-
-        edgeRadii = FloatList.__new__(FloatList, length = 2)
-        if radiusPerVertex:
-            edgeRadii.data[0] = radii.data[edges.data[i].v1]
-            edgeRadii.data[1] = radii.data[edges.data[i].v2]
-        else:
-            edgeRadii.data[0] = radii.data[i]
-            edgeRadii.data[1] = radii.data[i]
-
-        splines.append(PolySpline.__new__(PolySpline, edgeVertices, edgeRadii))
-    return splines
 
 def getMatricesAlongSpline(Spline spline, Py_ssize_t amount, distribution):
     assert spline.isEvaluable()
--- animation_nodes/nodes/spline/splines_from_edges.py
+++ animation_nodes/nodes/spline/splines_from_edges.py
@@ -1,10 +1,14 @@
 import bpy
 from bpy.props import *
-from . c_utils import splinesFromEdges
 from ... events import propertyChanged
-from ... data_structures import DoubleList
+from ... data_structures import VirtualDoubleList
 from ... base_types import AnimationNode, VectorizedSocket
+from . splines_from_edges_utils import splinesFromBranches, splinesFromEdges
 
+algorithmTypeItems = [
+    ("EDGE", "Spline Per Edge", "", "NONE", 0),
+    ("BRANCH", "Spline Per Branch", "", "NONE", 1)
+]
 radiusTypeItems = [
     ("EDGE", "Radius per Edge", "", "NONE", 0),
     ("VERTEX", "Radius per Vertex", "", "NONE", 1)
@@ -13,7 +17,12 @@ radiusTypeItems = [
 class SplinesFromEdgesNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SplinesFromEdgesNode"
     bl_label = "Splines from Edges"
+    errorHandlingType = "EXCEPTION"
     bl_width_default = 160
+
+    algorithmType = EnumProperty(name = "Algorithm Type", default = "EDGE",
+        description = "Choose the algorithm used for generating the splines.",
+        update = propertyChanged, items = algorithmTypeItems)
 
     radiusType = EnumProperty(name = "Radius Type", default = "EDGE",
         description = "Only important if there is a list of radii.",
@@ -26,27 +35,24 @@ class SplinesFromEdgesNode(bpy.types.Node, AnimationNode):
         self.newInput("Edge Indices List", "Edge Indices", "edgeIndices")
 
         self.newInput(VectorizedSocket("Float", "useRadiusList",
-            ("Radius", "radius", dict(value = 0.1, minValue = 0)),
-            ("Radii", "radii")))
+            ("Radius", "radii", dict(value = 0.1, minValue = 0)),
+            ("Radii", "radii"),
+            codeProperties = dict(default = 0.1)))
 
         self.newOutput("Spline List", "Splines", "splines")
 
     def draw(self, layout):
-        layout.prop(self, "radiusType", text = "")
+        layout.prop(self, "algorithmType", text = "")
+        if self.algorithmType == "EDGE":
+            layout.prop(self, "radiusType", text = "")
 
-    def execute(self, vertices, edgeIndices, radius):
-        radiiAmount = len(edgeIndices) if self.radiusType == "EDGE" else len(vertices)
+    def execute(self, vertices, edgeIndices, radii):
+        if len(edgeIndices) == 0: return []
+        if edgeIndices.getMaxIndex() >= len(vertices):
+            self.raiseErrorMessage("Invalid Edge Indices.")
 
-        radii = self.prepareRadiusList(radius, radiiAmount)
-        splines = splinesFromEdges(vertices, edgeIndices, radii, self.radiusType)
-
-        return splines
-
-    def prepareRadiusList(self, radii, edgeAmount):
-        if self.useRadiusList:
-            if len(radii) == 0:
-                return DoubleList.fromValue(0, edgeAmount)
-            else:
-                return radii.repeated(length = edgeAmount)
+        radii = VirtualDoubleList.create(radii, 0.1)
+        if self.algorithmType == "EDGE":
+            return splinesFromEdges(vertices, edgeIndices, radii, self.radiusType)
         else:
-            return DoubleList.fromValue(radii, edgeAmount)
+            return splinesFromBranches(vertices, edgeIndices, radii)
