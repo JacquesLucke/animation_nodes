ef19fe45797a2a2c4c11fdc0fdb03b24ec756f82
Jacques Lucke <mail@jlucke.com>
2018-11-29 15:20:41 +0100

    better auto-load implementation

--- animation_nodes/__init__.py
+++ animation_nodes/__init__.py
@@ -67,10 +67,10 @@ if counter > 1:
     raise Exception(message)
 
 
-try: from . import import_modules
+try: from . import auto_load
 except: pass
 
-if "import_modules" not in globals():
+if "auto_load" not in globals():
     message = ("\n\n"
         "The Animation Nodes addon cannot be registered correctly.\n"
         "Please try to remove and install it again.\n"
@@ -159,96 +159,19 @@ if "test_compile" not in globals():
 
 
 
-# Load all submodules
+# register
 ##################################
 
-from . import import_modules
-modules = import_modules.importAllSubmodules(__path__[0], __package__)
-
-if "bpy" in locals():
-    print("Animation Nodes can't be reloaded.")
-
-
-
-# Initialization
-##################################
+from . import auto_load
+auto_load.init()
 
 from . sockets.info import updateSocketInfo
 updateSocketInfo()
 
-
-
-# register
-##################################
-
-import bpy
-import inspect
-import typing
-
-types_to_register = tuple(getattr(bpy.types, name) for name in [
-    "Node",
-    "NodeSocket",
-    "NodeTree",
-    "Menu",
-    "Header",
-    "UIList",
-    "Panel",
-    "Operator",
-    "PropertyGroup",
-    "AddonPreferences"
-])
-
-classes = set()
-
-for module in modules:
-    for value in module.__dict__.values():
-        if inspect.isclass(value):
-            if issubclass(value, types_to_register):
-                classes.add(value)
-
-def iter_register_dependencies(cls):
-    if not hasattr(cls, "__annotations__"):
-        return
-    for value in typing.get_type_hints(cls, {}, {}).values():
-        if isinstance(value, tuple) and len(value) == 2:
-            if value[0] in (bpy.props.PointerProperty, bpy.props.CollectionProperty):
-                dependency = value[1]["type"]
-                yield dependency
-
-dependencies = {}
-for cls in classes:
-    dependencies[cls] = set(iter_register_dependencies(cls))
-
-def toposort(dependency_dict):
-    sorted_list = []
-    has_been_sorted = set()
-    while len(dependency_dict) > 0:
-        still_unsorted = []
-        for element, dependencies in dependency_dict.items():
-            if len(dependencies) == 0:
-                sorted_list.append(element)
-                has_been_sorted.add(element)
-            else:
-                still_unsorted.append(element)
-
-        dependency_dict = {element : dependency_dict[element] - has_been_sorted for element in still_unsorted}
-    return sorted_list
-
-
-sorted_classes = toposort(dependencies)
-
 def register():
-    for cls in sorted_classes:
-        bpy.utils.register_class(cls)
-    for module in modules:
-        if hasattr(module, "register"):
-            module.register()
-    print("Registered Animation Nodes with {} modules.".format(len(modules)))
+    auto_load.register()
+    print("Registered Animation Nodes")
 
 def unregister():
-    for cls in reversed(sorted_classes):
-        bpy.utils.unregister_class(cls)
-    for module in modules:
-        if hasattr(module, "unregister"):
-            module.unregister()
-    print("Unregistered Animation Nodes.")
+    auto_load.unregister()
+    print("Unregistered Animation Nodes")
