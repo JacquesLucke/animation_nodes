96aeced25b4a65924627c7d8423b4164b3d13f18
OmarSquircleArt <omar.squircleart@gmail.com>
2019-01-15 14:57:17 +0200

    Cleanup Set Object Materials node.

--- animation_nodes/nodes/material/set_object_materials.py
+++ animation_nodes/nodes/material/set_object_materials.py
@@ -5,30 +5,30 @@ class SetObjectMaterialsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SetObjectMaterialsNode"
     bl_label = "Set Object Materials"
 
-    useList: VectorizedSocket.newProperty()
+    useMaterialList: VectorizedSocket.newProperty()
 
     def create(self):
         self.newInput("Object", "Object", "object", defaultDrawType = "PROPERTY_ONLY")
-        self.newInput(VectorizedSocket("Material", "useList",
+        self.newInput(VectorizedSocket("Material", "useMaterialList",
             ("Material", "material"),
             ("Materials", "materials")), defaultDrawType = "PROPERTY_ONLY")
 
         self.newOutput("Object", "Object", "object")
 
     def getExecutionFunctionName(self):
-        if self.useList:
+        if self.useMaterialList:
             return "executeList"
         else:
             return "executeSingle"
 
     def executeSingle(self, object, material):
-        if object == None or not hasattr(object.data, "materials"): return object
+        if object is None or not hasattr(object.data, "materials"): return object
         object.data.materials.clear()
         object.data.materials.append(material)
         return object
 
     def executeList(self, object, materials):
-        if object == None or not hasattr(object.data, "materials"): return object
+        if object is None or not hasattr(object.data, "materials"): return object
         object.data.materials.clear()
         for material in materials:
             object.data.materials.append(material)
