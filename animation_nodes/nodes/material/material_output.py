bfafa7c39aab22ca25485f0f4f20be2157fd9568
3DSingh <kadarwalaks@gmail.com>
2019-02-07 21:28:10 +0530

    Fixed the color problem of Material Output Node

--- animation_nodes/nodes/material/material_output.py
+++ animation_nodes/nodes/material/material_output.py
@@ -38,7 +38,7 @@ class MaterialOutputNode(bpy.types.Node, AnimationNode):
             if self.inputs[i].isUsed: yield f"    self.{setFunction}(material, '{attr}', {attr})"
 
     def setAttribute_Color(self, material, attribute, color):
-        newColor = color[:3]
+        newColor = color[:4]
         oldColor = list(getattr(material, attribute))
         if not all(isclose(a, b) for a, b in zip(oldColor, newColor)):
             setattr(material, attribute, newColor)
