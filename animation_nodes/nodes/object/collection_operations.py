37ab5a2c12666c098f64e0c879eac23c56731adf
Unknown <hjahn25@gmail.com>
2019-04-29 15:28:05 +0200

    Vectorized object input

--- animation_nodes/nodes/object/collection_operations.py
+++ animation_nodes/nodes/object/collection_operations.py
@@ -1,24 +1,33 @@
 import bpy
-from ... base_types import AnimationNode
+from ... base_types import AnimationNode, VectorizedSocket
 
 class CollectionOperationsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_CollectionOperationsNode"
     bl_label = "Collection Operations"
 
+    useObjectList: VectorizedSocket.newProperty()
+
     def create(self):
         self.newInput("Collection", "Collection", "collection", defaultDrawType = "PROPERTY_ONLY")
-        self.newInput("Object", "Object", "object", defaultDrawType = "PROPERTY_ONLY")
+        self.newInput(VectorizedSocket("Object", "useObjectList",
+            ("Object", "object"),
+            ("Objects", "objects")))
+            
         self.newInput("Boolean", "Linked", "linked")
         self.newOutput("Collection", "Collection", "collection")
 
-    def execute(self, collection, object, linked):
+    def execute(self, collection, objects, linked):
         if collection is None: return collection
-        if object is None: return collection
+        if objects is None: return collection
+        
+        if not self.useObjectList: objects = [objects]
 
-        if object.name in collection.objects:
-            if not linked: collection.objects.unlink(object)
-        else:
-            if linked: collection.objects.link(object)
+        for object in objects:
+            if object is None: continue
+            if object.name in collection.objects:
+                if not linked: collection.objects.unlink(object)
+            else:
+                if linked: collection.objects.link(object)
 
         return collection
     
