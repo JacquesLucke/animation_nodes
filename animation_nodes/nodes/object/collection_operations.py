c515effc856de1a33e0d3e7cc1029c91be7f1b54
Unknown <hjahn25@gmail.com>
2019-04-30 16:37:52 +0200

    Start using codeEffect

--- animation_nodes/nodes/object/collection_operations.py
+++ animation_nodes/nodes/object/collection_operations.py
@@ -17,33 +17,17 @@ class CollectionOperationsNode(bpy.types.Node, AnimationNode):
         self.newInput("Boolean", "Linked", "linked")
         self.newOutput("Collection", "Collection", "collection")
 
-    def getExecutionFunctionName(self):
-        if self.useObjectList:
-            return "executeMultipleObject"
-        else:
-            return "executeSingleObject"
-
-    def executeMultipleObject(self, collection, objects, linked):
-        if collection is None: return collection
-        if objects is None: return collection
-        
-        for object in objects:
-            if object is None: continue
-            self.applyObject(collection, object, linked)
-
-        return collection
+    def getExecutionCode(self, required):
+        return "self.executeSingleObject(collection, object, linked)"
 
     def executeSingleObject(self, collection, object, linked):
         if collection is None: return collection
         if object is None: return collection
         
-        self.applyObject(collection, object, linked)
-
-        return collection
-
-    def applyObject(self, collection, object, linked):
         if object.name in collection.objects:
             if not linked: collection.objects.unlink(object)
         else:
             if linked: collection.objects.link(object)
+
+        return collection
     
