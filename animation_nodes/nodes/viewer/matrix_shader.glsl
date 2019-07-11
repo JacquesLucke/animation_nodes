41b545aa1a5f1117fb0373477457eefbd7d24c07
OmarSquircleArt <omar.squircleart@gmail.com>
2019-07-11 16:53:13 +0200

    Clean up GLSL code

--- animation_nodes/nodes/viewer/matrix_shader.glsl
+++ animation_nodes/nodes/viewer/matrix_shader.glsl
@@ -13,24 +13,23 @@ void main()
     {
         // case 0 ignored because of the flat interpolation qualifier of v_Color.
         case 1:
-            v_Color = vec4(1, s, s, 1);
+            v_Color = vec4(1.0, s, s, 1.0);
             break;
         case 2:
-            v_Color = vec4(s, 1, s, 1);
+            v_Color = vec4(s, 1.0, s, 1.0);
             break;
         case 3:
-            v_Color = vec4(s, s, 1, 1);
+            v_Color = vec4(s, s, 1.0, 1.0);
             break;
     }
-    gl_Position = u_ViewProjectionMatrix * vec4(pos, 1);
+    gl_Position = u_ViewProjectionMatrix * vec4(pos, 1.0);
 }
 
 // Fragment Shader
 
 flat in vec4 v_Color;
-out vec4 fragColor;
 
 void main()
 {
-    fragColor = v_Color;
+    gl_FragColor = v_Color;
 }
