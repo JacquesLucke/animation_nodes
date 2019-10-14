3b41b9c9b3ac338f61d7980026d6821958831e96
samytichadou <samytichadou@gmail.com>
2019-10-14 12:50:32 +0200

    add integer index offset operator and correct idnames for UI

--- animation_nodes/id_keys/data_types/integer_type.py
+++ animation_nodes/id_keys/data_types/integer_type.py
@@ -20,9 +20,8 @@ class IntegerDataType(SingleIDKeyDataType):
 
     @classmethod
     def drawExtras(cls, layout, object, name):
-        props = layout.operator("an.id_keys_from_sorted_objects", text = "Sort Objects", icon = "SORTSIZE")
-        props.idKeyName = name
-        layout.operator("an.index_offset", icon="LINENUMBERS_ON")
+        layout.operator("an.id_keys_from_sorted_objects", text = "Sort Objects", icon = "SORTSIZE").idKeyName=name
+        layout.operator("an.id_keys_integer_offset", icon="LINENUMBERS_ON").idKeyName=name
 
     @classmethod
     def drawCopyMenu(cls, layout, object, name):
@@ -174,3 +173,135 @@ class IDKeysFromSortedObjects(bpy.types.Operator):
             p1 = Vector(object.bound_box[0])
             p2 = Vector(object.bound_box[6])
             return object.matrix_world * ((p1 + p2) / 2)
+        
+def getIDKeyIntegerName(name):
+    IDKeyName="AN*Integer*%s" % name
+    return IDKeyName
+
+def getObjectsIDKeys(idKeyName):
+    object_list=[]
+    for obj in bpy.context.selected_objects:
+        try:
+            obj[idKeyName]
+            object_list.append(obj)
+        except KeyError: pass
+    sorted_list = sorted(object_list, key=lambda obj: obj[idKeyName])
+    return sorted_list
+
+class IDKeysIntegerOffset(bpy.types.Operator):
+    bl_idname = "an.id_keys_integer_offset"
+    bl_label = "ID Keys Offset"
+    bl_description = "Offset ID Keys of selected Objects."
+
+    idKeyName: StringProperty()
+    offset_type: EnumProperty(
+        name="Type",
+        default='ADD',
+        items=(
+            ('ADD', "Add",
+             "Add number to every selected object which have Index key.\n"
+             "Before :\n"
+             "0, 1, 2, 3\n"
+             "After Offset 1 :\n"
+             "1, 2, 3, 4"),
+            ('RANDOMIZE', "Randomize",
+             "Randomize Index key for all selected objects which have Index Key.\n"
+             "Before :\n"
+             "0, 1, 2, 3\n"
+             "After :\n"
+             "1, 3, 0, 2"),
+            ('REVERT', "Revert",
+             "Revert Index for every selected object which have Index Key.\n"
+             "Before :\n"
+             "0, 1, 2, 3\n"
+             "After :\n"
+             "3, 2, 1, 0"),
+            ('FILL', "Fill Gaps",
+             "Fill Gaps in a serie of Index.\n"
+             "Before :\n"
+             "0, 1, 3, 4\n"
+             "After :\n"
+             "0, 1, 2, 3"),
+        ))
+    offset_value: IntProperty(name="Offset value", default=0)
+    random_method: EnumProperty(
+        name="Method",
+        default='EXISTING',
+        items=(
+            ('EXISTING', "Existing",
+             "Randomize existing indexes through selected objects."),
+            ('CREATE', "Create",
+             "Create random index for each selected object."),
+        ))
+    random_seed: IntProperty(name="Seed", default=0)
+    random_min: IntProperty(name="Min", default=0)
+    random_max: IntProperty(name="Max", default=10)
+    start_at: BoolProperty(name="Start at", default=False)
+    remove_gaps: BoolProperty(name="Remove gaps", default=False)
+
+    @classmethod
+    def poll(cls, context):
+        return len(context.selected_objects) != 0
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self, width = 250 * getDpiFactor())
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "offset_type", expand = True)
+        if self.offset_type=="ADD":
+            layout.prop(self, "offset_value")
+        elif self.offset_type=="RANDOMIZE":
+            layout.prop(self, "random_method", text="")
+            if self.random_method=="CREATE":
+                row=layout.row(align=True)
+                row.prop(self, "random_min")
+                row.prop(self, "random_max")
+            else: layout.prop(self, "random_seed")
+        elif self.offset_type=="REVERT":
+            layout.prop(self, "remove_gaps")
+            if self.remove_gaps:
+                row = layout.row()
+                row.prop(self, "start_at")
+                if self.start_at: row.prop(self, "offset_value", text="")
+        elif self.offset_type=="FILL":
+            row=layout.row()
+            row.prop(self, "start_at")
+            if self.start_at: row.prop(self, "offset_value", text="")
+
+    def check(self, context):
+        return True
+
+    def execute(self, context):
+        idkey=getIDKeyIntegerName(self.idKeyName)
+        object_list=getObjectsIDKeys(idkey)
+        if self.offset_type=="ADD":
+            for obj in object_list: obj[idkey]=obj[idkey]+self.offset_value
+            #for obj in object_list: obj.id_keys.set("Integer", idkey, index + self.offset)
+        elif self.offset_type=="RANDOMIZE":
+            if self.random_method=="EXISTING":
+                random_numbers=[i for i in range(len(object_list))]
+                random.seed(self.random_seed)
+                random.shuffle(random_numbers)
+            else:
+                try: random_numbers=random.sample(range(self.random_min, self.random_max), len(object_list))
+                except ValueError: random_numbers = random.sample(range(self.random_min, len(object_list)), len(object_list))
+            for obj in object_list: obj[idkey] = random_numbers[object_list.index(obj)]
+        elif self.offset_type == "REVERT":
+            if self.remove_gaps:
+                if self.start_at: offset = self.offset_value
+                else: offset = object_list[0][idkey]
+                object_list.reverse()
+                for obj in object_list: obj[idkey] = object_list.index(obj)+offset
+            else:
+                index_list = []
+                for obj in object_list: index_list.append(obj[idkey])
+                index_list.reverse()
+                for obj in object_list: obj[idkey]=index_list[object_list.index(obj)]
+        elif self.offset_type == "FILL":
+            if self.start_at: offset=self.offset_value
+            else: offset=object_list[0][idkey]
+            for obj in object_list: obj[idkey] = object_list.index(obj)+offset
+        redrawAll()
+        self.offset_value=0
+        return {'FINISHED'}
