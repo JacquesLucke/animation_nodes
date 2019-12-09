9efde4920c3e957c78099c6017a05fe698ea379a
3DSingh <kadarwalaks@gmail.com>
2019-12-10 00:49:24 +0530

    Improved the code of Texture Input node.

--- animation_nodes/nodes/newnodes/Texture_Input.py
+++ animation_nodes/nodes/newnodes/Texture_Input.py
@@ -1,15 +1,15 @@
 import bpy
 from bpy.props import *
 from ... events import propertyChanged
-from ... data_structures import DoubleList
 from ... base_types import AnimationNode, VectorizedSocket
+from ... data_structures import DoubleList, Color, ColorList
 
 class TextureInputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TextureInputNode"
     bl_label = "Texture Input"
 
     useVectorList: VectorizedSocket.newProperty()
-    autoRefreshBool: BoolProperty(name="Auto Refresh", default=False, update=propertyChanged)    
+    autoRefreshBool: BoolProperty(name = "Auto Refresh", default = False, update = propertyChanged)    
     
     def create(self):
         self.newInput("Texture", "Texture", "texture", defaultDrawType = "PROPERTY_ONLY")
@@ -35,7 +35,7 @@ class TextureInputNode(bpy.types.Node, AnimationNode):
         layout.prop(self, "autoRefreshBool")
         box = layout.box()
         col = box.column(align = True)
-        col.label(text = "Info", icon="INFO")
+        col.label(text = "Info", icon = "INFO")
         col.label(text = "For External Texture, Alpha Output = Alpha")
         col.label(text = "For Internal Texture, Alpha Output = Luminance")
 
@@ -46,40 +46,34 @@ class TextureInputNode(bpy.types.Node, AnimationNode):
             return "executeSingle"
 
     def executeSingle(self, texture, location):
-        if len(bpy.data.textures) < 1 or texture is None:
+        if texture is None:
             return None, None, None, None, None, None
-        try:    
+        
+        if texture.image.source in ['SEQUENCE', 'MOVIE']: 
             texture.image_user.use_auto_refresh = self.autoRefreshBool
-        except:
-            pass
 
-        color = texture.evaluate(location)[:]
-        red = color[0]
-        green = color[1]
-        blue = color[2]
-        alpha = color[3]     
-        return color, red, green, blue, alpha, texture
+        color = Color(texture.evaluate(location))
+        return color, color.r, color.g, color.b, color.a, texture
         
     def executeList(self, texture, locations):
-        reds = []
-        greens = []
-        blues = []
-        alphas = []
-        colors = []
-        if len(bpy.data.textures) < 1 or texture is None:
-            return colors, DoubleList.fromValues(reds), DoubleList.fromValues(greens), DoubleList.fromValues(blues), DoubleList.fromValues(alphas), None
-        if locations is None or len(locations) < 1:
-            return colors, DoubleList.fromValues(reds), DoubleList.fromValues(greens), DoubleList.fromValues(blues), DoubleList.fromValues(alphas), texture
-        try:    
+        if texture is None or len(locations) == 0:
+            return ColorList(), DoubleList(), DoubleList(), DoubleList(), DoubleList(), texture
+        
+        if texture.image.source in ['SEQUENCE', 'MOVIE']: 
             texture.image_user.use_auto_refresh = self.autoRefreshBool
-        except:
-            pass
 
-        for location in locations:
-            color = texture.evaluate(location)[:]
-            reds.append(color[0])
-            greens.append(color[1])
-            blues.append(color[2])
-            alphas.append(color[3])
-            colors.append(color)     
-        return colors, DoubleList.fromValues(reds), DoubleList.fromValues(greens), DoubleList.fromValues(blues), DoubleList.fromValues(alphas), texture
+        locationCount = len(locations)
+        reds = DoubleList(length = locationCount)
+        greens = DoubleList(length = locationCount)
+        blues = DoubleList(length = locationCount)
+        alphas = DoubleList(length = locationCount)
+        colors = ColorList(length = locationCount)
+        for i, location in enumerate(locations):
+            color = Color(texture.evaluate(location))
+            reds[i] = color.r
+            greens[i] = color.g
+            blues[i] = color.b
+            alphas[i] = color.a 
+            colors[i] = color
+        return colors, reds, greens, blues, alphas, texture
+
