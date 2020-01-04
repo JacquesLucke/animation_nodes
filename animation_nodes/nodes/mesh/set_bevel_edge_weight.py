7155b38798d4daeccaa9f0d5b33f7d1d4e4755a4
3DSingh <kadarwalaks@gmail.com>
2020-01-04 23:11:03 +0530

    Modified the code of set_bevel_vertex_weight.py and set_bevel_edge_weight.py based on the 1st revision.

--- animation_nodes/nodes/mesh/set_bevel_edge_weight.py
+++ animation_nodes/nodes/mesh/set_bevel_edge_weight.py
@@ -12,28 +12,16 @@ class BevelEdgeWeight(bpy.types.Node, AnimationNode):
     def create(self):
         self.newInput("Object", "Object", "object", defaultDrawType = "PROPERTY_ONLY")
         self.newInput(VectorizedSocket("Float", "useWeightList",
-            ("Weight", "weight"), ("Weights", "weights")))
+            ("Weight", "weights"), ("Weights", "weights")))
 
         self.newOutput("Object", "Object", "object")
 
-    def getExecutionFunctionName(self):
-        if self.useWeightList:
-            return "execute_WeightList"
-        else:
-            return "execute_SingleWeight"
+    def execute(self, object, weights):
+        if object is None: return None
 
-    def execute_SingleWeight(self, object, weight):
-        if object is None or object.type != "MESH" or object.mode != "OBJECT": return
-        if object.data.use_customdata_edge_bevel is False:
-            object.data.use_customdata_edge_bevel = True
+        if object.type != "MESH" or object.mode != "OBJECT":
+            self.raiseErrorMessage("Object is not in object mode or is no mesh object.")
 
-        weights = VirtualDoubleList.create(weight, 0).materialize(len(object.data.edges))
-        object.data.edges.foreach_set('bevel_weight', weights)
-        object.data.update()
-        return object
-
-    def execute_WeightList(self, object, weights):
-        if object is None or object.type != "MESH" or object.mode != "OBJECT": return
         if object.data.use_customdata_edge_bevel is False:
             object.data.use_customdata_edge_bevel = True
 
