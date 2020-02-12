7f4cfd58baf04eb89e1d2a90a460d1a098746a06
3DSingh <kadarwalaks@gmail.com>
2020-02-13 01:14:09 +0530

    Updated the gp_material_output.py and object_material_output.py. Removed the gp object material output node.

--- animation_nodes/nodes/gpencil/gp_material_output.py
+++ animation_nodes/nodes/gpencil/gp_material_output.py
@@ -3,48 +3,49 @@ from bpy.props import *
 from ... events import propertyChanged
 from ... base_types import AnimationNode
 
-modeTypeItems = [
-    ("LINE", "Line", "", "NONE", 0),
-    ("DOTS", "Dots", "", "NONE", 1),
-    ("BOX", "Boxes", "", "NONE", 2)
-]
-
 class GPMaterialOutputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_GPMaterialOutputNode"
     bl_label = "GP Material Output"
     errorHandlingType = "EXCEPTION"
-    
-    modeType: EnumProperty(name = "Mode Type", default = "LINE",
-        items = modeTypeItems, update = AnimationNode.refresh)
-
-    strokeBool: BoolProperty(name="Stroke",
-                        description = "Stroke Mode for material",
-                        default=True, update=propertyChanged)
-    fillBool: BoolProperty(name="Fill",
-                        description = "Fill Mode for material",
-                        default=False, update=propertyChanged)
 
     def create(self):
         self.newInput("Material", "Material", "material", defaultDrawType = "PROPERTY_ONLY")
-        self.newInput("Color", "Color", "color")
-        self.newInput("Color", "Fill Color", "fillColor")
+        self.newInput("Boolean", "Show Stroke", "showStroke", value = True)
+        self.newInput("Text", "Stroke Draw Mode", "strokeDrawMode", value = "LINE")
+        self.newInput("Color", "Stroke Color", "strokeColor")
+        self.newInput("Boolean", "Show Fill", "showFill", value = False, hide = True)
+        self.newInput("Color", "Fill Color", "fillColor", hide = True)
+        self.newInput("Integer", "Pass Index", "passIndex", value = 0, minVlaue = 0, hide = True)
+
         self.newOutput("Material", "Material", "material")
 
-    def draw(self, layout):
-        layout.prop(self, "strokeBool")
-        layout.prop(self, "modeType", text = "")
-        layout.row().separator()
-        layout.prop(self, "fillBool")
+        for socket in self.inputs[1:]:
+            socket.useIsUsedProperty = True
+            socket.isUsed = False
 
-    def execute(self, material, color, fillColor):
-        if material is None: return None
-        bpy.data.materials.create_gpencil_data(material)
-        gmaterial = material.grease_pencil
-        gmaterial.show_stroke = self.strokeBool
-        gmaterial.mode = self.modeType
-        gmaterial.show_fill = self.fillBool
-        if self.strokeBool and  gmaterial.stroke_style == "SOLID":
-            gmaterial.color = color
-        if self.fillBool and gmaterial.fill_style in ["SOLID", "GRADIENT", "CHECKER_BOARD"]:
-            gmaterial.fill_color = fillColor
-        return material
+    def getExecutionCode(self, required):
+        yield "if material is not None:"
+        yield "    if not material.is_grease_pencil:"
+        yield "        bpy.data.materials.create_gpencil_data(material)"
+
+        s = self.inputs
+        isShowStroke = s["Show Stroke"].isUsed
+        isStrokeDrawMode = s["Stroke Draw Mode"].isUsed
+        isStrokeColor = s["Stroke Color"].isUsed
+        isShowFill = s["Show Fill"].isUsed
+        isFillColor = s["Fill Color"].isUsed
+        isPassIndex = s["Pass Index"].isUsed
+
+        if any([isShowStroke, isStrokeDrawMode, isStrokeColor, isShowFill, isFillColor, isPassIndex]):
+            yield                      "    gpMaterial = material.grease_pencil"
+            if isShowStroke:     yield "    gpMaterial.show_stroke = showStroke"
+            if isStrokeDrawMode: yield "    self.setStrokeDrawMode(gpMaterial, strokeDrawMode)"
+            if isStrokeColor:    yield "    gpMaterial.color = strokeColor"
+            if isShowFill:       yield "    gpMaterial.show_fill = showFill"
+            if isFillColor:      yield "    gpMaterial.fill_color = fillColor"
+            if isPassIndex:      yield "    gpMaterial.pass_index = passIndex"
+
+    def setStrokeDrawMode(self, gpMaterial, strokeDrawMode):
+        if strokeDrawMode not in ["LINE", "DOTS", "BOX"]:
+            self.raiseErrorMessage("The Stroke Draw Mode is invalid. \n\nPossible values for 'Stroke Draw Mode' are: 'LINE', 'DOTS', 'BOX'")
+        gpMaterial.mode = strokeDrawMode
