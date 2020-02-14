c776fb204216c3c3a56c59ba25083a4bcdd9720c
Kuldeep Singh <30294746+3DSinghVFX@users.noreply.github.com>
2020-02-15 00:27:15 +0530

    Add Tint Color, Tint Factor, and Stroke Thickness attributes to GP Layer. (#1317)

--- animation_nodes/nodes/gpencil/gp_layer_from_frames.py
+++ animation_nodes/nodes/gpencil/gp_layer_from_frames.py
@@ -16,12 +16,15 @@ class GPLayerFromFramesNode(bpy.types.Node, AnimationNode):
             ("Frame", "frame"), ("Frames", "frames")), dataIsModified = True)
         self.newInput("Text", "Name", "layerName", value = 'AN-Layer')
         self.newInput("Text", "Blend Mode", "blendMode", value = 'REGULAR', hide = True)
-        self.newInput("Float", "Opacity", "opacity", value = 1, hide = True)
-        self.newInput("Integer", "Pass Index", "passIndex", value = 0, hide = True)
+        self.newInput("Float", "Opacity", "opacity", value = 1, minValue = 0, maxValue = 1, hide = True)
+        self.newInput("Color", "Tint Color", "tintColor", hide = True)
+        self.newInput("Float", "Tint Factor", "tintFactor", value = 0, minValue = 0, maxValue = 1, hide = True)
+        self.newInput("Float", "Stroke Thickness", "lineChange", hide = True)
+        self.newInput("Integer", "Pass Index", "passIndex", value = 0, minValue = 0, hide = True)
         self.newInput("Boolean", "Mask Layer", "maskLayer", value = False, hide = True)
         self.newOutput("GPLayer", "Layer", "layer")
 
-    def execute(self, frames, layerName, blendMode, opacity, passIndex, maskLayer):
+    def execute(self, frames, layerName, blendMode, opacity, tintColor, tintFactor, lineChange, passIndex, maskLayer):
         if not self.useFrameList:
             frames = [frames]
 
@@ -30,4 +33,4 @@ class GPLayerFromFramesNode(bpy.types.Node, AnimationNode):
             self.raiseErrorMessage("Some Frame Numbers are repeated.")
         if blendMode not in ['REGULAR', 'OVERLAY', 'ADD', 'SUBTRACT', 'MULTIPLY', 'DIVIDE']:
             self.raiseErrorMessage("The blend mode is invalid. \n\nPossible values for 'Blend Mode' are: 'REGULAR', 'OVERLAY', 'ADD', 'SUBTRACT', 'MULTIPLY', 'DIVIDE'")
-        return GPLayer(layerName, frames, blendMode, opacity, passIndex, maskLayer)
+        return GPLayer(layerName, frames, blendMode, opacity, tintColor, tintFactor, lineChange, passIndex, maskLayer)
--- animation_nodes/nodes/gpencil/replicate_gp_layer.py
+++ animation_nodes/nodes/gpencil/replicate_gp_layer.py
@@ -1,8 +1,8 @@
 import bpy
 from bpy.props import *
 from mathutils import Matrix
-from ... data_structures import GPLayer, VirtualDoubleList
 from ... base_types import AnimationNode, VectorizedSocket
+from ... data_structures import GPLayer, Color, VirtualDoubleList, VirtualLongList, VirtualColorList
 
 transformationTypeItems = [
     ("Matrix List", "Matrices", "", "NONE", 0),
@@ -13,11 +13,14 @@ class ReplicateGPLayerNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ReplicateGPLayerNode"
     bl_label = "Replicate GP Layer"
 
-    useLayerList: VectorizedSocket.newProperty()
-    useOffsetList: VectorizedSocket.newProperty()
-
     transformationType: EnumProperty(name = "Transformation Type", default = "Matrix List",
         items = transformationTypeItems, update = AnimationNode.refresh)
+
+    useLayerList: VectorizedSocket.newProperty()
+    useOffsetList: VectorizedSocket.newProperty()
+    useTintColorList: VectorizedSocket.newProperty()
+    useTintFactorList: VectorizedSocket.newProperty()
+    useLineChangeList: VectorizedSocket.newProperty()
 
     def create(self):
         self.newInput(VectorizedSocket("GPLayer", "useLayerList",
@@ -26,6 +29,12 @@ class ReplicateGPLayerNode(bpy.types.Node, AnimationNode):
         self.newInput(self.transformationType, "Transformations", "transformations")
         self.newInput(VectorizedSocket("Float", "useOffsetList",
             ("Offset Frame", "offsets"), ("Offset Frames", "offsets")), value = 0, hide = True)
+        self.newInput(VectorizedSocket("Color", "useTintColorList",
+            ("Tint Color", "tintColors"), ("Tint Colors", "tintColors")), hide = True)
+        self.newInput(VectorizedSocket("Float", "useTintFactorList",
+            ("Tint Factor", "tintFactors"), ("Tint Factors", "tintFactors")), value = 0, hide = True)
+        self.newInput(VectorizedSocket("Float", "useLineChangeList",
+            ("Stroke Thickness", "lineChanges"), ("Stroke Thicknesses", "lineChanges")), value = 0, hide = True)
 
         self.newOutput("GPLayer List", "Layers", "outLayers")
 
@@ -38,32 +47,44 @@ class ReplicateGPLayerNode(bpy.types.Node, AnimationNode):
         elif self.transformationType == "Vector List":
             return "execute_VectorList"
 
-    def execute_MatrixList(self, layers, matrices, offsets):
+    def execute_MatrixList(self, layers, matrices, offsets, tintColors, tintFactors, lineChanges):
         if isinstance(layers, GPLayer):
             layers = [layers]
 
         _offsets = VirtualDoubleList.create(offsets, 0)
+        _tintColors = VirtualColorList.create(tintColors, Color((0, 0, 0, 0)))
+        _tintFactors = VirtualDoubleList.create(tintFactors, 0)
+        _lineChanges = VirtualDoubleList.create(lineChanges, 0)
 
         outLayers = []
         for i, matrix in enumerate(matrices):
             for layer in layers:
                 newLayer = layer.copy()
                 newLayer.layerName = layer.layerName + "-" + str(i)
+                newLayer.tintColor = _tintColors[i]
+                newLayer.tintFactor = _tintFactors[i]
+                newLayer.lineChange = _lineChanges[i]
                 self.transformLayerByMatrix(newLayer, _offsets[i], matrix)
                 outLayers.append(newLayer)
         return outLayers
 
-    def execute_VectorList(self, layers, vectors, offsets):
+    def execute_VectorList(self, layers, vectors, offsets, tintColors, tintFactors, lineChanges):
         if isinstance(layers, GPLayer):
             layers = [layers]
 
         _offsets = VirtualDoubleList.create(offsets, 0)
+        _tintColors = VirtualColorList.create(tintColors, Color((0, 0, 0, 0)))
+        _tintFactors = VirtualDoubleList.create(tintFactors, 0)
+        _lineChanges = VirtualDoubleList.create(lineChanges, 0)
 
         outLayers = []
         for i, vector in enumerate(vectors):
             for layer in layers:
                 newLayer = layer.copy()
                 newLayer.layerName = layer.layerName + "-" + str(i)
+                newLayer.tintColor = _tintColors[i]
+                newLayer.tintFactor = _tintFactors[i]
+                newLayer.lineChange = _lineChanges[i]
                 self.transformLayerByVector(newLayer, _offsets[i], vector)
                 outLayers.append(newLayer)
         return outLayers
