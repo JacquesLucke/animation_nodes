e02818c44e5c8fc8f7d1c64c0dc225a6002aa9f6
OmarEmaraDev <mail@OmarEmara.dev>
2020-02-17 21:28:23 +0200

    Add Deep Copy option to the Copy Object Data node.

--- animation_nodes/nodes/object/copy_object_data.py
+++ animation_nodes/nodes/object/copy_object_data.py
@@ -1,11 +1,16 @@
 import bpy
-from ... base_types import AnimationNode, VectorizedSocket
+from bpy.props import *
+from ... events import propertyChanged
 from ... utils.data_blocks import removeNotUsedDataBlock
+from ... base_types import AnimationNode, VectorizedSocket
 
 class CopyObjectDataNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_CopyObjectDataNode"
     bl_label = "Copy Object Data"
     codeEffects = [VectorizedSocket.CodeEffect]
+
+    deepCopy : BoolProperty(name = "Deep Copy", default = False, update = propertyChanged,
+        description = "Make the data independent of the source by copying it")
 
     useFromList: VectorizedSocket.newProperty()
     useToList: VectorizedSocket.newProperty()
@@ -21,16 +26,23 @@ class CopyObjectDataNode(bpy.types.Node, AnimationNode):
         self.newOutput(VectorizedSocket("Object", "useToList",
             ("To", "outObject"), ("To", "outObjects")))
 
+    def draw(self, layout):
+        layout.prop(self, "deepCopy")
+
     def getExecutionCode(self, required):
         return "outObject = self.copyObjectData(fromObject, toObject)"
 
     def copyObjectData(self, fromObject, toObject):
         if fromObject is None or toObject is None: return toObject
-        if toObject.data == fromObject.data: return toObject
+        if not self.deepCopy and toObject.data == fromObject.data: return toObject
 
         if toObject.type == fromObject.type:
             oldData = toObject.data
-            toObject.data = fromObject.data
+            if self.deepCopy:
+                toObject.data = fromObject.data.copy()
+                toObject.data.an_data.removeOnZeroUsers = True
+            else:
+                toObject.data = fromObject.data
 
             if oldData.users == 0 and oldData.an_data.removeOnZeroUsers:
                 removeNotUsedDataBlock(oldData, toObject.type)
