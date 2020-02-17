3b9222e921fd238572d2863438196fae8e553828
3DSingh <kadarwalaks@gmail.com>
2020-02-17 19:00:51 +0530

    Refined the code of the Set Polygon Material Index node.

--- animation_nodes/nodes/mesh/set_polygon_material_index.py
+++ animation_nodes/nodes/mesh/set_polygon_material_index.py
@@ -1,140 +1,41 @@
 import bpy
-from bpy.props import *
-from ... events import propertyChanged
 from ... data_structures import LongList, VirtualLongList
 from ... base_types import AnimationNode, VectorizedSocket
-
-modeItems = [
-    ("ALL", "All", "Set material index of every polygon", "NONE", 0),
-    ("INDEX", "Index", "Set material index of a specific polygon", "NONE", 1)
-]
 
 class SetPolygonMaterialIndexNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_SetPolygonMaterialIndexNode"
     bl_label = "Set Polygon Material Index"
     errorHandlingType = "EXCEPTION"
 
-    mode: EnumProperty(name = "Mode", default = "ALL",
-        items = modeItems, update = AnimationNode.refresh)
-
     useIndexList: VectorizedSocket.newProperty()
     useMaterialIndexList: VectorizedSocket.newProperty()
 
     def create(self):
         self.newInput("Object", "Object", "object", defaultDrawType = "PROPERTY_ONLY")
-
-        if self.mode == "INDEX":
-            self.newInput(VectorizedSocket("Integer", "useIndexList",
-                ("Polygon Index", "polyIndex"), ("Polygon Indices", "polyIndices")))
-
-        self.newInput(VectorizedSocket("Integer", ["useMaterialIndexList", "useIndexList"],
-                ("Material Index", "matIndex"), ("Material Indices", "matIndices")))
+        self.newInput(VectorizedSocket("Integer", "useMaterialIndexList",
+                ("Material Index", "materialIndex"), ("Material Indices", "materialIndices")))
 
         self.newOutput("Object", "Object", "object")
 
-    def draw(self, layout):
-        layout.prop(self, "mode", text = "")
-
-    def getExecutionFunctionName(self):
-        if self.mode == "INDEX":
-            if self.useIndexList:
-                if self.useMaterialIndexList:
-                    return "execute_PolygonIndices_MaterialIndexList"
-                else:
-                    return "execute_PolygonIndices_MaterialIndex"
-            else:
-                return "execute_PolygonIndex_MaterialIndex"
-
-        elif self.mode == "ALL":
-            if self.useMaterialIndexList:
-                return "execute_All_MaterialIndexList"
-            else:
-                return "execute_All_MaterialIndex"
-
-    def execute_PolygonIndex_MaterialIndex(self, object, polyIndex, matIndex):
+    def execute(self, object, materialIndices):
         if object is None: return None
 
-        polygons = self.getPolygons(object)
-        if polyIndex < 0 or polyIndex >= len(polygons):
-            self.raiseErrorMessage("This polygon index is out of range.")
-
-        if matIndex < 0:
-            self.raiseErrorMessage("Material index can't be negative.")
-
-        polygons[polyIndex].material_index = matIndex
-        object.data.update()
-        return object
-
-    def execute_PolygonIndices_MaterialIndex(self, object, polyIndices, matIndex):
-        if object is None: return None
-        if len(polyIndices) == 0: return object
-
-        polygons = self.getPolygons(object)
-        polygonAmount = len(polygons)
-        if len(polyIndices) > polygonAmount:
-            self.raiseErrorMessage("Polygon Indices are more than polygons.")
-        if min(polyIndices) < 0 or max(polyIndices) >= polygonAmount:
-            self.raiseErrorMessage("Some polygon indices are out of range.")
-
-        if matIndex < 0:
-            self.raiseErrorMessage("Material index can't be negative.")
-
-        for index in polyIndices:
-            polygons[index].material_index = matIndex
-        object.data.update()
-        return object
-
-    def execute_PolygonIndices_MaterialIndexList(self, object, polyIndices, matIndices):
-        if object is None: return None
-        if len(polyIndices) == 0: return object
-
-        polygons = self.getPolygons(object)
-        polygonAmount = len(polygons)
-        if len(polyIndices) > polygonAmount:
-            self.raiseErrorMessage("Polygon Indices are more than polygons.")
-        if min(polyIndices) < 0 or max(polyIndices) >= polygonAmount:
-            self.raiseErrorMessage("Some polygon indices are out of range.")
-
-        if min(matIndices) < 0:
-            self.raiseErrorMessage("Material index can't be negative.")
-
-        matIndices = VirtualLongList.create(matIndices, 0)
-
-        for i, index in enumerate(polyIndices):
-            polygons[index].material_index = matIndices[i]
-        object.data.update()
-        return object
-
-    def execute_All_MaterialIndex(self, object, matIndex):
-        if object is None: return None
-        polygons = self.getPolygons(object)
-
-        if matIndex < 0:
-            self.raiseErrorMessage("Material index can't be negative.")
-
-        matIndices = LongList(length = len(polygons))
-        matIndices.fill(matIndex)
-        polygons.foreach_set("material_index", matIndices)
-        object.data.update()
-        return object
-
-    def execute_All_MaterialIndexList(self, object, matIndices):
-        if object is None: return None
-        polygons = self.getPolygons(object)
-
-        if min(matIndices) < 0:
-            self.raiseErrorMessage("Material index can't be negative.")
-
-        matIndices = VirtualLongList.create(matIndices, 0).materialize(len(polygons))
-        polygons.foreach_set("material_index", matIndices)
-        object.data.update()
-        return object
-
-    def getPolygons(self, object):
         if object.type != "MESH":
-            self.raiseErrorMessage("No mesh object.")
+            self.raiseErrorMessage("Object is not a mesh object.")
 
-        if object.mode == "EDIT":
+        if object.mode != "OBJECT":
             self.raiseErrorMessage("Object is not in object mode.")
 
-        return object.data.polygons
+        if self.useMaterialIndexList:
+            if min(materialIndices) < 0:
+                self.raiseErrorMessage("Material index can't be negative.")
+        else:
+            if materialIndices < 0:
+                self.raiseErrorMessage("Material index can't be negative.")
+
+        polygons = object.data.polygons
+        materialIndices = VirtualLongList.create(materialIndices, 0).materialize(len(polygons))
+
+        polygons.foreach_set("material_index", materialIndices)
+        object.data.update()
+        return object
