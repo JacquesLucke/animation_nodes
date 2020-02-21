6e84b8c82b00ee71500031e4f2299f266dbd4572
3DSingh <kadarwalaks@gmail.com>
2020-02-21 14:16:03 +0530

    Refined the code of the Object Viewport Color node.

--- animation_nodes/nodes/object/object_viewport_color.py
+++ animation_nodes/nodes/object/object_viewport_color.py
@@ -1,10 +1,10 @@
 import bpy
-from ... data_structures import Color, VirtualColorList
 from ... base_types import AnimationNode, VectorizedSocket
 
 class ObjectViewportColorNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_ObjectViewportColorNode"
     bl_label = "Object Viewport Color"
+    codeEffects = [VectorizedSocket.CodeEffect]
 
     useObjectList: VectorizedSocket.newProperty()
     useColorList: VectorizedSocket.newProperty()
@@ -21,34 +21,10 @@ class ObjectViewportColorNode(bpy.types.Node, AnimationNode):
             ("Object", "object", dict(defaultDrawType = "PROPERTY_ONLY")),
             ("Objects", "objects")))
 
-    def getExecutionFunctionName(self):
-        if self.useObjectList:
-            if self.useColorList:
-                return "execute_Objects_ColorsList"
-            else:
-                return "execute_Objects_SingleColor"
-        else:
-            return "execute_Object_Color"
+    def getExecutionCode(self, required):
+        return "newSpline = self.execute_object_color(object, color)"
 
-    def execute_Object_Color(self, object, color):
+    def execute_object_color(self, object, color):
         if object is None: return None
         object.color = color
         return object
-
-    def execute_Objects_SingleColor(self, objects, color):
-        amount = len(objects)
-        if amount == 0: return objects
-        colors = VirtualColorList.create(color, Color((1, 1, 1, 1)))
-        for i, object in enumerate(objects):
-            if object is None: pass
-            object.color = colors[i]
-        return objects
-
-    def execute_Objects_ColorsList(self, objects, colors):
-        amount = len(objects)
-        if amount == 0 or len(colors) == 0: return objects
-        colors = VirtualColorList.create(colors, Color((1, 1, 1, 1)))
-        for i, object in enumerate(objects):
-            if object is None: pass
-            object.color = colors[i]
-        return objects
