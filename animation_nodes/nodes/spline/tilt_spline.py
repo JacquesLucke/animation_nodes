42a067e187cae80959db2f2ba07d486b9500d651
3DSingh <kadarwalaks@gmail.com>
2020-02-27 19:16:18 +0530

    Vectorized the Tilt Spline node.

--- animation_nodes/nodes/spline/tilt_spline.py
+++ animation_nodes/nodes/spline/tilt_spline.py
@@ -10,22 +10,65 @@ class TiltSplineNode(bpy.types.Node, AnimationNode):
     bl_label = "Tilt Spline"
 
     useTiltList: VectorizedSocket.newProperty()
+    useSplineList: VectorizedSocket.newProperty()
+
     accumulateTilts: BoolProperty(name = "Accumulate Tilts", default = False,
         update = propertyChanged)
 
     def create(self):
-        self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY")
+        socket = self.newInput(VectorizedSocket("Spline", "useSplineList",
+            ("Spline", "spline"), ("Splines", "splines")))
+        socket.defaultDrawType = "PROPERTY_ONLY"
+        socket.dataIsModified = True
         self.newInput(VectorizedSocket("Float", "useTiltList",
             ("Tilt", "tilt"), ("Tilts", "tilts")))
-        self.newOutput("Spline", "Spline", "spline")
+
+        self.newOutput(VectorizedSocket("Spline", "useSplineList",
+            ("Spline", "spline"),
+            ("Splines", "splines")))
 
     def draw(self, layout):
         layout.prop(self, "accumulateTilts", text = "Accumulate")
 
-    def execute(self, spline, tilts):
-        if self.useTiltList:
-            _tilts = VirtualFloatList.fromList(FloatList.fromValues(tilts), 0)
+    def getExecutionFunctionName(self):
+        if self.useSplineList:
+            if self.useTiltList:
+                return "execute_MultipleSplines_MultipleTilts"
+            else:
+                return "execute_MultipleSplines_SingleTilt"
         else:
-            _tilts = VirtualFloatList.fromElement(tilts)
+            if self.useTiltList:
+                return "execute_SingleSpline_MultipleTilts"
+            else:
+                return "execute_SingleSpline_SingleTilt"
+
+    def execute_MultipleSplines_MultipleTilts(self, splines, tilts):
+        for spline in splines:
+            self.execute_SingleSpline_MultipleTilts(spline, tilts)
+        return splines
+
+    def execute_MultipleSplines_SingleTilt(self, splines, tilt):
+        for spline in splines:
+            self.execute_SingleSpline_SingleTilt(spline, tilt)
+        return splines
+
+    def execute_SingleSpline_SingleTilt(self, spline, tilt):
+        _tilts = VirtualFloatList.fromElement(tilt)
         tiltSplinePoints(spline, _tilts, self.accumulateTilts)
         return spline
+
+    def execute_SingleSpline_MultipleTilts(self, spline, tilts):
+        _tilts = VirtualFloatList.fromList(self.prepareRadiusList(tilts, len(spline.points)), 0)
+        tiltSplinePoints(spline, _tilts, self.accumulateTilts)
+        return spline
+
+    def prepareRadiusList(self, tilts, length):
+        if len(tilts) == length:
+            return FloatList.fromValues(tilts)
+        elif len(tilts) < length:
+            if len(tilts) == 0:
+                return FloatList.fromValue(0, length = length)
+            else:
+                return FloatList.fromValues(tilts).repeated(length = length)
+        else:
+            return FloatList.fromValues(tilts[:length])
