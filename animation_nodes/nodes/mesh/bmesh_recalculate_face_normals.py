9bba87ff92f0a00c90e50078ea39da8720e88ac9
Hidesato Ikeya <iky.hdst@gmail.com>
2020-03-08 09:36:44 +0900

    added Bmesh Reverse Faces

--- animation_nodes/nodes/mesh/bmesh_recalculate_face_normals.py
+++ animation_nodes/nodes/mesh/bmesh_recalculate_face_normals.py
@@ -7,18 +7,13 @@ class BMeshRecalculateFaceNormalsNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_BMeshRecalculateFaceNormalsNode"
     bl_label = "BMesh Recalculate Normals"
 
-    invert: BoolProperty(name = "Invert Normals", update = executionCodeChanged)
-
     def create(self):
         self.newInput("BMesh", "BMesh", "bm").dataIsModified = True
         self.newOutput("BMesh", "BMesh", "bm")
 
-    def draw(self, layout):
-        layout.prop(self, "invert")
-
     def getExecutionCode(self, required):
         recalcString = "bmesh.ops.recalc_face_normals(bm, faces = bm.faces)"
-        return [recalcString] * 2 if self.invert else recalcString
+        return recalcString
 
     def getUsedModules(self):
         return ["bmesh"]
