5133c9d0e3f0054c2fa64c251c8794bcfddfdda9
3DSingh <kadarwalaks@gmail.com>
2020-04-21 17:54:08 +0530

    Added a Validate Check for triangulated mesh.

--- animation_nodes/nodes/mesh/triangulate_mesh.py
+++ animation_nodes/nodes/mesh/triangulate_mesh.py
@@ -1,11 +1,14 @@
 import bpy
 from bpy.props import *
+from ... data_structures import Mesh
 from ... events import propertyChanged
 from ... base_types import AnimationNode
+from ... data_structures.meshes.validate import checkMeshData
 
 class TriangulateMeshNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TriangulateMeshNode"
     bl_label = "Triangulate Mesh"
+    errorHandlingType = "EXCEPTION"
 
     methodType: BoolProperty(name = "Use Advanced Method", default = False,
                              description = "Use advanced method for Mesh Triangulation",
@@ -25,4 +28,9 @@ class TriangulateMeshNode(bpy.types.Node, AnimationNode):
             meshOut.triangulateMesh(method = "EAR")
         else:
             meshOut.triangulateMesh(method = "FAN")
-        return meshOut
+
+        try:
+            checkMeshData(meshOut.vertices, meshOut.edges, meshOut.polygons)
+            return meshOut
+        except Exception as e:
+            self.raiseErrorMessage(str(e))
