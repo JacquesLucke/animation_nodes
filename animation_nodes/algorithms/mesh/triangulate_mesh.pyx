22021991d71f771d40dae18a8a60a43d256542fc
3DSingh <kadarwalaks@gmail.com>
2020-04-23 00:26:18 +0530

    Refined the mesh triangulation method based on the 1st review.

---
+++ animation_nodes/algorithms/mesh/triangulate_mesh.pyx
@@ -0,0 +1,321 @@
+import numpy
+import cython
+from libc.stdlib cimport qsort
+from libc.math cimport sin, cos, pi
+from ... data_structures cimport (
+    LongList, Vector3DList, PolygonIndicesList)
+from ... math cimport (
+    Vector3, crossVec3, subVec3, dotVec3, toVector3, lengthVec3,
+    angleNormalizedVec3, normalizeVec3_InPlace
+)
+
+def triangulatePolygonsUsingFanSpanMethod(PolygonIndicesList polygons):
+    cdef unsigned int *oldPolyLengths = polygons.polyLengths.data
+    cdef Py_ssize_t polygonAmount = polygons.getLength()
+    cdef Py_ssize_t i, triAmount, polyLength
+
+    triAmount = 0
+    for i in range(polygonAmount):
+        polyLength = oldPolyLengths[i]
+        if polyLength > 3:
+            triAmount += polyLength - 2
+        else:
+            triAmount += 1
+
+    cdef unsigned int *oldIndices = polygons.indices.data
+    cdef unsigned int *oldPolyStarts = polygons.polyStarts.data
+
+    cdef PolygonIndicesList newPolygons = PolygonIndicesList(
+                                          indicesAmount = 3 * triAmount,
+                                          polygonAmount = triAmount)
+    cdef unsigned int *newIndices = newPolygons.indices.data
+    cdef unsigned int *newPolyStarts = newPolygons.polyStarts.data
+    cdef unsigned int *newPolyLengths = newPolygons.polyLengths.data
+
+    cdef Py_ssize_t j, index, triIndex, start
+
+    index = 0
+    triIndex = 0
+    for i in range(polygonAmount):
+        start = oldPolyStarts[i]
+        for j in range(oldPolyLengths[i] - 2):
+            newPolyStarts[triIndex] = index
+            newPolyLengths[triIndex] = 3
+            triIndex += 1
+
+            newIndices[index] = oldIndices[start]
+            newIndices[index + 1] = oldIndices[start + 1 + j]
+            newIndices[index + 2] = oldIndices[start + 2 + j]
+            index += 3
+
+    return newPolygons
+
+@cython.cdivision(True)
+def triangulatePolygonsUsingEarClipMethod(Vector3DList vertices, PolygonIndicesList polygons):
+    cdef unsigned int *oldPolyLengths = polygons.polyLengths.data
+    cdef Py_ssize_t polygonAmount = polygons.getLength()
+    cdef Py_ssize_t i, triAmount, polyLength
+
+    # Total number of triangle polygons.
+    triAmount = 0
+    for i in range(polygonAmount):
+        polyLength = oldPolyLengths[i]
+        triAmount += polyLength - 2
+
+    cdef unsigned int *oldIndices = polygons.indices.data
+    cdef unsigned int *oldPolyStarts = polygons.polyStarts.data
+
+    cdef PolygonIndicesList newPolygons = PolygonIndicesList(
+                                          indicesAmount = 3 * triAmount,
+                                          polygonAmount = triAmount)
+    cdef unsigned int *newIndices = newPolygons.indices.data
+    cdef unsigned int *newPolyStarts = newPolygons.polyStarts.data
+    cdef unsigned int *newPolyLengths = newPolygons.polyLengths.data
+
+    cdef LongList neighbors, angles, sortAngles, mask, earNeighbors, earNextNeighbors
+    cdef Vector3 preVertex, earVertex, nexVertex, v1, v2, v3
+    cdef Vector3DList polyVertices
+    cdef Py_ssize_t polyStart, triIndex, polyIndex, triCount
+    cdef Py_ssize_t j, k, index, preIndex, earIndex, nexIndex, prePreIndex, nexNexIndex
+
+    triIndex = 0
+    polyIndex = 0
+    for i in range(polygonAmount):
+        polyLength = oldPolyLengths[i]
+        polyStart = oldPolyStarts[i]
+
+        neighbors = LongList(length = polyLength)
+        mask = LongList(length = polyLength)
+        for j in range(polyLength):
+            neighbors.data[j] = oldIndices[polyStart + j]
+            mask.data[j] = 0
+
+        polyVertices = projectPolygonVertices(vertices, neighbors)
+        if not polyCounterClockwise(polyVertices):
+            neighbors = neighbors.reversed()
+            polyVertices = polyVertices.reversed()
+
+        # Calculate inner-angle for all vertices of polygon.
+        angles = LongList(length = polyLength)
+        for j in range(polyLength):
+            preVertex = polyVertices.data[(polyLength + j - 1) % polyLength]
+            earVertex = polyVertices.data[j]
+            nexVertex = polyVertices.data[(j + 1) % polyLength]
+            angles.data[j] = calculateAngle(preVertex, earVertex, nexVertex)
+
+        # Calculate the triangle polygon indices.
+        triCount = 0
+        for j in range(polyLength - 2):
+            if polyLength - 2 - triCount == 1:
+                newPolyStarts[triIndex] = polyIndex
+                newPolyLengths[triIndex] = 3
+                triIndex += 1
+
+                index = 0
+                for k in range(polyLength):
+                    if mask.data[k] < 1:
+                        newIndices[polyIndex + index] = neighbors.data[k]
+                        index += 1
+                        if index == 3: break
+                polyIndex += 3
+
+                break
+
+            sortAngles = LongList.fromNumpyArray(numpy.sort(angles.asMemoryView()).astype(int))
+
+            # Removing an ear which has smallest inner-angle
+            for k in range(polyLength):
+                earIndex = angles.index(sortAngles.data[k])
+                if mask.data[earIndex] < 1:
+                    earNeighbors = earNeighborIndices(earIndex, polyLength, mask)
+                    preIndex = earNeighbors.data[0]
+                    nexIndex = earNeighbors.data[1]
+
+                    v1 = polyVertices.data[preIndex]
+                    v2 = polyVertices.data[earIndex]
+                    v3 = polyVertices.data[nexIndex]
+                    if convexVertex(v1, v2, v3) and not pointsInTriangle(polyVertices, v1, v2, v3, preIndex, earIndex, nexIndex):
+                        earNextNeighbors = earNextNeighborIndices(earIndex, preIndex, nexIndex, polyLength, mask)
+                        triCount += 1
+                        mask.data[earIndex] = 1
+                        angles.data[earIndex] = 361
+                        angles.data[preIndex] = calculateAngle(polyVertices.data[earNextNeighbors.data[0]], v1, v3)
+                        angles.data[nexIndex] = calculateAngle(v1, v3, polyVertices.data[earNextNeighbors.data[1]])
+
+                        newPolyStarts[triIndex] = polyIndex
+                        newPolyLengths[triIndex] = 3
+                        triIndex += 1
+
+                        newIndices[polyIndex] = neighbors.data[preIndex]
+                        newIndices[polyIndex + 1] = neighbors.data[earIndex]
+                        newIndices[polyIndex + 2] = neighbors.data[nexIndex]
+                        polyIndex += 3
+
+                        break
+
+    return newPolygons
+
+# Make sure normal (+z) are correct.
+@cython.cdivision(True)
+cdef polyCounterClockwise(Vector3DList vertices):
+    cdef Py_ssize_t amount = vertices.length
+    cdef float area = 0.0
+    cdef Vector3 v1, v2
+    cdef Py_ssize_t i
+
+    for i in range(amount - 1):
+        v1 = vertices.data[i]
+        v2 = vertices.data[i + 1]
+        area += (v2.x - v1.x) * (v2.y + v1.y)
+    v1 = vertices.data[amount - 1]
+    v2 = vertices.data[0]
+    area += (v2.x - v1.x) * (v2.y + v1.y)
+
+    if area > 0.0: return False
+    return True
+
+# Calculate Inner angle (degree).
+cdef calculateAngle(Vector3 preVertex, Vector3 earVertex, Vector3 nexVertex):
+    cdef Vector3 ab, bc
+    cdef float angle
+
+    subVec3(&ab, &earVertex, &preVertex)
+    subVec3(&bc, &earVertex, &nexVertex)
+
+    normalizeVec3_InPlace(&ab)
+    normalizeVec3_InPlace(&bc)
+
+    angle = angleNormalizedVec3(&ab, &bc)
+    return int((angle - angle % 0.001) * 180.0 / pi)
+
+@cython.cdivision(True)
+cdef earNeighborIndices(Py_ssize_t earIndex, Py_ssize_t polyLength, LongList mask):
+    cdef LongList indices = LongList(length = 2)
+    cdef Py_ssize_t i, index
+    for i in range(polyLength - 1):
+        index = earIndex - 1 - i
+        if index < 0: index += polyLength
+        if mask.data[index] < 1:
+            indices.data[0] = index
+            break
+
+    for i in range(polyLength - 1):
+        index = (earIndex + 1 + i) % polyLength
+        if mask.data[index] < 1:
+            indices.data[1] = index
+            break
+
+    return indices
+
+@cython.cdivision(True)
+cdef earNextNeighborIndices(Py_ssize_t earIndex, Py_ssize_t preIndex, Py_ssize_t nexIndex,
+                            Py_ssize_t polyLength, LongList mask):
+    cdef LongList indices = LongList(length = 2)
+    cdef Py_ssize_t i, index
+
+    for i in range(polyLength - 1):
+        index = earIndex - 2 - i
+        if index < 0: index += polyLength
+        if mask.data[index] < 1 and index != earIndex and index != preIndex and index != nexIndex:
+            indices.data[0] = index
+            break
+
+    for i in range(polyLength - 1):
+        index = (earIndex + 2 + i) % polyLength
+        if mask.data[index] < 1 and index != earIndex and index != preIndex and index != nexIndex:
+            indices.data[1] = index
+            break
+
+    return indices
+
+# Make sure normal (+z) are correct, and polygon is counter clockwise.
+cdef convexVertex(Vector3 v1, Vector3 v2, Vector3 v3):
+    cdef float sign = (v2.x - v1.x) * (v3.y - v1.y) - (v2.y - v1.y) * (v3.x - v1.x)
+    if sign >= 0: return True
+    return False
+
+# Checking points (reflex type) lies in the new triangle.
+cdef pointsInTriangle(Vector3DList vertices, Vector3 v1, Vector3 v2, Vector3 v3,
+                      Py_ssize_t preIndex, Py_ssize_t earIndex, Py_ssize_t nexIndex):
+    cdef Py_ssize_t i
+    for i in range(vertices.length):
+        if i != preIndex and i != earIndex and i != nexIndex and pointInsideTriangle(v1, v2, v3, vertices.data[i]):
+            return True
+    return False
+
+@cython.cdivision(True)
+cdef pointInsideTriangle(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 p):
+    cdef Vector3 u, v, w, n
+    subVec3(&u, &v2, &v1)
+    subVec3(&v, &v3, &v1)
+    subVec3(&w, &p, &v1)
+
+    cdef float delta = 0.00001
+    v.x += delta
+    v.y += delta
+    v.z += delta
+
+    crossVec3(&n, &u, &v)
+    cdef float normSq = lengthVec3(&n)
+    normSq = normSq * normSq
+
+    cdef Vector3 uw, wv
+    crossVec3(&uw, &u, &w)
+    crossVec3(&wv, &w, &v)
+
+    cdef float alpha = dotVec3(&uw, &n) / normSq
+    cdef float beta = dotVec3(&wv, &n) / normSq
+    cdef float gamma = 1.0 - alpha - beta
+    if alpha >= 0.0 and alpha <= 1.0 and beta >= 0.0 and beta <= 1.0 and gamma >= 0.0 and gamma <= 1.0:
+        return True
+    else:
+        return False
+
+# Transformation of vertices of polygon into xy-plane.
+@cython.cdivision(True)
+cdef projectPolygonVertices(Vector3DList vertices, LongList polygonIndices):
+    cdef Py_ssize_t polyLength = polygonIndices.length
+    cdef Vector3DList polyVertices = Vector3DList(length = polyLength)
+
+    # Compute polygon normal with Nowell's method.
+    cdef Vector3 polyNormal = toVector3((0, 0, 0))
+    cdef Py_ssize_t i
+    for i in range(polyLength):
+        v1 = vertices.data[polygonIndices.data[i]]
+        v2 = vertices.data[polygonIndices.data[(i + 1) % polyLength]]
+
+        polyNormal.x += (v1.y - v2.y) * (v1.z + v2.z)
+        polyNormal.y += (v1.z - v2.z) * (v1.x + v2.x)
+        polyNormal.z += (v1.x - v2.x) * (v1.y + v2.y)
+
+        polyVertices.data[i] = v1
+    normalizeVec3_InPlace(&polyNormal)
+
+    # Calculate the coefficient for Rodrigues's rotation formula.
+    cdef Vector3 zAxis = toVector3((0, 0, 1))
+    cdef Vector3 rotAxis
+    crossVec3(&rotAxis, &polyNormal, &zAxis)
+    normalizeVec3_InPlace(&rotAxis)
+
+    cdef float angle = angleNormalizedVec3(&polyNormal, &zAxis)
+    cdef float sint = sin(angle)
+    cdef float cost = cos(angle)
+    cdef float fact = 1.0 - cost
+
+    # Rotating vertices of polygon, and droping the z-comp.
+    cdef Vector3DList rotPolyVertices = Vector3DList(length = polyLength)
+    cdef Vector3 vertex, cross, rotPolyVertex
+    cdef float dot
+    for i in range(polyLength):
+        vertex = polyVertices.data[i]
+        crossVec3(&cross, &rotAxis, &vertex)
+        dot = dotVec3(&rotAxis, &vertex)
+
+        rotPolyVertex.x = cost * vertex.x + sint * cross.x + dot * fact * rotAxis.x
+        rotPolyVertex.y = cost * vertex.y + sint * cross.y + dot * fact * rotAxis.y
+        rotPolyVertex.z = cost * vertex.z + sint * cross.z + dot * fact * rotAxis.z
+        rotPolyVertices.data[i].x = rotPolyVertex.x
+        rotPolyVertices.data[i].y = rotPolyVertex.y
+        rotPolyVertices.data[i].z = 0.0
+
+    return rotPolyVertices
--- animation_nodes/data_structures/meshes/mesh_data.pyx
+++ animation_nodes/data_structures/meshes/mesh_data.pyx
@@ -1,18 +1,18 @@
 # cython: profile=True
-import numpy
-import cython
 import textwrap
 import functools
-from libc.math cimport sin, cos, pi
 from collections import OrderedDict
 from . validate import createValidEdgesList
 from . validate import checkMeshData, calculateLoopEdges
+from ... algorithms.mesh.triangulate_mesh import (
+    triangulatePolygonsUsingFanSpanMethod, triangulatePolygonsUsingEarClipMethod
+)
 from .. lists.base_lists cimport (
-    UIntegerList, EdgeIndices, EdgeIndicesList, Vector3DList, Vector2DList, ColorList, LongList)
+    UIntegerList, EdgeIndices, EdgeIndicesList, Vector3DList, Vector2DList, ColorList, LongList
+)
 from ... math cimport (
-    Vector3, crossVec3, subVec3, dotVec3, toVector3, crossVec3, lengthVec3, addVec3_Inplace,
-    scaleVec3_Inplace, angleNormalizedVec3, normalizeVec3_InPlace, isExactlyZeroVec3,
-    normalizeVec3, Matrix4, toMatrix4
+    Vector3, crossVec3, subVec3, addVec3_Inplace, isExactlyZeroVec3, normalizeVec3,
+    Matrix4, toMatrix4
 )
 
 def derivedMeshDataCacheHelper(name, handleNormalization = False):
@@ -179,7 +179,8 @@ cdef class Mesh:
         self.verticesMoved()
 
     def triangulateMesh(self, str method = "FAN"):
-        polygons = self.polygons
+        cdef PolygonIndicesList polygons = self.polygons
+        cdef PolygonIndicesList newPolygons
         if method == "FAN":
             newPolygons = triangulatePolygonsUsingFanSpanMethod(polygons)
         elif method == "EAR":
@@ -384,272 +385,3 @@ def calculateLinkedVertices(int verticesAmount, EdgeIndicesList edges):
         usedSlots.data[v2] += 1
 
     return neighboursAmounts, neighboursStarts, neighbours, neighbourEdges
-
-def triangulatePolygonsUsingFanSpanMethod(PolygonIndicesList polygons):
-    cdef unsigned int *_oldPolyLengths = polygons.polyLengths.data
-    cdef Py_ssize_t polygonAmount = polygons.getLength()
-    cdef Py_ssize_t i, triAmount, polyLength
-
-    triAmount = 0
-    for i in range(polygonAmount):
-        polyLength = _oldPolyLengths[i]
-        if polyLength > 3:
-            triAmount += polyLength - 2
-        else:
-            triAmount += 1
-
-    cdef unsigned int *_oldIndices = polygons.indices.data
-    cdef unsigned int *_oldPolyStarts = polygons.polyStarts.data
-
-    cdef PolygonIndicesList newPolygons = PolygonIndicesList(
-                                          indicesAmount = 3 * triAmount,
-                                          polygonAmount = triAmount)
-    cdef unsigned int *_newIndices = newPolygons.indices.data
-    cdef unsigned int *_newPolyStarts = newPolygons.polyStarts.data
-    cdef unsigned int *_newPolyLengths = newPolygons.polyLengths.data
-
-    cdef Py_ssize_t j, index, triIndex, start
-
-    index = 0
-    triIndex = 0
-    for i in range(polygonAmount):
-        start = _oldPolyStarts[i]
-        for j in range(_oldPolyLengths[i] - 2):
-            _newPolyStarts[triIndex] = index
-            _newPolyLengths[triIndex] = 3
-            triIndex += 1
-
-            _newIndices[index] = _oldIndices[start]
-            _newIndices[index + 1] = _oldIndices[start + 1 + j]
-            _newIndices[index + 2] = _oldIndices[start + 2 + j]
-            index += 3
-
-    return newPolygons
-
-@cython.cdivision(True)
-def triangulatePolygonsUsingEarClipMethod(Vector3DList oldVertices, PolygonIndicesList polygons):
-    cdef unsigned int *_oldPolyLengths = polygons.polyLengths.data
-    cdef Py_ssize_t polygonAmount = polygons.getLength()
-    cdef Py_ssize_t i, triAmount, polyLength
-
-    # Total number of triangle polygons
-    triAmount = 0
-    for i in range(polygonAmount):
-        polyLength = _oldPolyLengths[i]
-        if polyLength > 3:
-            triAmount += polyLength - 2
-        else:
-            triAmount += 1
-
-    cdef unsigned int *_oldIndices = polygons.indices.data
-    cdef unsigned int *_oldPolyStarts = polygons.polyStarts.data
-
-    cdef PolygonIndicesList newPolygons = PolygonIndicesList(
-                                          indicesAmount = 3 * triAmount,
-                                          polygonAmount = triAmount)
-    cdef unsigned int *_newIndices = newPolygons.indices.data
-    cdef unsigned int *_newPolyStarts = newPolygons.polyStarts.data
-    cdef unsigned int *_newPolyLengths = newPolygons.polyLengths.data
-
-    cdef Vector3 preVertex, earVertex, nexVertex, ab, bc, v1, v2, v3
-    cdef LongList neighbors, indices, angles, sortAngles
-    cdef Vector3DList polyVertices
-    cdef float angle
-    cdef Py_ssize_t polyStart, triIndex, polyIndex
-    cdef Py_ssize_t j, k, amount, index, preIndex, earIndex, nexIndex
-
-    triIndex = 0
-    polyIndex = 0
-    for i in range(polygonAmount):
-        polyLength = _oldPolyLengths[i]
-        polyStart = _oldPolyStarts[i]
-        neighbors = LongList(length = polyLength)
-        indices = LongList(length = polyLength)
-        for j in range(polyLength):
-            neighbors.data[j] = _oldIndices[polyStart + j]
-            indices.data[j] = j
-
-        polyVertices = transformVerticesOfPolygon(oldVertices, neighbors)
-        if not polyCounterClockwise(polyVertices):
-            neighbors = neighbors.reversed()
-            polyVertices = polyVertices.reversed()
-
-        for j in range(polyLength):
-            amount = indices.length
-
-            if amount == 3:
-                _newPolyStarts[triIndex] = polyIndex
-                _newPolyLengths[triIndex] = 3
-                triIndex += 1
-
-                _newIndices[polyIndex] = neighbors.data[indices.data[0]]
-                _newIndices[polyIndex + 1] = neighbors.data[indices.data[1]]
-                _newIndices[polyIndex + 2] = neighbors.data[indices.data[2]]
-                polyIndex += 3
-
-                break
-
-            # Calculate inner-angle for all vertices of polygon
-            angles = LongList(length = amount)
-            for k in range(amount):
-                preVertex = polyVertices.data[indices.data[(amount + k - 1) % amount]]
-                earVertex = polyVertices.data[indices.data[k]]
-                nexVertex = polyVertices.data[indices.data[(k + 1) % amount]]
-
-                subVec3(&ab, &earVertex, &preVertex)
-                subVec3(&bc, &earVertex, &nexVertex)
-
-                normalizeVec3_InPlace(&ab)
-                normalizeVec3_InPlace(&bc)
-
-                angle = angleNormalizedVec3(&ab, &bc)
-                angles.data[k] = int((angle - angle % 0.001) * 180.0 / pi)
-            sortAngles = LongList.fromNumpyArray(numpy.sort(angles.asMemoryView()).astype(int))
-
-            # Removing an ear which has smallest inner-angle
-            for k in range(amount):
-                index = angles.index(sortAngles[k])
-                preIndex = indices.data[(amount + index - 1) % amount]
-                earIndex = indices.data[index]
-                nexIndex = indices.data[(index + 1) % amount]
-
-                v1 = polyVertices.data[preIndex]
-                v2 = polyVertices.data[earIndex]
-                v3 = polyVertices.data[nexIndex]
-                if convexVertex(v1, v2, v3) and not pointsInTriangle(polyVertices, v1, v2, v3, preIndex, earIndex, nexIndex):
-                    indices = LongList.fromNumpyArray(numpy.delete(indices.asMemoryView(), index).astype(int))
-
-                    _newPolyStarts[triIndex] = polyIndex
-                    _newPolyLengths[triIndex] = 3
-                    triIndex += 1
-
-                    _newIndices[polyIndex] = neighbors.data[preIndex]
-                    _newIndices[polyIndex + 1] = neighbors.data[earIndex]
-                    _newIndices[polyIndex + 2] = neighbors.data[nexIndex]
-                    polyIndex += 3
-
-                    break
-
-    return newPolygons
-
-# Make sure normal (+z) are correct.
-@cython.cdivision(True)
-cdef polyCounterClockwise(Vector3DList vertices):
-    cdef Py_ssize_t amount = vertices.length
-    cdef float area = 0
-    cdef Vector3 v1, v2
-    cdef Py_ssize_t i
-
-    for i in range(amount):
-        v1 = vertices.data[i]
-        v2 = vertices.data[(i + 1) % amount]
-        area += (v2.x - v1.x) * (v2.y + v1.y)
-    if area > 0: return False
-    return True
-
-# Make sure normal (+z) are correct, and polygon is counter clockwise.
-cdef convexVertex(Vector3 v1, Vector3 v2, Vector3 v3):
-    cdef float sign = (v2.x - v1.x) * (v3.y - v1.y) - (v2.y - v1.y) * (v3.x - v1.x)
-    if sign >= 0: return True
-    return False
-
-# Checking points (reflex type) lies in the new triangle
-cdef pointsInTriangle(Vector3DList vertices, Vector3 v1, Vector3 v2, Vector3 v3,
-                      Py_ssize_t preIndex, Py_ssize_t earIndex, Py_ssize_t nexIndex):
-    cdef Py_ssize_t i
-    for i in range(vertices.length):
-        if i != preIndex and i != earIndex and i != nexIndex and pointInsideTriangle(v1, v2, v3, vertices.data[i]):
-            return True
-    return False
-
-@cython.cdivision(True)
-cdef pointInsideTriangle(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 p):
-    cdef Vector3 u, v, w, n
-    subVec3(&u, &v2, &v1)
-    subVec3(&v, &v3, &v1)
-    subVec3(&w, &p, &v1)
-
-    cdef float delta = 0.00001
-    v.x += delta
-    v.y += delta
-    v.z += delta
-
-    crossVec3(&n, &u, &v)
-    cdef float normSq = lengthVec3(&n)
-    normSq = normSq * normSq
-
-    cdef Vector3 uw, wv
-    crossVec3(&uw, &u, &w)
-    crossVec3(&wv, &w, &v)
-
-    cdef float alpha = dotVec3(&uw, &n) / normSq
-    cdef float beta = dotVec3(&wv, &n) / normSq
-    cdef float gamma = 1 - alpha - beta
-    if alpha >= 0 and alpha <= 1 and beta >= 0 and beta <= 1 and gamma >= 0 and gamma <= 1:
-        return True
-    else:
-        return False
-
-# Transformation of vertices of polygon into xy-plane
-@cython.cdivision(True)
-cdef transformVerticesOfPolygon(Vector3DList vertices, LongList polygonIndices):
-    cdef Py_ssize_t polyLength = polygonIndices.length
-    cdef Vector3DList polyVertices = Vector3DList(length = polyLength)
-
-    # Compute polygon normal with Nowell's method
-    cdef Vector3 polyNormal = toVector3((0, 0, 0))
-    cdef Py_ssize_t i
-    for i in range(polyLength):
-        v1 = vertices.data[polygonIndices.data[i]]
-        v2 = vertices.data[polygonIndices.data[(i + 1) % polyLength]]
-
-        polyNormal.x += (v1.y - v2.y) * (v1.z + v2.z)
-        polyNormal.y += (v1.z - v2.z) * (v1.x + v2.x)
-        polyNormal.z += (v1.x - v2.x) * (v1.y + v2.y)
-
-        polyVertices.data[i] = v1
-    normalizeVec3_InPlace(&polyNormal)
-
-    # Calculate the coefficient for Rodrigues's rotation formula
-    cdef Vector3 zAxis = toVector3((0, 0, 1))
-    cdef Vector3 rotAxis
-    crossVec3(&rotAxis, &polyNormal, &zAxis)
-    normalizeVec3_InPlace(&rotAxis)
-
-    cdef float angle = angleNormalizedVec3(&polyNormal, &zAxis)
-    cdef float sint = sin(angle)
-    cdef float cost = cos(angle)
-    cdef float fact = 1 - cost
-
-    # Rotating vertices of polygon, and droping the z-comp
-    cdef Vector3DList rotPolyVertices = Vector3DList(length = polyLength)
-    cdef Vector3 vertex, cross, rotPolyVertex
-    cdef float dot
-    for i in range(polyLength):
-        vertex = polyVertices.data[i]
-        crossVec3(&cross, &rotAxis, &vertex)
-        dot = dotVec3(&rotAxis, &vertex)
-
-        rotPolyVertex.x = cost * vertex.x + sint * cross.x + dot * fact * rotAxis.x
-        rotPolyVertex.y = cost * vertex.y + sint * cross.y + dot * fact * rotAxis.y
-        rotPolyVertex.z = cost * vertex.z + sint * cross.z + dot * fact * rotAxis.z
-        rotPolyVertices.data[i].x = rotPolyVertex.x
-        rotPolyVertices.data[i].y = rotPolyVertex.y
-        rotPolyVertices.data[i].z = 0
-
-    # Compute avg. vector
-    cdef Vector3 avgVector = toVector3((0, 0, 0))
-    for i in range(polyLength):
-        avgVector.x += rotPolyVertices.data[i].x
-        avgVector.y += rotPolyVertices.data[i].y
-        avgVector.z += rotPolyVertices.data[i].z
-    scaleVec3_Inplace(&avgVector, 1.0 / polyLength)
-
-    # Bring vertices of polygon to the world center
-    cdef Vector3DList offsetPolyVertices = Vector3DList(length = polyLength)
-    for i in range(polyLength):
-        offsetPolyVertices.data[i].x =  rotPolyVertices.data[i].x - avgVector.x
-        offsetPolyVertices.data[i].y =  rotPolyVertices.data[i].y - avgVector.y
-        offsetPolyVertices.data[i].z =  rotPolyVertices.data[i].z - avgVector.z
-
-    return offsetPolyVertices
