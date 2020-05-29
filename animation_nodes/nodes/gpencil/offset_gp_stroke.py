7fbb6ba6257a64964a0914feb75c38f6f1807d1f
3DSingh <kadarwalaks@gmail.com>
2020-05-29 22:10:39 +0530

    Vertex color mixing Method in the advanced settings.

--- animation_nodes/nodes/gpencil/offset_gp_stroke.py
+++ animation_nodes/nodes/gpencil/offset_gp_stroke.py
@@ -7,6 +7,12 @@ from .. vector.c_utils import offset3DVectors
 from ... base_types import AnimationNode, VectorizedSocket
 from ... data_structures import VirtualVector3DList, VirtualDoubleList, VirtualColorList
 
+methodTypeItems = [
+    ("MIX", "Mix", "Mix vertex colors", "NONE", 0),
+    ("ADD", "Add", "Add vertex colors", "NONE", 1),
+    ("MULTIPLY", "Multiply", "Multiply vertex colors", "NONE", 2)
+]
+
 class OffsetGPStrokeNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_OffsetGPStrokeNode"
     bl_label = "Offset GP Stroke"
@@ -15,6 +21,9 @@ class OffsetGPStrokeNode(bpy.types.Node, AnimationNode):
     def checkedPropertiesChanged(self, context):
         self.updateSocketVisibility()
         executionCodeChanged()
+
+    methodType: EnumProperty(name = "Method Type", default = "MIX",
+        items = methodTypeItems, update = AnimationNode.refresh)
 
     useLocation: BoolProperty(update = checkedPropertiesChanged)
     useStrength: BoolProperty(update = checkedPropertiesChanged)
@@ -46,7 +55,6 @@ class OffsetGPStrokeNode(bpy.types.Node, AnimationNode):
         self.newInput(VectorizedSocket("Color", "useVertexColorList",
             ("Color", "offsetVertexColor", dict(value = (1, 0, 0, 1))),
             ("Colors", "offsetVertexColors")))
-        self.newInput("Text", "Method", "method", value = "MIX")
 
         self.newOutput("GPStroke", "Stroke", "stroke")
 
@@ -66,16 +74,20 @@ class OffsetGPStrokeNode(bpy.types.Node, AnimationNode):
         row = col.row(align = True)
         row.prop(self, "useVertexColor", text = "Vertex Color", toggle = True)
 
+    def drawAdvanced(self, layout):
+        col = layout.column()
+        col.active = self.useVertexColor
+        col.prop(self, "methodType", text = "Type")
+
     def updateSocketVisibility(self):
         self.inputs[2].hide = not self.useLocation
         self.inputs[3].hide = not self.useStrength
         self.inputs[4].hide = not self.usePressure
         self.inputs[5].hide = not self.useUVRotation
         self.inputs[6].hide = not self.useVertexColor
-        self.inputs[7].hide = not self.useVertexColor
 
     def execute(self, stroke, falloff, offsetLocations, offsetStrengths, offsetPressures, offsetUVRotations,
-                offsetVertexColors, method):
+                offsetVertexColors):
         if not any((self.useLocation, self.useStrength, self.usePressure, self.useUVRotation, self.useVertexColor)):
             return stroke
 
@@ -96,9 +108,7 @@ class OffsetGPStrokeNode(bpy.types.Node, AnimationNode):
 
         if self.useVertexColor:
             _offsetVertexColors = VirtualColorList.create(offsetVertexColors, (0, 0, 0, 0))
-            if method not in ["ADD", "MULTIPLY", "MIX"]:
-                self.raiseErrorMessage("Invalid color method, possible methods are: 'ADD', 'MULTIPLY', 'MIX'.")
-            offsetColors(stroke.vertexColors, _offsetVertexColors, influences, method)
+            offsetColors(stroke.vertexColors, _offsetVertexColors, influences, self.methodType)
 
         if self.useLocation:
             _offsetLocations = VirtualVector3DList.create(offsetLocations, (0, 0, 0))
