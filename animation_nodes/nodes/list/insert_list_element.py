873203ae8e50746b462f73dde4b933fc5773910e
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2020-08-22 22:43:58 +0530

    Update Insert List Element node
    Vectorized element input of Insert List Element node.

--- animation_nodes/nodes/list/insert_list_element.py
+++ animation_nodes/nodes/list/insert_list_element.py
@@ -1,19 +1,20 @@
 import bpy
 from ... sockets.info import isBase, toBaseDataType
-from ... base_types import AnimationNode, ListTypeSelectorSocket
+from ... base_types import AnimationNode, ListTypeSelectorSocket, VectorizedSocket
 
 class InsertListElementNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_InsertListElementNode"
     bl_label = "Insert List Element"
 
     assignedType: ListTypeSelectorSocket.newProperty(default = "Float")
+    useList: VectorizedSocket.newProperty()
 
     def create(self):
         prop = ("assignedType", "BASE")
+        print(self.assignedType)
         self.newInput(ListTypeSelectorSocket(
             "List", "list", "LIST", prop, dataIsModified = True))
-        self.newInput(ListTypeSelectorSocket(
-            "Element", "element", "BASE", prop, dataIsModified = True))
+        self.newInput(VectorizedSocket(str(self.assignedType), "useList", ("Element", "element"), ("Elements", "elements")))
         self.newInput("Integer", "Index", "index")
         self.newOutput(ListTypeSelectorSocket(
             "List", "list", "LIST", prop))
@@ -23,7 +24,13 @@ class InsertListElementNode(bpy.types.Node, AnimationNode):
             dataTypes = "LIST", text = "Change Type", icon = "TRIA_RIGHT")
 
     def getExecutionCode(self, required):
-        return "list.insert(index, element)"
+        if not self.useList:
+            yield "list.insert(index, element)"
+        else:
+            yield "temp = list"
+            yield "list = list[:index]"
+            yield "list.extend(elements)"
+            yield "list.extend(temp[index:])"
 
     def assignListDataType(self, listDataType):
         self.assignType(toBaseDataType(listDataType))
