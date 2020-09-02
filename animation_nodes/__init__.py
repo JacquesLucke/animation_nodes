21543d58bbb53d94639c43d2bc071b83e1e33b5e
Jacques Lucke <mail@jlucke.com>
2020-09-02 17:54:18 +0200

    Add Omar Emara as author in bl_info

--- animation_nodes/__init__.py
+++ animation_nodes/__init__.py
@@ -22,7 +22,7 @@ Created by Jacques Lucke
 bl_info = {
     "name":        "Animation Nodes",
     "description": "Node based visual scripting system designed for motion graphics in Blender.",
-    "author":      "Jacques Lucke",
+    "author":      "Jacques Lucke, Omar Emara",
     "version":     (2, 2, 1),
     "blender":     (2, 83, 0),
     "location":    "Animation Nodes Editor",
