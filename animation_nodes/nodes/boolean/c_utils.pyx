c3a736415f1ad92b0c4e2b4db181d20d5c19bcbc
Norway 23 <64368228+kdsin23@users.noreply.github.com>
2020-09-11 14:08:58 +0800

    Update c_utils.pyx

--- animation_nodes/nodes/boolean/c_utils.pyx
+++ animation_nodes/nodes/boolean/c_utils.pyx
@@ -1,4 +1,5 @@
 from ... data_structures cimport DoubleList, BooleanList, LongList
+from ... algorithms.random_number_generators cimport XoShiRo256StarStar
 
 def convert_DoubleList_to_BooleanList(DoubleList inList):
     cdef BooleanList outList = BooleanList(length = inList.length)
@@ -27,3 +28,12 @@ def convert_BooleanList_to_DoubleList(BooleanList inList):
     for i in range(len(inList)):
         outList.data[i] = 1 if inList.data[i] != 0 else 0
     return outList
+
+def generateRandomBooleans(Py_ssize_t count, Py_ssize_t seed):
+    cdef Py_ssize_t i
+    cdef XoShiRo256StarStar rng = XoShiRo256StarStar(seed)
+    cdef BooleanList bools = BooleanList(length = count)
+    for i in range(count):
+        bools.data[i] = rng.nextBoolean()
+
+    return bools
