0d94ce6fd3e4f377c53e15a0fd3aff9d0f4bf500
Norway 23 <64368228+kdsin23@users.noreply.github.com>
2020-09-17 11:07:06 +0800

    Update c_utils.pyx

--- animation_nodes/nodes/rotation/c_utils.pyx
+++ animation_nodes/nodes/rotation/c_utils.pyx
@@ -1,5 +1,7 @@
 from libc.math cimport M_PI as PI, sqrt, sin, cos
-from ... math cimport quaternionNormalize_InPlace
+from ... math cimport (asin_Save, acos_Save, atan2, copySign,
+    quaternionNormalize_InPlace, normalizeVec3_InPlace
+)
 from ... algorithms.random_number_generators cimport XoShiRo256Plus
 
 from ... data_structures cimport (
@@ -121,3 +123,144 @@ def randomQuaternionList(int seed, int amount):
         quaternionNormalize_InPlace(result.data + i)
 
     return result
+
+def quaternionsToMatrices(QuaternionList q):
+    cdef Py_ssize_t count = len(q)
+    cdef Matrix4x4List m = Matrix4x4List(length = count)
+    cdef double sqw, sqx, sqy, sqz, invs, tmp1, tmp2
+
+    for i in range(count):
+        sqw = q.data[i].w * q.data[i].w
+        sqx = q.data[i].x * q.data[i].x
+        sqy = q.data[i].y * q.data[i].y
+        sqz = q.data[i].z * q.data[i].z
+        invs = 1 / (sqx + sqy + sqz + sqw)
+
+        m.data[i].a11 = ( sqx - sqy - sqz + sqw)*invs
+        m.data[i].a22 = (-sqx + sqy - sqz + sqw)*invs
+        m.data[i].a33 = (-sqx - sqy + sqz + sqw)*invs
+
+        tmp1 = q.data[i].x * q.data[i].y
+        tmp2 = q.data[i].z * q.data[i].w
+        m.data[i].a21 = 2.0 * (tmp1 + tmp2)*invs
+        m.data[i].a12 = 2.0 * (tmp1 - tmp2)*invs
+
+        tmp1 = q.data[i].x * q.data[i].z
+        tmp2 = q.data[i].y * q.data[i].w
+        m.data[i].a31 = 2.0 * (tmp1 - tmp2)*invs
+        m.data[i].a13 = 2.0 * (tmp1 + tmp2)*invs
+
+        tmp1 = q.data[i].y * q.data[i].z
+        tmp2 = q.data[i].x * q.data[i].w
+        m.data[i].a32 = 2.0 * (tmp1 + tmp2)*invs
+        m.data[i].a23 = 2.0 * (tmp1 - tmp2)*invs
+
+        m.data[i].a14 = m.data[i].a24 = m.data[i].a34 = 0
+        m.data[i].a41 = m.data[i].a42 = m.data[i].a43 = 0
+        m.data[i].a44 = 1
+
+    return m
+
+def eulersToQuaternions(EulerList eulers):
+    cdef Py_ssize_t i
+    cdef int count = eulers.getLength()
+    cdef QuaternionList q = QuaternionList(length=count)
+    cdef double cr, sr, cp, sp, cy, sy
+
+    for i in range(count):
+        cr = cos(eulers.data[i].x*0.5)
+        sr = sin(eulers.data[i].x*0.5)
+        cp = cos(eulers.data[i].y*0.5)
+        sp = sin(eulers.data[i].y*0.5)
+        cy = cos(eulers.data[i].z*0.5)
+        sy = sin(eulers.data[i].z*0.5)
+
+        q.data[i].w = cr * cp * cy + sr * sp * sy
+        q.data[i].x = sr * cp * cy - cr * sp * sy
+        q.data[i].y = cr * sp * cy + sr * cp * sy
+        q.data[i].z = cr * cp * sy - sr * sp * cy
+        quaternionNormalize_InPlace(&q.data[i])
+
+    return q
+
+def quaternionsToEulers(QuaternionList q):
+    cdef Py_ssize_t i
+    cdef int count = q.getLength()
+    cdef EulerList eulers = EulerList(length=count)
+    cdef double sinr_cosp, cosr_cosp, sinp, siny_cosp, cosy_cosp, sqw, sqx, sqy, sqz, invs
+
+    for i in range(count):
+        sqw = q.data[i].w * q.data[i].w
+        sqx = q.data[i].x * q.data[i].x
+        sqy = q.data[i].y * q.data[i].y
+        sqz = q.data[i].z * q.data[i].z
+        invs = 1 / sqrt(sqx + sqy + sqz + sqw)
+
+        sinr_cosp = 2 * (q.data[i].w * q.data[i].x + q.data[i].y * q.data[i].z)*invs
+        cosr_cosp = 1 - 2 * (q.data[i].x * q.data[i].x + q.data[i].y * q.data[i].y)*invs
+        sinp = 2 * (q.data[i].w * q.data[i].y - q.data[i].z * q.data[i].x)*invs
+        siny_cosp = 2 * (q.data[i].w * q.data[i].z + q.data[i].x * q.data[i].y)*invs
+        cosy_cosp = 1 - 2 * (q.data[i].y * q.data[i].y + q.data[i].z * q.data[i].z)*invs
+
+        eulers.data[i].x = atan2(sinr_cosp, cosr_cosp)
+        if absNumber(sinp) >= 1:
+            eulers.data[i].y = copySign(PI / 2, sinp)
+        else:
+            eulers.data[i].y = asin_Save(sinp)
+        eulers.data[i].z = atan2(siny_cosp, cosy_cosp)
+        eulers.data[i].order = 0
+
+    return eulers
+
+def axises_AnglesToQuaternions(Vector3DList a, DoubleList angles, bint usedegree=False):
+    cdef Py_ssize_t i
+    cdef int count = len(a)
+    cdef double u1, u2
+    cdef Vector3DList an = normalizeVectors(a)
+    cdef QuaternionList q = QuaternionList(length=count)
+
+    for i in range(count):
+        u1 = sin(angles.data[i]/2)
+        u2 = cos(angles.data[i]/2)
+        if usedegree == False:
+            angles.data[i] = angles.data[i]
+        else:
+            angles.data[i] = angles.data[i] * (PI/180)
+        q.data[i].x = an.data[i].x * u1
+        q.data[i].y = an.data[i].y * u1
+        q.data[i].z = an.data[i].z * u1
+        q.data[i].w = u2
+        quaternionNormalize_InPlace(&q.data[i])
+
+    return q
+
+def quaternionsToAxises_Angles(QuaternionList qs, bint usedegree=False):
+    cdef Py_ssize_t i
+    cdef int count = len(qs)
+    cdef double k, u
+    cdef QuaternionList q = normalizeQuaternions(qs)
+    cdef Vector3DList a = Vector3DList(length=count)
+    cdef DoubleList angles = DoubleList(length=count)
+
+    for i in range(count):
+        k = sqrt(1 - q.data[i].w * q.data[i].w)
+        u = 2 * acos_Save(q.data[i].w)
+        if usedegree == False:
+            angles.data[i] = u
+        else:
+            angles.data[i] = u * (180/PI)
+        a.data[i].x = q.data[i].x / k
+        a.data[i].y = q.data[i].y / k
+        a.data[i].z = q.data[i].z / k
+        normalizeVec3_InPlace(&a.data[i])
+
+    return a,angles
+
+def normalizeQuaternions(QuaternionList q):
+    cdef Py_ssize_t i
+    cdef Py_ssize_t count = len(q)
+
+    for i in range(count):
+        quaternionNormalize_InPlace(&q.data[i])
+
+    return q
