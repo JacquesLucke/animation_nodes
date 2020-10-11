b3972065edbcd0fd3497d2151678879207482052
3DSinghVFX <kadarwalaks@gmail.com>
2020-10-11 17:42:40 +0530

    Improve Marching Squares node.

--- animation_nodes/nodes/mesh/marching_squares.pyx
+++ animation_nodes/nodes/mesh/marching_squares.pyx
@@ -21,52 +21,42 @@ class MarchingSquaresNode(bpy.types.Node, AnimationNode):
     def create(self):
         self.newInput("Integer", "X Divisions", "xDivisions", value = 3, minValue = 2)
         self.newInput("Integer", "Y Divisions", "yDivisions", value = 3, minValue = 2)
-        self.newInput("Integer", "Z Divisions", "zDivisions", value = 1, minValue = 1)
         self.newInput("Float", "Size", "size", value = 5)
         self.newInput("Integer", "Cell Index", "index")
         self.newInput("Falloff", "Falloff", "falloff")
 
         self.newOutput("Mesh", "Mesh", "mesh")
-        self.newOutput("Vector List", "Points", "points")
 
-    def execute(self, xDivisions, yDivisions, zDivisions, size, index, falloff):
+    def execute(self, xDivisions, yDivisions, size, index, falloff):
         cdef double xDis, yDis
         cdef Vector3DList points
-        points, xDis, yDis = getGridPoints(xDivisions, yDivisions, zDivisions, size, size, size)
+        points, xDis, yDis = getGridPoints(xDivisions, yDivisions, 1, size, size, size)
 
         cdef FloatList strengths = self.getFieldStrengths(points, falloff)
-
-        cdef int nx, ny, amountCell
+        cdef long nx, ny, amountCell
         nx, ny = limitAmount(xDivisions), limitAmount(yDivisions)
-        amountCell = (nx - 1) * (ny - 1)
 
-        cdef Vector3DList _points = Vector3DList()
-        cdef float a, b, c, d
-        cdef Py_ssize_t i, j, k, ia, ib, ic, id
+        cdef Py_ssize_t i, j, offset, ia, ib, ic, id
         cdef double _xDis, _yDis
+        cdef long _nx, _ny
+
         _xDis, _yDis = xDis / 2.0, yDis / 2.0
+        _nx, _ny = nx - 1, ny - 1
+        amountCell = _nx * _ny
 
-        k = 0
         meshes = []
-        for i in range(ny - 1):
+        for i in range(_ny):
             offset = nx * i
-            for j in range(nx - 1):
-                # ia = j + offset
-                # ib = ia + 1
-                # ic = nx + offset + j
-                # id = ic + 1
-
+            for j in range(_nx):
                 # Counter-Clockwise
                 ia = nx + offset + j
                 ib = ia + 1
                 id = j + offset
                 ic = id + 1
 
-                a, b, c, d = strengths[ia], strengths[ib], strengths[ic], strengths[id]
-                meshes.append(getMesh(points, _xDis, _yDis, ia, ib, ic, id, a, b, c, d))
-                k += 1
+                meshes.append(getMesh(points, strengths, _xDis, _yDis, ia, ib, ic, id))
 
-        return Mesh.join(*meshes), _points
+        return Mesh.join(*meshes)
 
     def getFieldStrengths(self, Vector3DList vectors, falloff):
         cdef FalloffEvaluator falloffEvaluator = self.getFalloffEvaluator(falloff)
@@ -77,119 +67,136 @@ class MarchingSquaresNode(bpy.types.Node, AnimationNode):
         try: return falloff.getEvaluator("LOCATION", self.clampFalloff)
         except: self.raiseErrorMessage("This falloff cannot be evaluated for vectors")
 
-def getMesh(Vector3DList points, double _xDis, double _yDis, ia, ib, ic, id, float a, float b, float c, float d):
-    cdef int index = binaryToDecimal(a, b, c, d)
+def getMesh(Vector3DList points, FloatList strengths, double _xDis, double _yDis,
+            Py_ssize_t ia, Py_ssize_t ib, Py_ssize_t ic, Py_ssize_t id):
+    cdef int index = binaryToDecimal(strengths.data[ia], strengths.data[ib],
+                                     strengths.data[ic], strengths.data[id])
+
     cdef Vector3DList vertices = Vector3DList(length = 2)
-    cdef EdgeIndicesList edges = EdgeIndicesList()
+    cdef EdgeIndicesList edges = EdgeIndicesList(length = 1)
     cdef PolygonIndicesList polygons = PolygonIndicesList()
 
     if index == 0:
         return Mesh()
     elif index == 1:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[id]
         vertices.data[1] = points.data[id]
         vertices.data[0].x += _xDis
         vertices.data[1].y += _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 2:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ib]
         vertices.data[1] = points.data[ic]
         vertices.data[0].y -= _yDis
         vertices.data[1].x -= _xDis
         return Mesh(vertices, edges, polygons)
     elif index == 3:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ib]
         vertices.data[1] = points.data[id]
         vertices.data[0].y -= _yDis
         vertices.data[1].y += _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 4:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[ib]
         vertices.data[0].x += _xDis
         vertices.data[1].y -= _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 5:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ib]
         vertices.data[1] = points.data[ic]
         vertices.data[0].y -= _yDis
         vertices.data[1].x -= _xDis
 
         edges.append((2,3))
-        vertices.append(points[id])
-        vertices.append(points[ia])
+        vertices.append_LowLevel(points.data[id])
+        vertices.append_LowLevel(points.data[ia])
         vertices.data[2].y += _yDis
         vertices.data[3].x += _xDis
         return Mesh(vertices, edges, polygons)
     elif index == 6:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ib]
         vertices.data[1] = points.data[ic]
         vertices.data[0].x -= _xDis
         vertices.data[1].x -= _xDis
         return Mesh(vertices, edges, polygons)
     elif index == 7:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[id]
         vertices.data[0].x += _xDis
         vertices.data[1].y += _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 8:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[id]
         vertices.data[0].x += _xDis
         vertices.data[1].y += _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 9:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[id]
         vertices.data[0].x += _xDis
         vertices.data[1].x += _xDis
         return Mesh(vertices, edges, polygons)
     elif index == 10:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[ib]
         vertices.data[0].x += _xDis
         vertices.data[1].y -= _yDis
 
         edges.append((2,3))
-        vertices.append(points[ic])
-        vertices.append(points[id])
+        vertices.append_LowLevel(points.data[ic])
+        vertices.append_LowLevel(points.data[id])
         vertices.data[2].x -= _xDis
         vertices.data[3].y += _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 11:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[ib]
         vertices.data[0].x += _xDis
         vertices.data[1].y -= _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 12:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ia]
         vertices.data[1] = points.data[ib]
         vertices.data[0].y -= _yDis
         vertices.data[1].y -= _yDis
         return Mesh(vertices, edges, polygons)
     elif index == 13:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ib]
         vertices.data[1] = points.data[ic]
         vertices.data[0].y -= _yDis
         vertices.data[1].x -= _xDis
         return Mesh(vertices, edges, polygons)
     elif index == 14:
-        edges.append((0,1))
+        edges.data[0].v1 = 0
+        edges.data[0].v2 = 1
         vertices.data[0] = points.data[ic]
         vertices.data[1] = points.data[id]
         vertices.data[0].x -= _xDis
@@ -214,7 +221,8 @@ cdef int binaryToDecimal(float a, float b, float c, float d):
 
     return <int>(8 * a + 4 * b + 2 * c + d)
 
-cdef getGridPoints(xDivisions, yDivisions, zDivisions, size1, size2, size3, distanceMode = "SIZE"):
+cdef getGridPoints(long xDivisions, long yDivisions, long zDivisions, float size1,
+                   float size2, float size3, distanceMode = "SIZE"):
     cdef:
         int xDiv = limitAmount(xDivisions)
         int yDiv = limitAmount(yDivisions)
