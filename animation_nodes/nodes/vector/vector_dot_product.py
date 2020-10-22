1801a438b507538508d3cb676742352216683eb8
Harisreedhar <harisreedhar@users.noreply.github.com>
2020-10-22 11:01:50 +0530

    Update vector_dot_product.py

--- animation_nodes/nodes/vector/vector_dot_product.py
+++ animation_nodes/nodes/vector/vector_dot_product.py
@@ -1,14 +1,32 @@
 import bpy
-from ... base_types import AnimationNode
+from . c_utils import calculateVectorDotProducts
+from ... data_structures import VirtualVector3DList
+from ... base_types import AnimationNode, VectorizedSocket
 
 class VectorDotProductNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_VectorDotProductNode"
     bl_label = "Vector Dot Product"
 
+    useListA: VectorizedSocket.newProperty()
+    useListB: VectorizedSocket.newProperty()
+
     def create(self):
-        self.newInput("Vector", "A", "a")
-        self.newInput("Vector", "B", "b")
-        self.newOutput("Float", "Dot Product", "dotProduct")
+        self.newInput(VectorizedSocket("Vector", "useListA",
+            ("A", "a"), ("A", "a")))
+        self.newInput(VectorizedSocket("Vector", "useListB",
+            ("B", "b"), ("B", "b")))
+
+        self.newOutput(VectorizedSocket("Float", ["useListA", "useListB"],
+            ("Dot Product", "dotProduct"), ("Dot Product", "dotProducts")))
 
     def getExecutionCode(self, required):
-        return "dotProduct = a.dot(b)"
+        if self.useListA or self.useListB:
+            yield "dotProducts = self.calculateDotProducts(a, b)"
+        else:
+            yield "distance = (a - b).length"
+            yield "dotProduct = a.dot(b)"
+
+    def calculateDotProducts(self, a, b):
+        vectors1 = VirtualVector3DList.create(a, (0, 0, 0))
+        vectors2 = VirtualVector3DList.create(b, (0, 0, 0))
+        return calculateVectorDotProducts(vectors1, vectors2)
