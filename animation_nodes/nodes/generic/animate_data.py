200984cac882d370276d99b21d54b34fd09156be
kdsin23 <chingwei2018@protonmail.com>
2021-01-22 12:59:36 +0800

    vectorize animate_node

--- animation_nodes/nodes/generic/animate_data.py
+++ animation_nodes/nodes/generic/animate_data.py
@@ -1,8 +1,29 @@
 import bpy
 from bpy.props import *
 from . mix_data import getMixCode
-from ... base_types import AnimationNode
+from ... utils.math import mixEulers
 from ... events import executionCodeChanged
+from ... base_types import AnimationNode, VectorizedSocket
+
+from . c_utils import (
+    mixColorLists,
+    mixEulerLists,
+    mixDoubleLists,
+    mixVectorLists,
+    mixQuaternionLists,
+    executeTimeList,
+    calculateInfluenceList,
+)
+from ... data_structures import (
+    Color,
+    Matrix4x4List,
+    VirtualColorList,
+    VirtualEulerList,
+    VirtualDoubleList,
+    VirtualVector3DList,
+    VirtualMatrix4x4List,
+    VirtualQuaternionList,
+)
 
 class AnimateDataNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_AnimateDataNode"
@@ -20,22 +41,81 @@ class AnimateDataNode(bpy.types.Node, AnimationNode):
 
     dataType: StringProperty(default = "Float", update = AnimationNode.refresh)
 
-    def create(self):
-        self.newInput("Float", "Time", "time")
-        self.newInput(self.dataType, "Start", "start")
-        self.newInput(self.dataType, "End", "end")
-        self.newInput("Interpolation", "Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
-        self.newInput("Float", "Duration", "duration", value = 20, minValue = 0.001)
+    useListA: VectorizedSocket.newProperty()
+    useListB: VectorizedSocket.newProperty()
+    useListC: VectorizedSocket.newProperty()
+    useListD: VectorizedSocket.newProperty()
 
-        self.newOutput("Float", "Time", "outTime")
-        self.newOutput(self.dataType, "Result", "result")
+    def create(self):
+        self.newInput(VectorizedSocket("Float", "useListA",
+            ("Time", "time"), ("Times", "times")))
+        self.newInput(VectorizedSocket(self.dataType, ["useListB"],
+            ("Start", "start"), ("Starts", "starts")))
+        self.newInput(VectorizedSocket(self.dataType, ["useListC"],
+            ("End", "end"), ("Ends", "ends")))
+        self.newInput("Interpolation", "interpolation", defaultDrawType = "PROPERTY_ONLY")
+        self.newInput(VectorizedSocket("Float", "useListD",
+            ("Duration", "duration", dict(value = 20, minValue = 0.001)),
+            ("Durations", "durations")))
+
+        self.newOutput(VectorizedSocket("Float", ["useListA", "useListB",
+            "useListC", "useListD"], ("Time", "outTime"), ("Times", "outTimes")))
+        self.newOutput(VectorizedSocket(self.dataType, ["useListA", "useListB", 
+            "useListC", "useListD"], ("Result", "result"), ("Results", "results")))
 
     def drawLabel(self):
         return "Animate " + self.inputs[1].dataType
 
     def getExecutionCode(self, required):
-        yield "finalDuration = max(duration, 0.0001)"
-        yield "influence = max(min(time / finalDuration, 1.0), 0.0)"
-        yield "influence = interpolation(influence)"
-        yield getMixCode(self.dataType, "start", "end", "influence", "result")
-        yield "outTime = time - finalDuration"
+        if any([self.useListA, self.useListB, self.useListC, self.useListD]):
+            args = ", ".join(socket.identifier for socket in self.inputs)
+            yield "results, outTimes = self.executeDataList({})".format(args)
+
+        else:
+            yield "finalDuration = max(duration, 0.0001)"
+            yield "influence = max(min(time / finalDuration, 1.0), 0.0)"
+            yield "influence = interpolation(influence)"
+            yield getMixCode(self.dataType, "start", "end", "influence", "result")
+            yield "outTime = time - finalDuration"
+
+    def executeDataList(self, time, start, end, interpolation, duration):
+        times = VirtualDoubleList.create(time, 0)
+        durations = VirtualDoubleList.create(duration, 0)
+
+        if self.dataType == "Float":
+            starts, ends = VirtualDoubleList.createMultiple((start, 0), (end, 0))      
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixDoubleLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+        elif self.dataType == "Vector":
+            starts, ends = VirtualVector3DList.createMultiple((start, 0), (end, 0))
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixVectorLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+        elif self.dataType == "Quaternion":
+            starts, ends = VirtualQuaternionList.createMultiple((start, 0), (end, 0))
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixQuaternionLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+        elif self.dataType == "Matrix":
+            starts, ends = VirtualMatrix4x4List.createMultiple((start, 0), (end, 0))
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixMatrixLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+        elif self.dataType == "Color":
+            starts, ends = VirtualColorList.createMultiple((start, 0), (end, 0))
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixColorLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+        elif self.dataType == "Euler":
+            starts, ends = VirtualEulerList.createMultiple((start, 0), (end, 0))
+            amount = VirtualDoubleList.getMaxRealLength(times, starts, ends, durations)
+            factors = calculateInfluenceList(times, interpolation, durations, amount)
+            _factors = VirtualDoubleList.create(factors, 0)
+            return mixEulerLists(starts, ends, _factors, amount), executeTimeList(times, durations, amount)
+
