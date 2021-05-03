39d8f0ecfc5e8c92afa90dcb4e7fe079cc3ff871
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2021-05-03 21:59:06 +0530

    Vectorize Transform Vector node

--- animation_nodes/nodes/vector/transform_vector.py
+++ animation_nodes/nodes/vector/transform_vector.py
@@ -6,19 +6,27 @@ class TransformVectorNode(bpy.types.Node, AnimationNode):
     bl_label = "Transform Vector"
 
     useVectorList: VectorizedSocket.newProperty()
+    useMatrixList: VectorizedSocket.newProperty()
 
     def create(self):
         self.newInput(VectorizedSocket("Vector", "useVectorList",
             ("Vector", "vector"),
             ("Vectors", "vectors", dict(dataIsModified = True))))
+        self.newInput(VectorizedSocket("Matrix", "useMatrixList",
+            ("Matrix", "matrix"), ("Matrices", "matrices")))
 
-        self.newInput("Matrix", "Matrix", "matrix")
-
-        self.newOutput(VectorizedSocket("Vector", "useVectorList",
-            ("Vector", "transformedVector"), ("Vectors", "vectors")))
+        if any((self.useVectorList, self.useMatrixList)):
+            self.newOutput("Vector List", "Vectors", "vectors")
+        else:
+            self.newOutput("Vector", "Vector", "transformedVector")
 
     def getExecutionCode(self, required):
-        if self.useVectorList:
-            return "vectors.transform(matrix)"
+        if any((self.useVectorList, self.useMatrixList)):
+            if not self.useVectorList: yield "vectors = Vector3DList.fromValue(vector)"
+            if not self.useMatrixList: yield "matrices = Matrix4x4List.fromValue(matrix)"
+            yield "_vectors = VirtualVector3DList.create(vectors, (0,0,0))"
+            yield "_matrices = VirtualMatrix4x4List.create(matrices, Matrix.Identity(4))"
+            yield "amount = max(len(vectors), len(matrices))"
+            yield "vectors = AN.nodes.vector.c_utils.transformVirtualVectorList(amount, _vectors, _matrices)"
         else:
-            return "transformedVector = matrix @ vector"
+            yield "transformedVector = matrix @ vector"
