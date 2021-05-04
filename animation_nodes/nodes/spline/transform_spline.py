0469e979ddf6e99d43dcd21341b74aa5e2dd0b6c
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2021-05-04 20:02:39 +0530

    Vectorized Transform Spline node (#1714)

--- animation_nodes/nodes/spline/transform_spline.py
+++ animation_nodes/nodes/spline/transform_spline.py
@@ -1,22 +1,63 @@
 import bpy
+from mathutils import Matrix
 from ... base_types import AnimationNode, VectorizedSocket
+from ... data_structures import VirtualPyList, VirtualMatrix4x4List, Spline
 
 class TransformSplineNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TransformSplineNode"
     bl_label = "Transform Spline"
-    codeEffects = [VectorizedSocket.CodeEffect]
 
     useSplineList: VectorizedSocket.newProperty()
+    useMatrixList: VectorizedSocket.newProperty()
 
     def create(self):
         socket = self.newInput(VectorizedSocket("Spline", "useSplineList",
             ("Spline", "spline"), ("Splines", "splines")))
-        socket.dataIsModified = True
+        socket.dataIsModified = not self.useMatrixList
         socket.defaultDrawType = "PROPERTY_ONLY"
 
-        self.newInput("Matrix", "Transformation", "matrix")
-        self.newOutput(VectorizedSocket("Spline", "useSplineList",
+        self.newInput(VectorizedSocket("Matrix", "useMatrixList",
+            ("Matrix", "matrix"), ("Matrices", "matrices")))
+
+        self.newOutput(VectorizedSocket("Spline", ["useSplineList", "useMatrixList"],
             ("Spline", "spline"), ("Splines", "splines")))
 
-    def getExecutionCode(self, required):
-        return "spline.transform(matrix)"
+    def getExecutionFunctionName(self):
+        if self.useSplineList:
+            if self.useMatrixList:
+                return "execute_MultipleSpline_MultipleMatrix"
+            else:
+                return "execute_MultipleSpline_SingleMatrix"
+        else:
+            if self.useMatrixList:
+                return "execute_SingleSpline_MultipleMatrix"
+            else:
+                return "execute_SingleSpline_SingleMatrix"
+
+    def execute_SingleSpline_SingleMatrix(self, spline, matrix):
+        spline.transform(matrix)
+        return spline
+
+    def execute_SingleSpline_MultipleMatrix(self, spline, matrices):
+        outSplines = []
+        for matrix in matrices:
+            s = spline.copy()
+            s.transform(matrix)
+            outSplines.append(s)
+        return outSplines
+
+    def execute_MultipleSpline_SingleMatrix(self, splines, matrix):
+        for spline in splines:
+            spline.transform(matrix)
+        return splines
+
+    def execute_MultipleSpline_MultipleMatrix(self, splines, matrices):
+        _splines = VirtualPyList.create(splines, Spline())
+        _matrices = VirtualMatrix4x4List.create(matrices, Matrix.Identity(4))
+        amount = VirtualPyList.getMaxRealLength(_splines, _matrices)
+        outSplines = []
+        for i in range(amount):
+            s = _splines[i].copy()
+            s.transform(_matrices[i])
+            outSplines.append(s)
+        return outSplines
